ef975a9811aac4b9529ac30911f8294d1cce72f3
Tamás Tóth <totht@mit.bme.hu>
2016-03-03 15:49:14 +0100

    ProgramModel and related interfaces added

---
+++ hu.bme.mit.inf.ttmc.program.ui/src/hu/bme/mit/inf/ttmc/program/ui/ProcedureModel.java
@@ -0,0 +1,23 @@
+package hu.bme.mit.inf.ttmc.program.ui;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
+import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
+import hu.bme.mit.inf.ttmc.constraint.type.Type;
+import hu.bme.mit.inf.ttmc.program.cfa.CFA;
+import hu.bme.mit.inf.ttmc.program.cfa.CFALoc;
+import hu.bme.mit.inf.ttmc.program.decl.ProcDecl;
+import hu.bme.mit.inf.ttmc.program.stmt.Stmt;
+
+public interface ProcedureModel {
+	public ProcDecl<? extends Type> getProcDecl();
+	
+	public Collection<Expr<? extends BoolType>> getPreConds();
+	public Collection<Expr<? extends BoolType>> getPostConds();
+	
+	public Optional<Stmt> getStmt();
+	public Optional<CFA> getCFA();
+	public Optional<Expr<? extends BoolType>> getAnnotation(CFALoc loc);
+}
