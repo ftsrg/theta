b5ef52c81e79d7b98792f3fc2d40ee8636c872c8
Hajdu Ákos <sonic18291@hotmail.com>
2016-03-07 10:36:37 +0100

    Created separate visitor interfaces for each advanced expression.
    Refactored default implementations from the interface to the
    implementation.

--- hu.bme.mit.inf.ttmc.program/src/hu/bme/mit/inf/ttmc/program/utils/ProgExprVisitor.java
+++ hu.bme.mit.inf.ttmc.program/src/hu/bme/mit/inf/ttmc/program/utils/ProgExprVisitor.java
@@ -1,17 +1,16 @@
 package hu.bme.mit.inf.ttmc.program.utils;
 
-import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.constraint.utils.ExprVisitor;
-import hu.bme.mit.inf.ttmc.program.expr.PrimedExpr;
-import hu.bme.mit.inf.ttmc.program.expr.ProcCallExpr;
-import hu.bme.mit.inf.ttmc.program.expr.ProcRefExpr;
-import hu.bme.mit.inf.ttmc.program.expr.VarRefExpr;
+import hu.bme.mit.inf.ttmc.program.expr.visitor.PrimedExprVisitor;
+import hu.bme.mit.inf.ttmc.program.expr.visitor.ProcCallExprVisitor;
+import hu.bme.mit.inf.ttmc.program.expr.visitor.ProcRefExprVisitor;
+import hu.bme.mit.inf.ttmc.program.expr.visitor.VarRefExprVisitor;
 
-public interface ProgExprVisitor<P, R> extends ExprVisitor<P, R> {
-
-	public <ExprType extends Type> R visit(PrimedExpr<ExprType> expr, P param);
-	public <DeclType extends Type> R visit(VarRefExpr<DeclType> expr, P param);
-	public <ReturnType extends Type> R visit(ProcRefExpr<ReturnType> expr, P param);
-	public <ReturnType extends Type> R visit(ProcCallExpr<ReturnType> expr, P param);
+public interface ProgExprVisitor<P, R> extends 
+	ExprVisitor<P, R>,
+	PrimedExprVisitor<P, R>,
+	ProcCallExprVisitor<P, R>,
+	ProcRefExprVisitor<P, R>,
+	VarRefExprVisitor<P, R> {
 
 }
