f243d77967df0d421f60cbf64fba4f64634edc66
Hajdu Ákos <sonic18291@hotmail.com>
2016-03-08 14:40:35 +0100

    Added VarRef to STSFactory, started implementing CNF transformation

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/factory/STSFactory.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/factory/STSFactory.java
@@ -4,10 +4,13 @@ import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.decl.VarDecl;
 import hu.bme.mit.inf.ttmc.formalism.expr.PrimedExpr;
+import hu.bme.mit.inf.ttmc.formalism.expr.VarRefExpr;
 
 public interface STSFactory {
 	
 	public <T extends Type> VarDecl<T> Var(final String name, final T type);
+
+	public <T extends Type> VarRefExpr<T> Ref(final VarDecl<T> varDecl);
 
 	public <T extends Type> PrimedExpr<T> Prime(final Expr<? extends T> op);
 }
