7c10d2935db8007a30884461e6f599e85e482972
Tamás Tóth <totht@mit.bme.hu>
2016-03-10 15:43:29 +0100

    VarFactory renamed to VarDeclFactory

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/STSFactory.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/STSFactory.java
@@ -4,7 +4,7 @@ import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.expr.PrimedExpr;
 
-public interface STSFactory extends VarFactory {
+public interface STSFactory extends VarDeclFactory {
 	
 	public <T extends Type> PrimedExpr<T> Prime(final Expr<? extends T> op);
 }
--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/VarDeclFactory.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/VarDeclFactory.java
@@ -4,7 +4,7 @@ import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 
 public interface VarDeclFactory {
-	
+
 	public <T extends Type> VarDecl<T> Var(final String name, final T type);
-	
+
 }
