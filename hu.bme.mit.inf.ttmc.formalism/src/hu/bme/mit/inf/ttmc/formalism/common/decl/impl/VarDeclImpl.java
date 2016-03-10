fac163ddf3a7355dfb9c2d790e44fbc7a2a3208e
Tamás Tóth <totht@mit.bme.hu>
2016-03-10 15:01:10 +0100

    VarDeclImpl extends AbstractDecl

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/decl/impl/VarDeclImpl.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/decl/impl/VarDeclImpl.java
@@ -1,27 +1,16 @@
 package hu.bme.mit.inf.ttmc.formalism.common.decl.impl;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+
+import hu.bme.mit.inf.ttmc.constraint.decl.impl.AbstractDecl;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 
-public class VarDeclImpl<DeclType extends Type> implements VarDecl<DeclType> {
+public class VarDeclImpl<DeclType extends Type> extends AbstractDecl<DeclType> implements VarDecl<DeclType> {
 
-	private final String name;
-	private final DeclType type;
-		
+
 	public VarDeclImpl(final String name, final DeclType type) {
-		this.name = checkNotNull(name);
-		this.type = checkNotNull(type);
-	}
-	
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public DeclType getType() {
-		return type;
+		super(name, type);
 	}
 	
 	@Override
