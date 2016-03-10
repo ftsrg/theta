6bab3ddaf4e06a880c4715c912562a60d0fb0169
Tamás Tóth <totht@mit.bme.hu>
2016-03-10 11:26:57 +0100

    ProcedureModel interface modified

--- hu.bme.mit.inf.ttmc.program.ui/src/hu/bme/mit/inf/ttmc/program/ui/ProcedureModel.java
+++ hu.bme.mit.inf.ttmc.program.ui/src/hu/bme/mit/inf/ttmc/program/ui/ProcedureModel.java
@@ -1,21 +1,24 @@
 package hu.bme.mit.inf.ttmc.program.ui;
 
 import java.util.Collection;
-import java.util.Optional;
+
+import com.google.common.base.Optional;
 
 import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
 import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
-import hu.bme.mit.inf.ttmc.formalism.cfa.CFA;
-import hu.bme.mit.inf.ttmc.formalism.cfa.CFALoc;
 import hu.bme.mit.inf.ttmc.formalism.decl.ProcDecl;
+import hu.bme.mit.inf.ttmc.formalism.decl.VarDecl;
+import hu.bme.mit.inf.ttmc.formalism.stmt.Stmt;
 
 public interface ProcedureModel {
 	public ProcDecl<? extends Type> getProcDecl();
 	
+	public Optional<Stmt> getStmt();
+	
 	public Collection<Expr<? extends BoolType>> getPreConds();
 	public Collection<Expr<? extends BoolType>> getPostConds();
+	public Collection<Expr<? extends BoolType>> getLoopInvars(Stmt stmt);
 	
-	public Optional<CFA> getCFA();
-	public Optional<Expr<? extends BoolType>> getAnnotation(CFALoc loc);
+	public Collection<VarDecl<? extends Type>> getVarDecls();	
 }
