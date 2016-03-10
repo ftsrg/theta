556733c8526ed93291b25e1c9e3c613665b06e3c
Tamás Tóth <totht@mit.bme.hu>
2016-03-10 15:47:37 +0100

    STSFactory and STSFactoryImpl moved to package sts

--- hu.bme.mit.inf.ttmc.system.tests/src/hu/bme/mit/inf/ttmc/system/tests/SystemModelTests.java
+++ hu.bme.mit.inf.ttmc.system.tests/src/hu/bme/mit/inf/ttmc/system/tests/SystemModelTests.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 
 import hu.bme.mit.inf.ttmc.constraint.ConstraintManager;
 import hu.bme.mit.inf.ttmc.constraint.ConstraintManagerImpl;
-import hu.bme.mit.inf.ttmc.formalism.common.factory.impl.STSFactoryImpl;
 import hu.bme.mit.inf.ttmc.formalism.sts.STS;
+import hu.bme.mit.inf.ttmc.formalism.sts.impl.STSFactoryImpl;
 import hu.bme.mit.inf.ttmc.formalism.utils.sts.impl.STSCNFTransformation;
 import hu.bme.mit.inf.ttmc.formalism.utils.sts.impl.STSITETransformation;
 import hu.bme.mit.inf.ttmc.system.model.SystemSpecification;
