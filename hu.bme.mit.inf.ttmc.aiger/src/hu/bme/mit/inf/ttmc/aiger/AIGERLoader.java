3faa566bd582aaaeecf181317abc29445f7a7cb3
Hajdu Ákos <sonic18291@hotmail.com>
2016-03-11 12:36:27 +0100

    Implemented clever AIGER loading

--- hu.bme.mit.inf.ttmc.aiger/src/hu/bme/mit/inf/ttmc/aiger/AIGERLoader.java
+++ hu.bme.mit.inf.ttmc.aiger/src/hu/bme/mit/inf/ttmc/aiger/AIGERLoader.java
@@ -7,84 +7,76 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.collect.ImmutableSet;
-
-import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
-import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
-import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
-import hu.bme.mit.inf.ttmc.formalism.common.factory.VarDeclFactory;
+import hu.bme.mit.inf.ttmc.aiger.elements.AndGate;
+import hu.bme.mit.inf.ttmc.aiger.elements.FalseConst;
+import hu.bme.mit.inf.ttmc.aiger.elements.HWElement;
+import hu.bme.mit.inf.ttmc.aiger.elements.InVar;
+import hu.bme.mit.inf.ttmc.aiger.elements.Latch;
+import hu.bme.mit.inf.ttmc.aiger.elements.OutVar;
 import hu.bme.mit.inf.ttmc.formalism.sts.STS;
 import hu.bme.mit.inf.ttmc.formalism.sts.STSManager;
-import hu.bme.mit.inf.ttmc.formalism.sts.factory.STSExprFactory;
 import hu.bme.mit.inf.ttmc.formalism.sts.impl.STSImpl;
 
 public class AIGERLoader {
 	public STS load(String fileName, STSManager manager) throws IOException {
-		STSExprFactory ef = manager.getExprFactory();
-		VarDeclFactory varDeclF = manager.getDeclFactory();
-		
 		STSImpl.Builder builder = new STSImpl.Builder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 		
-		int maxVars, inputs, latches, outputs, andGates;
+		int inputs, latches, outputs, andGates;
 		// Parse header
 		String[] header = br.readLine().split(" ");
-		maxVars = Integer.parseInt(header[1]);
 		inputs = Integer.parseInt(header[2]);
 		latches = Integer.parseInt(header[3]);
 		outputs = Integer.parseInt(header[4]);
 		andGates = Integer.parseInt(header[5]);
-		// Create variables
-		List<VarDecl<BoolType>> vars = new ArrayList<>(maxVars + 1);
-		List<Expr<? extends BoolType>> outVars = new ArrayList<>(1);
-		for(int i = 0; i <= maxVars; ++i) vars.add(varDeclF.Var("v" + i, manager.getTypeFactory().Bool()));
-		// v0 is the constant 'false'
-		builder.addInvar(ef.Not(vars.get(0).getRef()));
 		
-		// Inputs
-		for(int i = 0; i < inputs; ++i) br.readLine();
+		List<HWElement> elements = new ArrayList<>(inputs + latches + andGates + 1);
+		for (int i = 0; i < inputs + latches + andGates + 1; ++i) elements.add(null);
+		elements.set(0, new FalseConst());
+		List<OutVar> outVarElements = new ArrayList<>(outputs);
+		List<Latch> latchElements = new ArrayList<>(latches);
 		
-		// Latches
+		// Parse inputs
+		for(int i = 0; i < inputs; ++i) {
+			InVar inVar = new InVar(br.readLine(), manager);
+			elements.set(inVar.getVarId(), inVar);
+		}
+		
+		// Parse latches
 		for (int i = 0; i < latches; ++i) {
-			String v[] = br.readLine().split(" ");
-			int v1 = Integer.parseInt(v[0]);
-			int v2 = Integer.parseInt(v[1]);
-			builder.addInit(ef.Not(vars.get(v1/2).getRef()));
-			builder.addTrans(ef.Iff(ef.Prime(vars.get(v1/2).getRef()),
-					v2%2 == 0 ?
-							vars.get(v2/2).getRef() :
-							ef.Not(vars.get(v2/2).getRef())
-					));
+			Latch latch = new Latch(br.readLine().split(" "), manager);
+			elements.set(latch.getVarId(), latch);
+			latchElements.add(latch);
 		}
 		
-		// Outputs
+		// Parse outputs
 		for (int i = 0; i < outputs; ++i) {
-			int v = Integer.parseInt(br.readLine());
-			outVars.add(v%2 == 0 ? vars.get(v/2).getRef() : ef.Not(vars.get(v/2).getRef()));
+			OutVar outVar = new OutVar(br.readLine());
+			outVarElements.add(outVar);
 		}
 		
-		// And gates
+		// Parse and gates
 		for (int i = 0; i < andGates; ++i) {
-			String[] v = br.readLine().split(" ");
-			int vo = Integer.parseInt(v[0]);
-			int vi1 = Integer.parseInt(v[1]);
-			int vi2 = Integer.parseInt(v[2]);
-			builder.addInvar(ef.Iff(vars.get(vo/2).getRef(),
-					ef.And(ImmutableSet.of(
-						vi1%2 == 0 ? vars.get(vi1/2).getRef() : ef.Not(vars.get(vi1/2).getRef()),
-						vi2%2 == 0 ? vars.get(vi2/2).getRef() : ef.Not(vars.get(vi2/2).getRef())	
-					))));
-		}
-		
-		br.close();
-		
-		// Requirement
-		if (outVars.size() == 1) {
-			builder.setProp(ef.Not(outVars.get(0)));
-		} else {
-			throw new UnsupportedOperationException("Currently only models with a single output variable are supported (this model has " + outVars.size() + ").");
+			AndGate andGate = new AndGate(br.readLine().split(" "));
+			elements.set(andGate.getVarId(), andGate);
 		}
 
+		br.close();
+		
+		// Process latches
+		for (Latch latch : latchElements) {
+			builder.addInit(latch.getInitExpr(manager));
+			builder.addTrans(latch.getTransExpr(manager, elements));
+		}
+		
+		// Process output
+		if (outVarElements.size() == 1) {
+			builder.setProp(manager.getExprFactory().Not(outVarElements.get(0).getExpr(manager, elements)));
+		} else {
+			throw new UnsupportedOperationException("Currently only models with a single output variable are supported (this model has " + outVarElements.size() + ").");
+		}
+		
 		return builder.build();
 	}
+
 }
---
+++ hu.bme.mit.inf.ttmc.aiger/src/hu/bme/mit/inf/ttmc/aiger/elements/Latch.java
@@ -0,0 +1,42 @@
+package hu.bme.mit.inf.ttmc.aiger.elements;
+
+import java.util.List;
+
+import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
+import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
+import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
+import hu.bme.mit.inf.ttmc.formalism.sts.STSManager;
+
+public final class Latch extends HWElement {
+	private final int nextState;
+	private final VarDecl<BoolType> varDecl;
+
+	public Latch(String[] tokens, STSManager manager) {
+		this(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), manager);
+	}
+	
+	public Latch(int actualState, int nextState, STSManager manager) {
+		super(actualState/2);
+		this.nextState = nextState;
+		varDecl = manager.getDeclFactory().Var("v" + varId, manager.getTypeFactory().Bool());
+	}
+
+	@Override
+	public Expr<? extends BoolType> getExpr(STSManager manager, List<HWElement> elements) {
+		return varDecl.getRef();
+	}
+	
+	public Expr<? extends BoolType> getInitExpr(STSManager manager) {
+		return manager.getExprFactory().Not(varDecl.getRef());
+	}
+	
+	public Expr<? extends BoolType> getTransExpr(STSManager manager, List<HWElement> elements) {
+		Expr<? extends BoolType> expr = elements.get(nextState / 2).getExpr(manager, elements);
+		if (nextState % 2 == 1) expr = manager.getExprFactory().Not(expr);
+		return manager.getExprFactory().Iff(
+				manager.getExprFactory().Prime(varDecl.getRef()),
+				expr
+				);
+	}
+
+}
