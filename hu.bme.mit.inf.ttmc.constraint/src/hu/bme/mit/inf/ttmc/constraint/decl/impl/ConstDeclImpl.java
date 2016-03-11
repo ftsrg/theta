8e6e4d13d89132d64093fc0c559d4ed7356cd820
Tamás Tóth <totht@mit.bme.hu>
2016-03-11 14:00:32 +0100

    Unnecessary "implements" keyword removed

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ConstDeclImpl.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ConstDeclImpl.java
@@ -1,10 +1,9 @@
 package hu.bme.mit.inf.ttmc.constraint.decl.impl;
 
-import hu.bme.mit.inf.ttmc.constraint.decl.ConstDecl;
 import hu.bme.mit.inf.ttmc.constraint.decl.defaults.AbstractConstDecl;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
-public final class ConstDeclImpl<DeclType extends Type> extends AbstractConstDecl<DeclType> implements ConstDecl<DeclType> {
+public final class ConstDeclImpl<DeclType extends Type> extends AbstractConstDecl<DeclType> {
 
 	public ConstDeclImpl(String name, DeclType type) {
 		super(name, type);
--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ParamDeclImpl.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ParamDeclImpl.java
@@ -1,10 +1,9 @@
 package hu.bme.mit.inf.ttmc.constraint.decl.impl;
 
-import hu.bme.mit.inf.ttmc.constraint.decl.ParamDecl;
 import hu.bme.mit.inf.ttmc.constraint.decl.defaults.AbstractParamDecl;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
-public final class ParamDeclImpl<DeclType extends Type> extends AbstractParamDecl<DeclType> implements ParamDecl<DeclType> {
+public final class ParamDeclImpl<DeclType extends Type> extends AbstractParamDecl<DeclType> {
 
 	public ParamDeclImpl(String name, DeclType type) {
 		super(name, type);
