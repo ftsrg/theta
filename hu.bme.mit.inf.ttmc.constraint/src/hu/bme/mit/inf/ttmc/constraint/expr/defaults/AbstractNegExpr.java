3054e30bb559cc465ed91c6ef87ffd0237cfb37a
Tamás Tóth <totht@mit.bme.hu>
2016-03-16 13:20:25 +0100

    equals(...) implemented for AbstractUnaryExpr subclasses

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/defaults/AbstractNegExpr.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/defaults/AbstractNegExpr.java
@@ -15,6 +15,29 @@ public abstract class AbstractNegExpr<ExprType extends ClosedUnderNeg> extends A
 	}
 
 	@Override
+	public NegExpr<ExprType> withOp(final Expr<? extends ExprType> op) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+	@Override
+	public <P, R> R accept(final ExprVisitor<? super P, ? extends R> visitor, final P param) {
+		return visitor.visit(this, param);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof NegExpr<?>) {
+			final NegExpr<?> that = (NegExpr<?>) obj;
+			return this.getOp().equals(that.getOp());
+		} else {
+			return false;
+		}
+	}
+
+	@Override
 	protected final String getOperatorString() {
 		return OPERATOR;
 	}
@@ -22,16 +45,5 @@ public abstract class AbstractNegExpr<ExprType extends ClosedUnderNeg> extends A
 	@Override
 	protected int getHashSeed() {
 		return 97;
-	}
-
-	@Override
-	public NegExpr<ExprType> withOp(Expr<? extends ExprType> op) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("TODO: auto-generated method stub");
-	}
-
-	@Override
-	public <P, R> R accept(ExprVisitor<? super P, ? extends R> visitor, P param) {
-		return visitor.visit(this, param);
 	}
 }
--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/defaults/AbstractUnaryExpr.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/defaults/AbstractUnaryExpr.java
@@ -6,11 +6,11 @@ import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
 import hu.bme.mit.inf.ttmc.constraint.expr.UnaryExpr;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
-public abstract class AbstractUnaryExpr<OpType extends Type, ExprType extends Type>
-		extends AbstractExpr<ExprType> implements UnaryExpr<OpType, ExprType> {
+public abstract class AbstractUnaryExpr<OpType extends Type, ExprType extends Type> extends AbstractExpr<ExprType>
+		implements UnaryExpr<OpType, ExprType> {
 
 	private final Expr<? extends OpType> op;
-	
+
 	private volatile int hashCode = 0;
 
 	public AbstractUnaryExpr(final Expr<? extends OpType> op) {
@@ -21,7 +21,7 @@ public abstract class AbstractUnaryExpr<OpType extends Type, ExprType extends Ty
 	public Expr<? extends OpType> getOp() {
 		return op;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		if (hashCode == 0) {
@@ -30,20 +30,6 @@ public abstract class AbstractUnaryExpr<OpType extends Type, ExprType extends Ty
 		}
 
 		return hashCode;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		} else if (obj == null) {
-			return false;
-		} else if (this.getClass() == obj.getClass()) {
-			final AbstractUnaryExpr<?, ?> that = (AbstractUnaryExpr<?, ?>) obj;
-			return this.op.equals(that.op);
-		} else {
-			return false;
-		}
 	}
 
 	@Override
