1a02bd6048f39e461f71f64856900595cfa0733e
Tamás Tóth <totht@mit.bme.hu>
2016-03-16 11:37:40 +0100

    hashCode() and equals(...) implemented for abstract type classes

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/type/defaults/AbstractArrayType.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/type/defaults/AbstractArrayType.java
@@ -5,16 +5,21 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import hu.bme.mit.inf.ttmc.constraint.type.ArrayType;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
-public abstract class AbstractArrayType<IndexType extends Type, ElemType extends Type> extends AbstractType implements ArrayType<IndexType, ElemType> {
-	
+public abstract class AbstractArrayType<IndexType extends Type, ElemType extends Type> extends AbstractType
+		implements ArrayType<IndexType, ElemType> {
+
+	private final static int HASH_SEED = 4919;
+
+	private volatile int hashCode = 0;
+
 	private final IndexType indexType;
 	private final ElemType elemType;
-	
+
 	public AbstractArrayType(final IndexType indexType, final ElemType elemType) {
 		this.indexType = checkNotNull(indexType);
 		this.elemType = checkNotNull(elemType);
 	}
-	
+
 	@Override
 	public IndexType getIndexType() {
 		return indexType;
@@ -24,7 +29,30 @@ public abstract class AbstractArrayType<IndexType extends Type, ElemType extends
 	public ElemType getElemType() {
 		return elemType;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		if (hashCode == 0) {
+			hashCode = HASH_SEED;
+			hashCode = 31 * hashCode + indexType.hashCode();
+			hashCode = 31 * hashCode + elemType.hashCode();
+		}
+
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof ArrayType<?, ?>) {
+			final ArrayType<?, ?> that = (ArrayType<?, ?>) obj;
+			return this.getIndexType().equals(that.getIndexType()) && this.getElemType().equals(that.getElemType());
+		} else {
+			return false;
+		}
+	}
+
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder();
