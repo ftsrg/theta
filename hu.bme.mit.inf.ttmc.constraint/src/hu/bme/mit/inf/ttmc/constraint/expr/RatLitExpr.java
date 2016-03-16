935a19a4c4a1819af0f047b1b7b8d58ddb8dad04
Tamás Tóth <totht@mit.bme.hu>
2016-03-16 16:16:27 +0100

    RatLitExpr implements Comparable<RatLitExpr>

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/RatLitExpr.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/expr/RatLitExpr.java
@@ -2,7 +2,10 @@ package hu.bme.mit.inf.ttmc.constraint.expr;
 
 import hu.bme.mit.inf.ttmc.constraint.type.RatType;
 
-public interface RatLitExpr extends NullaryExpr<RatType> {
+public interface RatLitExpr extends NullaryExpr<RatType>, Comparable<RatLitExpr> {
+
 	public long getNum();
+
 	public long getDenom();
+
 }
