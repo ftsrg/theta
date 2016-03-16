9f310241ea3f0d7399fcf9d9c2f2c56d7760136c
Tamás Tóth <totht@mit.bme.hu>
2016-03-16 17:00:46 +0100

    Erroneous reference instantiation removed from abstract declaration classes

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/defaults/AbstractParamDecl.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/defaults/AbstractParamDecl.java
@@ -2,24 +2,19 @@ package hu.bme.mit.inf.ttmc.constraint.decl.defaults;
 
 import hu.bme.mit.inf.ttmc.constraint.decl.ParamDecl;
 import hu.bme.mit.inf.ttmc.constraint.expr.ParamRefExpr;
-import hu.bme.mit.inf.ttmc.constraint.expr.impl.ParamRefExprImpl;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
-public abstract class AbstractParamDecl<DeclType extends Type> extends AbstractDecl<DeclType> implements ParamDecl<DeclType> {
-	
-	protected volatile ParamRefExpr<DeclType> ref;
-	
+public abstract class AbstractParamDecl<DeclType extends Type> extends AbstractDecl<DeclType>
+		implements ParamDecl<DeclType> {
+
 	public AbstractParamDecl(final String name, final DeclType type) {
 		super(name, type);
 	}
-	
+
 	@Override
 	public ParamRefExpr<DeclType> getRef() {
-		if (ref == null) {
-			ref = new ParamRefExprImpl<>(this);
-		}
-		
-		return ref;
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
 	}
 
 	@Override
@@ -32,5 +27,5 @@ public abstract class AbstractParamDecl<DeclType extends Type> extends AbstractD
 		sb.append(")");
 		return sb.toString();
 	}
-	
+
 }
