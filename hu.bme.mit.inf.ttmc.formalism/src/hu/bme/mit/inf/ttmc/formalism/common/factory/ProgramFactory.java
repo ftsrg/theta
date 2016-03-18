6ea5e9e9bd9b92a629da06eb4e7d5ed91c259fbe
Hajdu Ákos <sonic18291@hotmail.com>
2016-03-18 14:45:51 +0100

    Extracted PrimedExpr creation to separate interface, made Fold and UnfoldVisitors public

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/ProgramFactory.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/ProgramFactory.java
@@ -10,7 +10,6 @@ import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.ProcDecl;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
-import hu.bme.mit.inf.ttmc.formalism.common.expr.PrimedExpr;
 import hu.bme.mit.inf.ttmc.formalism.common.expr.ProcCallExpr;
 import hu.bme.mit.inf.ttmc.formalism.common.expr.ProcRefExpr;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.AssertStmt;
@@ -27,40 +26,38 @@ import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.WhileStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.type.ProcType;
 
-public interface ProgramFactory extends VarDeclFactory {
-	
-	public <R extends Type> ProcDecl<R> Proc(final String name, List<? extends ParamDecl<? extends Type>> paramDecls, final R returnType);
+public interface ProgramFactory extends VarDeclFactory, PrimedExprFactory {
+
+	public <R extends Type> ProcDecl<R> Proc(final String name, List<? extends ParamDecl<? extends Type>> paramDecls,
+			final R returnType);
 
 	////
-	
+
 	public <R extends Type> ProcType<R> Proc(final List<? extends Type> paramTypes, final R returnType);
-	
+
 	////
-	
+
 	public <R extends Type> ProcRefExpr<R> Ref(final ProcDecl<R> procDecl);
-	
+
 	public <R extends Type> ProcCallExpr<R> Call(final Expr<? extends ProcType<? extends R>> proc,
 			final List<? extends Expr<?>> params);
 
-	public <T extends Type> PrimedExpr<T> Prime(final Expr<? extends T> op);
-
 	////
-	
+
 	public AssumeStmt Assume(final Expr<? extends BoolType> cond);
 
 	public AssertStmt Assert(final Expr<? extends BoolType> cond);
-	
-	public <T1 extends Type, T2 extends T1> AssignStmt<T1, T2> Assign(final VarDecl<T1> varDecl,
-			final Expr<T2> expr);
+
+	public <T1 extends Type, T2 extends T1> AssignStmt<T1, T2> Assign(final VarDecl<T1> varDecl, final Expr<T2> expr);
 
 	public <T extends Type> HavocStmt<T> Havoc(final VarDecl<T> varDecl);
 
 	public BlockStmt Block(final List<? extends Stmt> stmts);
-	
-	public default BlockStmt Block(Stmt... stmts) {
+
+	public default BlockStmt Block(final Stmt... stmts) {
 		return Block(ImmutableList.copyOf(stmts));
 	}
-	
+
 	public <T extends Type> ReturnStmt<T> Return(final Expr<? extends T> expr);
 
 	public IfStmt If(final Expr<? extends BoolType> cond, final Stmt then);
