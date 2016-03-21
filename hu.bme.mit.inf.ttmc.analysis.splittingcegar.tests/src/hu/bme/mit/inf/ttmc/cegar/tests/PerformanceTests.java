3f3e9b5fc031bf6b9fd883924208fe9646f3a9a8
Hajdu Ákos <sonic18291@hotmail.com>
2016-03-21 15:58:14 +0100

    Loader (AIGER/System) can be specified at the tests

--- hu.bme.mit.inf.ttmc.analysis.splittingcegar.tests/src/hu/bme/mit/inf/ttmc/cegar/tests/PerformanceTests.java
+++ hu.bme.mit.inf.ttmc.analysis.splittingcegar.tests/src/hu/bme/mit/inf/ttmc/cegar/tests/PerformanceTests.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import org.junit.Assert;
 import org.junit.Test;
 
+import hu.bme.mit.inf.ttmc.aiger.impl.AIGERLoaderOptimized;
 import hu.bme.mit.inf.ttmc.aiger.impl.AIGERLoaderSimple;
 import hu.bme.mit.inf.ttmc.cegar.clusteredcegar.ClusteredCEGARBuilder;
 import hu.bme.mit.inf.ttmc.cegar.common.CEGARResult;
@@ -33,19 +34,21 @@ public class PerformanceTests {
 	@SuppressWarnings("serial")
 	//@Test
 	public void testSimple() {
+		final IModelLoader loader = new SystemFileModelLoader();
+
 		final List<TestCase> testCases = new ArrayList<TestCase>() {
 			{
-				add(new TestCase("models/simple/simple1.system", false));
-				add(new TestCase("models/simple/simple2.system", true));
-				add(new TestCase("models/simple/simple3.system", false));
-				add(new TestCase("models/simple/counter.system", true));
-				add(new TestCase("models/simple/counter_bad.system", false));
-				add(new TestCase("models/simple/counter_parametric.system", true));
-				add(new TestCase("models/simple/boolean1.system", false));
-				add(new TestCase("models/simple/boolean2.system", false));
-				add(new TestCase("models/simple/readerswriters.system", true));
-				add(new TestCase("models/simple/loop.system", true));
-				add(new TestCase("models/simple/loop_bad.system", false));
+				add(new TestCase("models/simple/simple1.system", false, loader));
+				add(new TestCase("models/simple/simple2.system", true, loader));
+				add(new TestCase("models/simple/simple3.system", false, loader));
+				add(new TestCase("models/simple/counter.system", true, loader));
+				add(new TestCase("models/simple/counter_bad.system", false, loader));
+				add(new TestCase("models/simple/counter_parametric.system", true, loader));
+				add(new TestCase("models/simple/boolean1.system", false, loader));
+				add(new TestCase("models/simple/boolean2.system", false, loader));
+				add(new TestCase("models/simple/readerswriters.system", true, loader));
+				add(new TestCase("models/simple/loop.system", true, loader));
+				add(new TestCase("models/simple/loop_bad.system", false, loader));
 			}
 		};
 		final List<ICEGARBuilder> configurations = new ArrayList<ICEGARBuilder>() {
@@ -71,19 +74,21 @@ public class PerformanceTests {
 	@SuppressWarnings("serial")
 	//@Test
 	public void testCERNPLC() {
+		final IModelLoader loader = new SystemFileModelLoader();
+
 		final List<TestCase> testCases = new ArrayList<TestCase>() {
 			{
-				add(new TestCase("models/cern/LOCAL_vc1.system", false));
-				add(new TestCase("models/cern/LOCAL_vc2.system", false));
-				add(new TestCase("models/cern/REQ_1-1.system", true));
-				add(new TestCase("models/cern/REQ_1-8_correct.system", true));
-				add(new TestCase("models/cern/REQ_1-8_incorrect.system", false));
-				add(new TestCase("models/cern/REQ_1-9.system", true));
-				add(new TestCase("models/cern/REQ_2-3b_correct.system", true));
-				add(new TestCase("models/cern/REQ_2-3b_incorrect.system", false));
-				add(new TestCase("models/cern/REQ_3-1.system", true));
-				add(new TestCase("models/cern/REQ_3-2.system", false));
-				add(new TestCase("models/cern/UCPC-1721.system", true));
+				add(new TestCase("models/cern/LOCAL_vc1.system", false, loader));
+				add(new TestCase("models/cern/LOCAL_vc2.system", false, loader));
+				add(new TestCase("models/cern/REQ_1-1.system", true, loader));
+				add(new TestCase("models/cern/REQ_1-8_correct.system", true, loader));
+				add(new TestCase("models/cern/REQ_1-8_incorrect.system", false, loader));
+				add(new TestCase("models/cern/REQ_1-9.system", true, loader));
+				add(new TestCase("models/cern/REQ_2-3b_correct.system", true, loader));
+				add(new TestCase("models/cern/REQ_2-3b_incorrect.system", false, loader));
+				add(new TestCase("models/cern/REQ_3-1.system", true, loader));
+				add(new TestCase("models/cern/REQ_3-2.system", false, loader));
+				add(new TestCase("models/cern/UCPC-1721.system", true, loader));
 			}
 		};
 		final List<ICEGARBuilder> configurations = new ArrayList<ICEGARBuilder>() {
@@ -106,12 +111,14 @@ public class PerformanceTests {
 	@SuppressWarnings("serial")
 	//@Test
 	public void testFischer() {
+		final IModelLoader loader = new SystemFileModelLoader();
+
 		final List<TestCase> testCases = new ArrayList<TestCase>() {
 			{
-				add(new TestCase("models/fischer/fischer2.system", true));
-				add(new TestCase("models/fischer/fischer2_bad.system", false));
-				add(new TestCase("models/fischer/fischer3_bool.system", true));
-				add(new TestCase("models/fischer/fischer3_bool_bad.system", false));
+				add(new TestCase("models/fischer/fischer2.system", true, loader));
+				add(new TestCase("models/fischer/fischer2_bad.system", false, loader));
+				add(new TestCase("models/fischer/fischer3_bool.system", true, loader));
+				add(new TestCase("models/fischer/fischer3_bool_bad.system", false, loader));
 			}
 		};
 		final List<ICEGARBuilder> configurations = new ArrayList<ICEGARBuilder>() {
@@ -129,26 +136,29 @@ public class PerformanceTests {
 	@SuppressWarnings("serial")
 	@Test
 	public void testHardware() {
+		final IModelLoader loader = new AIGERFileModelLoaderOptimized();
+		//final IModelLoader loader = new AIGERFileModelLoaderSimple();
+
 		final List<TestCase> testCases = new ArrayList<TestCase>() {
 			{
-				add(new TestCase("models/hardware/ringp0.aag", false));
-				add(new TestCase("models/hardware/ringp0neg.aag", false));
-				add(new TestCase("models/hardware/shortp0.aag", false));
-				add(new TestCase("models/hardware/shortp0neg.aag", false));
-				add(new TestCase("models/hardware/srg5ptimo.aag", false));
-				add(new TestCase("models/hardware/srg5ptimoneg.aag", false));
-				add(new TestCase("models/hardware/srg5ptimonegnv.aag", false));
-				add(new TestCase("models/hardware/nusmv.syncarb5_2.B.aag", true));
-				add(new TestCase("models/hardware/nusmv.syncarb10_2.B.aag", true));
-				add(new TestCase("models/hardware/mutexp0.aag", false));
-				add(new TestCase("models/hardware/mutexp0neg.aag", false));
-				add(new TestCase("models/hardware/pdtpmsarbiter.aag", true));
+				add(new TestCase("models/hardware/ringp0.aag", false, loader));
+				add(new TestCase("models/hardware/ringp0neg.aag", false, loader));
+				add(new TestCase("models/hardware/shortp0.aag", false, loader));
+				add(new TestCase("models/hardware/shortp0neg.aag", false, loader));
+				add(new TestCase("models/hardware/srg5ptimo.aag", false, loader));
+				add(new TestCase("models/hardware/srg5ptimoneg.aag", false, loader));
+				add(new TestCase("models/hardware/srg5ptimonegnv.aag", false, loader));
+				add(new TestCase("models/hardware/nusmv.syncarb5_2.B.aag", true, loader));
+				add(new TestCase("models/hardware/nusmv.syncarb10_2.B.aag", true, loader));
+				add(new TestCase("models/hardware/mutexp0.aag", false, loader));
+				add(new TestCase("models/hardware/mutexp0neg.aag", false, loader));
+				add(new TestCase("models/hardware/pdtpmsarbiter.aag", true, loader));
 
 			}
 		};
 		final List<ICEGARBuilder> configurations = new ArrayList<ICEGARBuilder>() {
 			{
-				add(new VisibleCEGARBuilder().logger(null).visualizer(null).manager(manager).useCNFTransformation(false));
+				//add(new VisibleCEGARBuilder().logger(null).visualizer(null).manager(manager).useCNFTransformation(false));
 				add(new InterpolatingCEGARBuilder().logger(null).visualizer(null).interpolationMethod(InterpolationMethod.Craig).incrementalModelChecking(true)
 						.useCNFTransformation(false));
 				add(new InterpolatingCEGARBuilder().logger(null).visualizer(null).interpolationMethod(InterpolationMethod.Sequence)
@@ -217,7 +227,7 @@ public class PerformanceTests {
 		manager = new STSManagerImpl(new Z3ConstraintManager());
 		STS system;
 		try {
-			system = loadModel(testCase.path);
+			system = testCase.loader.load(testCase.path, manager);
 		} catch (final IOException e1) {
 			return new TestResult(TestResult.ResultType.IOException, new ArrayList<>());
 		}
@@ -247,7 +257,7 @@ public class PerformanceTests {
 					for (int i = 0; i < rerun; ++i) {
 						manager = new STSManagerImpl(new Z3ConstraintManager());
 						try {
-							system = loadModel(testCase.path);
+							system = testCase.loader.load(testCase.path, manager);
 						} catch (final IOException e1) {
 							return new TestResult(TestResult.ResultType.IOException, new ArrayList<>());
 						}
@@ -264,14 +274,30 @@ public class PerformanceTests {
 		return null;
 	}
 
-	private STS loadModel(final String path) throws IOException {
-		if (path.endsWith(".system")) {
-			// It is assumed that the file contains _exactly one_ STS
+	private static interface IModelLoader {
+		STS load(final String path, final STSManager manager) throws IOException;
+	}
+
+	private static class SystemFileModelLoader implements IModelLoader {
+		@Override
+		public STS load(final String path, final STSManager manager) {
 			return SystemModelCreator.create(manager, SystemModelLoader.getInstance().load(path)).getSTSs().iterator().next();
-		} else if (path.endsWith(".aag")) {
+		}
+	}
+
+	private static class AIGERFileModelLoaderOptimized implements IModelLoader {
+
+		@Override
+		public STS load(final String path, final STSManager manager) throws IOException {
+			return new AIGERLoaderOptimized().load(path, manager);
+		}
+	}
+
+	private static class AIGERFileModelLoaderSimple implements IModelLoader {
+
+		@Override
+		public STS load(final String path, final STSManager manager) throws IOException {
 			return new AIGERLoaderSimple().load(path, manager);
-		} else {
-			throw new UnsupportedOperationException("Cannot determine model type by extension.");
 		}
 	}
 
@@ -296,10 +322,12 @@ public class PerformanceTests {
 	private static class TestCase {
 		public String path;
 		public boolean expected;
+		public IModelLoader loader;
 
-		public TestCase(final String path, final boolean expected) {
+		public TestCase(final String path, final boolean expected, final IModelLoader loader) {
 			this.path = path;
 			this.expected = expected;
+			this.loader = loader;
 		}
 	}
 
