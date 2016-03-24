3dcd0e2518eb9593a65d84be1ae6cf009539e6c2
Tamás Tóth <totht@mit.bme.hu>
2016-03-24 12:25:33 +0100

    DeclStmt added

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/StmtFactory.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/factory/StmtFactory.java
@@ -12,6 +12,7 @@ import hu.bme.mit.inf.ttmc.formalism.common.stmt.AssertStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.AssignStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.AssumeStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.BlockStmt;
+import hu.bme.mit.inf.ttmc.formalism.common.stmt.DeclStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.DoStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.HavocStmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.IfElseStmt;
@@ -22,6 +23,8 @@ import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.WhileStmt;
 
 public interface StmtFactory {
+
+	public <T extends Type> DeclStmt<T> Decl(final VarDecl<T> varDecl);
 
 	public AssumeStmt Assume(final Expr<? extends BoolType> cond);
 
