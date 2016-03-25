c0c246047cebee325ba48331c1051fc539646277
Tamás Tóth <totht@mit.bme.hu>
2016-03-25 16:45:08 +0100

    CFACreator::createSBE() implemented

---
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/cfa/CFACreator.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.inf.ttmc.formalism.cfa;
+
+import hu.bme.mit.inf.ttmc.formalism.cfa.impl.SBECreator;
+import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
+import hu.bme.mit.inf.ttmc.formalism.program.ProgramManager;
+
+public class CFACreator {
+
+	public static CFA createSBE(final ProgramManager manager, final Stmt stmt) {
+		return SBECreator.create(manager, stmt);
+	}
+
+}
