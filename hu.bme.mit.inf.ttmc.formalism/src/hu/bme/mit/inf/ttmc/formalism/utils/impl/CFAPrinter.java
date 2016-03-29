5e12c620fc63a5b7bbd30c0f27bc67cab747bc4d
Tamás Tóth <totht@mit.bme.hu>
2016-03-29 12:32:17 +0200

    CFAPrinter improved

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/utils/impl/CFAPrinter.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/utils/impl/CFAPrinter.java
@@ -9,61 +9,71 @@ import hu.bme.mit.inf.ttmc.formalism.cfa.CFALoc;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
 
 public class CFAPrinter {
-	
+
 	private CFAPrinter() {
-		
+
 	}
-	
+
 	public static String toGraphvizSting(final CFA cfa) {
-		final Map<CFALoc, Integer> ids = createIds(cfa);
+		final Map<CFALoc, String> labels = createLocLabels(cfa);
 		final StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("digraph cfa {\n");
 		sb.append("edge [fontname = \"courier\"]\n");
-		for (CFALoc loc : cfa.getLocs()) {
-			sb.append(toGraphvizString(cfa, loc, ids));
+		for (final CFALoc loc : cfa.getLocs()) {
+			sb.append(toGraphvizString(cfa, loc, labels));
 		}
-		
-		for (CFAEdge edge : cfa.getEdges()) {
-			sb.append(toGraphvizString(cfa, edge, ids));
+
+		for (final CFAEdge edge : cfa.getEdges()) {
+			sb.append(toGraphvizString(cfa, edge, labels));
 		}
-		
+
 		sb.append("}");
 		return sb.toString();
 	}
-	
-	private static Map<CFALoc, Integer> createIds(final CFA cfa) {
-		final Map<CFALoc, Integer> ids = new HashMap<>();
+
+	private static Map<CFALoc, String> createLocLabels(final CFA cfa) {
+		final Map<CFALoc, String> labels = new HashMap<>();
 		int id = 0;
 		for (final CFALoc loc : cfa.getLocs()) {
-			ids.put(loc, id);
-			id++;
+			if (loc == cfa.getInitLoc()) {
+				labels.put(loc, "begin");
+			} else if (loc == cfa.getFinalLoc()) {
+				labels.put(loc, "end");
+			} else if (loc == cfa.getErrorLoc()) {
+				labels.put(loc, "error");
+			} else {
+				labels.put(loc, Integer.toString(id));
+				id++;
+			}
 		}
-		return ids;
+		return labels;
 	}
-	
-	private static String toGraphvizString(final CFA cfa, final CFALoc loc, final Map<CFALoc, Integer> ids) {
+
+	private static String toGraphvizString(final CFA cfa, final CFALoc loc, final Map<CFALoc, String> labels) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append(ids.get(loc));
+		sb.append(labels.get(loc));
+
 		if (loc == cfa.getErrorLoc()) {
 			sb.append(" [peripheries=2]");
 		}
+
 		sb.append("\n");
 		return sb.toString();
 	}
-	
-	private static String toGraphvizString(final CFA cfa, final CFAEdge edge, final Map<CFALoc, Integer> ids) {
+
+	private static String toGraphvizString(final CFA cfa, final CFAEdge edge, final Map<CFALoc, String> labels) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append(ids.get(edge.getSource()));
+		sb.append(labels.get(edge.getSource()));
 		sb.append(" -> ");
-		sb.append(ids.get(edge.getTarget()));
+		sb.append(labels.get(edge.getTarget()));
 		sb.append("[label=\"\\\n");
-		for (Stmt stmt : edge.getStmts()) {
+		for (final Stmt stmt : edge.getStmts()) {
 			sb.append(stmt.toString());
 			sb.append("\\n\\\n");
 		}
 		sb.append("\"]\n");
 		return sb.toString();
 	}
-	
+
 }
