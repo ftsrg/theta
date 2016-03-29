c683d9b91b41029bc082b3479272bc4b368bc4d3
Tamás Tóth <totht@mit.bme.hu>
2016-03-29 16:12:27 +0200

    CFACreator::createLBE() implemented

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/cfa/CFACreator.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/cfa/CFACreator.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.inf.ttmc.formalism.cfa;
 
+import hu.bme.mit.inf.ttmc.formalism.cfa.impl.LBECreator;
 import hu.bme.mit.inf.ttmc.formalism.cfa.impl.SBECreator;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
 import hu.bme.mit.inf.ttmc.formalism.program.ProgramManager;
@@ -8,6 +9,10 @@ public class CFACreator {
 
 	public static CFA createSBE(final ProgramManager manager, final Stmt stmt) {
 		return SBECreator.create(manager, stmt);
+	}
+
+	public static CFA createLBE(final ProgramManager manager, final Stmt stmt) {
+		return LBECreator.create(manager, stmt);
 	}
 
 }
