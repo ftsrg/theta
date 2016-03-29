29ac8134f7e90e12365731d26474d7b34716dcd5
Tamás Tóth <totht@mit.bme.hu>
2016-03-29 16:50:57 +0200

    TCFA extends Automaton

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFA.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFA.java
@@ -2,16 +2,21 @@ package hu.bme.mit.inf.ttmc.formalism.tcfa;
 
 import java.util.Collection;
 
-public interface TCFA {
+import hu.bme.mit.inf.ttmc.formalism.common.Automaton;
 
+public interface TCFA extends Automaton {
+
+	@Override
 	public TCFALoc getInitLoc();
 
 	public TCFALoc getFinalLoc();
 
 	public TCFALoc getErrorLoc();
 
+	@Override
 	public Collection<? extends TCFALoc> getLocs();
 
+	@Override
 	public Collection<? extends TCFAEdge> getEdges();
 
 }
--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFALoc.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFALoc.java
@@ -4,17 +4,18 @@ import java.util.Collection;
 
 import hu.bme.mit.inf.ttmc.constraint.expr.Expr;
 import hu.bme.mit.inf.ttmc.constraint.type.BoolType;
+import hu.bme.mit.inf.ttmc.formalism.common.Loc;
 
-public interface TCFALoc {
-
-	public TCFA getTCFA();
+public interface TCFALoc extends Loc {
 
 	public boolean isUrgent();
 
 	public Expr<? extends BoolType> getInvar();
 
+	@Override
 	public Collection<? extends TCFAEdge> getInEdges();
 
+	@Override
 	public Collection<? extends TCFAEdge> getOutEdges();
 
 }
