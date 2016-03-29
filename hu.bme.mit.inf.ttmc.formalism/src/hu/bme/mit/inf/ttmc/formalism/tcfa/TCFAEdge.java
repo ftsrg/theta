7c77f810bf96adc72b4bd69897f0efaff632055f
Tamás Tóth <totht@mit.bme.hu>
2016-03-29 16:50:57 +0200

    TCFA extends Automaton

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFAEdge.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFAEdge.java
@@ -2,14 +2,15 @@ package hu.bme.mit.inf.ttmc.formalism.tcfa;
 
 import java.util.List;
 
+import hu.bme.mit.inf.ttmc.formalism.common.Edge;
 import hu.bme.mit.inf.ttmc.formalism.common.stmt.Stmt;
 
-public interface TCFAEdge {
+public interface TCFAEdge extends Edge {
 
-	public TCFA getTCFA();
-
+	@Override
 	public TCFALoc getSource();
 
+	@Override
 	public TCFALoc getTarget();
 
 	public List<Stmt> getStmts();
