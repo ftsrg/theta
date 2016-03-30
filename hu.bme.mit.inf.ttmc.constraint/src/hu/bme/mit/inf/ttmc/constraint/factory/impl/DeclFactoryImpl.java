3e853206b93fa57d8e4ad94502485c1deead9c10
Tamás Tóth <totht@mit.bme.hu>
2016-03-30 15:20:18 +0200

    Unique name constraints removed

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/factory/impl/DeclFactoryImpl.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/factory/impl/DeclFactoryImpl.java
@@ -3,8 +3,6 @@ package hu.bme.mit.inf.ttmc.constraint.factory.impl;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.HashMap;
-
 import hu.bme.mit.inf.ttmc.constraint.ConstraintManager;
 import hu.bme.mit.inf.ttmc.constraint.decl.ConstDecl;
 import hu.bme.mit.inf.ttmc.constraint.decl.ParamDecl;
@@ -15,13 +13,10 @@ import hu.bme.mit.inf.ttmc.constraint.type.Type;
 
 public class DeclFactoryImpl implements DeclFactory {
 
-	private final HashMap<String, ConstDecl<?>> nameToConst;
-
 	private final ConstraintManager manager;
 
 	public DeclFactoryImpl(final ConstraintManager manager) {
 		this.manager = manager;
-		nameToConst = new HashMap<String, ConstDecl<?>>();
 	}
 
 	@Override
@@ -29,10 +24,8 @@ public class DeclFactoryImpl implements DeclFactory {
 		checkNotNull(name);
 		checkNotNull(type);
 		checkArgument(name.length() > 0);
-		checkArgument(nameToConst.get(name) == null);
 
 		final ConstDecl<T> constDecl = new ConstDeclImpl<>(manager, name, type);
-		nameToConst.put(name, constDecl);
 		return constDecl;
 	}
 
