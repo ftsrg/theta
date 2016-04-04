0b3c42f5dbc58eecd7444e4d7a9b6217e142f15f
Tamás Tóth <totht@mit.bme.hu>
2016-04-04 16:43:36 +0200

    Z3 transformation classes moved to own package

--- hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/trasform/Z3DeclVisitor.java
+++ hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/trasform/Z3DeclVisitor.java
@@ -1,4 +1,4 @@
-package hu.bme.mit.inf.ttmc.constraint.z3.solver2;
+package hu.bme.mit.inf.ttmc.constraint.z3.trasform;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
--- hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/trasform/Z3Transformator.java
+++ hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/trasform/Z3Transformator.java
@@ -1,4 +1,4 @@
-package hu.bme.mit.inf.ttmc.constraint.z3.solver2;
+package hu.bme.mit.inf.ttmc.constraint.z3.trasform;
 
 import com.microsoft.z3.Context;
 
@@ -9,10 +9,12 @@ import hu.bme.mit.inf.ttmc.constraint.type.Type;
 public class Z3Transformator {
 
 	final Z3TypeVisitor typeVisitor;
+	final Z3DeclVisitor declVisitor;
 	final Z3ExprVisitor exprVisitor;
 
 	public Z3Transformator(final Context context) {
 		this.typeVisitor = new Z3TypeVisitor(this, context);
+		this.declVisitor = new Z3DeclVisitor(this, context);
 		this.exprVisitor = new Z3ExprVisitor(this, context);
 	}
 
@@ -21,7 +23,7 @@ public class Z3Transformator {
 	}
 
 	public com.microsoft.z3.FuncDecl transform(final Decl<?, ?> decl) {
-		return null;
+		return decl.accept(declVisitor, null);
 	}
 
 	public com.microsoft.z3.Expr transform(final Expr<?> expr) {
