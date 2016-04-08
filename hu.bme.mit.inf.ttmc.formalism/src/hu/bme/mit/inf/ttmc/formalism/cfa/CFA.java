45acefc2f0a91b5b545c9f618b2ed34b99c18a2c
Tamás Tóth <totht@mit.bme.hu>
2016-04-08 15:16:26 +0200

    Automaton type parameter signature strengthened

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/cfa/CFA.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/cfa/CFA.java
@@ -1,22 +1,11 @@
 package hu.bme.mit.inf.ttmc.formalism.cfa;
 
-import java.util.Collection;
-
 import hu.bme.mit.inf.ttmc.formalism.common.Automaton;
 
-public interface CFA extends Automaton {
-
-	@Override
-	public CFALoc getInitLoc();
+public interface CFA extends Automaton<CFALoc, CFAEdge> {
 
 	public CFALoc getFinalLoc();
 
 	public CFALoc getErrorLoc();
-
-	@Override
-	public Collection<? extends CFALoc> getLocs();
-
-	@Override
-	public Collection<? extends CFAEdge> getEdges();
 
 }
