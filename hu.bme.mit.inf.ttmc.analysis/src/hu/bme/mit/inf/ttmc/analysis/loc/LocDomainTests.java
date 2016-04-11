b76a7df6ea9e20ea2ca91cb743be93451f75c579
Tamás Tóth <totht@mit.bme.hu>
2016-04-11 13:32:38 +0200

    Simple test added for location analysis

--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/loc/LocDomainTests.java
+++
@@ -1,5 +0,0 @@
-package hu.bme.mit.inf.ttmc.analysis.loc;
-
-public class LocDomainTests {
-
-}
---
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/loc/LocTests.java
@@ -0,0 +1,46 @@
+package hu.bme.mit.inf.ttmc.analysis.loc;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import hu.bme.mit.inf.ttmc.analysis.TransferRelation;
+import hu.bme.mit.inf.ttmc.analysis.algorithm.Algorithm;
+import hu.bme.mit.inf.ttmc.analysis.algorithm.impl.BasicAlgorithm;
+import hu.bme.mit.inf.ttmc.formalism.cfa.CFA;
+import hu.bme.mit.inf.ttmc.formalism.cfa.CFALoc;
+import hu.bme.mit.inf.ttmc.formalism.cfa.impl.MutableCFA;
+
+public class LocTests {
+
+	@Test
+	public void exploreLocs() {
+		final CFA cfa = createSimpleCFA();
+		final TransferRelation<LocState<CFALoc>> trel = new LocTransferRelation<>();
+
+		final LocState<CFALoc> initState = LocState.create(cfa.getInitLoc());
+		final LocState<CFALoc> finalState = LocState.create(cfa.getFinalLoc());
+		final Collection<LocState<CFALoc>> reachedSet = Collections.singleton(initState);
+		final Algorithm<LocState<CFALoc>> algorithm = new BasicAlgorithm<>(trel);
+		final Collection<LocState<CFALoc>> result = algorithm.run(reachedSet);
+
+		assertTrue(result.contains(finalState));
+	}
+
+	private static CFA createSimpleCFA() {
+		final MutableCFA cfa = new MutableCFA();
+		final CFALoc begin = cfa.getInitLoc();
+		final CFALoc end = cfa.getFinalLoc();
+		final CFALoc l1 = cfa.createLoc();
+		final CFALoc l2 = cfa.createLoc();
+		cfa.createEdge(begin, l1);
+		cfa.createEdge(l1, begin);
+		cfa.createEdge(l1, l2);
+		cfa.createEdge(l2, end);
+		return cfa;
+	}
+
+}
