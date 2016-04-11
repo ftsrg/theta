212442a0b91fab4bf53f7f40f4ef2f686355c4d8
Tamás Tóth <totht@mit.bme.hu>
2016-04-11 15:19:33 +0200

    Original Z3 solver implementation removed

--- hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/Z3ConstraintManager.java
+++ hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/Z3ConstraintManager.java
@@ -1,45 +1,48 @@
 package hu.bme.mit.inf.ttmc.constraint.z3;
 
-import com.microsoft.z3.InterpolationContext;
-
 import hu.bme.mit.inf.ttmc.constraint.ConstraintManager;
+import hu.bme.mit.inf.ttmc.constraint.ConstraintManagerImpl;
 import hu.bme.mit.inf.ttmc.constraint.factory.DeclFactory;
 import hu.bme.mit.inf.ttmc.constraint.factory.ExprFactory;
 import hu.bme.mit.inf.ttmc.constraint.factory.SolverFactory;
 import hu.bme.mit.inf.ttmc.constraint.factory.TypeFactory;
-import hu.bme.mit.inf.ttmc.constraint.z3.factory.Z3DeclFactory;
-import hu.bme.mit.inf.ttmc.constraint.z3.factory.Z3ExprFactory;
-import hu.bme.mit.inf.ttmc.constraint.z3.factory.Z3SolverFactory;
-import hu.bme.mit.inf.ttmc.constraint.z3.factory.Z3TypeFactory;
-import hu.bme.mit.inf.ttmc.constraint.z3.solver.Z3TermWrapper;
-import hu.bme.mit.inf.ttmc.constraint.z3.solver.Z3TermWrapperImpl;
+import hu.bme.mit.inf.ttmc.constraint.z3.factory.Z3SolverFactory2;
 
-public final class Z3ConstraintManager implements ConstraintManager {
+public class Z3ConstraintManager implements ConstraintManager {
 
-	final Z3DeclFactory declFactory;
-	final Z3TypeFactory typeFactory;
-	final Z3ExprFactory exprFactory;
-	final Z3SolverFactory solverFactory;
-
-	public Z3ConstraintManager() {
-		// final Map<String, String> config = new HashMap<String, String>();
-		// // Turn model generation on
-		// config.put("model", "true");
-		// // Turn proof generation on
-		// config.put("proof", "true");
-		//
-		final InterpolationContext context = new InterpolationContext();
-		declFactory = new Z3DeclFactory(this, context);
-		typeFactory = new Z3TypeFactory(this, context);
-		exprFactory = new Z3ExprFactory(this, context);
-
-		final Z3TermWrapper termWrapper = new Z3TermWrapperImpl(this, context, declFactory);
-		solverFactory = new Z3SolverFactory(this, context, termWrapper);
-	}
+	final ConstraintManager manager;
+	final SolverFactory solverFactory;
 
 	static {
 		loadLibraries();
 	}
+
+	public Z3ConstraintManager() {
+		manager = new ConstraintManagerImpl();
+		solverFactory = new Z3SolverFactory2(manager);
+	}
+
+	@Override
+	public DeclFactory getDeclFactory() {
+		return manager.getDeclFactory();
+	}
+
+	@Override
+	public TypeFactory getTypeFactory() {
+		return manager.getTypeFactory();
+	}
+
+	@Override
+	public ExprFactory getExprFactory() {
+		return manager.getExprFactory();
+	}
+
+	@Override
+	public SolverFactory getSolverFactory() {
+		return solverFactory;
+	}
+
+	////////
 
 	private static void loadLibraries() {
 		System.loadLibrary("msvcr110");
@@ -47,26 +50,6 @@ public final class Z3ConstraintManager implements ConstraintManager {
 		System.loadLibrary("vcomp110");
 		System.loadLibrary("libz3");
 		System.loadLibrary("libz3java");
-	}
-
-	@Override
-	public DeclFactory getDeclFactory() {
-		return declFactory;
-	}
-
-	@Override
-	public TypeFactory getTypeFactory() {
-		return typeFactory;
-	}
-
-	@Override
-	public ExprFactory getExprFactory() {
-		return exprFactory;
-	}
-
-	@Override
-	public SolverFactory getSolverFactory() {
-		return solverFactory;
 	}
 
 }
--- hu.bme.mit.inf.ttmc.constraint.z3/src/hu/bme/mit/inf/ttmc/constraint/z3/solver/Z3TermWrapper.java
+++
@@ -1,7 +0,0 @@
-package hu.bme.mit.inf.ttmc.constraint.z3.solver;
-
-import hu.bme.mit.inf.ttmc.constraint.solver.impl.TermWrapper;
-
-public interface Z3TermWrapper extends TermWrapper<com.microsoft.z3.Expr> {
-
-}
