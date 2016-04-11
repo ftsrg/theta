17a22fdca0696473d0881359a594e46735d002ec
Tamás Tóth <totht@mit.bme.hu>
2016-04-11 15:58:18 +0200

    Abstract Decl classes removed

--- hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ParamDeclImpl.java
+++ hu.bme.mit.inf.ttmc.constraint/src/hu/bme/mit/inf/ttmc/constraint/decl/impl/ParamDeclImpl.java
@@ -1,12 +1,47 @@
 package hu.bme.mit.inf.ttmc.constraint.decl.impl;
 
 import hu.bme.mit.inf.ttmc.constraint.ConstraintManager;
-import hu.bme.mit.inf.ttmc.constraint.decl.defaults.AbstractParamDecl;
+import hu.bme.mit.inf.ttmc.constraint.decl.ParamDecl;
+import hu.bme.mit.inf.ttmc.constraint.expr.ParamRefExpr;
 import hu.bme.mit.inf.ttmc.constraint.type.Type;
+import hu.bme.mit.inf.ttmc.constraint.utils.DeclVisitor;
 
-public final class ParamDeclImpl<DeclType extends Type> extends AbstractParamDecl<DeclType> {
+public final class ParamDeclImpl<DeclType extends Type> extends AbstractDecl<DeclType, ParamDecl<DeclType>>
+		implements ParamDecl<DeclType> {
+
+	private static final int HASH_SEED = 6949;
+	private static final String DECL_LABEL = "Param";
+
+	private final ConstraintManager manager;
+
+	private volatile ParamRefExpr<DeclType> ref;
 
 	public ParamDeclImpl(final ConstraintManager manager, final String name, final DeclType type) {
-		super(manager, name, type);
+		super(name, type);
+		this.manager = manager;
+	}
+
+	@Override
+	public ParamRefExpr<DeclType> getRef() {
+		if (ref == null) {
+			ref = manager.getExprFactory().Ref(this);
+		}
+
+		return ref;
+	}
+
+	@Override
+	public <P, R> R accept(final DeclVisitor<? super P, ? extends R> visitor, final P param) {
+		return visitor.visit(this, param);
+	}
+
+	@Override
+	protected int getHashSeed() {
+		return HASH_SEED;
+	}
+
+	@Override
+	protected String getDeclLabel() {
+		return DECL_LABEL;
 	}
 }
