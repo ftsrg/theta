f017a25ea57f63ff379b017f3846e5208c3f4768
Tamás Tóth <totht@mit.bme.hu>
2016-04-12 15:34:12 +0200

    Unused interfaces removed from solver plugin

--- hu.bme.mit.inf.ttmc.solver/src/hu/bme/mit/inf/ttmc/solver/impl/SymbolWrapper.java
+++
@@ -1,9 +0,0 @@
-package hu.bme.mit.inf.ttmc.solver.impl;
-
-import hu.bme.mit.inf.ttmc.core.decl.ConstDecl;
-
-public interface SymbolWrapper<Symbol> {
-
-	public ConstDecl<?> wrap(final Symbol symbol);
-	
-}
