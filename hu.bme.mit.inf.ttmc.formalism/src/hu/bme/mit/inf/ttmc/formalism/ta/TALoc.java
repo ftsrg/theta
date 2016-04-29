9314d9e79a76a5c9dddc7e7655f1d7bacff69607
Tamás Tóth <totht@mit.bme.hu>
2016-04-29 13:54:53 +0200

    TA interfaces modified to refer to Clock and Constr

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/ta/TALoc.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/ta/TALoc.java
@@ -1,13 +1,10 @@
 package hu.bme.mit.inf.ttmc.formalism.ta;
 
-import java.util.Collection;
-
-import hu.bme.mit.inf.ttmc.core.expr.Expr;
-import hu.bme.mit.inf.ttmc.core.type.BoolType;
 import hu.bme.mit.inf.ttmc.formalism.common.Loc;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.Constr;
 
 public interface TALoc extends Loc<TALoc, TAEdge> {
 
-	public Collection<Expr<? extends BoolType>> getInvars();
+	public Constr getInvar();
 
 }
