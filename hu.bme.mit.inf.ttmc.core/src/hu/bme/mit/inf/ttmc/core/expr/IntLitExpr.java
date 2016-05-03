26b991c968cd5a4285d8122ca95a73a5371323fe
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-03 15:13:35 +0200

    Introduced LitExpr interface for literals

--- hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/expr/IntLitExpr.java
+++ hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/expr/IntLitExpr.java
@@ -2,6 +2,6 @@ package hu.bme.mit.inf.ttmc.core.expr;
 
 import hu.bme.mit.inf.ttmc.core.type.IntType;
 
-public interface IntLitExpr extends NullaryExpr<IntType>, Comparable<IntLitExpr> {
+public interface IntLitExpr extends LitExpr<IntType>, NullaryExpr<IntType>, Comparable<IntLitExpr> {
 	public long getValue();
 }
