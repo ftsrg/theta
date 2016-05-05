44a90ce028984d091a054ae865168d8df67ce1b3
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-05 11:13:55 +0200

    'Model' extends 'Assignment'

--- hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/model/Model.java
+++ hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/model/Model.java
@@ -1,20 +1,14 @@
 
 package hu.bme.mit.inf.ttmc.core.model;
 
-import java.util.Collection;
 import java.util.Optional;
 
-import hu.bme.mit.inf.ttmc.core.decl.ConstDecl;
 import hu.bme.mit.inf.ttmc.core.expr.Expr;
 import hu.bme.mit.inf.ttmc.core.expr.LitExpr;
 import hu.bme.mit.inf.ttmc.core.type.BoolType;
 import hu.bme.mit.inf.ttmc.core.type.Type;
 
-public interface Model {
-
-	public Collection<? extends ConstDecl<?>> getConstDecls();
-
-	public <T extends Type> Optional<LitExpr<T>> eval(final ConstDecl<T> decl);
+public interface Model extends Assignment {
 
 	public <T extends Type> Optional<LitExpr<T>> eval(final Expr<T> expr);
 
