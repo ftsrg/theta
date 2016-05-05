fa3ce5d68b6a38f5504477a55de229537dfcd94b
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-05 11:22:29 +0200

    'Z3Model' accepts only 'ConstDecl's

--- hu.bme.mit.inf.ttmc.solver.z3/src/hu/bme/mit/inf/ttmc/solver/z3/solver/Z3Model.java
+++ hu.bme.mit.inf.ttmc.solver.z3/src/hu/bme/mit/inf/ttmc/solver/z3/solver/Z3Model.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.inf.ttmc.solver.z3.solver;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Collection;
@@ -31,7 +32,7 @@ class Z3Model implements Model {
 	final com.microsoft.z3.Model z3Model;
 
 	final Collection<ConstDecl<?>> constDecls;
-	final Map<Decl<?, ?>, LitExpr<?>> constToExpr;
+	final Map<ConstDecl<?>, LitExpr<?>> constToExpr;
 
 	public Z3Model(final Z3SymbolTable symbolTable, final Z3TransformationManager transformationManager, final Z3TermTransformer termTransformer,
 			final com.microsoft.z3.Model z3Model) {
@@ -52,14 +53,18 @@ class Z3Model implements Model {
 	@Override
 	public <DeclType extends Type, DeclKind extends Decl<DeclType, DeclKind>> Optional<LitExpr<DeclType>> eval(final Decl<DeclType, DeclKind> decl) {
 		checkNotNull(decl);
+		checkArgument(decl instanceof ConstDecl<?>);
 
-		LitExpr<?> val = constToExpr.get(decl);
+		@SuppressWarnings("unchecked")
+		final ConstDecl<DeclType> constDecl = (ConstDecl<DeclType>) decl;
+
+		LitExpr<?> val = constToExpr.get(constDecl);
 		if (val == null) {
-			final FuncDecl funcDecl = transformationManager.toSymbol(decl);
+			final FuncDecl funcDecl = transformationManager.toSymbol(constDecl);
 			final com.microsoft.z3.Expr term = z3Model.getConstInterp(funcDecl);
 			if (term != null) {
 				val = (LitExpr<?>) termTransformer.toExpr(term);
-				constToExpr.put(decl, val);
+				constToExpr.put(constDecl, val);
 			}
 		}
 
