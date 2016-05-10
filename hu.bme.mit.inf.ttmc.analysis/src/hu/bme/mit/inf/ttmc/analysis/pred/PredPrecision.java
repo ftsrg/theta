44cd13494b04880db83c6a0b3acb2ef0229245f9
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-11 00:09:25 +0200

    Made negation private for predicate precision

--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/pred/PredPrecision.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/pred/PredPrecision.java
@@ -1,13 +1,8 @@
 package hu.bme.mit.inf.ttmc.analysis.pred;
 
 import hu.bme.mit.inf.ttmc.analysis.Precision;
-import hu.bme.mit.inf.ttmc.core.expr.Expr;
-import hu.bme.mit.inf.ttmc.core.type.BoolType;
 import hu.bme.mit.inf.ttmc.formalism.common.Valuation;
 
 public interface PredPrecision extends Precision {
-
-	public Expr<? extends BoolType> negate(final Expr<? extends BoolType> pred);
-
 	public PredState mapToAbstractState(final Valuation valuation);
 }
