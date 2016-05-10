1a6ccb8a04f4014ab734584cc9fadd79d87a278e
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-11 00:08:09 +0200

    Separated precision interfaces and implementations

--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/expl/ExplPrecision.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/expl/ExplPrecision.java
@@ -1,54 +1,9 @@
 package hu.bme.mit.inf.ttmc.analysis.expl;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
 import hu.bme.mit.inf.ttmc.analysis.Precision;
-import hu.bme.mit.inf.ttmc.core.expr.LitExpr;
-import hu.bme.mit.inf.ttmc.core.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.Valuation;
-import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 
-public class ExplPrecision implements Precision {
+public interface ExplPrecision extends Precision {
 
-	private final Set<VarDecl<? extends Type>> visibleVars;
-	private final Set<VarDecl<? extends Type>> invisibleVars;
-
-	public static ExplPrecision create(final Collection<VarDecl<? extends Type>> visibleVars, final Collection<VarDecl<? extends Type>> invisibleVars) {
-		return new ExplPrecision(visibleVars, invisibleVars);
-	}
-
-	private ExplPrecision(final Collection<VarDecl<? extends Type>> visibleVars, final Collection<VarDecl<? extends Type>> invisibleVars) {
-		checkNotNull(visibleVars);
-		checkNotNull(invisibleVars);
-		this.visibleVars = Collections.unmodifiableSet(new HashSet<>(visibleVars));
-		this.invisibleVars = Collections.unmodifiableSet(new HashSet<>(invisibleVars));
-	}
-
-	public Collection<VarDecl<? extends Type>> getVisibleVars() {
-		return visibleVars;
-	}
-
-	public Collection<VarDecl<? extends Type>> getInvisibleVars() {
-		return invisibleVars;
-	}
-
-	public ExplState mapToAbstractState(final Valuation valuation) {
-		checkNotNull(valuation);
-		final Valuation.Builder builder = new Valuation.Builder();
-		for (final VarDecl<? extends Type> visibleVar : visibleVars) {
-			final Optional<? extends LitExpr<? extends Type>> eval = valuation.eval(visibleVar);
-			if (eval.isPresent()) {
-				builder.put(visibleVar, eval.get());
-			} else {
-				builder.put(visibleVar, visibleVar.getType().getAny());
-			}
-		}
-		return ExplState.create(builder.build());
-	}
+	public ExplState mapToAbstractState(final Valuation valuation);
 }
--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/pred/PredPrecision.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/pred/PredPrecision.java
@@ -1,64 +1,13 @@
 package hu.bme.mit.inf.ttmc.analysis.pred;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import hu.bme.mit.inf.ttmc.analysis.Precision;
 import hu.bme.mit.inf.ttmc.core.expr.Expr;
-import hu.bme.mit.inf.ttmc.core.expr.LitExpr;
-import hu.bme.mit.inf.ttmc.core.expr.impl.Exprs;
 import hu.bme.mit.inf.ttmc.core.type.BoolType;
 import hu.bme.mit.inf.ttmc.formalism.common.Valuation;
-import hu.bme.mit.inf.ttmc.formalism.utils.impl.FormalismUtils;
 
-public class PredPrecision implements Precision {
-	private final Map<Expr<? extends BoolType>, Expr<? extends BoolType>> preds;
+public interface PredPrecision extends Precision {
 
-	public static PredPrecision create(final Collection<Expr<? extends BoolType>> preds) {
-		return new PredPrecision(preds);
-	}
+	public Expr<? extends BoolType> negate(final Expr<? extends BoolType> pred);
 
-	private PredPrecision(final Collection<Expr<? extends BoolType>> preds) {
-		checkNotNull(preds);
-		this.preds = new HashMap<>();
-
-		for (final Expr<? extends BoolType> pred : preds) {
-			if (!this.preds.containsKey(pred)) {
-				this.preds.put(pred, Exprs.Not(pred));
-			}
-		}
-	}
-
-	public Collection<Expr<? extends BoolType>> getPreds() {
-		return Collections.unmodifiableCollection(preds.keySet());
-	}
-
-	public Expr<? extends BoolType> negate(final Expr<? extends BoolType> pred) {
-		final Expr<? extends BoolType> negated = preds.get(pred);
-		checkArgument(negated != null);
-		return negated;
-	}
-
-	public PredState mapToAbstractState(final Valuation valuation) {
-		checkNotNull(valuation);
-		final Set<Expr<? extends BoolType>> statePreds = new HashSet<>();
-
-		for (final Expr<? extends BoolType> pred : getPreds()) {
-			final LitExpr<? extends BoolType> predHolds = FormalismUtils.evaluate(pred, valuation);
-			if (predHolds.equals(Exprs.True())) {
-				statePreds.add(pred);
-			} else {
-				statePreds.add(negate(pred));
-			}
-		}
-
-		return PredState.create(statePreds);
-	}
+	public PredState mapToAbstractState(final Valuation valuation);
 }
