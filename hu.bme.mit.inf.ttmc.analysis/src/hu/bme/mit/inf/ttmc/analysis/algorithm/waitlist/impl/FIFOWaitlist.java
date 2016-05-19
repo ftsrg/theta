1e92bf1a4d813aa0e0ca59533432ae1cf7c108bc
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-19 16:34:03 +0200

    Added 'Waitlist' interface and implemented a simple FIFO and LIFO

---
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/algorithm/waitlist/impl/FIFOWaitlist.java
@@ -0,0 +1,39 @@
+package hu.bme.mit.inf.ttmc.analysis.algorithm.waitlist.impl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Queue;
+
+import hu.bme.mit.inf.ttmc.analysis.State;
+import hu.bme.mit.inf.ttmc.analysis.algorithm.waitlist.Waitlist;
+
+public class FIFOWaitlist<S extends State> implements Waitlist<S> {
+
+	private final Queue<S> waitlist;
+
+	public FIFOWaitlist() {
+		waitlist = new ArrayDeque<>();
+	}
+
+	public FIFOWaitlist(final Collection<? extends S> states) {
+		waitlist = new ArrayDeque<>(states);
+	}
+
+	@Override
+	public void add(final S state) {
+		waitlist.add(checkNotNull(state));
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return waitlist.isEmpty();
+	}
+
+	@Override
+	public S remove() {
+		return waitlist.remove();
+	}
+
+}
---
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/algorithm/waitlist/impl/LIFOWaitlist.java
@@ -0,0 +1,38 @@
+package hu.bme.mit.inf.ttmc.analysis.algorithm.waitlist.impl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+
+import hu.bme.mit.inf.ttmc.analysis.State;
+import hu.bme.mit.inf.ttmc.analysis.algorithm.waitlist.Waitlist;
+
+public class LIFOWaitlist<S extends State> implements Waitlist<S> {
+
+	private final Deque<S> waitlist;
+
+	public LIFOWaitlist() {
+		waitlist = new ArrayDeque<>();
+	}
+
+	public LIFOWaitlist(final Collection<? extends S> states) {
+		waitlist = new ArrayDeque<>(states);
+	}
+
+	@Override
+	public void add(final S state) {
+		waitlist.push(checkNotNull(state));
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return waitlist.isEmpty();
+	}
+
+	@Override
+	public S remove() {
+		return waitlist.pop();
+	}
+}
