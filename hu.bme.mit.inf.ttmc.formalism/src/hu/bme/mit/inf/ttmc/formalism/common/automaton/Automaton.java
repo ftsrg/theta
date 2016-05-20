0f054476b20cd893dc7563a3a4b1ab2304fa6399
Hajdu Ákos <sonic18291@hotmail.com>
2016-05-20 15:46:55 +0200

    Added 'Formalism' interface

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/automaton/Automaton.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/automaton/Automaton.java
@@ -2,7 +2,9 @@ package hu.bme.mit.inf.ttmc.formalism.common.automaton;
 
 import java.util.Collection;
 
-public interface Automaton<L extends Loc<L, E>, E extends Edge<L, E>> {
+import hu.bme.mit.inf.ttmc.formalism.Formalism;
+
+public interface Automaton<L extends Loc<L, E>, E extends Edge<L, E>> extends Formalism {
 
 	public L getInitLoc();
 
