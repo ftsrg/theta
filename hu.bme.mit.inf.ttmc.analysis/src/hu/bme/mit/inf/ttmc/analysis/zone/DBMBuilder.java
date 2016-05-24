db36a50479a1440cdfed993eb246ef4b7b50c7c1
Tamás Tóth <totht@mit.bme.hu>
2016-05-24 16:49:04 +0200

    DBM classes reimplemented

---
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/zone/DBMBuilder.java
@@ -0,0 +1,402 @@
+package hu.bme.mit.inf.ttmc.analysis.zone;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.Inf;
+import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.Leq;
+import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.Lt;
+import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.add;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import hu.bme.mit.inf.ttmc.common.matrix.IntMatrix;
+import hu.bme.mit.inf.ttmc.formalism.common.decl.ClockDecl;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.AndConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.ClockConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.DiffEqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.DiffGeqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.DiffGtConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.DiffLeqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.DiffLtConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.TrueConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.UnitEqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.UnitGeqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.UnitGtConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.UnitLeqConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.constr.UnitLtConstr;
+import hu.bme.mit.inf.ttmc.formalism.ta.utils.ClockConstrVisitor;
+
+public final class DBMBuilder {
+
+	final LinkedHashMap<ClockDecl, Integer> clockToIndex;
+	final IntMatrix matrix;
+
+	private final AndOperationVisitor visitor;
+
+	DBMBuilder(final Set<? extends ClockDecl> clockDecls) {
+		clockToIndex = createIndexMapFrom(clockDecls);
+		matrix = IntMatrix.create(clockDecls.size() + 1, clockDecls.size() + 1);
+		matrix.fill((i, j) -> i == 0 || i == j ? Leq(0) : Inf());
+		visitor = new AndOperationVisitor();
+	}
+
+	DBMBuilder(final Map<ClockDecl, Integer> clockToIndex, final IntMatrix matrix) {
+		this.clockToIndex = new LinkedHashMap<>(clockToIndex);
+		this.matrix = IntMatrix.copyOf(matrix);
+		visitor = new AndOperationVisitor();
+	}
+
+	public DBM build() {
+		return new DBM(this);
+	}
+
+	////
+
+	public DBMBuilder up() {
+		for (int i = 1; i < size(); i++) {
+			matrix.set(i, 0, Inf());
+		}
+		assert isClosed();
+		return this;
+	}
+
+	public DBMBuilder down() {
+		for (int i = 1; i < size(); i++) {
+			matrix.set(0, i, Leq(0));
+			for (int j = 1; j < size(); j++) {
+				if (matrix.get(j, i) < matrix.get(0, i)) {
+					matrix.set(0, i, matrix.get(j, i));
+				}
+			}
+		}
+		assert isClosed();
+		return this;
+	}
+
+	//
+
+	public DBMBuilder and(final ClockConstr constr) {
+		constr.accept(visitor, null);
+		return this;
+	}
+
+	//
+
+	public DBMBuilder free(final ClockDecl clock) {
+		checkNotNull(clock);
+		checkNotZeroClock(clock);
+
+		final Integer x = clockToIndex.get(clock);
+		if (x != null) {
+			free(x);
+		}
+		return this;
+	}
+
+	private void free(final int x) {
+		for (int i = 0; i < size(); i++) {
+			if (i != x) {
+				matrix.set(x, i, Inf());
+				matrix.set(i, x, matrix.get(i, 0));
+			}
+		}
+		assert isClosed();
+	}
+
+	//
+
+	public DBMBuilder reset(final ClockDecl clock, final int m) {
+		checkNotNull(clock);
+		checkNotZeroClock(clock);
+
+		final int x = ensureDeclaredAndGetIndex(clock);
+		reset(x, m);
+
+		return this;
+	}
+
+	private void reset(final int x, final int m) {
+		for (int i = 0; i < size(); i++) {
+			matrix.set(x, i, add(Leq(m), matrix.get(0, i)));
+			matrix.set(i, x, add(matrix.get(i, 0), Leq(-m)));
+		}
+		assert isClosed();
+	}
+
+	//
+
+	public DBMBuilder copy(final ClockDecl lhs, final ClockDecl rhs) {
+		checkNotNull(lhs);
+		checkNotNull(rhs);
+		checkNotZeroClock(lhs);
+		checkNotZeroClock(rhs);
+
+		final Integer lhsIndex = clockToIndex.get(lhs);
+		final Integer rhsIndex = clockToIndex.get(rhs);
+
+		if (rhsIndex != null) {
+			final int x = ensureDeclaredAndGetIndex(lhs);
+			final int y = rhsIndex;
+			copy(x, y);
+		} else if (lhsIndex != null) {
+			final int x = lhsIndex;
+			free(x);
+		}
+
+		return this;
+	}
+
+	private void copy(final int x, final int y) {
+		for (int i = 0; i < size(); i++) {
+			if (i != x) {
+				matrix.set(x, i, matrix.get(y, i));
+				matrix.set(i, x, matrix.get(i, y));
+			}
+		}
+		matrix.set(x, y, Leq(0));
+		matrix.set(y, x, Leq(0));
+		assert isClosed();
+	}
+
+	//
+
+	public DBMBuilder shift(final ClockDecl clock, final int m) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+	@SuppressWarnings("unused")
+	private void shift(final int x, final int m) {
+		for (int i = 0; i < size(); i++) {
+			if (i != x) {
+				matrix.set(x, i, add(matrix.get(x, i), Leq(m)));
+				matrix.set(i, x, add(matrix.get(i, x), Leq(-m)));
+			}
+		}
+		matrix.set(x, 0, max(matrix.get(x, 0), Leq(0)));
+		matrix.set(0, x, min(matrix.get(0, x), Leq(0)));
+		assert isClosed();
+	}
+
+	//
+
+	public DBMBuilder norm(final Map<ClockDecl, Integer> bounds) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+	@SuppressWarnings("unused")
+	private void norm(final int[] k) {
+		for (int i = 0; i < size(); i++) {
+			for (int j = 0; j < size(); j++) {
+				if (matrix.get(i, j) != Inf()) {
+					if (matrix.get(i, j) > Leq(k[i])) {
+						matrix.set(i, j, Inf());
+					} else if (matrix.get(i, j) < Leq(-k[j])) {
+						matrix.set(i, j, Lt(-k[j]));
+					}
+				}
+			}
+		}
+		close();
+	}
+
+	private void close() {
+		for (int k = 0; k < size(); k++) {
+			for (int i = 0; i < size(); i++) {
+				for (int j = 0; j < size(); j++) {
+					matrix.set(i, j, min(matrix.get(i, j), add(matrix.get(i, k), matrix.get(k, j))));
+				}
+			}
+		}
+		assert isClosed();
+	}
+
+	////
+
+	private boolean isClosed() {
+		for (int i = 0; i < size(); i++) {
+			for (int j = 0; j <= size(); j++) {
+				for (int k = 0; k <= size(); k++) {
+					if (matrix.get(i, j) > add(matrix.get(i, k), matrix.get(k, j))) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	private static void checkNotZeroClock(final ClockDecl clock) {
+		checkArgument(!clock.equals(DBM.zeroClock()));
+	}
+
+	private static LinkedHashMap<ClockDecl, Integer> createIndexMapFrom(final Set<? extends ClockDecl> clockDecls) {
+		final LinkedHashMap<ClockDecl, Integer> result = new LinkedHashMap<ClockDecl, Integer>();
+		result.put(DBM.zeroClock(), 0);
+		int i = 1;
+		for (final ClockDecl clockDecl : clockDecls) {
+			result.put(clockDecl, i);
+			i++;
+		}
+		return result;
+	}
+
+	private int ensureDeclaredAndGetIndex(final ClockDecl clockDecl) {
+		Integer index = clockToIndex.get(clockDecl);
+		if (index == null) {
+			index = size();
+
+			clockToIndex.put(clockDecl, index);
+			matrix.expand(size() + 1, size() + 1);
+
+			final int x = index;
+			matrix.set(0, x, Leq(0));
+			matrix.set(x, x, Leq(0));
+			matrix.set(x, 0, Inf());
+			for (int i = 1; i < size() - 1; i++) {
+				matrix.set(x, i, Inf());
+				matrix.set(i, x, matrix.get(i, 0));
+			}
+		}
+		return index;
+	}
+
+	private int size() {
+		return matrix.rows();
+	}
+
+	////
+
+	private final class AndOperationVisitor implements ClockConstrVisitor<Void, Void> {
+
+		private void and(final int x, final int y, final int b) {
+			if (add(matrix.get(y, x), b) < 0) {
+				matrix.set(0, 0, Leq(-1));
+
+			} else if (b < matrix.get(x, y)) {
+				matrix.set(x, y, b);
+
+				for (int i = 0; i < size(); i++) {
+					for (int j = 0; j < size(); j++) {
+						if (add(matrix.get(i, x), matrix.get(x, j)) < matrix.get(i, j)) {
+							matrix.set(i, j, add(matrix.get(i, x), matrix.get(x, j)));
+						}
+						if (add(matrix.get(i, y), matrix.get(y, j)) < matrix.get(i, j)) {
+							matrix.set(i, j, add(matrix.get(i, y), matrix.get(y, j)));
+						}
+					}
+				}
+			}
+			assert isClosed();
+		}
+
+		@Override
+		public Void visit(final TrueConstr constr, final Void param) {
+			return null;
+		}
+
+		@Override
+		public Void visit(final UnitLtConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getClock());
+			final int m = constr.getBound();
+			and(x, 0, Lt(m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final UnitLeqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getClock());
+			final int m = constr.getBound();
+			and(x, 0, Leq(m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final UnitGtConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getClock());
+			final int m = constr.getBound();
+			and(0, x, Lt(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final UnitGeqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getClock());
+			final int m = constr.getBound();
+			and(0, x, Leq(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final UnitEqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getClock());
+			final int m = constr.getBound();
+			and(x, 0, Leq(m));
+			and(0, x, Leq(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final DiffLtConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getLeftClock());
+			final int y = ensureDeclaredAndGetIndex(constr.getRightClock());
+			final int m = constr.getBound();
+			and(x, y, Lt(m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final DiffLeqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getLeftClock());
+			final int y = ensureDeclaredAndGetIndex(constr.getRightClock());
+			final int m = constr.getBound();
+			and(x, y, Leq(m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final DiffGtConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getLeftClock());
+			final int y = ensureDeclaredAndGetIndex(constr.getRightClock());
+			final int m = constr.getBound();
+			and(y, x, Lt(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final DiffGeqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getLeftClock());
+			final int y = ensureDeclaredAndGetIndex(constr.getRightClock());
+			final int m = constr.getBound();
+			and(y, x, Leq(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final DiffEqConstr constr, final Void param) {
+			final int x = ensureDeclaredAndGetIndex(constr.getLeftClock());
+			final int y = ensureDeclaredAndGetIndex(constr.getRightClock());
+			final int m = constr.getBound();
+			and(x, y, Leq(m));
+			and(y, x, Leq(-m));
+			return null;
+		}
+
+		@Override
+		public Void visit(final AndConstr constr, final Void param) {
+			for (final ClockConstr atomicConstr : constr.getConstrs()) {
+				atomicConstr.accept(this, param);
+				if (matrix.get(0, 0) < 0) {
+					return null;
+				}
+			}
+			return null;
+		}
+	}
+
+}
--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/zone/DBMTests.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/zone/DBMTests.java
@@ -1,35 +1,28 @@
 package hu.bme.mit.inf.ttmc.analysis.zone;
 
-import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.Leq;
-import static hu.bme.mit.inf.ttmc.analysis.zone.DiffBounds.Lt;
+import static hu.bme.mit.inf.ttmc.formalism.common.decl.impl.Decls2.Clock;
+
+import java.util.Set;
 
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
+
+import hu.bme.mit.inf.ttmc.formalism.common.decl.ClockDecl;
 
 public class DBMTests {
 
 	@Test
 	public void test() {
-		final DBM dbm = DBM.top(2);
+		final ClockDecl x = Clock("x");
+		final ClockDecl y = Clock("y");
+		final ClockDecl z = Clock("z");
+
+		final Set<ClockDecl> clocks = ImmutableSet.of(x, y, z);
+
+		final DBM dbm = DBM.zero(clocks).transform().up().free(x).reset(z, 3).up().build();
 
 		System.out.println(dbm);
-		System.out.println();
-
-		dbm.up();
-
-		System.out.println(dbm);
-		System.out.println();
-
-		dbm.and(1, 0, Lt(20));
-		dbm.and(2, 0, Leq(20));
-		dbm.and(2, 1, Leq(10));
-		dbm.and(1, 2, Leq(-10));
-
-		System.out.println(dbm);
-		System.out.println();
-
-		dbm.and(1, 0, Lt(-10));
-
-		System.out.println(dbm);
-
 	}
+
 }
