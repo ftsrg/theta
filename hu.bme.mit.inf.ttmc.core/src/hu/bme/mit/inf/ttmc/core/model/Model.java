47d6f10f00683e292c81429265998a3cf7c187ba
Tamás Tóth <totht@mit.bme.hu>
2016-05-31 10:57:29 +0200

    Model.getDecls() returns Collection<? extends ConstDecl<?>>

--- hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/model/Model.java
+++ hu.bme.mit.inf.ttmc.core/src/hu/bme/mit/inf/ttmc/core/model/Model.java
@@ -1,6 +1,13 @@
 
 package hu.bme.mit.inf.ttmc.core.model;
 
+import java.util.Collection;
+
+import hu.bme.mit.inf.ttmc.core.decl.ConstDecl;
+
 public interface Model extends Assignment {
+
+	@Override
+	Collection<? extends ConstDecl<?>> getDecls();
 
 }
