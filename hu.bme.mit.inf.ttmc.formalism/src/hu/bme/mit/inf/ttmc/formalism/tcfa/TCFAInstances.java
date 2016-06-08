882eeb171f174fbd8c8d6f0ee100c7544cc076bb
Tamás Tóth <totht@mit.bme.hu>
2016-06-08 16:38:34 +0200

    Variable x changed to clock in TCFAInstances.fischer()

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFAInstances.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/TCFAInstances.java
@@ -6,14 +6,14 @@ import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Int;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Leq;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Neq;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Rat;
-import static hu.bme.mit.inf.ttmc.core.type.impl.Types.Rat;
-import static hu.bme.mit.inf.ttmc.formalism.common.decl.impl.Decls2.Var;
+import static hu.bme.mit.inf.ttmc.formalism.common.decl.impl.Decls2.Clock;
 import static hu.bme.mit.inf.ttmc.formalism.common.stmt.impl.Stmts.Assign;
 import static hu.bme.mit.inf.ttmc.formalism.common.stmt.impl.Stmts.Assume;
 
 import hu.bme.mit.inf.ttmc.core.expr.Expr;
 import hu.bme.mit.inf.ttmc.core.type.IntType;
 import hu.bme.mit.inf.ttmc.core.type.RatType;
+import hu.bme.mit.inf.ttmc.formalism.common.decl.ClockDecl;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 import hu.bme.mit.inf.ttmc.formalism.tcfa.impl.MutableTCFA;
 
@@ -24,9 +24,9 @@ public final class TCFAInstances {
 
 	public static TCFA fischer(final int id, final int ia, final int ib, final VarDecl<IntType> vlock) {
 
-		final VarDecl<RatType> vx = Var("x_" + id, Rat());
+		final ClockDecl cx = Clock("x_" + id);
 
-		final Expr<RatType> x = vx.getRef();
+		final Expr<RatType> x = cx.getRef();
 		final Expr<IntType> lock = vlock.getRef();
 
 		final Expr<IntType> i = Int(id);
@@ -47,10 +47,10 @@ public final class TCFAInstances {
 
 		final TCFAEdge edge1 = tcfa.createEdge(l0, l1);
 		edge1.getStmts().add(Assume(Eq(lock, izero)));
-		edge1.getStmts().add(Assign(vx, rzero));
+		edge1.getStmts().add(Assign(cx, rzero));
 
 		final TCFAEdge edge2 = tcfa.createEdge(l1, l2);
-		edge2.getStmts().add(Assign(vx, rzero));
+		edge2.getStmts().add(Assign(cx, rzero));
 		edge2.getStmts().add(Assign(vlock, i));
 
 		final TCFAEdge edge3 = tcfa.createEdge(l2, l3);
@@ -62,7 +62,7 @@ public final class TCFAInstances {
 		edge4.getStmts().add(Assume(Neq(lock, i)));
 
 		final TCFAEdge edge5 = tcfa.createEdge(l3, l0);
-		edge5.getStmts().add(Assign(vx, rzero));
+		edge5.getStmts().add(Assign(cx, rzero));
 		edge5.getStmts().add(Assign(vlock, izero));
 
 		return tcfa;
