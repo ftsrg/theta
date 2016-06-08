2454df69b25fea43789d5c0842c17a858f19de7f
Tamás Tóth <totht@mit.bme.hu>
2016-06-08 16:40:32 +0200

    TCFATargetPredicate generic type parameter fixed

--- hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/TCFATargetPredicate.java
+++ hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/TCFATargetPredicate.java
@@ -1,12 +1,13 @@
 package hu.bme.mit.inf.ttmc.analysis.tcfa;
 
+import hu.bme.mit.inf.ttmc.analysis.State;
 import hu.bme.mit.inf.ttmc.analysis.TargetPredicate;
 import hu.bme.mit.inf.ttmc.formalism.tcfa.TCFALoc;
 
-public class TCFATargetPredicate implements TargetPredicate<TCFAState<?>, TCFALoc> {
+public class TCFATargetPredicate implements TargetPredicate<TCFAState<? extends State>, TCFALoc> {
 
 	@Override
-	public boolean isTargetState(final TCFAState<?> state, final TCFALoc targetLoc) {
+	public boolean isTargetState(final TCFAState<? extends State> state, final TCFALoc targetLoc) {
 		return state.getLoc().equals(targetLoc);
 	}
 
