575320d23afbe8f8ae424d407094dc73e0a3b3a9
Tamás Tóth <totht@mit.bme.hu>
2016-06-08 14:58:56 +0200

    Method getIndex(VarDecl<?>) renamed to get(VarDecl<?>) in class VarIndexes

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/utils/tests/VarIndexesTests.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/utils/tests/VarIndexesTests.java
@@ -20,31 +20,31 @@ public class VarIndexesTests {
 		final VarIndexes all0 = VarIndexes.all(0);
 		final VarIndexes all1 = VarIndexes.all(1);
 
-		assertEquals(0, all0.getIndex(x));
-		assertEquals(0, all0.getIndex(y));
-		assertEquals(0, all0.getIndex(z));
+		assertEquals(0, all0.get(x));
+		assertEquals(0, all0.get(y));
+		assertEquals(0, all0.get(z));
 
-		assertEquals(1, all1.getIndex(x));
-		assertEquals(1, all1.getIndex(y));
-		assertEquals(1, all1.getIndex(z));
+		assertEquals(1, all1.get(x));
+		assertEquals(1, all1.get(y));
+		assertEquals(1, all1.get(z));
 	}
 
 	@Test
 	public void testInc() {
 		final VarIndexes indexes = VarIndexes.builder(0).inc(x).inc(z).inc(x).build();
 
-		assertEquals(2, indexes.getIndex(x));
-		assertEquals(0, indexes.getIndex(y));
-		assertEquals(1, indexes.getIndex(z));
+		assertEquals(2, indexes.get(x));
+		assertEquals(0, indexes.get(y));
+		assertEquals(1, indexes.get(z));
 	}
 
 	@Test
 	public void testIncAll() {
 		final VarIndexes indexes = VarIndexes.builder(0).inc(x).incAll().build();
 
-		assertEquals(2, indexes.getIndex(x));
-		assertEquals(1, indexes.getIndex(y));
-		assertEquals(1, indexes.getIndex(z));
+		assertEquals(2, indexes.get(x));
+		assertEquals(1, indexes.get(y));
+		assertEquals(1, indexes.get(z));
 	}
 
 }
