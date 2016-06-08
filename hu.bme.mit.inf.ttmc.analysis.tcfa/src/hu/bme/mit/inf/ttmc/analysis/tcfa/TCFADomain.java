3c724d7850cb84b26fc617cf5e6e6d0b9bb3a599
Tamás Tóth <totht@mit.bme.hu>
2016-06-08 17:41:46 +0200

    TCFADomain added

---
+++ hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/TCFADomain.java
@@ -0,0 +1,39 @@
+package hu.bme.mit.inf.ttmc.analysis.tcfa;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import hu.bme.mit.inf.ttmc.analysis.Domain;
+import hu.bme.mit.inf.ttmc.analysis.State;
+
+public final class TCFADomain<S extends State> implements Domain<TCFAState<S>> {
+
+	private final Domain<S> domain;
+
+	public TCFADomain(final Domain<S> domain) {
+		this.domain = checkNotNull(domain);
+	}
+
+	@Override
+	public boolean isTop(final TCFAState<S> state) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+	@Override
+	public boolean isBottom(final TCFAState<S> state) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+	@Override
+	public boolean isLeq(final TCFAState<S> state1, final TCFAState<S> state2) {
+		return state1.getLoc().equals(state2.getLoc()) && domain.isLeq(state1.getState(), state2.getState());
+	}
+
+	@Override
+	public TCFAState<S> join(final TCFAState<S> state1, final TCFAState<S> state2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+}
