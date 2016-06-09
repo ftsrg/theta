e8b4340a9eb7c538ae6aefadbffc420ad54cfbe8
Tamás Tóth <totht@mit.bme.hu>
2016-06-09 18:45:09 +0200

    CompositeDomain implemented

---
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/composite/CompositeDomain.java
@@ -0,0 +1,39 @@
+package hu.bme.mit.inf.ttmc.analysis.composite;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import hu.bme.mit.inf.ttmc.analysis.Domain;
+import hu.bme.mit.inf.ttmc.analysis.State;
+
+public class CompositeDomain<S1 extends State, S2 extends State> implements Domain<CompositeState<S1, S2>> {
+
+	private final Domain<S1> domain1;
+	private final Domain<S2> domain2;
+
+	public CompositeDomain(final Domain<S1> domain1, final Domain<S2> domain2) {
+		this.domain1 = checkNotNull(domain1);
+		this.domain2 = checkNotNull(domain2);
+	}
+
+	@Override
+	public boolean isTop(final CompositeState<S1, S2> state) {
+		return domain1.isTop(state._1()) && domain2.isTop(state._2());
+	}
+
+	@Override
+	public boolean isBottom(final CompositeState<S1, S2> state) {
+		return domain1.isBottom(state._1()) && domain2.isBottom(state._2());
+	}
+
+	@Override
+	public boolean isLeq(final CompositeState<S1, S2> state1, final CompositeState<S1, S2> state2) {
+		return domain1.isLeq(state1._1(), state2._1()) && domain2.isLeq(state1._2(), state2._2());
+	}
+
+	@Override
+	public CompositeState<S1, S2> join(final CompositeState<S1, S2> state1, final CompositeState<S1, S2> state2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+
+}
