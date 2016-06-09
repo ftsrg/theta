2295ac7401f3484d2694a2a4dba44fd5ae4430db
Tamás Tóth <totht@mit.bme.hu>
2016-06-09 14:54:14 +0200

    ClockDeclImpl refactored

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/decl/impl/ClockDeclImpl.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/decl/impl/ClockDeclImpl.java
@@ -6,26 +6,37 @@ import static hu.bme.mit.inf.ttmc.core.type.impl.Types.Rat;
 import java.util.HashMap;
 import java.util.Map;
 
-import hu.bme.mit.inf.ttmc.core.decl.impl.AbstractDecl;
 import hu.bme.mit.inf.ttmc.core.type.RatType;
 import hu.bme.mit.inf.ttmc.core.utils.DeclVisitor;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.ClockDecl;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.IndexedConstDecl;
-import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 import hu.bme.mit.inf.ttmc.formalism.common.expr.ClockRefExpr;
 
-class ClockDeclImpl extends AbstractDecl<RatType, VarDecl<RatType>> implements ClockDecl {
+class ClockDeclImpl implements ClockDecl {
 
 	private static final int HASH_SEED = 8053;
 	private static final String DECL_LABEL = "Clock";
 
+	private final String name;
 	private final ClockRefExpr ref;
 	private final Map<Integer, IndexedConstDecl<RatType>> indexToConst;
 
+	private volatile int hashCode = 0;
+
 	ClockDeclImpl(final String name) {
-		super(name, Rat());
+		this.name = name;
 		ref = new ClockRefExprImpl(this);
 		indexToConst = new HashMap<>();
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public RatType getType() {
+		return Rat();
 	}
 
 	@Override
@@ -45,24 +56,40 @@ class ClockDeclImpl extends AbstractDecl<RatType, VarDecl<RatType>> implements C
 	}
 
 	@Override
+	public int compareTo(final ClockDecl that) {
+		return this.getName().compareTo(that.getName());
+	}
+
+	@Override
 	public <P, R> R accept(final DeclVisitor<? super P, ? extends R> visitor, final P param) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("TODO: auto-generated method stub");
 	}
 
 	@Override
-	public int compareTo(final ClockDecl that) {
-		return this.getName().compareTo(that.getName());
+	public int hashCode() {
+		int result = hashCode;
+		if (result == 0) {
+			result = HASH_SEED;
+			result = 31 * result + name.hashCode();
+			hashCode = result;
+		}
+		return result;
 	}
 
 	@Override
-	protected int getHashSeed() {
-		return HASH_SEED;
+	public boolean equals(final Object obj) {
+		return this == obj;
 	}
 
 	@Override
-	protected String getDeclLabel() {
-		return DECL_LABEL;
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(DECL_LABEL);
+		sb.append("(");
+		sb.append(name);
+		sb.append(")");
+		return sb.toString();
 	}
 
 }
