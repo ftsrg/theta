9a30da5926ef214b9ca345d9cd4f8d20efac3ee1
Tamás Tóth <totht@mit.bme.hu>
2016-06-09 14:56:17 +0200

    ZoneDomain is singleton

--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/zone/ZoneDomain.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/zone/ZoneDomain.java
@@ -4,6 +4,15 @@ import hu.bme.mit.inf.ttmc.analysis.Domain;
 
 public final class ZoneDomain implements Domain<ZoneState> {
 
+	private static final ZoneDomain INSTANCE = new ZoneDomain();
+
+	private ZoneDomain() {
+	}
+
+	public static ZoneDomain getInstance() {
+		return INSTANCE;
+	}
+
 	@Override
 	public boolean isTop(final ZoneState state) {
 		// TODO Auto-generated method stub
