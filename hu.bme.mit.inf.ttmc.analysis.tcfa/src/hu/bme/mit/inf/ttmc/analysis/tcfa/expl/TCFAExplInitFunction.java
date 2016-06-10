4548a25b9151d530fe2bf12eb7adecb4faa0e4f6
Tamás Tóth <totht@mit.bme.hu>
2016-06-10 11:47:30 +0200

    Init functions for TCFA analysis are independent from initial location

--- hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/expl/TCFAExplInitFunction.java
+++ hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/expl/TCFAExplInitFunction.java
@@ -1,18 +1,22 @@
 package hu.bme.mit.inf.ttmc.analysis.tcfa.expl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Collection;
 import java.util.Collections;
 
 import hu.bme.mit.inf.ttmc.analysis.InitFunction;
 import hu.bme.mit.inf.ttmc.analysis.expl.ExplPrecision;
 import hu.bme.mit.inf.ttmc.analysis.expl.ExplState;
+import hu.bme.mit.inf.ttmc.common.Unit;
 import hu.bme.mit.inf.ttmc.formalism.common.Valuation;
-import hu.bme.mit.inf.ttmc.formalism.tcfa.TCFALoc;
 
-public class TCFAExplInitFunction implements InitFunction<ExplState, ExplPrecision, TCFALoc> {
+public class TCFAExplInitFunction implements InitFunction<ExplState, ExplPrecision, Unit> {
 
 	@Override
-	public Collection<ExplState> getInitStates(final ExplPrecision precision, final TCFALoc init) {
+	public Collection<ExplState> getInitStates(final ExplPrecision precision, final Unit init) {
+		checkNotNull(precision);
+		checkNotNull(init);
 		return Collections.singleton(ExplState.create(Valuation.builder().build()));
 	}
 
--- hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/pred/TCFAPredInitFunction.java
+++ hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/pred/TCFAPredInitFunction.java
@@ -1,17 +1,21 @@
 package hu.bme.mit.inf.ttmc.analysis.tcfa.pred;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Collection;
 import java.util.Collections;
 
 import hu.bme.mit.inf.ttmc.analysis.InitFunction;
 import hu.bme.mit.inf.ttmc.analysis.pred.PredPrecision;
 import hu.bme.mit.inf.ttmc.analysis.pred.PredState;
-import hu.bme.mit.inf.ttmc.formalism.tcfa.TCFALoc;
+import hu.bme.mit.inf.ttmc.common.Unit;
 
-public class TCFAPredInitFunction implements InitFunction<PredState, PredPrecision, TCFALoc> {
+public class TCFAPredInitFunction implements InitFunction<PredState, PredPrecision, Unit> {
 
 	@Override
-	public Collection<PredState> getInitStates(final PredPrecision precision, final TCFALoc init) {
+	public Collection<PredState> getInitStates(final PredPrecision precision, final Unit init) {
+		checkNotNull(precision);
+		checkNotNull(init);
 		return Collections.singleton(PredState.create(Collections.emptySet()));
 	}
 
