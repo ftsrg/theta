c466c229e85e49ede2dbd0580850925032bc39de
Tamás Tóth <totht@mit.bme.hu>
2016-06-24 11:27:12 +0200

    Pointer type added

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/expr/impl/Exprs2.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/common/expr/impl/Exprs2.java
@@ -4,9 +4,16 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import hu.bme.mit.inf.ttmc.core.expr.Expr;
 import hu.bme.mit.inf.ttmc.core.type.Type;
+import hu.bme.mit.inf.ttmc.formalism.common.expr.NullExpr;
 import hu.bme.mit.inf.ttmc.formalism.common.expr.PrimedExpr;
 
 public final class Exprs2 {
+
+	private static final NullExpr<?> NULL_EXPR;
+
+	static {
+		NULL_EXPR = new NullExprImpl<>();
+	}
 
 	private Exprs2() {
 	}
@@ -14,6 +21,11 @@ public final class Exprs2 {
 	public static <T extends Type> PrimedExpr<T> Prime(final Expr<? extends T> op) {
 		checkNotNull(op);
 		return new PrimedExprImpl<>(op);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T extends Type> NullExpr<T> Null() {
+		return (NullExpr<T>) NULL_EXPR;
 	}
 
 }
