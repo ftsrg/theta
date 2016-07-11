4f344cc5d71b954eb149f433b733f2a34bb111e9
Hajdu Ákos <sonic18291@hotmail.com>
2016-07-11 13:54:05 +0200

    Moved refiner operator implementations to the new interfaces

--- hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/algorithm/impl/refineroperator/GlobalExplItpRefinerOperator.java
+++ hu.bme.mit.inf.ttmc.analysis/src/hu/bme/mit/inf/ttmc/analysis/algorithm/impl/refineroperator/GlobalExplItpRefinerOperator.java
@@ -1,10 +1,11 @@
-package hu.bme.mit.inf.ttmc.analysis.algorithm.impl.old.refiner;
+package hu.bme.mit.inf.ttmc.analysis.algorithm.impl.refineroperator;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import hu.bme.mit.inf.ttmc.analysis.algorithm.old.Counterexample;
-import hu.bme.mit.inf.ttmc.analysis.algorithm.old.Refiner;
+import hu.bme.mit.inf.ttmc.analysis.Action;
+import hu.bme.mit.inf.ttmc.analysis.Counterexample;
+import hu.bme.mit.inf.ttmc.analysis.algorithm.impl.RefinerOperator;
 import hu.bme.mit.inf.ttmc.analysis.expl.ExplState;
 import hu.bme.mit.inf.ttmc.analysis.expl.GlobalExplPrecision;
 import hu.bme.mit.inf.ttmc.analysis.refutation.ItpRefutation;
@@ -14,25 +15,10 @@ import hu.bme.mit.inf.ttmc.core.type.Type;
 import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
 import hu.bme.mit.inf.ttmc.formalism.utils.FormalismUtils;
 
-public class GlobalExplItpRefiner implements Refiner<ExplState, GlobalExplPrecision, ItpRefutation> {
-
-	private static final GlobalExplItpRefiner INSTANCE;
-
-	static {
-		INSTANCE = new GlobalExplItpRefiner();
-	}
-
-	private GlobalExplItpRefiner() {
-
-	}
-
-	public static GlobalExplItpRefiner create() {
-		return INSTANCE;
-	}
+public class GlobalExplItpRefinerOperator<A extends Action> implements RefinerOperator<ExplState, A, ItpRefutation, GlobalExplPrecision> {
 
 	@Override
-	public GlobalExplPrecision refine(final GlobalExplPrecision precision, final Counterexample<ExplState> abstractCex,
-			final ItpRefutation refutation) {
+	public GlobalExplPrecision refine(final GlobalExplPrecision precision, final ItpRefutation refutation, final Counterexample<ExplState, A> counterexample) {
 		final Set<VarDecl<? extends Type>> newVisiblevars = new HashSet<>();
 		for (final Expr<? extends BoolType> pred : refutation) {
 			FormalismUtils.collectVars(pred, newVisiblevars);
