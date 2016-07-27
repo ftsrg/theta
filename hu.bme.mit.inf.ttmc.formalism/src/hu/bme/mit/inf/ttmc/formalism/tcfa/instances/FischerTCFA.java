e067d96d062f56068f5f5bebbfe804d330478514
Tamás Tóth <totht@mit.bme.hu>
2016-07-27 13:31:04 +0200

    Clock resets fixed in FischerTCFA

--- hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/instances/FischerTCFA.java
+++ hu.bme.mit.inf.ttmc.formalism/src/hu/bme/mit/inf/ttmc/formalism/tcfa/instances/FischerTCFA.java
@@ -7,7 +7,6 @@ import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Geq;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Int;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Leq;
 import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Neq;
-import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Rat;
 import static hu.bme.mit.inf.ttmc.formalism.common.decl.impl.Decls2.Clock;
 import static hu.bme.mit.inf.ttmc.formalism.common.stmt.impl.Stmts.Assign;
 import static hu.bme.mit.inf.ttmc.formalism.common.stmt.impl.Stmts.Assume;
@@ -80,8 +79,7 @@ public final class FischerTCFA {
 		final Expr<IntType> a = Int(ia);
 		final Expr<IntType> b = Int(ib);
 
-		final Expr<RatType> rzero = Rat(0, 1);
-		final Expr<IntType> izero = Int(0);
+		final Expr<IntType> zero = Int(0);
 
 		final TCFALoc l0 = tcfa.getInitLoc();
 		final TCFALoc l1 = tcfa.createLoc("wait");
@@ -94,11 +92,11 @@ public final class FischerTCFA {
 		l1.getInvars().add(Leq(x, a));
 
 		final TCFAEdge edge1 = tcfa.createEdge(l0, l1);
-		edge1.getStmts().add(Assume(Eq(lock, izero)));
-		edge1.getStmts().add(Assign(cx, rzero));
+		edge1.getStmts().add(Assume(Eq(lock, zero)));
+		edge1.getStmts().add(Assign(cx, zero));
 
 		final TCFAEdge edge2 = tcfa.createEdge(l1, l2);
-		edge2.getStmts().add(Assign(cx, rzero));
+		edge2.getStmts().add(Assign(cx, zero));
 		edge2.getStmts().add(Assign(vlock, i));
 
 		final TCFAEdge edge3 = tcfa.createEdge(l2, l3);
@@ -110,8 +108,8 @@ public final class FischerTCFA {
 		edge4.getStmts().add(Assume(Neq(lock, i)));
 
 		final TCFAEdge edge5 = tcfa.createEdge(l3, l0);
-		edge5.getStmts().add(Assign(cx, rzero));
-		edge5.getStmts().add(Assign(vlock, izero));
+		edge5.getStmts().add(Assign(cx, zero));
+		edge5.getStmts().add(Assign(vlock, zero));
 	}
 
 }
