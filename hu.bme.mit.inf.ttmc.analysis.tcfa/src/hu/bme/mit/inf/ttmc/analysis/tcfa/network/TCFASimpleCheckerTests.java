7da613ba7c9c4f30626acd9d0d965906bff5a829
Tamás Tóth <totht@mit.bme.hu>
2016-08-01 11:18:07 +0200

    Class SimpleChecker created

---
+++ hu.bme.mit.inf.ttmc.analysis.tcfa/src/hu/bme/mit/inf/ttmc/analysis/tcfa/network/TCFASimpleCheckerTests.java
@@ -0,0 +1,114 @@
+package hu.bme.mit.inf.ttmc.analysis.tcfa.network;
+
+import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Eq;
+import static hu.bme.mit.inf.ttmc.core.expr.impl.Exprs.Int;
+import static hu.bme.mit.inf.ttmc.core.type.impl.Types.Int;
+import static hu.bme.mit.inf.ttmc.formalism.common.decl.impl.Decls2.Var;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import hu.bme.mit.inf.ttmc.analysis.algorithm.impl.SimpleChecker;
+import hu.bme.mit.inf.ttmc.analysis.composite.CompositeAnalysis;
+import hu.bme.mit.inf.ttmc.analysis.composite.CompositePrecision;
+import hu.bme.mit.inf.ttmc.analysis.composite.CompositeState;
+import hu.bme.mit.inf.ttmc.analysis.expl.ExplPrecision;
+import hu.bme.mit.inf.ttmc.analysis.expl.ExplState;
+import hu.bme.mit.inf.ttmc.analysis.expl.GlobalExplPrecision;
+import hu.bme.mit.inf.ttmc.analysis.pred.GlobalPredPrecision;
+import hu.bme.mit.inf.ttmc.analysis.pred.PredPrecision;
+import hu.bme.mit.inf.ttmc.analysis.pred.PredState;
+import hu.bme.mit.inf.ttmc.analysis.tcfa.TCFAAnalyis;
+import hu.bme.mit.inf.ttmc.analysis.tcfa.expl.TCFAExplAnalysis;
+import hu.bme.mit.inf.ttmc.analysis.tcfa.pred.TCFAPredAnalysis;
+import hu.bme.mit.inf.ttmc.analysis.tcfa.zone.TCFAZoneAnalysis;
+import hu.bme.mit.inf.ttmc.analysis.zone.ZonePrecision;
+import hu.bme.mit.inf.ttmc.analysis.zone.ZoneState;
+import hu.bme.mit.inf.ttmc.core.expr.Expr;
+import hu.bme.mit.inf.ttmc.core.type.IntType;
+import hu.bme.mit.inf.ttmc.formalism.common.decl.ClockDecl;
+import hu.bme.mit.inf.ttmc.formalism.common.decl.VarDecl;
+import hu.bme.mit.inf.ttmc.formalism.tcfa.TCFALoc;
+import hu.bme.mit.inf.ttmc.formalism.tcfa.instances.FischerTCFA;
+import hu.bme.mit.inf.ttmc.solver.Solver;
+import hu.bme.mit.inf.ttmc.solver.SolverManager;
+import hu.bme.mit.inf.ttmc.solver.z3.Z3SolverManager;
+
+public class TCFASimpleCheckerTests {
+
+	@Test
+	@Ignore
+	public void testExplicit() {
+		final int n = 4;
+
+		final VarDecl<IntType> vlock = Var("lock", Int());
+
+		final HashMap<ClockDecl, Integer> ceilings = new HashMap<>();
+
+		final List<FischerTCFA> network = new ArrayList<>(n);
+		for (int i = 0; i < n; i++) {
+			final FischerTCFA fischer = new FischerTCFA(i + 1, 1, 2, vlock);
+			ceilings.put(fischer.getClock(), 2);
+			network.add(fischer);
+		}
+
+		final List<TCFALoc> initLocs = network.stream().map(comp -> comp.getInitial()).collect(toList());
+
+		////
+
+		final SolverManager manager = new Z3SolverManager();
+		final Solver solver = manager.createSolver(true, true);
+
+		final TCFAAnalyis<CompositeState<ZoneState, ExplState>, CompositePrecision<ZonePrecision, ExplPrecision>> analysis = new TCFAAnalyis<>(
+				TCFANetworkLoc.create(initLocs),
+				new CompositeAnalysis<>(TCFAZoneAnalysis.getInstance(), new TCFAExplAnalysis(solver)));
+
+		final CompositePrecision<ZonePrecision, ExplPrecision> precision = CompositePrecision.create(
+				new ZonePrecision(ceilings),
+				GlobalExplPrecision.create(Collections.singleton(vlock), Collections.emptySet()));
+
+		System.out.println(SimpleChecker.run(analysis, s -> false, precision));
+	}
+
+	@Test
+	public void testPredicate() {
+		final int n = 3;
+
+		final VarDecl<IntType> vlock = Var("lock", Int());
+		final Expr<IntType> lock = vlock.getRef();
+
+		final HashMap<ClockDecl, Integer> ceilings = new HashMap<>();
+
+		final List<FischerTCFA> network = new ArrayList<>(n);
+		for (int i = 0; i < n; i++) {
+			final FischerTCFA fischer = new FischerTCFA(i + 1, 1, 2, vlock);
+			ceilings.put(fischer.getClock(), 2);
+			network.add(fischer);
+		}
+
+		final List<TCFALoc> initLocs = network.stream().map(comp -> comp.getInitial()).collect(toList());
+
+		////
+
+		final SolverManager manager = new Z3SolverManager();
+		final Solver solver = manager.createSolver(true, true);
+
+		final TCFAAnalyis<CompositeState<ZoneState, PredState>, CompositePrecision<ZonePrecision, PredPrecision>> analysis = new TCFAAnalyis<>(
+				TCFANetworkLoc.create(initLocs),
+				new CompositeAnalysis<>(TCFAZoneAnalysis.getInstance(), new TCFAPredAnalysis(solver)));
+
+		final CompositePrecision<ZonePrecision, PredPrecision> precision = CompositePrecision.create(
+				new ZonePrecision(ceilings),
+				GlobalPredPrecision.create(Arrays.asList(Eq(lock, Int(0)), Eq(lock, Int(1)))));
+
+		System.out.println(SimpleChecker.run(analysis, s -> false, precision));
+	}
+
+}
