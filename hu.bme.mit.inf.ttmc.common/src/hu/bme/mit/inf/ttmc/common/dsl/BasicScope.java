8e72145dd7bf54f0f03f1691f6c2169ae8439023
Tamás Tóth <totht@mit.bme.hu>
2016-08-11 10:11:11 +0200

    Class SymbolTable extracted

--- hu.bme.mit.inf.ttmc.common/src/hu/bme/mit/inf/ttmc/common/dsl/BasicScope.java
+++ hu.bme.mit.inf.ttmc.common/src/hu/bme/mit/inf/ttmc/common/dsl/BasicScope.java
@@ -1,20 +1,18 @@
 package hu.bme.mit.inf.ttmc.common.dsl;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 public abstract class BasicScope implements Scope {
 
 	final Optional<Scope> enclosingScope;
-	final Map<String, Symbol> stringToSymbol;
+
+	final SymbolTable symbolTable;
 
 	public BasicScope(final Scope eclosingScope) {
 		this.enclosingScope = Optional.ofNullable(eclosingScope);
-		stringToSymbol = new HashMap<>();
+		symbolTable = new SymbolTable();
 	}
 
 	@Override
@@ -25,21 +23,17 @@ public abstract class BasicScope implements Scope {
 	@Override
 	public Optional<Symbol> resolve(final String name) {
 		checkNotNull(name);
-		final Symbol symbol = stringToSymbol.get(name);
-		if (symbol != null) {
-			return Optional.of(symbol);
-		} else if (enclosingScope.isPresent()) {
-			return enclosingScope.get().resolve(name);
+		final Optional<Symbol> symbol = symbolTable.get(name);
+		if (symbol.isPresent() || !enclosingScope.isPresent()) {
+			return symbol;
 		} else {
-			return Optional.empty();
+			return enclosingScope.get().resolve(name);
 		}
 	}
 
 	@Override
 	public void declare(final Symbol symbol) {
-		checkNotNull(symbol);
-		checkArgument(!stringToSymbol.containsKey(symbol.getName()));
-		stringToSymbol.put(symbol.getName(), symbol);
+		symbolTable.add(symbol);
 	}
 
 }
