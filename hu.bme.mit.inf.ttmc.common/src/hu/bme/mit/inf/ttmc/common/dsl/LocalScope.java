537ff2496eb391976ad87fa69adc0d1413062eae
Tamás Tóth <totht@mit.bme.hu>
2016-08-11 10:56:34 +0200

    Visibility of class LocalScope changed to public

--- hu.bme.mit.inf.ttmc.common/src/hu/bme/mit/inf/ttmc/common/dsl/LocalScope.java
+++ hu.bme.mit.inf.ttmc.common/src/hu/bme/mit/inf/ttmc/common/dsl/LocalScope.java
@@ -2,9 +2,9 @@ package hu.bme.mit.inf.ttmc.common.dsl;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-class LocalScope extends BasicScope {
+public final class LocalScope extends BasicScope {
 
-	LocalScope(final Scope enclosingScope) {
+	public LocalScope(final Scope enclosingScope) {
 		super(checkNotNull(enclosingScope));
 	}
 
