3c021f898038672cac98b125955640a2b3ca368e
Sallai Gyula <salla016@gmail.com>
2016-08-13 23:31:03 +0200

    Added new dead branch eliminator

--- hu.bme.mit.inf.ttmc.tac/src/hu/bme/mit/inf/ttmc/frontend/transform/DeadBranchEliminator.java
+++ hu.bme.mit.inf.ttmc.tac/src/hu/bme/mit/inf/ttmc/frontend/transform/DeadBranchEliminator.java
@@ -1,7 +1,6 @@
 package hu.bme.mit.inf.ttmc.frontend.transform;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import hu.bme.mit.inf.ttmc.core.expr.BoolLitExpr;
 import hu.bme.mit.inf.ttmc.frontend.ir.BasicBlock;
@@ -10,23 +9,37 @@ import hu.bme.mit.inf.ttmc.frontend.ir.node.JumpIfNode;
 
 import static hu.bme.mit.inf.ttmc.frontend.ir.node.NodeFactory.Goto;
 
+/**
+ * A function transformer which replaces trivial branches with goto jumps.
+ *
+ * This transformation may introduce orphaned and split blocks, so a normalization must be run on the resulting graph.
+ */
 public class DeadBranchEliminator implements FunctionTransformer {
 
 	@Override
 	public void transform(Function function) {
-		List<BasicBlock> blocks = function.getBlocks().stream()
-			.filter(b -> b.getTerminator() instanceof JumpIfNode)
-			.filter(b -> ((JumpIfNode) b.getTerminator()).getCond() instanceof BoolLitExpr)
-			.collect(Collectors.toList());
+		boolean change = true;
+		while (change) {
+			change = false;
 
-		for (BasicBlock block : blocks) {
-			JumpIfNode terminator = (JumpIfNode) block.getTerminator();
-			BoolLitExpr cond = (BoolLitExpr) terminator.getCond();
+			// Find a suitable basic block
+			Optional<BasicBlock> result = function.getBlocksDFS().stream()
+				.filter(b -> b.getTerminator() instanceof JumpIfNode)
+				.filter(b -> ((JumpIfNode) b.getTerminator()).getCond() instanceof BoolLitExpr)
+				.findFirst();
 
-			BasicBlock target = cond.getValue() == true ? terminator.getThenTarget() : terminator.getElseTarget();
-			BasicBlock newBlock = new BasicBlock(block.getName(), function, block.getNodes());
+			if (result.isPresent()) {
+				BasicBlock block = result.get();
+				JumpIfNode terminator = (JumpIfNode) block.getTerminator();
+				BoolLitExpr cond = (BoolLitExpr) terminator.getCond();
 
-			function.replaceBlock(block, newBlock, Goto(target));
+				BasicBlock target = cond.getValue() == true ? terminator.getThenTarget() : terminator.getElseTarget();
+
+				block.clearTerminator();
+				block.terminate(Goto(target));
+
+				change = true;
+			}
 		}
 	}
 
