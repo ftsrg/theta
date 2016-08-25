e2d7b3a42eac420fbe2cdf75d8e0eb61c8b4ed4f
Tamás Tóth <totht@mit.bme.hu>
2016-08-25 10:42:04 +0200

    EMF and Xtext plugins removed

--- hu.bme.mit.inf.theta.constraint.ui/src/hu/bme/mit/inf/theta/constraint/ui/transform/TypeTransformator.java
+++
@@ -1,9 +0,0 @@
-package hu.bme.mit.inf.theta.constraint.ui.transform;
-
-import hu.bme.mit.inf.theta.core.type.Type;
-
-public interface TypeTransformator {
-
-	public Type transform(hu.bme.mit.inf.theta.constraint.model.Type type);
-
-}
