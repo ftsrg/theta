15dc21e386dbd5d187ee6ad235c6424111910da5
Tamás Tóth <totht@mit.bme.hu>
2016-09-10 11:55:45 +0200

    DSL test classes renamed

--- hu.bme.mit.theta.formalism.sts/src/test/java/hu/bme/mit/theta/formalism/sts/dsl/StsDslTest.java
+++ hu.bme.mit.theta.formalism.sts/src/test/java/hu/bme/mit/theta/formalism/sts/dsl/StsDslTest.java
@@ -8,20 +8,19 @@ import java.io.IOException;
 import org.junit.Test;
 
 import hu.bme.mit.theta.formalism.sts.STS;
-import hu.bme.mit.theta.formalism.sts.dsl.StsDslManager;
 import hu.bme.mit.theta.formalism.sts.dsl.impl.StsSpec;
 
-public class StsDslTests {
+public class StsDslTest {
 
 	@Test
-	public void testFischer() throws FileNotFoundException, IOException {
+	public void testReadersWriters() throws FileNotFoundException, IOException {
 		final StsSpec spec = StsDslManager.parse("src/test/resources/readerswriters.system", emptyList());
-		final STS sts = spec.getSts("s");
+		final STS sts = spec.getSts("safe");
 		System.out.println(sts);
 	}
 
 	@Test
-	public void testProsigma() throws FileNotFoundException, IOException {
+	public void testSimple() throws FileNotFoundException, IOException {
 		final StsSpec spec = StsDslManager.parse("src/test/resources/simple1.system", emptyList());
 		final STS sts = spec.getSts("safe");
 		System.out.println(sts);
