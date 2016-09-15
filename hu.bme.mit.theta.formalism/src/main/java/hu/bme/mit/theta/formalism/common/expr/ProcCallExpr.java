ded59ad1803f19ae2321bcee377546102ed440b7
Tamás Tóth <totht@mit.bme.hu>
2016-09-15 10:31:02 +0200

    ProcCallExpr.getParams() returns a List

--- hu.bme.mit.theta.formalism/src/main/java/hu/bme/mit/theta/formalism/common/expr/ProcCallExpr.java
+++ hu.bme.mit.theta.formalism/src/main/java/hu/bme/mit/theta/formalism/common/expr/ProcCallExpr.java
@@ -1,6 +1,6 @@
 package hu.bme.mit.theta.formalism.common.expr;
 
-import java.util.Collection;
+import java.util.List;
 
 import hu.bme.mit.theta.core.expr.Expr;
 import hu.bme.mit.theta.core.type.Type;
@@ -8,5 +8,6 @@ import hu.bme.mit.theta.formalism.common.type.ProcType;
 
 public interface ProcCallExpr<ReturnType extends Type> extends Expr<ReturnType> {
 	public Expr<? extends ProcType<? extends ReturnType>> getProc();
-	public Collection<? extends Expr<? extends Type>> getParams();
+
+	public List<? extends Expr<? extends Type>> getParams();
 }
