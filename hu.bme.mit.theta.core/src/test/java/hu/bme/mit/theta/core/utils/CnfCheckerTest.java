4f3c1d11474b53193af38534a9e7ec7f2f4c376f
Hajdu Ákos <hajdua@mit.bme.hu>
2016-09-17 18:27:34 +0200

    Cnf checker tests made parametric

--- hu.bme.mit.theta.core/src/test/java/hu/bme/mit/theta/core/utils/CnfCheckerTest.java
+++ hu.bme.mit.theta.core/src/test/java/hu/bme/mit/theta/core/utils/CnfCheckerTest.java
@@ -6,105 +6,70 @@ import static hu.bme.mit.theta.core.expr.impl.Exprs.Iff;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.Imply;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.Not;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.Or;
-import static hu.bme.mit.theta.core.expr.impl.Exprs.Prime;
 import static hu.bme.mit.theta.core.type.impl.Types.Bool;
 import static hu.bme.mit.theta.core.utils.impl.ExprUtils.isExprCNF;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import hu.bme.mit.theta.core.expr.ConstRefExpr;
+import hu.bme.mit.theta.core.expr.Expr;
 import hu.bme.mit.theta.core.type.BoolType;
 
+@RunWith(Parameterized.class)
 public class CnfCheckerTest {
 	// Constants for testing
-	private ConstRefExpr<BoolType> a, b, c;
+	private static final ConstRefExpr<BoolType> a = Const("a", Bool()).getRef();
+	private static final ConstRefExpr<BoolType> b = Const("b", Bool()).getRef();
+	private static final ConstRefExpr<BoolType> c = Const("c", Bool()).getRef();
 
-	@Before
-	public void before() {
-		// Create constants
-		a = Const("a", Bool()).getRef();
-		b = Const("b", Bool()).getRef();
-		c = Const("c", Bool()).getRef();
+	@Parameter(value = 0)
+	public Expr<? extends BoolType> expr;
+
+	@Parameter(value = 1)
+	public boolean expectedResult;
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+				// A
+				{ a, true },
+				// !A
+				{ Not(a), true },
+				// !A or B or !C
+				{ Or(Not(a), b, Not(c)), true },
+				// !A and B and !C
+				{ And(Not(a), b, Not(c)), true },
+				// !A and (B and !C)
+				{ And(Not(a), And(b, Not(c))), true },
+				// !A and (B or !C)
+				{ And(Not(a), Or(b, Not(c))), true },
+				// !!A
+				{ Not(Not(a)), false },
+				// !A and B and !C
+				{ And(Not(a), b, Not(c)), true },
+				// !A or (B and !C)
+				{ Or(Not(a), And(b, Not(c))), false },
+				// !(A and B)
+				{ Not(And(a, b)), false },
+				// !(A or B)
+				{ Not(Or(a, b)), false },
+				// A -> B
+				{ Imply(a, b), false },
+				// A <-> B
+				{ Iff(a, b), false }, });
+
 	}
 
 	@Test
-	public void testCnfCheckerTrue() {
-		// A
-		assertTrue(isExprCNF(a));
-		// !A
-		assertTrue(isExprCNF(Not(a)));
-		// !A or B or !C
-		assertTrue(isExprCNF(Or(Not(a), b, Not(c))));
-		// !A and B and !C
-		assertTrue(isExprCNF(And(Not(a), b, Not(c))));
-		// !A and (B and !C)
-		assertTrue(isExprCNF(And(Not(a), And(b, Not(c)))));
-		// !A and (B or !C)
-		assertTrue(isExprCNF(And(Not(a), Or(b, Not(c)))));
-	}
-
-	@Test
-	public void testCnfCheckerFalse() {
-		// !!A
-		assertFalse(isExprCNF(Not(Not(a))));
-		// !A and B and !C
-		assertTrue(isExprCNF(And(Not(a), b, Not(c))));
-		// !A or (B and !C)
-		assertFalse(isExprCNF(Or(Not(a), And(b, Not(c)))));
-		// !(A and B)
-		assertFalse(isExprCNF(Not(And(a, b))));
-		// !(A or B)
-		assertFalse(isExprCNF(Not(Or(a, b))));
-		// A -> B
-		assertFalse(isExprCNF(Imply(a, b)));
-		// A <-> B
-		assertFalse(isExprCNF(Iff(a, b)));
-	}
-
-	@Test
-	public void testCnfProgExprCheckerTrue() {
-		// A
-		assertTrue(isExprCNF(a));
-		// A'
-		assertTrue(isExprCNF(Prime(a)));
-		// !A
-		assertTrue(isExprCNF(Not(a)));
-		// !(A')
-		assertTrue(isExprCNF(Not(Prime(a))));
-		// !A or B' or !C
-		assertTrue(isExprCNF(Or(Not(a), Prime(b), Not(c))));
-		// !A and B' and !C
-		assertTrue(isExprCNF(And(Not(a), Prime(b), Not(c))));
-		// !A and (B and !C)
-		assertTrue(isExprCNF(And(Not(a), And(b, Not(c)))));
-		// !A and (B' or !C)
-		assertTrue(isExprCNF(And(Not(a), Or(Prime(b), Not(c)))));
-	}
-
-	@Test
-	public void testCnfProgExprCheckerFalse() {
-		// !!A
-		assertFalse(isExprCNF(Not(Not(a))));
-		// !A and B and !C
-		assertTrue(isExprCNF(And(Not(a), b, Not(c))));
-		// !A or (B and !C)
-		assertFalse(isExprCNF(Or(Not(a), And(b, Not(c)))));
-		// !(A and B)
-		assertFalse(isExprCNF(Not(And(a, b))));
-		// !(A or B)
-		assertFalse(isExprCNF(Not(Or(a, b))));
-		// A -> B
-		assertFalse(isExprCNF(Imply(a, b)));
-		// A <-> B
-		assertFalse(isExprCNF(Iff(a, b)));
-		// (!A)'
-		assertFalse(isExprCNF(Not(Not(a))));
-		// (A and B)'
-		assertFalse(isExprCNF(Prime(And(a, b))));
-		// (A or B)'
-		assertFalse(isExprCNF(Prime(Or(a, b))));
+	public void test() {
+		Assert.assertEquals(expectedResult, isExprCNF(expr));
 	}
 }
