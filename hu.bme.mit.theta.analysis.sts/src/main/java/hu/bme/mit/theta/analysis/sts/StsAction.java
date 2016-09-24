15066146b40ed615879e4c311671b20eccffa60c
Tamás Tóth <totht@mit.bme.hu>
2016-09-24 12:51:36 +0200

    Method ExprAction.nextIndexes() added

--- hu.bme.mit.theta.analysis.sts/src/main/java/hu/bme/mit/theta/analysis/sts/StsAction.java
+++ hu.bme.mit.theta.analysis.sts/src/main/java/hu/bme/mit/theta/analysis/sts/StsAction.java
@@ -2,10 +2,12 @@ package hu.bme.mit.theta.analysis.sts;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.And;
+import static hu.bme.mit.theta.core.utils.impl.VarIndexes.all;
 
 import hu.bme.mit.theta.analysis.expr.ExprAction;
 import hu.bme.mit.theta.core.expr.Expr;
 import hu.bme.mit.theta.core.type.BoolType;
+import hu.bme.mit.theta.core.utils.impl.VarIndexes;
 import hu.bme.mit.theta.formalism.sts.STS;
 
 public class StsAction implements ExprAction {
@@ -24,6 +26,11 @@ public class StsAction implements ExprAction {
 	@Override
 	public Expr<? extends BoolType> toExpr() {
 		return trans;
+	}
+
+	@Override
+	public VarIndexes nextIndexes() {
+		return all(1);
 	}
 
 	@Override
