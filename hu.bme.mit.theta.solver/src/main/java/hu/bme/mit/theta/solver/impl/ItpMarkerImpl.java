279c2ed023960b901ee0388bd002756b700fd36c
Hajdu Ákos <hajdua@mit.bme.hu>
2016-09-26 15:57:23 +0200

    Added checkNotNulls

--- hu.bme.mit.theta.solver/src/main/java/hu/bme/mit/theta/solver/impl/ItpMarkerImpl.java
+++ hu.bme.mit.theta.solver/src/main/java/hu/bme/mit/theta/solver/impl/ItpMarkerImpl.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.theta.solver.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Collection;
 
 import hu.bme.mit.theta.core.expr.Expr;
@@ -10,23 +12,23 @@ import hu.bme.mit.theta.solver.Stack;
 public class ItpMarkerImpl implements ItpMarker {
 
 	private final Stack<Expr<? extends BoolType>> assertions;
-	
+
 	public ItpMarkerImpl() {
 		assertions = new StackImpl<>();
 	}
-	
-	public void add(Expr<? extends BoolType> assertion) {
-		assertions.add(assertion);
+
+	public void add(final Expr<? extends BoolType> assertion) {
+		assertions.add(checkNotNull(assertion));
 	}
-	
+
 	public void push() {
 		assertions.push();
 	}
-	
+
 	public void pop(final int n) {
 		assertions.pop(n);
 	}
-	
+
 	@Override
 	public Collection<? extends Expr<? extends BoolType>> getAssertions() {
 		return assertions.toCollection();
