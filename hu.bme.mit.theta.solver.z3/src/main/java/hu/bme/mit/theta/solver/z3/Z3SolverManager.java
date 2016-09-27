0948f3a46a9febb3b69f4ba781f9009ec33b3a79
Tamás Tóth <totht@mit.bme.hu>
2016-09-27 15:36:15 +0200

    Context is no longer shared between Z3 solver instances, fixes #10

--- hu.bme.mit.theta.solver.z3/src/main/java/hu/bme/mit/theta/solver/z3/Z3SolverManager.java
+++ hu.bme.mit.theta.solver.z3/src/main/java/hu/bme/mit/theta/solver/z3/Z3SolverManager.java
@@ -34,30 +34,30 @@ public final class Z3SolverManager implements SolverManager {
 
 	}
 
-	///////
-
-	final com.microsoft.z3.InterpolationContext z3Context;
-
-	final Z3SymbolTable symbolTable;
-
 	public Z3SolverManager() {
-		z3Context = new com.microsoft.z3.InterpolationContext();
-		symbolTable = new Z3SymbolTable();
 	}
 
 	@Override
 	public Solver createSolver() {
+		final com.microsoft.z3.Context z3Context = new com.microsoft.z3.Context();
 		final com.microsoft.z3.Solver z3Solver = z3Context.mkSimpleSolver();
+
+		final Z3SymbolTable symbolTable = new Z3SymbolTable();
 		final Z3TransformationManager transformationManager = new Z3TransformationManager(symbolTable, z3Context);
 		final Z3TermTransformer termTransformer = new Z3TermTransformer(symbolTable);
+
 		return new Z3Solver(symbolTable, transformationManager, termTransformer, z3Context, z3Solver);
 	}
 
 	@Override
 	public ItpSolver createItpSolver() {
+		final com.microsoft.z3.InterpolationContext z3Context = new com.microsoft.z3.InterpolationContext();
 		final com.microsoft.z3.Solver z3Solver = z3Context.mkSimpleSolver();
+
+		final Z3SymbolTable symbolTable = new Z3SymbolTable();
 		final Z3TransformationManager transformationManager = new Z3TransformationManager(symbolTable, z3Context);
 		final Z3TermTransformer termTransformer = new Z3TermTransformer(symbolTable);
+
 		return new Z3ItpSolver(symbolTable, transformationManager, termTransformer, z3Context, z3Solver);
 	}
 
