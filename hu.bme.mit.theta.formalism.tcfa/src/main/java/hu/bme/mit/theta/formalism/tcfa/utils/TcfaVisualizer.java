c320f29db64662e727d4dfd9756eb6579e0c0ee4
Hajdu Ákos <hajdua@mit.bme.hu>
2016-09-27 16:40:13 +0200

    Modified TCFA visualizer to traverse the TCFA
    (instead of using getLocs and getEdges)

--- hu.bme.mit.theta.formalism.tcfa/src/main/java/hu/bme/mit/theta/formalism/tcfa/utils/TcfaVisualizer.java
+++ hu.bme.mit.theta.formalism.tcfa/src/main/java/hu/bme/mit/theta/formalism/tcfa/utils/TcfaVisualizer.java
@@ -18,29 +18,41 @@ public class TcfaVisualizer {
 	private static final Color EDGE_COLOR = Color.BLACK;
 	private static final String LOC_LINE_STYLE = "";
 	private static final String EDGE_LINE_STYLE = "";
+	private static final String PHANTOM_INIT_ID = "phantom_init";
 
 	public static Graph visualize(final TCFA tcfa) {
 		final Graph graph = new Graph("tcfa", "");
-
 		final Map<TcfaLoc, String> ids = new HashMap<>();
-
-		graph.addNode("phantom_init", "", Color.WHITE, Color.WHITE, "", 1);
-		for (final TcfaLoc loc : tcfa.getLocs()) {
-			final String id = LOC_ID_PREFIX + ids.size();
-			ids.put(loc, id);
-			final StringJoiner label = new StringJoiner("\n", loc.getName() + "\n", "");
-			loc.getInvars().forEach(expr -> label.add(expr.toString()));
-			graph.addNode(id, label.toString(), LOC_FILL_COLOR, LOC_EDGE_COLOR, LOC_LINE_STYLE, 1);
-		}
-
-		graph.addEdge("phantom_init", ids.get(tcfa.getInitLoc()), "", EDGE_COLOR, "");
-		for (final TcfaEdge edge : tcfa.getEdges()) {
-			final StringJoiner label = new StringJoiner("\n");
-			edge.getStmts().stream().forEach(stmt -> label.add(stmt.toString()));
-			graph.addEdge(ids.get(edge.getSource()), ids.get(edge.getTarget()), label.toString(), EDGE_COLOR,
-					EDGE_LINE_STYLE);
-		}
-
+		traverse(graph, tcfa.getInitLoc(), ids);
+		graph.addNode(PHANTOM_INIT_ID, "", Color.WHITE, Color.WHITE, "", 1);
+		graph.addEdge(PHANTOM_INIT_ID, ids.get(tcfa.getInitLoc()), "", EDGE_COLOR, "");
 		return graph;
 	}
+
+	private static void traverse(final Graph graph, final TcfaLoc loc, final Map<TcfaLoc, String> ids) {
+		if (!ids.containsKey(loc)) {
+			addLocation(graph, loc, ids);
+			for (final TcfaEdge outEdge : loc.getOutEdges()) {
+				final TcfaLoc target = outEdge.getTarget();
+				traverse(graph, target, ids);
+				addEdge(graph, ids, outEdge);
+			}
+		}
+	}
+
+	private static void addLocation(final Graph graph, final TcfaLoc loc, final Map<TcfaLoc, String> ids) {
+		final String id = LOC_ID_PREFIX + ids.size();
+		ids.put(loc, id);
+		final StringJoiner locLabel = new StringJoiner("\n", loc.getName() + "\n", "");
+		loc.getInvars().forEach(expr -> locLabel.add(expr.toString()));
+		graph.addNode(id, locLabel.toString(), LOC_FILL_COLOR, LOC_EDGE_COLOR, LOC_LINE_STYLE, 1);
+	}
+
+	private static void addEdge(final Graph graph, final Map<TcfaLoc, String> ids, final TcfaEdge outEdge) {
+		final StringJoiner edgeLabel = new StringJoiner("\n");
+		outEdge.getStmts().stream().forEach(stmt -> edgeLabel.add(stmt.toString()));
+		graph.addEdge(ids.get(outEdge.getSource()), ids.get(outEdge.getTarget()), edgeLabel.toString(), EDGE_COLOR,
+				EDGE_LINE_STYLE);
+	}
+
 }
