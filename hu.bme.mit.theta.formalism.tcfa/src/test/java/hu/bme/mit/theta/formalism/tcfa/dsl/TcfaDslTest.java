85aab8405340062679d608b84b4b2cea724fab0e
Hajdu Ákos <hajdua@mit.bme.hu>
2016-09-28 14:34:57 +0200

    TcfaPrinter removed (functionality covered by TcfaVisualizer)

--- hu.bme.mit.theta.formalism.tcfa/src/test/java/hu/bme/mit/theta/formalism/tcfa/dsl/TcfaDslTest.java
+++ hu.bme.mit.theta.formalism.tcfa/src/test/java/hu/bme/mit/theta/formalism/tcfa/dsl/TcfaDslTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import hu.bme.mit.theta.common.visualization.YedPrinter;
 import hu.bme.mit.theta.formalism.tcfa.TCFA;
-import hu.bme.mit.theta.formalism.tcfa.TcfaPrinter;
 import hu.bme.mit.theta.formalism.tcfa.utils.TcfaVisualizer;
 
 public class TcfaDslTest {
@@ -18,8 +17,6 @@ public class TcfaDslTest {
 	public void testFischer() throws FileNotFoundException, IOException {
 		final TcfaSpec spec = TcfaDslManager.parse("src/test/resources/fischer.tcfa", Int(1), Int(2));
 		final TCFA fischer = spec.getTcfa("fischers");
-		System.out.println(TcfaPrinter.toGraphvizString(fischer));
-		System.out.println("==================");
 		System.out.println(new YedPrinter().print(TcfaVisualizer.visualize(fischer)));
 	}
 
@@ -27,8 +24,6 @@ public class TcfaDslTest {
 	public void testProsigma() throws FileNotFoundException, IOException {
 		final TcfaSpec spec = TcfaDslManager.parse("src/test/resources/prosigma.tcfa", Int(3), Int(7));
 		final TCFA field = spec.getTcfa("prosigma");
-		System.out.println(TcfaPrinter.toGraphvizString(field));
-		System.out.println("==================");
 		System.out.println(new YedPrinter().print(TcfaVisualizer.visualize(field)));
 	}
 
