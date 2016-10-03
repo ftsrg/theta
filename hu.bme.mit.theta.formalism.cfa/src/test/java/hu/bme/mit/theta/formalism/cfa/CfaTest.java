64fae870e85d9ca751d3c462ea9aa8843b84481f
Hajdu Ákos <hajdua@mit.bme.hu>
2016-10-03 16:34:17 +0200

    Replaced CfaPrinter with CfaVisualizer

--- hu.bme.mit.theta.formalism.cfa/src/test/java/hu/bme/mit/theta/formalism/cfa/CfaTest.java
+++ hu.bme.mit.theta.formalism.cfa/src/test/java/hu/bme/mit/theta/formalism/cfa/CfaTest.java
@@ -2,13 +2,13 @@ package hu.bme.mit.theta.formalism.cfa;
 
 import org.junit.Test;
 
-import hu.bme.mit.theta.formalism.cfa.CFA;
-import hu.bme.mit.theta.formalism.cfa.CfaLoc;
+import hu.bme.mit.theta.common.visualization.GraphVizWriter;
 import hu.bme.mit.theta.formalism.cfa.impl.ImmutableCfa;
 import hu.bme.mit.theta.formalism.cfa.impl.MutableCfa;
+import hu.bme.mit.theta.formalism.cfa.utils.CfaVisualizer;
 
 public class CfaTest {
-	
+
 	@Test
 	public void testCreation() {
 		final MutableCfa cfa = new MutableCfa();
@@ -17,17 +17,17 @@ public class CfaTest {
 		final CfaLoc errorLoc = cfa.getErrorLoc();
 		final CfaLoc l1 = cfa.createLoc();
 		final CfaLoc l2 = cfa.createLoc();
-		
+
 		cfa.createEdge(initLoc, l1);
 		cfa.createEdge(initLoc, finalLoc);
 		cfa.createEdge(l1, l2);
 		cfa.createEdge(l2, l1);
 		cfa.createEdge(l2, l2);
 		cfa.createEdge(l2, errorLoc);
-		
+
 		final CFA cfa2 = ImmutableCfa.copyOf(cfa);
-		
-		System.out.println(CfaPrinter.toGraphvizSting(cfa2));
+
+		System.out.println(new GraphVizWriter().writeString(CfaVisualizer.visualize(cfa2)));
 	}
-	
+
 }
