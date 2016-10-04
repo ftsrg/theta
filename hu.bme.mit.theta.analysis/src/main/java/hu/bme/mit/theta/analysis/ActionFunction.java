4ae07b712107885b7aa0b95ef9e9a14b88310951
Hajdu Ákos <hajdua@mit.bme.hu>
2016-10-04 17:00:55 +0200

    Added @FunctionalInterface to ActionFunction

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/ActionFunction.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/ActionFunction.java
@@ -2,6 +2,7 @@ package hu.bme.mit.theta.analysis;
 
 import java.util.Collection;
 
+@FunctionalInterface
 public interface ActionFunction<S extends State, A extends Action> {
 
 	public Collection<? extends A> getEnabledActionsFor(S state);
