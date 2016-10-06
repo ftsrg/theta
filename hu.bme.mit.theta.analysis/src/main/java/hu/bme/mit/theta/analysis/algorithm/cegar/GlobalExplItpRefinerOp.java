d6a454304547b74709aa7e380fd00d8c5bd965b1
Hajdu Ákos <hajdua@mit.bme.hu>
2016-10-06 11:34:35 +0200

    Replaced Collection with Iterable in ExprUtils.get/collectVars

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/GlobalExplItpRefinerOp.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/GlobalExplItpRefinerOp.java
@@ -1,29 +1,17 @@
 package hu.bme.mit.theta.analysis.algorithm.cegar;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import hu.bme.mit.theta.analysis.Action;
 import hu.bme.mit.theta.analysis.Trace;
-import hu.bme.mit.theta.analysis.expl.ExplState;
 import hu.bme.mit.theta.analysis.expl.ExplPrecision;
-import hu.bme.mit.theta.core.decl.VarDecl;
-import hu.bme.mit.theta.core.expr.Expr;
-import hu.bme.mit.theta.core.type.BoolType;
-import hu.bme.mit.theta.core.type.Type;
+import hu.bme.mit.theta.analysis.expl.ExplState;
 import hu.bme.mit.theta.core.utils.impl.ExprUtils;
 
-public class GlobalExplItpRefinerOp<A extends Action>
-		implements RefinerOp<ExplState, A, ItpRefutation, ExplPrecision> {
+public class GlobalExplItpRefinerOp<A extends Action> implements RefinerOp<ExplState, A, ItpRefutation, ExplPrecision> {
 
 	@Override
 	public ExplPrecision refine(final ExplPrecision precision, final ItpRefutation refutation,
 			final Trace<ExplState, A> counterexample) {
-		final Set<VarDecl<? extends Type>> newVisibleVars = new HashSet<>();
-		for (final Expr<? extends BoolType> pred : refutation) {
-			ExprUtils.collectVars(pred, newVisibleVars);
-		}
-		return precision.with(newVisibleVars);
+		return precision.with(ExprUtils.getVars(refutation));
 	}
 
 }
