587d0cff0ff1d13a94f6a0d08e7122c594efa22f
Hajdu Ákos <hajdua@mit.bme.hu>
2016-10-07 12:47:42 +0200

    Simplified PredDomain operations using PredState.toExpr

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredDomain.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredDomain.java
@@ -1,15 +1,9 @@
 package hu.bme.mit.theta.analysis.pred;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.expr.impl.Exprs.And;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.Not;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import hu.bme.mit.theta.analysis.Domain;
-import hu.bme.mit.theta.core.expr.Expr;
-import hu.bme.mit.theta.core.type.BoolType;
 import hu.bme.mit.theta.core.utils.impl.PathUtils;
 import hu.bme.mit.theta.solver.Solver;
 
@@ -28,14 +22,8 @@ public final class PredDomain implements Domain<PredState> {
 	@Override
 	public boolean isTop(final PredState state) {
 		solver.push();
-
-		final Collection<Expr<? extends BoolType>> conjuncts = new ArrayList<>();
-		for (final Expr<? extends BoolType> pred : state.getPreds()) {
-			conjuncts.add(PathUtils.unfold(pred, 0));
-		}
-		solver.add(Not(And(conjuncts)));
+		solver.add(PathUtils.unfold(Not(state.toExpr()), 0));
 		final boolean result = solver.check().isUnsat();
-
 		solver.pop();
 		return result;
 	}
@@ -43,13 +31,8 @@ public final class PredDomain implements Domain<PredState> {
 	@Override
 	public boolean isBottom(final PredState state) {
 		solver.push();
-
-		for (final Expr<? extends BoolType> pred : state.getPreds()) {
-			solver.add(PathUtils.unfold(pred, 0));
-		}
-
+		solver.add(PathUtils.unfold(state.toExpr(), 0));
 		final boolean result = solver.check().isUnsat();
-
 		solver.pop();
 		return result;
 	}
@@ -57,17 +40,8 @@ public final class PredDomain implements Domain<PredState> {
 	@Override
 	public boolean isLeq(final PredState state1, final PredState state2) {
 		solver.push();
-
-		for (final Expr<? extends BoolType> pred : state1.getPreds()) {
-			solver.add(PathUtils.unfold(pred, 0));
-		}
-
-		final Collection<Expr<? extends BoolType>> unfoldedPreds = new ArrayList<>();
-		for (final Expr<? extends BoolType> pred : state2.getPreds()) {
-			unfoldedPreds.add(PathUtils.unfold(pred, 0));
-		}
-		solver.add(Not(And(unfoldedPreds)));
-
+		solver.add(PathUtils.unfold(state1.toExpr(), 0));
+		solver.add(PathUtils.unfold(Not(state2.toExpr()), 0));
 		final boolean isLeq = solver.check().isUnsat();
 		solver.pop();
 		return isLeq;
