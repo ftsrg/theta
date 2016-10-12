ab88b6d03386496414bb331c6f84084e3ec5e218
Tamás Tóth <totht@mit.bme.hu>
2016-10-12 11:02:54 +0200

    Interface SafetyChecker added

---
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/SafetyChecker.java
@@ -0,0 +1,11 @@
+package hu.bme.mit.theta.analysis.algorithm;
+
+import hu.bme.mit.theta.analysis.Action;
+import hu.bme.mit.theta.analysis.Precision;
+import hu.bme.mit.theta.analysis.State;
+
+public interface SafetyChecker<S extends State, A extends Action, P extends Precision> {
+
+	public SafetyStatus<S, A> check(final P precision);
+
+}
---
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/SafetyStatus.java
@@ -0,0 +1,48 @@
+package hu.bme.mit.theta.analysis.algorithm;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import hu.bme.mit.theta.analysis.Action;
+import hu.bme.mit.theta.analysis.State;
+import hu.bme.mit.theta.analysis.Trace;
+
+public abstract class SafetyStatus<S extends State, A extends Action> {
+
+	private SafetyStatus() {
+	}
+
+	public static <S extends State, A extends Action> Safe<S, A> safe(final ARG<S, A> proof) {
+		return new Safe<>(proof);
+	}
+
+	public static <S extends State, A extends Action> Unsafe<S, A> safe(final Trace<S, A> cex) {
+		return new Unsafe<>(cex);
+	}
+
+	////
+
+	public static final class Safe<S extends State, A extends Action> extends SafetyStatus<S, A> {
+		private final ARG<S, A> proof;
+
+		private Safe(final ARG<S, A> proof) {
+			this.proof = checkNotNull(proof);
+		}
+
+		public ARG<S, A> getProof() {
+			return proof;
+		}
+	}
+
+	public static final class Unsafe<S extends State, A extends Action> extends SafetyStatus<S, A> {
+		private final Trace<S, A> cex;
+
+		private Unsafe(final Trace<S, A> cex) {
+			this.cex = checkNotNull(cex);
+		}
+
+		public Trace<S, A> getTrace() {
+			return cex;
+		}
+	}
+
+}
