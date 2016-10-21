030500d4210b2e4cf2ba135bb2fc9b677d9502c2
Hajdu Ákos <hajdua@mit.bme.hu>
2016-10-21 10:26:03 +0200

    Added conversion functions to SafetyStatus

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/SafetyStatus.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/SafetyStatus.java
@@ -19,6 +19,14 @@ public abstract class SafetyStatus<S extends State, A extends Action> {
 		return new Unsafe<>(cex);
 	}
 
+	public abstract boolean isSafe();
+
+	public abstract boolean isUnsafe();
+
+	public abstract Safe<S, A> asSafe();
+
+	public abstract Unsafe<S, A> asUnsafe();
+
 	////
 
 	public static final class Safe<S extends State, A extends Action> extends SafetyStatus<S, A> {
@@ -31,6 +39,26 @@ public abstract class SafetyStatus<S extends State, A extends Action> {
 		public ARG<S, A> getProof() {
 			return proof;
 		}
+
+		@Override
+		public boolean isSafe() {
+			return true;
+		}
+
+		@Override
+		public boolean isUnsafe() {
+			return false;
+		}
+
+		@Override
+		public Safe<S, A> asSafe() {
+			return this;
+		}
+
+		@Override
+		public Unsafe<S, A> asUnsafe() {
+			throw new ClassCastException("Trying to cast a Safe to Unsafe");
+		}
 	}
 
 	public static final class Unsafe<S extends State, A extends Action> extends SafetyStatus<S, A> {
@@ -42,6 +70,26 @@ public abstract class SafetyStatus<S extends State, A extends Action> {
 
 		public Trace<S, A> getTrace() {
 			return cex;
+		}
+
+		@Override
+		public boolean isSafe() {
+			return false;
+		}
+
+		@Override
+		public boolean isUnsafe() {
+			return true;
+		}
+
+		@Override
+		public Safe<S, A> asSafe() {
+			throw new ClassCastException("Trying to cast an Unsafe as Safe");
+		}
+
+		@Override
+		public Unsafe<S, A> asUnsafe() {
+			return this;
 		}
 	}
 
