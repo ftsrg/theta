72cf23fc91b454e68d69eec18c081c526070ee88
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-04 13:23:01 +0100

    Renamed internal collection in stack

--- hu.bme.mit.theta.solver/src/main/java/hu/bme/mit/theta/solver/impl/StackImpl.java
+++ hu.bme.mit.theta.solver/src/main/java/hu/bme/mit/theta/solver/impl/StackImpl.java
@@ -10,27 +10,27 @@ import hu.bme.mit.theta.solver.Stack;
 
 public class StackImpl<T> implements Stack<T> {
 
-	public final LinkedList<T> stack;
+	public final LinkedList<T> items;
 	private final LinkedList<Integer> sizes;
 
 	public StackImpl() {
-		stack = new LinkedList<>();
+		items = new LinkedList<>();
 		sizes = new LinkedList<>();
 	}
 
 	@Override
 	public void add(final T elem) {
-		stack.add(elem);
+		items.add(elem);
 	}
 
 	@Override
 	public void add(final Collection<? extends T> elems) {
-		stack.addAll(elems);
+		items.addAll(elems);
 	}
 
 	@Override
 	public void push() {
-		sizes.add(stack.size());
+		sizes.add(items.size());
 	}
 
 	@Override
@@ -41,12 +41,12 @@ public class StackImpl<T> implements Stack<T> {
 
 		final int size = sizes.get(depth - n);
 		sizes.subList(depth - n, depth).clear();
-		stack.subList(size, stack.size()).clear();
+		items.subList(size, items.size()).clear();
 	}
 
 	@Override
 	public Collection<T> toCollection() {
-		return Collections.unmodifiableCollection(stack);
+		return Collections.unmodifiableCollection(items);
 	}
 
 }
