2e658a9ef84ea5f86c5ff16b9381614aba6786ff
Tamás Tóth <totht@mit.bme.hu>
2016-11-04 15:28:23 +0100

    Class ExprTraceStatus moved from ExprTrace

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprTrace.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprTrace.java
@@ -5,11 +5,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.collect.ImmutableList;
-
 import hu.bme.mit.theta.core.expr.Expr;
 import hu.bme.mit.theta.core.model.Model;
-import hu.bme.mit.theta.core.model.impl.Valuation;
 import hu.bme.mit.theta.core.type.BoolType;
 import hu.bme.mit.theta.core.utils.impl.PathUtils;
 import hu.bme.mit.theta.core.utils.impl.VarIndexing;
@@ -42,7 +39,7 @@ public final class ExprTrace {
 		return new ExprTrace(actions);
 	}
 
-	public TraceStatus check(final ItpSolver solver) {
+	public ExprTraceStatus check(final ItpSolver solver) {
 		solver.push();
 
 		final List<ItpMarker> markers = new ArrayList<>();
@@ -57,14 +54,14 @@ public final class ExprTrace {
 		final ItpPattern pattern = solver.createSeqPattern(markers);
 
 		final SolverStatus status = solver.check();
-		final TraceStatus result;
+		final ExprTraceStatus result;
 
 		if (status.isSat()) {
 			final Model model = solver.getModel();
-			result = TraceStatus.feasable(model, indexings);
+			result = ExprTraceStatus.feasable(model, indexings);
 		} else {
 			final Interpolant interpolant = solver.getInterpolant(pattern);
-			result = TraceStatus.unfeasable(interpolant, markers, indexings);
+			result = ExprTraceStatus.unfeasable(interpolant, markers, indexings);
 		}
 		solver.pop();
 
@@ -72,107 +69,5 @@ public final class ExprTrace {
 	}
 
 	////
-
-	public static abstract class TraceStatus {
-
-		private TraceStatus() {
-		}
-
-		private static Feasable feasable(final Model model, final List<? extends VarIndexing> indexings) {
-			return new Feasable(model, indexings);
-		}
-
-		private static Unfeasable unfeasable(final Interpolant interpolant, final List<? extends ItpMarker> markers,
-				final List<? extends VarIndexing> indexings) {
-			return new Unfeasable(interpolant, markers, indexings);
-		}
-
-		public abstract boolean isFeasable();
-
-		public abstract boolean isUnfeasable();
-
-		public abstract Feasable asFeasable();
-
-		public abstract Unfeasable asUnfeasable();
-
-		////
-
-		public static final class Feasable extends TraceStatus {
-			private final List<Valuation> valuations;
-
-			private Feasable(final Model model, final List<? extends VarIndexing> indexings) {
-				final ImmutableList.Builder<Valuation> builder = ImmutableList.builder();
-				for (final VarIndexing indexing : indexings) {
-					builder.add(PathUtils.extractValuation(model, indexing));
-				}
-				valuations = builder.build();
-			}
-
-			public List<Valuation> getValuations() {
-				return valuations;
-			}
-
-			@Override
-			public boolean isFeasable() {
-				return true;
-			}
-
-			@Override
-			public boolean isUnfeasable() {
-				return false;
-			}
-
-			@Override
-			public Feasable asFeasable() {
-				return this;
-			}
-
-			@Override
-			public Unfeasable asUnfeasable() {
-				throw new ClassCastException();
-			}
-		}
-
-		////
-
-		public static final class Unfeasable extends TraceStatus {
-			private final List<Expr<? extends BoolType>> exprs;
-
-			private Unfeasable(final Interpolant interpolant, final List<? extends ItpMarker> markers,
-					final List<? extends VarIndexing> indexings) {
-				final ImmutableList.Builder<Expr<? extends BoolType>> builder = ImmutableList.builder();
-				for (int i = 0; i < markers.size(); i++) {
-					final ItpMarker marker = markers.get(i);
-					final VarIndexing indexing = indexings.get(i);
-					builder.add(PathUtils.foldin(interpolant.eval(marker), indexing));
-				}
-				exprs = builder.build();
-			}
-
-			public List<Expr<? extends BoolType>> getExprs() {
-				return exprs;
-			}
-
-			@Override
-			public boolean isFeasable() {
-				return false;
-			}
-
-			@Override
-			public boolean isUnfeasable() {
-				return true;
-			}
-
-			@Override
-			public Feasable asFeasable() {
-				throw new ClassCastException();
-			}
-
-			@Override
-			public Unfeasable asUnfeasable() {
-				return this;
-			}
-		}
-	}
 
 }
