d18431756f511947dc53c7d9f0302973cd85eecc
Tamás Tóth <totht@mit.bme.hu>
2016-11-07 11:43:48 +0100

    Class NullActionFunction added

---
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/impl/NullActionFunction.java
@@ -0,0 +1,31 @@
+package hu.bme.mit.theta.analysis.impl;
+
+import java.util.Collection;
+
+import hu.bme.mit.theta.analysis.Action;
+import hu.bme.mit.theta.analysis.ActionFunction;
+import hu.bme.mit.theta.analysis.State;
+
+public final class NullActionFunction<A extends Action> implements ActionFunction<State, A> {
+
+	private static final NullActionFunction<?> INSTANCE = new NullActionFunction<>();
+
+	private NullActionFunction() {
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <A extends Action> NullActionFunction<A> getInstance() {
+		return (NullActionFunction<A>) INSTANCE;
+	}
+
+	@Override
+	public Collection<A> getEnabledActionsFor(final State state) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName();
+	}
+
+}
