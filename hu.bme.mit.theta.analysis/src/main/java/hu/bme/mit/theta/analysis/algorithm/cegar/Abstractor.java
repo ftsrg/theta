e1f8d998ca14e46284baa86cca9a53414038f0c7
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-07 14:51:58 +0100

    Abstractor works with AbstractionState

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/Abstractor.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/Abstractor.java
@@ -3,17 +3,16 @@ package hu.bme.mit.theta.analysis.algorithm.cegar;
 import hu.bme.mit.theta.analysis.Action;
 import hu.bme.mit.theta.analysis.Precision;
 import hu.bme.mit.theta.analysis.State;
-import hu.bme.mit.theta.analysis.algorithm.ARG;
 
 public interface Abstractor<S extends State, A extends Action, P extends Precision> {
 
-	ARG<S, A> init(final P precision);
+	AbstractionState<S, A, P> init(final P precision);
 
-	AbstractorStatus<S, A, P> check(ARG<S, A> arg, final P precision);
+	AbstractorStatus<S, A, P> check(AbstractionState<S, A, P> abstractionState);
 
 	default AbstractorStatus<S, A, P> initAndCheck(final P precision) {
-		final ARG<S, A> arg = init(precision);
-		final AbstractorStatus<S, A, P> status = check(arg, precision);
+		final AbstractionState<S, A, P> init = init(precision);
+		final AbstractorStatus<S, A, P> status = check(init);
 		return status;
 	}
 }
--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/CegarChecker.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/CegarChecker.java
@@ -12,29 +12,29 @@ import hu.bme.mit.theta.analysis.algorithm.SafetyStatus;
 public final class CegarChecker<S extends State, A extends Action, P extends Precision>
 		implements SafetyChecker<S, A, P> {
 
-	private final Abstractor<S, A, ? super P> abstractor;
+	private final Abstractor<S, A, P> abstractor;
 	private final Refiner<S, A, P> refiner;
 
-	private CegarChecker(final Abstractor<S, A, ? super P> abstractor, final Refiner<S, A, P> refiner) {
+	private CegarChecker(final Abstractor<S, A, P> abstractor, final Refiner<S, A, P> refiner) {
 		this.abstractor = checkNotNull(abstractor);
 		this.refiner = checkNotNull(refiner);
 	}
 
 	public static <S extends State, A extends Action, P extends Precision> CegarChecker<S, A, P> create(
-			final Abstractor<S, A, ? super P> abstractor, final Refiner<S, A, P> refiner) {
+			final Abstractor<S, A, P> abstractor, final Refiner<S, A, P> refiner) {
 		return new CegarChecker<>(abstractor, refiner);
 	}
 
 	@Override
 	public SafetyStatus<S, A> check(final P initPrecision) {
 		RefinerStatus<S, A, P> refinerStatus = null;
-		AbstractorStatus<S, A, ? super P> abstractorStatus = null;
+		AbstractorStatus<S, A, P> abstractorStatus = null;
 		P precision = initPrecision;
 		do {
 			// TODO: currently the ARG is not pruned, so the abstractor simply
 			// restarts at every iteration
-			final ARG<S, A> initArg = abstractor.init(precision);
-			abstractorStatus = abstractor.check(initArg, precision);
+			final AbstractionState<S, A, P> initArg = abstractor.init(precision);
+			abstractorStatus = abstractor.check(initArg);
 
 			if (abstractorStatus.isUnsafe()) {
 				final ARG<S, A> arg = abstractorStatus.getArg();
