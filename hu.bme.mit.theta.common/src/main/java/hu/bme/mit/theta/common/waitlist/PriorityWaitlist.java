3e46c414a32aba3677f89b15c05c89d67e31aa03
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-10 10:56:41 +0100

    Implemented PriorityWaitlist

---
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/waitlist/PriorityWaitlist.java
@@ -0,0 +1,82 @@
+package hu.bme.mit.theta.common.waitlist;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import hu.bme.mit.theta.common.ObjectUtils;
+
+public class PriorityWaitlist<T> implements Waitlist<T> {
+
+	private final PriorityQueue<T> items;
+
+	private PriorityWaitlist(final Comparator<? super T> comparator) {
+		items = new PriorityQueue<>(checkNotNull(comparator));
+	}
+
+	private PriorityWaitlist() {
+		items = new PriorityQueue<>();
+	}
+
+	public static <T> PriorityWaitlist<T> create(final Comparator<? super T> comparator) {
+		return new PriorityWaitlist<>(comparator);
+	}
+
+	public static <T> PriorityWaitlist<T> create() {
+		return new PriorityWaitlist<>();
+	}
+
+	@Override
+	public void add(final T item) {
+		items.add(item);
+	}
+
+	@Override
+	public void addAll(final Collection<? extends T> items) {
+		checkNotNull(items);
+		items.forEach(this::add);
+	}
+
+	@Override
+	public void addAll(final Stream<? extends T> items) {
+		checkNotNull(items);
+		items.forEach(this::add);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+
+	@Override
+	public T remove() {
+		return items.remove();
+	}
+
+	@Override
+	public int size() {
+		return items.size();
+	}
+
+	@Override
+	public void clear() {
+		items.clear();
+	}
+
+	public static <T> Supplier<PriorityWaitlist<T>> supplier(final Comparator<? super T> comparator) {
+		return () -> PriorityWaitlist.create(comparator);
+	}
+
+	public static <T> Supplier<PriorityWaitlist<T>> supplier() {
+		return PriorityWaitlist::create;
+	}
+
+	@Override
+	public String toString() {
+		return ObjectUtils.toStringBuilder("PriorityWaitlist").addAll(items).toString();
+	}
+}
--- hu.bme.mit.theta.common/src/test/java/hu/bme/mit/theta/common/waitlist/WaitlistTest.java
+++ hu.bme.mit.theta.common/src/test/java/hu/bme/mit/theta/common/waitlist/WaitlistTest.java
@@ -101,6 +101,52 @@ public class WaitlistTest {
 		Assert.assertTrue(waitlist.isEmpty());
 	}
 
+	@Test
+	public void testPriority() {
+		final Waitlist<String> waitlist = PriorityWaitlist.create();
+		assertEquals(0, waitlist.size());
+		Assert.assertTrue(waitlist.isEmpty());
+
+		waitlist.add("C");
+		assertEquals(1, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		waitlist.add("A");
+		assertEquals(2, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		waitlist.add("D");
+		assertEquals(3, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		waitlist.add("B");
+		assertEquals(4, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		final String item1 = waitlist.remove();
+		assertEquals("A", item1);
+		assertEquals(3, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		final String item2 = waitlist.remove();
+		assertEquals("B", item2);
+		assertEquals(2, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		waitlist.add("E");
+		assertEquals(3, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		final String item3 = waitlist.remove();
+		assertEquals("C", item3);
+		assertEquals(2, waitlist.size());
+		Assert.assertFalse(waitlist.isEmpty());
+
+		waitlist.clear();
+		assertEquals(0, waitlist.size());
+		Assert.assertTrue(waitlist.isEmpty());
+	}
+
 	@Test(expected = NoSuchElementException.class)
 	public void testLifoException() {
 		final Waitlist<String> waitlist = LifoWaitlist.create();
@@ -110,6 +156,12 @@ public class WaitlistTest {
 	@Test(expected = NoSuchElementException.class)
 	public void testFifoException() {
 		final Waitlist<String> waitlist = FifoWaitlist.create();
+		waitlist.remove();
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testPriorityException() {
+		final Waitlist<String> waitlist = PriorityWaitlist.create();
 		waitlist.remove();
 	}
 }
