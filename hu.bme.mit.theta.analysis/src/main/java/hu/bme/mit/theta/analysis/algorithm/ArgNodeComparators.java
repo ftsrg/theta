b9641ba8d6f16718c3ac7e1ce57aaa6e19d1fdb2
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-10 16:36:19 +0100

    Added new comparators

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/ArgNodeComparators.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/ArgNodeComparators.java
@@ -12,25 +12,30 @@ public class ArgNodeComparators {
 
 	////
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> invert(
-			final Comparator<ArgNode<S, A>> comparator) {
-		return new Inverter<>(comparator);
-	}
-
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> bfs() {
-		return new DepthOrder<>();
-	}
-
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> dfs() {
-		return invert(bfs());
-	}
-
 	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> creationAsc() {
 		return new CreationOrder<>();
 	}
 
 	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> creationDesc() {
 		return invert(creationAsc());
+	}
+
+	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> invert(
+			final Comparator<ArgNode<S, A>> comparator) {
+		return new Inverter<>(comparator);
+	}
+
+	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> combine(
+			final Comparator<ArgNode<S, A>> first, final Comparator<ArgNode<S, A>> then) {
+		return new Combinator<>(first, then);
+	}
+
+	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> bfs() {
+		return combine(new DepthOrder<>(), new CreationOrder<>());
+	}
+
+	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> dfs() {
+		return combine(invert(new DepthOrder<>()), new CreationOrder<>());
 	}
 
 	////
@@ -60,5 +65,26 @@ public class ArgNodeComparators {
 		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
 			return comparator.compare(n1, n2) * -1;
 		}
+	}
+
+	private static final class Combinator<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
+		private final Comparator<ArgNode<S, A>> first, then;
+
+		private Combinator(final Comparator<ArgNode<S, A>> first, final Comparator<ArgNode<S, A>> then) {
+			this.first = first;
+			this.then = then;
+		}
+
+		@Override
+		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+			final int compareFirst = first.compare(n1, n2);
+			if (compareFirst != 0) {
+				return compareFirst;
+			} else {
+				return then.compare(n1, n2);
+			}
+
+		}
+
 	}
 }
