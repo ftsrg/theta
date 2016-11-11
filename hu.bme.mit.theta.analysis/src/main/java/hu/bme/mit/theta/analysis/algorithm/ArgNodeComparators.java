ad3706b3084bf843de564ebf88f6d7ba8f545bf0
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-11 13:12:28 +0100

    Removed template parameters from ArgNodeComparators

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/ArgNodeComparators.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/ArgNodeComparators.java
@@ -12,82 +12,89 @@ public class ArgNodeComparators {
 
 	////
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> creationAsc() {
-		return new CreationOrder<>();
+	public static Comparator<ArgNode<? extends State, ? extends Action>> creationAsc() {
+		return new CreationOrder();
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> creationDesc() {
+	public static Comparator<ArgNode<? extends State, ? extends Action>> creationDesc() {
 		return invert(creationAsc());
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> invert(
-			final Comparator<ArgNode<S, A>> comparator) {
-		return new Inverter<>(comparator);
+	public static Comparator<ArgNode<? extends State, ? extends Action>> invert(
+			final Comparator<ArgNode<? extends State, ? extends Action>> comparator) {
+		return new Inverter(comparator);
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> combine(
-			final Comparator<ArgNode<S, A>> first, final Comparator<ArgNode<S, A>> then) {
-		return new Combinator<>(first, then);
+	public static Comparator<ArgNode<? extends State, ? extends Action>> combine(
+			final Comparator<ArgNode<? extends State, ? extends Action>> first,
+			final Comparator<ArgNode<? extends State, ? extends Action>> then) {
+		return new Combinator(first, then);
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> bfs() {
-		return combine(new DepthOrder<>(), new CreationOrder<>());
+	public static Comparator<ArgNode<? extends State, ? extends Action>> bfs() {
+		return combine(new DepthOrder(), new CreationOrder());
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> dfs() {
-		return combine(invert(new DepthOrder<>()), new CreationOrder<>());
+	public static Comparator<ArgNode<? extends State, ? extends Action>> dfs() {
+		return combine(invert(new DepthOrder()), new CreationOrder());
 	}
 
-	public static <S extends State, A extends Action> Comparator<ArgNode<S, A>> targetFirst() {
-		return new TargetFirst<>();
+	public static Comparator<ArgNode<? extends State, ? extends Action>> targetFirst() {
+		return new TargetFirst();
 	}
 
 	////
 
-	private static final class DepthOrder<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
+	private static final class DepthOrder implements Comparator<ArgNode<? extends State, ? extends Action>> {
 		@Override
-		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+		public int compare(final ArgNode<? extends State, ? extends Action> n1,
+				final ArgNode<? extends State, ? extends Action> n2) {
 			return Integer.compare(n1.getDepth(), n2.getDepth());
 		}
 	}
 
-	private static final class CreationOrder<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
+	private static final class CreationOrder implements Comparator<ArgNode<? extends State, ? extends Action>> {
 		@Override
-		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+		public int compare(final ArgNode<? extends State, ? extends Action> n1,
+				final ArgNode<? extends State, ? extends Action> n2) {
 			return Integer.compare(n1.getId(), n2.getId());
 		}
 	}
 
-	private static final class TargetFirst<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
+	private static final class TargetFirst implements Comparator<ArgNode<? extends State, ? extends Action>> {
 		@Override
-		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+		public int compare(final ArgNode<? extends State, ? extends Action> n1,
+				final ArgNode<? extends State, ? extends Action> n2) {
 			return Boolean.compare(n1.isTarget(), n2.isTarget()) * -1;
 		}
 	}
 
-	private static final class Inverter<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
-		private final Comparator<ArgNode<S, A>> comparator;
+	private static final class Inverter implements Comparator<ArgNode<? extends State, ? extends Action>> {
+		private final Comparator<ArgNode<? extends State, ? extends Action>> comparator;
 
-		private Inverter(final Comparator<ArgNode<S, A>> comparator) {
+		private Inverter(final Comparator<ArgNode<? extends State, ? extends Action>> comparator) {
 			this.comparator = comparator;
 		}
 
 		@Override
-		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+		public int compare(final ArgNode<? extends State, ? extends Action> n1,
+				final ArgNode<? extends State, ? extends Action> n2) {
 			return comparator.compare(n1, n2) * -1;
 		}
 	}
 
-	private static final class Combinator<S extends State, A extends Action> implements Comparator<ArgNode<S, A>> {
-		private final Comparator<ArgNode<S, A>> first, then;
+	private static final class Combinator implements Comparator<ArgNode<? extends State, ? extends Action>> {
+		private final Comparator<ArgNode<? extends State, ? extends Action>> first, then;
 
-		private Combinator(final Comparator<ArgNode<S, A>> first, final Comparator<ArgNode<S, A>> then) {
+		private Combinator(final Comparator<ArgNode<? extends State, ? extends Action>> first,
+				final Comparator<ArgNode<? extends State, ? extends Action>> then) {
 			this.first = first;
 			this.then = then;
 		}
 
 		@Override
-		public int compare(final ArgNode<S, A> n1, final ArgNode<S, A> n2) {
+		public int compare(final ArgNode<? extends State, ? extends Action> n1,
+				final ArgNode<? extends State, ? extends Action> n2) {
 			final int compareFirst = first.compare(n1, n2);
 			if (compareFirst != 0) {
 				return compareFirst;
