ef819aac11983924106ca87a373ccfcdc29d3bbc
Tamás Tóth <totht@mit.bme.hu>
2016-11-15 09:47:45 +0100

    CfaImpactChecker generalized and renamed to PredImpactChecker

--- hu.bme.mit.theta.analysis.cfa/src/test/java/hu/bme/mit/theta/analysis/cfa/impact/CfaPredImpactCheckerTest.java
+++ hu.bme.mit.theta.analysis.cfa/src/test/java/hu/bme/mit/theta/analysis/cfa/impact/CfaPredImpactCheckerTest.java
@@ -21,19 +21,25 @@ import org.junit.Test;
 import hu.bme.mit.theta.analysis.algorithm.ARG;
 import hu.bme.mit.theta.analysis.algorithm.ArgChecker;
 import hu.bme.mit.theta.analysis.algorithm.SafetyStatus;
+import hu.bme.mit.theta.analysis.algorithm.impact.PredImpactChecker;
+import hu.bme.mit.theta.analysis.cfa.CfaAction;
 import hu.bme.mit.theta.analysis.expr.ExprAction;
 import hu.bme.mit.theta.analysis.expr.ExprState;
 import hu.bme.mit.theta.analysis.impl.NullPrecision;
+import hu.bme.mit.theta.analysis.utils.ArgVisualizer;
+import hu.bme.mit.theta.common.visualization.GraphvizWriter;
 import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.stmt.Stmt;
 import hu.bme.mit.theta.core.type.BoolType;
 import hu.bme.mit.theta.core.type.IntType;
 import hu.bme.mit.theta.formalism.cfa.CFA;
 import hu.bme.mit.theta.formalism.cfa.CfaCreator;
+import hu.bme.mit.theta.formalism.cfa.CfaEdge;
+import hu.bme.mit.theta.formalism.cfa.CfaLoc;
 import hu.bme.mit.theta.solver.ItpSolver;
 import hu.bme.mit.theta.solver.z3.Z3SolverFactory;
 
-public final class CfaImpactCheckerTest {
+public final class CfaPredImpactCheckerTest {
 
 	@Test
 	public void test() {
@@ -55,7 +61,8 @@ public final class CfaImpactCheckerTest {
 
 		final ItpSolver solver = Z3SolverFactory.getInstace().createItpSolver();
 
-		final CfaImpactChecker checker = CfaImpactChecker.create(cfa, solver);
+		final PredImpactChecker<CfaLoc, CfaEdge> checker = PredImpactChecker.create(cfa.getInitLoc(),
+				l -> l.equals(cfa.getErrorLoc()), CfaAction::create, solver);
 
 		// Act
 		final SafetyStatus<? extends ExprState, ? extends ExprAction> status = checker
@@ -65,7 +72,12 @@ public final class CfaImpactCheckerTest {
 		assertTrue(status.isSafe());
 		final ARG<? extends ExprState, ? extends ExprAction> arg = status.asSafe().getArg();
 
+		assertTrue(arg.isSafe());
+		assertTrue(arg.isComplete());
+
 		final ArgChecker argChecker = ArgChecker.create(solver);
 		assertTrue(argChecker.isWellLabeled(arg));
+
+		System.out.println(new GraphvizWriter().writeString(ArgVisualizer.visualize(arg)));
 	}
 }
