6f3a31bd96985484864d27b86791843fafdbbb92
Tamás Tóth <totht@mit.bme.hu>
2016-11-23 15:13:45 +0100

    RatLitExpr extended with methods for basic arithmetic

--- hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/expr/RatLitExpr.java
+++ hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/expr/RatLitExpr.java
@@ -8,4 +8,28 @@ public interface RatLitExpr extends LitExpr<RatType>, NullaryExpr<RatType>, Comp
 
 	public long getDenom();
 
+	public int sign();
+
+	/**
+	 * @return the largest integer less than or equal to the number
+	 */
+	public long floor();
+
+	/**
+	 * @return the smallest integer greater than or equal to the number
+	 */
+	public long ceil();
+
+	public RatLitExpr add(final RatLitExpr that);
+
+	public RatLitExpr sub(final RatLitExpr that);
+
+	public RatLitExpr mul(final RatLitExpr that);
+
+	public RatLitExpr div(final RatLitExpr that);
+
+	public RatLitExpr abs();
+
+	public RatLitExpr frac();
+
 }
