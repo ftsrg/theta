3a39c6c7e07f4fb340966998675c87b83e6e2658
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-29 09:53:02 +0100

    Logging in ExplItpRefiner

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/ExplItpRefiner.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/ExplItpRefiner.java
@@ -14,19 +14,22 @@ import hu.bme.mit.theta.analysis.expr.ExprAction;
 import hu.bme.mit.theta.analysis.expr.ExprTraceChecker;
 import hu.bme.mit.theta.analysis.expr.ExprTraceStatus2;
 import hu.bme.mit.theta.analysis.expr.ItpRefutation;
+import hu.bme.mit.theta.common.logging.Logger;
 import hu.bme.mit.theta.core.utils.impl.ExprUtils;
 
 public final class ExplItpRefiner<A extends ExprAction> implements Refiner<ExplState, A, ExplPrecision> {
 
 	private final ExprTraceChecker<ItpRefutation> exprTraceChecker;
+	private final Logger logger;
 
-	private ExplItpRefiner(final ExprTraceChecker<ItpRefutation> exprTraceChecker) {
+	private ExplItpRefiner(final ExprTraceChecker<ItpRefutation> exprTraceChecker, final Logger logger) {
 		this.exprTraceChecker = checkNotNull(exprTraceChecker);
+		this.logger = checkNotNull(logger);
 	}
 
 	public static <A extends ExprAction> ExplItpRefiner<A> create(
-			final ExprTraceChecker<ItpRefutation> exprTraceChecker) {
-		return new ExplItpRefiner<>(exprTraceChecker);
+			final ExprTraceChecker<ItpRefutation> exprTraceChecker, final Logger logger) {
+		return new ExplItpRefiner<>(exprTraceChecker, logger);
 	}
 
 	@Override
@@ -38,17 +41,24 @@ public final class ExplItpRefiner<A extends ExprAction> implements Refiner<ExplS
 
 		final ArgTrace<ExplState, A> cexToConcretize = arg.getCexs().findFirst().get();
 		final Trace<ExplState, A> traceToConcretize = cexToConcretize.toTrace();
+		logger.writeln("Trace length: ", traceToConcretize.length(), 3, 2);
+		logger.writeln("Trace: ", traceToConcretize, 4, 3);
 
+		logger.write("Checking...", 3, 2);
 		final ExprTraceStatus2<ItpRefutation> cexStatus = exprTraceChecker.check(traceToConcretize);
+		logger.writeln("done.", 3);
+		logger.writeln(cexStatus, 3, 2);
 
 		if (cexStatus.isFeasible()) {
 			return RefinerResult.unsafe(traceToConcretize);
 		} else if (cexStatus.isInfeasible()) {
 			final ItpRefutation interpolant = cexStatus.asInfeasible().getRefutation();
+			logger.writeln(interpolant, 4, 3);
 
 			final ExplPrecision refinedPrecision = precision.refine(ExprUtils.getVars(interpolant));
 			final int pruneIndex = interpolant.getPruneIndex();
 			checkState(0 <= pruneIndex && pruneIndex <= cexToConcretize.length());
+			logger.writeln("Pruning from index ", pruneIndex, 3, 2);
 			final ArgNode<ExplState, A> nodeToPrune = cexToConcretize.node(pruneIndex);
 			arg.prune(nodeToPrune);
 			return RefinerResult.spurious(refinedPrecision);
