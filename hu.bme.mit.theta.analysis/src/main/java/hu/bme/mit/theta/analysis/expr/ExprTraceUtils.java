4fa1ec70b4e31e55dd85664c8552b2181d96020d
Tamás Tóth <totht@mit.bme.hu>
2016-11-30 11:15:40 +0100

    Method ExprTraceUtils.isInductive() added

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprTraceUtils.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expr/ExprTraceUtils.java
@@ -1,12 +1,17 @@
 package hu.bme.mit.theta.analysis.expr;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static hu.bme.mit.theta.analysis.expr.ExprStateUtils.anyUncoveredSuccessor;
 import static hu.bme.mit.theta.core.expr.impl.Exprs.True;
+import static java.util.Collections.singleton;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import hu.bme.mit.theta.analysis.Trace;
+import hu.bme.mit.theta.core.model.impl.Valuation;
+import hu.bme.mit.theta.solver.Solver;
 
 public final class ExprTraceUtils {
 
@@ -20,6 +25,22 @@ public final class ExprTraceUtils {
 			states.add(SimpleExprState.of(True()));
 		}
 		return Trace.of(states, actions);
+	}
+
+	public static boolean isInductive(final Trace<? extends ExprState, ? extends ExprAction> trace,
+			final Solver solver) {
+		for (int i = 0; i < trace.length(); i++) {
+			final ExprState sourceState = trace.getState(i);
+			final ExprAction action = trace.getAction(i);
+			final ExprState targetState = trace.getState(i + 1);
+
+			final Optional<Valuation> uncoveredSuccessor = anyUncoveredSuccessor(sourceState, action,
+					singleton(targetState), solver);
+			if (uncoveredSuccessor.isPresent()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
