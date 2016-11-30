62a23eed768b23d4e6e2087ed306473bc7315b6f
Hajdu Ákos <hajdua@mit.bme.hu>
2016-11-30 12:12:17 +0100

    Initial version of configuration building

---
+++ hu.bme.mit.theta.frontend.benchmark/src/main/java/hu/bme/mit/theta/frontend/benchmark/Configuration.java
@@ -0,0 +1,27 @@
+package hu.bme.mit.theta.frontend.benchmark;
+
+import hu.bme.mit.theta.analysis.Action;
+import hu.bme.mit.theta.analysis.Precision;
+import hu.bme.mit.theta.analysis.State;
+import hu.bme.mit.theta.analysis.algorithm.SafetyChecker;
+import hu.bme.mit.theta.analysis.algorithm.SafetyStatus;
+
+public class Configuration<S extends State, A extends Action, P extends Precision> {
+	private final SafetyChecker<S, A, P> checker;
+	private final P initPrecision;
+
+	private Configuration(final SafetyChecker<S, A, P> checker, final P initPrecision) {
+		this.checker = checker;
+		this.initPrecision = initPrecision;
+	}
+
+	public static <S extends State, A extends Action, P extends Precision> Configuration<S, A, P> create(
+			final SafetyChecker<S, A, P> checker, final P initPrecision) {
+		return new Configuration<>(checker, initPrecision);
+	}
+
+	public SafetyStatus<S, A> check() {
+		return checker.check(initPrecision);
+	}
+
+}
