2a937cd6820580a2fbcaec219b729642f7962716
Hajdu Ákos <hajdua@mit.bme.hu>
2016-12-04 19:41:56 +0100

    Renamed AIGER parsing classes

--- hu.bme.mit.theta.splittingcegar.ui/src/test/java/hu/bme/mit/theta/splittingcegar/ui/SandBox.java
+++ hu.bme.mit.theta.splittingcegar.ui/src/test/java/hu/bme/mit/theta/splittingcegar/ui/SandBox.java
@@ -14,7 +14,7 @@ import hu.bme.mit.theta.core.type.BoolType;
 import hu.bme.mit.theta.formalism.sts.STS;
 import hu.bme.mit.theta.formalism.sts.dsl.StsDslManager;
 import hu.bme.mit.theta.formalism.sts.dsl.impl.StsSpec;
-import hu.bme.mit.theta.frontend.aiger.impl.SimpleAigerLoader;
+import hu.bme.mit.theta.frontend.aiger.impl.AigerParserSimple;
 import hu.bme.mit.theta.splittingcegar.common.CEGARLoop;
 import hu.bme.mit.theta.splittingcegar.common.CEGARResult;
 import hu.bme.mit.theta.splittingcegar.common.data.AbstractState;
@@ -49,7 +49,7 @@ public class SandBox {
 			final StsSpec stsSpec = StsDslManager.parse(MODELSPATH + subPath + modelName);
 			problem = stsSpec.getAllSts().iterator().next();
 		} else if (modelName.endsWith(".aag")) {
-			problem = new SimpleAigerLoader().load(MODELSPATH + subPath + modelName);
+			problem = new AigerParserSimple().parse(MODELSPATH + subPath + modelName);
 		}
 
 		CEGARLoop cegar = null;
