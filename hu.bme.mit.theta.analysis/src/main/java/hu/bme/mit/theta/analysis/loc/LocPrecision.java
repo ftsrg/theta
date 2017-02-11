3173b3781cbe6b77240755238fef6192903cd9c2
Hajdu Ákos <hajdua@mit.bme.hu>
2017-02-11 14:16:00 +0100

    Polymorphic LocPrecision

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/loc/LocPrecision.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/loc/LocPrecision.java
@@ -5,31 +5,67 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.function.Function;
 
 import hu.bme.mit.theta.analysis.Precision;
+import hu.bme.mit.theta.common.ObjectUtils;
 import hu.bme.mit.theta.formalism.common.Edge;
 import hu.bme.mit.theta.formalism.common.Loc;
 
-public final class LocPrecision<P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>>
+public abstract class LocPrecision<P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>>
 		implements Precision {
-
-	private final Function<? super L, ? extends P> mapping;
-
-	private LocPrecision(final Function<? super L, ? extends P> mapping) {
-		this.mapping = checkNotNull(mapping);
-	}
 
 	public static <P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>> LocPrecision<P, L, E> create(
 			final Function<? super L, ? extends P> mapping) {
-		return new LocPrecision<P, L, E>(mapping);
+		return new Generic<P, L, E>(mapping);
 	}
 
 	public static <P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>> LocPrecision<P, L, E> constant(
 			final P precision) {
-		return create(l -> precision);
+		return new Constant<P, L, E>(precision);
 	}
 
-	public P getPrecision(final L loc) {
-		checkNotNull(loc);
-		return mapping.apply(loc);
+	public abstract P getPrecision(final L loc);
+
+	/////
+
+	public static final class Generic<P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>>
+			extends LocPrecision<P, L, E> {
+		private final Function<? super L, ? extends P> mapping;
+
+		private Generic(final Function<? super L, ? extends P> mapping) {
+			this.mapping = checkNotNull(mapping);
+		}
+
+		@Override
+		public P getPrecision(final L loc) {
+			checkNotNull(loc);
+			return mapping.apply(loc);
+		}
+
+		@Override
+		public String toString() {
+			return ObjectUtils.toStringBuilder(LocPrecision.class.getSimpleName()).add(getClass().getSimpleName())
+					.toString();
+		}
+	}
+
+	public static final class Constant<P extends Precision, L extends Loc<L, E>, E extends Edge<L, E>>
+			extends LocPrecision<P, L, E> {
+		private final P precision;
+
+		private Constant(final P precision) {
+			this.precision = checkNotNull(precision);
+		}
+
+		@Override
+		public P getPrecision(final L loc) {
+			checkNotNull(loc);
+			return precision;
+		}
+
+		@Override
+		public String toString() {
+			return ObjectUtils.toStringBuilder(LocPrecision.class.getSimpleName()).add(getClass().getSimpleName())
+					.add(precision).toString();
+		}
 	}
 
 }
