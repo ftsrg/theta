bc877c492809fbe57a1fb6ea7183e7bfd5d3d8da
Hajdu Ákos <hajdua@mit.bme.hu>
2017-02-12 11:42:56 +0100

    Some comments for the benchmark script

--- benchmark/general/Run-Measurements.ps1
+++ benchmark/general/Run-Measurements.ps1
@@ -32,10 +32,14 @@ param (
     [string]$outDir = "./"
 )
 
+# Temp file for individual runs
 $tmpFile = [System.IO.Path]::GetTempFileName()
+# Final log file collecting results from all runs
+# (The temp file is needed because I could not redirect the output of the application to a file
+#  in append mode. Therefore, the temp file is always overwritten, but the script always appends
+#  the contents of the temp file to the final log file.)
 $logFile = $outDir + "log_" + (Get-Date -format "yyyyMMdd_HHmmss") + ".csv"
-# Header
-"Model,Vars,Size,Domain,Refinement,InitPrec,Search,Safe,TimeMs,Iterations,ARGsize,ARGdepth,CEXlen" | Out-File $logFile
+"Model,Vars,Size,Domain,Refinement,InitPrec,Search,Safe,TimeMs,Iterations,ARGsize,ARGdepth,CEXlen" | Out-File $logFile # Header
 
 # Load models and configurations from external files
 $models = @(Import-CSV $modelsFile -Header Name, Prop, Expected)
@@ -55,17 +59,19 @@ foreach($model in $models) {
         for($r = 0; $r -lt $runs; $r++) {
             Write-Progress -Activity " " -PercentComplete (($r)*100/$runs) -Status "Run: $($r+1)/$runs " -Id 2
             
-            # Run the jar file with the given parameters, the output is redirected to a temp file
+            # Collect arguments for the jar file
             $args = @('-jar', $jarFile, '-m', $model.Name, '-d', $conf.Domain, '-r', $conf.Refinement, '-i', $conf.InitPrec, '-s', $conf.Search)
             # If expected result is present in the CSV add it as a parameter
             if ($model.Expected -ne $Null) {
                 $args += '-e'
                 $args += $model.Expected
             }
+            # If preperty is present in the CSV add it as a parameter
             if ($model.Prop) {
                 $args += '-p'
                 $args += $model.Prop
             }
+            # Run the jar file with the given parameters, the output is redirected to a temp file
             $p = Start-Process java -ArgumentList $args -RedirectStandardOutput $tmpFile -PassThru -NoNewWindow
             $id = $p.id
             if (!$p.WaitForExit($timeOut*1000)){ # Timeout
