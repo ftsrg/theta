d7c86fb7013c621ced062b107d74af67e1f1c74a
Hajdu Ákos <hajdua@mit.bme.hu>
2017-03-07 10:44:23 +0100

    Fixed fat jar build with gradle

--- benchmark/general/build.bat
+++ benchmark/general/build.bat
@@ -1,5 +1,5 @@
 pushd "../../"
 call gradlew.bat stsMain
 popd
-copy "..\..\hu.bme.mit.theta.frontend.benchmark\build\libs\hu.bme.mit.theta.frontend.benchmark-1.0.0-SNAPSHOT-fat.jar" "theta.jar" /Y
+copy "..\..\hu.bme.mit.theta.frontend.benchmark\build\libs\hu.bme.mit.theta.frontend.benchmark-1.0.0-SNAPSHOT.jar" "theta.jar" /Y
 copy "..\..\lib\*.dll" "*.dll"
