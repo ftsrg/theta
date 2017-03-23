ca4b3788ede32c3083485901014a09cf1b6522a6
Tamás Tóth <totht@mit.bme.hu>
2017-03-23 12:28:26 +0100

    Checking for supported types added to XtaVariableSymbol

--- hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/dsl/XtaVariableSymbol.java
+++ hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/dsl/XtaVariableSymbol.java
@@ -8,8 +8,12 @@ import hu.bme.mit.theta.common.dsl.Symbol;
 import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.decl.impl.Decls;
 import hu.bme.mit.theta.core.expr.Expr;
+import hu.bme.mit.theta.core.type.ArrayType;
+import hu.bme.mit.theta.core.type.BoolType;
+import hu.bme.mit.theta.core.type.IntType;
 import hu.bme.mit.theta.core.type.Type;
 import hu.bme.mit.theta.formalism.ta.decl.impl.Decls2;
+import hu.bme.mit.theta.formalism.xta.ChanType;
 import hu.bme.mit.theta.formalism.xta.dsl.gen.XtaDslParser.TypeContext;
 import hu.bme.mit.theta.formalism.xta.dsl.gen.XtaDslParser.VariableIdContext;
 import hu.bme.mit.theta.formalism.xta.utils.ClockType;
@@ -47,7 +51,10 @@ final class XtaVariableSymbol implements Symbol {
 
 	public Expr<?> instantiate(final String prefix, final Environment env) {
 		final Type varType = type.instantiate(env);
-		checkArgument(!(varType instanceof RangeType));
+
+		if (!isSupportedType(varType)) {
+			throw new UnsupportedOperationException(varType + " is not supported for variables");
+		}
 
 		if (constant) {
 			final Expr<?> expr = initialiser.instantiate(varType, env);
@@ -61,6 +68,37 @@ final class XtaVariableSymbol implements Symbol {
 			}
 			final Expr<?> varRef = varDecl.getRef();
 			return varRef;
+		}
+	}
+
+	private static boolean isSupportedType(final Type type) {
+		if (type instanceof BoolType) {
+			return true;
+		} else if (type instanceof IntType) {
+			return true;
+		} else if (type instanceof ClockType) {
+			return true;
+		} else if (type instanceof ChanType) {
+			return true;
+		} else if (type instanceof ArrayType) {
+			return isSupportedArrayType((ArrayType<?, ?>) type);
+		} else {
+			return false;
+		}
+	}
+
+	private static boolean isSupportedArrayType(final ArrayType<?, ?> type) {
+		final Type indexType = type.getIndexType();
+		final Type elemType = type.getElemType();
+
+		if (!(indexType instanceof RangeType)) {
+			return false;
+		} else if (elemType instanceof ChanType) {
+			return true;
+		} else if (elemType instanceof ArrayType) {
+			return isSupportedArrayType((ArrayType<?, ?>) elemType);
+		} else {
+			return false;
 		}
 	}
 
