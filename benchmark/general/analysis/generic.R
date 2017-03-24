59b039261ccd9c47d68a4b5764be6c3f4be03bb4
Hajdu Ákos <hajdua@mit.bme.hu>
2017-03-24 14:18:24 +0100

    Working on benchmark script, added markdown

--- benchmark/general/analysis/generic.R
+++ benchmark/general/analysis/generic.R
@@ -8,6 +8,11 @@ library(stringr)
 csv_path = "../log_20170310_101235.csv" # Path of the data
 timeout_ms = 10 * 60 * 1000 # Timeout used during the measurements
 
+
+# Common themes ------------------------------------------------------------------
+theme_rotate_x <- theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+theme_noticks <- theme(axis.ticks = element_blank())
+
 # Read data ----------------------------------------------------------------------
 
 d <- read_csv(
@@ -44,3 +49,43 @@ ex_rows <- str_detect(d$Safe, "\\[EX\\]")
 exs <- sum(ex_rows, na.rm = T)
 if (exs > 0) warning(paste(c(exs, " rows contain exceptions that are converted to NAs.")))
 d$Safe <- as.logical(d$Safe)
+
+# Create 'Config' column
+d <- d %>% mutate(Config = paste(
+  substr(d$Domain, 1, 1),
+  substr(d$Refinement, 1, 1),
+  substr(d$InitPrec, 1, 1),
+  substr(d$Search, 1, 1),
+  ifelse(is.na(d$PredSplit), "", substr(d$PredSplit, 1, 1)),
+  sep = ""
+))
+
+# Trim name
+d$Model <- as.factor(gsub("models/", "", d$Model))
+
+# Separate 'Model' into 'Name' and 'Type'
+d <- d %>% separate(Model, into = c("Type", "Name"), sep = "/", remove = F)
+
+
+# Analysis -----------------------------------------------------------------------
+
+
+# Heatmaps -----------------------------------------------------------------------
+
+d_model_config <- d %>% group_by(Config, Model) %>%
+  summarise(
+    SafeCnt = sum(Safe, na.rm = T),
+    SafeRatio = ifelse(SafeCnt == 0, 0, SafeCnt / sum(!is.na(Safe))),
+    Consistent = near(SafeRatio, 1) || near(SafeRatio, 0),
+    TimeAvg = mean(TimeMs),
+    TimeRSD = sd(TimeMs) / mean(TimeMs))
+
+ggplot(d_model_config, aes(Model, Config)) +
+  geom_tile(aes(fill = Consistent), color = "black") +
+  theme_noticks + theme_rotate_x
+
+ggplot(d_model_config, aes(Model, Config)) +
+  geom_tile(aes(fill = log(TimeAvg)), color = "black") +
+  scale_fill_gradient(low = "green", high = "red", na.value = "white") +
+  theme_noticks + theme_rotate_x
+  
