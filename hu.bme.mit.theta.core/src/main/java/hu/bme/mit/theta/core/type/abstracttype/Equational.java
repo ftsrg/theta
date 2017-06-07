2ec2033a9bd17eef773016e964f1444b7d9f5d6e
Tamás Tóth <totht@mit.bme.hu>
2017-06-07 17:26:18 +0200

    Interface Extensional renamed to Equational

--- hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/type/abstracttype/Equational.java
+++ hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/type/abstracttype/Equational.java
@@ -3,7 +3,7 @@ package hu.bme.mit.theta.core.type.abstracttype;
 import hu.bme.mit.theta.core.Expr;
 import hu.bme.mit.theta.core.Type;
 
-public interface Extensional<OpType extends Extensional<OpType>> extends Type {
+public interface Equational<OpType extends Equational<OpType>> extends Type {
 
 	EqExpr<OpType> Eq(Expr<OpType> leftOp, Expr<OpType> rightOp);
 
