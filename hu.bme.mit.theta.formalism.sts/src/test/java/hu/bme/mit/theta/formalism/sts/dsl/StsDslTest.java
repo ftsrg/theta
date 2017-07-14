fd40c5fc695bc9c23da31d9637a6b3cafede9117
Hajdu Ákos <hajdua@mit.bme.hu>
2017-07-14 17:33:29 +0200

    Added assertions to STS DSL test

--- hu.bme.mit.theta.formalism.sts/src/test/java/hu/bme/mit/theta/formalism/sts/dsl/StsDslTest.java
+++ hu.bme.mit.theta.formalism.sts/src/test/java/hu/bme/mit/theta/formalism/sts/dsl/StsDslTest.java
@@ -3,27 +3,47 @@ package hu.bme.mit.theta.formalism.sts.dsl;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import hu.bme.mit.theta.formalism.sts.STS;
 
+@RunWith(Parameterized.class)
 public class StsDslTest {
 
-	@Test
-	public void testReadersWriters() throws FileNotFoundException, IOException {
-		final InputStream inputStream = StsDslTest.class.getResourceAsStream("/readerswriters.system");
-		final StsSpec spec = StsDslManager.createStsSpec(inputStream);
-		final STS sts = spec.createProp("safe");
-		System.out.println(sts);
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+
+				{ "/readerswriters.system", "safe", 3 },
+
+				{ "/simple1.system", "safe", 2 }
+
+		});
 	}
 
+	@Parameter(0)
+	public String filepath;
+
+	@Parameter(1)
+	public String propertyName;
+
+	@Parameter(2)
+	public int varCount;
+
 	@Test
-	public void testSimple() throws FileNotFoundException, IOException {
-		final InputStream inputStream = StsDslTest.class.getResourceAsStream("/simple1.system");
+	public void test() throws FileNotFoundException, IOException {
+		final InputStream inputStream = StsDslTest.class.getResourceAsStream(filepath);
 		final StsSpec spec = StsDslManager.createStsSpec(inputStream);
-		final STS sts = spec.createProp("safe");
-		System.out.println(sts);
+		final STS sts = spec.createProp(propertyName);
+		Assert.assertEquals(varCount, sts.getVars().size());
 	}
 
 }
