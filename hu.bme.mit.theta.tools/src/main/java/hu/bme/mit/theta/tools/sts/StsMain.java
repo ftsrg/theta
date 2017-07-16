9261f195406aed3c4c8b06bf3fd0699dd663e402
Hajdu Ákos <hajdua@mit.bme.hu>
2017-07-16 12:41:54 +0200

    STS main uses JCommander

--- hu.bme.mit.theta.tools/src/main/java/hu/bme/mit/theta/tools/sts/StsMain.java
+++ hu.bme.mit.theta.tools/src/main/java/hu/bme/mit/theta/tools/sts/StsMain.java
@@ -3,16 +3,10 @@ package hu.bme.mit.theta.tools.sts;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
-import java.util.StringJoiner;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import hu.bme.mit.theta.analysis.algorithm.SafetyResult;
 import hu.bme.mit.theta.analysis.algorithm.cegar.CegarStatistics;
@@ -39,27 +33,44 @@ import hu.bme.mit.theta.tools.sts.StsConfigurationBuilder.InitPrec;
  * A command line interface for running a CEGAR configuration on an STS.
  */
 public class StsMain {
-	private static final String JAR_NAME = "theta-sts.jar";
+	String[] args;
+	TableWriter tableWriter;
 
-	private final String[] args;
-	private final TableWriter tableWriter;
-	private final Options options;
+	@Parameter(names = { "-m", "--model" }, description = "Path of the input model", required = true)
+	String model;
 
-	private String model;
-	private String prop;
-	private Domain domain;
-	private Refinement refinement;
-	private InitPrec initPrec;
-	private Search search;
-	private Optional<Boolean> expected;
-	private PredSplit predSplit;
-	private boolean benchmarkMode;
-	private Logger logger;
+	@Parameter(names = { "-p", "--property" }, description = "Property to be verified", required = true)
+	String property;
+
+	@Parameter(names = { "-d", "--domain" }, description = "Abstract domain", required = true)
+	Domain domain;
+
+	@Parameter(names = { "-r", "--refinement" }, description = "Refinement strategy", required = true)
+	Refinement refinement;
+
+	@Parameter(names = { "-i", "--initprec" }, description = "Initial precision")
+	InitPrec initPrec = InitPrec.EMPTY;
+
+	@Parameter(names = { "-s", "--search" }, description = "Search strategy")
+	Search search = Search.BFS;
+
+	@Parameter(names = { "-ps", "--predsplit" }, description = "Predicate splitting")
+	PredSplit predSplit = PredSplit.WHOLE;
+
+	@Parameter(names = { "-e", "--expected" }, description = "Expected result", arity = 1)
+	Boolean expected;
+
+	@Parameter(names = { "-ll", "--loglevel" }, description = "Detailedness of logging")
+	Integer logLevel = 1;
+
+	@Parameter(names = { "-bm", "--benchmark" }, description = "Benchmark mode (only print metrics)")
+	Boolean benchmarkMode = false;
+
+	Logger logger;
 
 	public StsMain(final String[] args) {
 		this.args = args;
 		tableWriter = new SimpleTableWriter(System.out, ",", "\"", "\"");
-		options = new Options();
 	}
 
 	public static void main(final String[] args) {
@@ -72,14 +83,16 @@ public class StsMain {
 			printHeader();
 			return;
 		}
-
 		try {
-			parseArgs();
-		} catch (final ParseException e) {
-			new HelpFormatter().printHelp(JAR_NAME, options, true);
+			final JCommander cmd = JCommander.newBuilder().addObject(this).build();
+			cmd.setProgramName("theta-sts.jar");
+			cmd.parse(args);
+			logger = benchmarkMode ? NullLogger.getInstance() : new ConsoleLogger(logLevel);
+		} catch (final ParameterException ex) {
+			System.out.println(ex.getMessage());
+			ex.usage();
 			return;
 		}
-
 		try {
 			final STS sts = loadModel();
 			final Configuration<?, ?, ?> configuration = buildConfiguration(sts);
@@ -107,73 +120,13 @@ public class StsMain {
 		tableWriter.newRow();
 	}
 
-	private void parseArgs() throws ParseException {
-		final Option optModel = Option.builder("m").longOpt("model").hasArg().argName("MODEL").type(String.class)
-				.desc("Path of the input model").required().build();
-		options.addOption(optModel);
-
-		final Option optProp = Option.builder("p").longOpt("property").hasArg().argName("PROPERTY").type(String.class)
-				.desc("Property to be verified").build();
-		options.addOption(optProp);
-
-		final Option optDomain = Option.builder("d").longOpt("domain").hasArg().argName(optionsFor(Domain.values()))
-				.type(Domain.class).desc("Abstract domain").required().build();
-		options.addOption(optDomain);
-
-		final Option optRefinement = Option.builder("r").longOpt("refinement").hasArg()
-				.argName(optionsFor(Refinement.values())).type(Refinement.class).desc("Refinement strategy").required()
-				.build();
-		options.addOption(optRefinement);
-
-		final Option optInitPrec = Option.builder("i").longOpt("initprec").hasArg()
-				.argName(optionsFor(InitPrec.values())).type(InitPrec.class).desc("Initial precision").build();
-		options.addOption(optInitPrec);
-
-		final Option optSearch = Option.builder("s").longOpt("search").hasArg().argName(optionsFor(Search.values()))
-				.type(Search.class).desc("Search strategy").build();
-		options.addOption(optSearch);
-
-		final Option optPredSplit = Option.builder("ps").longOpt("predsplit").hasArg()
-				.argName(optionsFor(PredSplit.values())).type(PredSplit.class).desc("Predicate splitting").build();
-		options.addOption(optPredSplit);
-
-		final Option optExpected = Option.builder("e").longOpt("expected").hasArg().argName("true|false")
-				.type(Boolean.class).desc("Expected result (safe)").build();
-		options.addOption(optExpected);
-
-		final Option optLogLevel = Option.builder("ll").longOpt("loglevel").hasArg().argName("INT").type(Integer.class)
-				.desc("Level of logging (detailedness)").build();
-		options.addOption(optLogLevel);
-
-		final Option optBenchmark = Option.builder("bm").longOpt("benchmark")
-				.desc("Benchmark mode (only print output values)").build();
-		options.addOption(optBenchmark);
-
-		final CommandLineParser parser = new DefaultParser();
-		final CommandLine cmd = parser.parse(options, args);
-
-		// Convert string arguments to the proper values
-		model = cmd.getOptionValue(optModel.getOpt());
-		prop = cmd.getOptionValue(optProp.getOpt(), "");
-		domain = Domain.valueOf(cmd.getOptionValue(optDomain.getOpt()));
-		refinement = Refinement.valueOf(cmd.getOptionValue(optRefinement.getOpt()));
-		initPrec = InitPrec.valueOf(cmd.getOptionValue(optInitPrec.getOpt(), InitPrec.EMPTY.toString()));
-		search = Search.valueOf(cmd.getOptionValue(optSearch.getOpt(), Search.BFS.toString()));
-		expected = cmd.hasOption(optExpected.getOpt())
-				? Optional.of(Boolean.parseBoolean(cmd.getOptionValue(optExpected.getOpt()))) : Optional.empty();
-		predSplit = PredSplit.valueOf(cmd.getOptionValue(optPredSplit.getOpt(), PredSplit.WHOLE.toString()));
-		benchmarkMode = cmd.hasOption(optBenchmark.getOpt());
-		final int logLevel = Integer.parseInt(cmd.getOptionValue(optLogLevel.getOpt(), "1"));
-		logger = benchmarkMode ? NullLogger.getInstance() : new ConsoleLogger(logLevel);
-	}
-
 	private STS loadModel() throws IOException {
 		if (model.endsWith(".aag")) {
 			return new AigerParserSimple().parse(model);
 		} else if (model.endsWith(".system")) {
 			final InputStream inputStream = new FileInputStream(model);
 			final StsSpec spec = StsDslManager.createStsSpec(inputStream);
-			return StsUtils.eliminateIte(spec.createProp(prop));
+			return StsUtils.eliminateIte(spec.createProp(property));
 		} else {
 			throw new IOException("Unknown format");
 		}
@@ -185,8 +138,8 @@ public class StsMain {
 	}
 
 	private void checkResult(final SafetyResult<?, ?> status) throws Exception {
-		if (expected.isPresent() && !expected.get().equals(status.isSafe())) {
-			throw new Exception("Expected safe = " + expected.get() + " but was " + status.isSafe());
+		if (expected != null && !expected.equals(status.isSafe())) {
+			throw new Exception("Expected safe = " + expected + " but was " + status.isSafe());
 		}
 	}
 
@@ -217,13 +170,5 @@ public class StsMain {
 			logger.writeln("Exception occured: " + ex.getClass().getSimpleName(), 0);
 			logger.writeln("Message: " + ex.getMessage(), 0, 1);
 		}
-	}
-
-	private static String optionsFor(final Object[] objs) {
-		final StringJoiner sj = new StringJoiner("|");
-		for (final Object o : objs) {
-			sj.add(o.toString());
-		}
-		return sj.toString();
 	}
 }
