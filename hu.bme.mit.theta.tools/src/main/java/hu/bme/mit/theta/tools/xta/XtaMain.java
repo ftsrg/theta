59bdfc5c2c1427c0193af836b834842e43c641ce
Hajdu Ákos <hajdua@mit.bme.hu>
2017-07-18 11:49:35 +0200

    XTA Main uses JCommander

--- hu.bme.mit.theta.tools/src/main/java/hu/bme/mit/theta/tools/xta/XtaMain.java
+++ hu.bme.mit.theta.tools/src/main/java/hu/bme/mit/theta/tools/xta/XtaMain.java
@@ -1,16 +1,12 @@
 package hu.bme.mit.theta.tools.xta;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.StringJoiner;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import hu.bme.mit.theta.analysis.algorithm.SafetyChecker;
 import hu.bme.mit.theta.analysis.algorithm.SafetyResult;
@@ -29,6 +25,21 @@ import hu.bme.mit.theta.formalism.xta.XtaSystem;
 import hu.bme.mit.theta.formalism.xta.dsl.XtaDslManager;
 
 public final class XtaMain {
+	private static final String JAR_NAME = "theta-xta.jar";
+	private final String[] args;
+	private final TableWriter writer;
+
+	@Parameter(names = { "-a", "--algorithm" }, description = "Algorithm", required = true)
+	Algorithm algorithm;
+
+	@Parameter(names = { "-m", "--model" }, description = "Path of the input model", required = true)
+	String model;
+
+	@Parameter(names = { "-s", "--search" }, description = "Search strategy", required = true)
+	Search search;
+
+	@Parameter(names = { "-bm", "--benchmark" }, description = "Benchmark mode (only print metrics)")
+	Boolean benchmarkMode = false;
 
 	private static enum Algorithm {
 
@@ -96,134 +107,103 @@ public final class XtaMain {
 		public abstract SearchStrategy create();
 	}
 
-	private static final Option ALGORITHM = Option.builder("a").longOpt("algorithm").numberOfArgs(1)
-			.argName(optionsFor(Algorithm.values())).type(Algorithm.class).desc("Algorithm").required().build();
-
-	private static final Option MODEL = Option.builder("m").longOpt("model").numberOfArgs(1).argName("MODEL")
-			.type(String.class).desc("Path of the input model").required().build();
-
-	private static final Option SEARCH = Option.builder("s").longOpt("search").numberOfArgs(1)
-			.argName(optionsFor(Search.values())).type(SearchStrategy.class).desc("Search strategy").required().build();
-
-	private static final Option BENCHMARK = Option.builder("bm").longOpt("benchmark")
-			.desc("Benchmark mode (only print output values)").build();
+	public XtaMain(final String[] args) {
+		this.args = args;
+		this.writer = new SimpleTableWriter(System.out, ",", "\"", "\"");
+	}
 
 	public static void main(final String[] args) {
-		final TableWriter writer = new SimpleTableWriter(System.out, ",", "\"", "\"");
+		final XtaMain mainApp = new XtaMain(args);
+		mainApp.run();
+	}
 
-		// Setting up argument parser
-		final Options options = new Options();
-
-		options.addOption(MODEL);
-		options.addOption(ALGORITHM);
-		options.addOption(SEARCH);
-		options.addOption(BENCHMARK);
-
-		final CommandLineParser parser = new DefaultParser();
-		final HelpFormatter helpFormatter = new HelpFormatter();
-		final CommandLine cmd;
-
-		// Parse arguments
-		try {
-			cmd = parser.parse(options, args);
-		} catch (final ParseException e) {
-			// If called with a single --header argument, print header and exit
-			if (args.length == 1 && "--header".equals(args[0])) {
-				writer.cell("Result");
-				writer.cell("AlgorithmTimeInMs");
-				writer.cell("RefinementTimeInMs");
-				writer.cell("InterpolationTimeInMs");
-				writer.cell("RefinementSteps");
-				writer.cell("ArgDepth");
-				writer.cell("ArgNodes");
-				writer.cell("ArgNodesFeasible");
-				writer.cell("ArgNodesExpanded");
-				writer.cell("DiscreteStatesExpanded");
-				writer.newRow();
-				return;
-			} else {
-				helpFormatter.printHelp("theta-xta.jar", options, true);
-				return;
-			}
+	private void run() {
+		if (calledWithHeaderArg()) {
+			printHeader();
+			return;
 		}
-
-		final String modelOption = cmd.getOptionValue(MODEL.getOpt());
-		final XtaSystem system;
 		try {
-			final InputStream inputStream = new FileInputStream(modelOption);
-			system = XtaDslManager.createSystem(inputStream);
-		} catch (final Exception e) {
-			System.out.println("Path \"" + modelOption + "\" is invalid");
+			final JCommander cmd = JCommander.newBuilder().addObject(this).build();
+			cmd.setProgramName(JAR_NAME);
+			cmd.parse(args);
+		} catch (final ParameterException ex) {
+			System.out.println(ex.getMessage());
+			ex.usage();
 			return;
 		}
 
-		final String algorithmOption = cmd.getOptionValue(ALGORITHM.getOpt());
-		final Algorithm algorithm;
 		try {
-			algorithm = Enum.valueOf(Algorithm.class, algorithmOption);
-		} catch (final Exception e) {
-			System.out.println("\"" + algorithmOption + "\" is not a valid value for --algorithm");
-			return;
-		}
-
-		final String searchOption = cmd.getOptionValue(SEARCH.getOpt());
-		final Search search;
-		try {
-			search = Enum.valueOf(Search.class, searchOption);
-		} catch (final Exception e) {
-			System.out.println("\"" + searchOption + "\" is not a valid value for --strategy");
-			return;
-		}
-
-		final boolean benchmarkMode = cmd.hasOption(BENCHMARK.getOpt());
-
-		try {
-			final LazyXtaChecker.AlgorithmStrategy<?> algorithmStrategy = algorithm.create(system);
-			final SearchStrategy searchStrategy = search.create();
-
-			final SafetyChecker<?, ?, UnitPrec> checker =
-
-					LazyXtaChecker.create(system, algorithmStrategy, searchStrategy, l -> false);
-
+			final XtaSystem xta = loadModel();
+			final SafetyChecker<?, ?, UnitPrec> checker = buildChecker(xta);
 			final SafetyResult<?, ?> result = checker.check(UnitPrec.getInstance());
-			final LazyXtaStatistics stats = (LazyXtaStatistics) result.getStats().get();
-
-			if (benchmarkMode) {
-				writer.cell(result.isSafe());
-				writer.cell(stats.getAlgorithmTimeInMs());
-				writer.cell(stats.getRefinementTimeInMs());
-				writer.cell(stats.getInterpolationTimeInMs());
-				writer.cell(stats.getRefinementSteps());
-				writer.cell(stats.getArgDepth());
-				writer.cell(stats.getArgNodes());
-				writer.cell(stats.getArgNodesFeasible());
-				writer.cell(stats.getArgNodesExpanded());
-				writer.cell(stats.getDiscreteStatesExpanded());
-			} else {
-				System.out.println(stats.toString());
-			}
-
-		} catch (final Throwable e) {
-			final String message = e.getMessage() == null ? "" : ": " + e.getMessage();
-			if (benchmarkMode) {
-				writer.cell("[EX] " + e.getClass().getSimpleName() + message);
-			} else {
-				System.out.println("Exception occured: " + e.getClass().getSimpleName());
-				System.out.println("Message: " + e.getMessage());
-			}
+			printResult(result);
+		} catch (final Throwable ex) {
+			printError(ex);
 		}
-
 		if (benchmarkMode) {
 			writer.newRow();
 		}
 	}
 
-	private static String optionsFor(final Object[] objs) {
-		final StringJoiner sj = new StringJoiner("|");
-		for (final Object o : objs) {
-			sj.add(o.toString());
+	private boolean calledWithHeaderArg() {
+		return args.length == 1 && "--header".equals(args[0]);
+	}
+
+	private void printHeader() {
+		writer.cell("Result");
+		writer.cell("AlgorithmTimeInMs");
+		writer.cell("RefinementTimeInMs");
+		writer.cell("InterpolationTimeInMs");
+		writer.cell("RefinementSteps");
+		writer.cell("ArgDepth");
+		writer.cell("ArgNodes");
+		writer.cell("ArgNodesFeasible");
+		writer.cell("ArgNodesExpanded");
+		writer.cell("DiscreteStatesExpanded");
+		writer.newRow();
+	}
+
+	private XtaSystem loadModel() throws IOException {
+		final InputStream inputStream = new FileInputStream(model);
+		return XtaDslManager.createSystem(inputStream);
+	}
+
+	private SafetyChecker<?, ?, UnitPrec> buildChecker(final XtaSystem xta) {
+		final LazyXtaChecker.AlgorithmStrategy<?> algorithmStrategy = algorithm.create(xta);
+		final SearchStrategy searchStrategy = search.create();
+
+		final SafetyChecker<?, ?, UnitPrec> checker = LazyXtaChecker.create(xta, algorithmStrategy, searchStrategy,
+				l -> false);
+		return checker;
+	}
+
+	private void printResult(final SafetyResult<?, ?> result) {
+		final LazyXtaStatistics stats = (LazyXtaStatistics) result.getStats().get();
+
+		if (benchmarkMode) {
+			writer.cell(result.isSafe());
+			writer.cell(stats.getAlgorithmTimeInMs());
+			writer.cell(stats.getRefinementTimeInMs());
+			writer.cell(stats.getInterpolationTimeInMs());
+			writer.cell(stats.getRefinementSteps());
+			writer.cell(stats.getArgDepth());
+			writer.cell(stats.getArgNodes());
+			writer.cell(stats.getArgNodesFeasible());
+			writer.cell(stats.getArgNodesExpanded());
+			writer.cell(stats.getDiscreteStatesExpanded());
+		} else {
+			System.out.println(stats.toString());
 		}
-		return sj.toString();
+	}
+
+	private void printError(final Throwable ex) {
+		final String message = ex.getMessage() == null ? "" : ": " + ex.getMessage();
+		if (benchmarkMode) {
+			writer.cell("[EX] " + ex.getClass().getSimpleName() + message);
+		} else {
+			System.out.println("Exception occured: " + ex.getClass().getSimpleName());
+			System.out.println("Message: " + ex.getMessage());
+		}
 	}
 
 }
