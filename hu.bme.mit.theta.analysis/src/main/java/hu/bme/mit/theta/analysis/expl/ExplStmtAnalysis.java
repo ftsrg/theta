a0bf16aa962bdd7197182cc8d8acaa055fbb22f2
Hajdu Ákos <hajdua@mit.bme.hu>
2017-08-29 18:45:36 +0200

    Implemented explicit analysis for statements

---
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplStmtAnalysis.java
@@ -0,0 +1,47 @@
+package hu.bme.mit.theta.analysis.expl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import hu.bme.mit.theta.analysis.Analysis;
+import hu.bme.mit.theta.analysis.Domain;
+import hu.bme.mit.theta.analysis.InitFunction;
+import hu.bme.mit.theta.analysis.TransferFunction;
+import hu.bme.mit.theta.core.Expr;
+import hu.bme.mit.theta.core.type.booltype.BoolType;
+import hu.bme.mit.theta.solver.Solver;
+
+public class ExplStmtAnalysis implements Analysis<ExplState, StmtAction, ExplPrec> {
+
+	private final Domain<ExplState> domain;
+	private final InitFunction<ExplState, ExplPrec> initFunction;
+	private final TransferFunction<ExplState, StmtAction, ExplPrec> transferFunction;
+
+	private ExplStmtAnalysis(final Solver solver, final Expr<BoolType> initExpr, final int maxStatesFromSolver) {
+		checkNotNull(solver);
+		checkNotNull(initExpr);
+		this.domain = ExplDomain.getInstance();
+		this.initFunction = ExplInitFunction.create(solver, initExpr);
+		this.transferFunction = ExplStmtTransferFunction.create(solver, maxStatesFromSolver);
+	}
+
+	public static ExplStmtAnalysis create(final Solver solver, final Expr<BoolType> initExpr,
+			final int maxStatesFromSolver) {
+		return new ExplStmtAnalysis(solver, initExpr, maxStatesFromSolver);
+	}
+
+	@Override
+	public Domain<ExplState> getDomain() {
+		return domain;
+	}
+
+	@Override
+	public InitFunction<ExplState, ExplPrec> getInitFunction() {
+		return initFunction;
+	}
+
+	@Override
+	public TransferFunction<ExplState, StmtAction, ExplPrec> getTransferFunction() {
+		return transferFunction;
+	}
+
+}
--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/loc/LocAction.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/loc/LocAction.java
@@ -1,10 +1,10 @@
 package hu.bme.mit.theta.analysis.loc;
 
-import hu.bme.mit.theta.analysis.expr.ExprAction;
+import hu.bme.mit.theta.analysis.expl.StmtAction;
 import hu.bme.mit.theta.formalism.common.Edge;
 import hu.bme.mit.theta.formalism.common.Loc;
 
-public interface LocAction<L extends Loc<L, E>, E extends Edge<L, E>> extends ExprAction {
+public interface LocAction<L extends Loc<L, E>, E extends Edge<L, E>> extends StmtAction {
 
 	E getEdge();
 
