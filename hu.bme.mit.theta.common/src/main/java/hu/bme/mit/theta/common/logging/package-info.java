c593e56b08862e0f84a454435d7d321e0db02250
Hajdu Ákos <hajdua@mit.bme.hu>
2017-09-03 11:26:41 +0200

    Added package-info files to the common project

---
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/logging/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains utility classes for logging into files or console.
+ *
+ * @see hu.bme.mit.theta.common.logging.Logger
+ * @see hu.bme.mit.theta.common.logging.impl.ConsoleLogger
+ * @see hu.bme.mit.theta.common.logging.impl.FileLogger
+ */
+
+package hu.bme.mit.theta.common.logging;
---
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/visualization/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains a generic graph representation for visualization
+ * purposes. There are also implementations for writing the graphs in different
+ * formats.
+ *
+ * @see hu.bme.mit.theta.common.visualization.Graph
+ * @see hu.bme.mit.theta.common.visualization.writer.GraphWriter
+ */
+
+package hu.bme.mit.theta.common.visualization;
