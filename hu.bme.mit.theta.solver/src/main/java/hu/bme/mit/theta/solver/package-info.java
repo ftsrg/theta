54eadb6e2adf654ebaf8db34cd23bbb2e98f8cf6
Hajdu Ákos <hajdua@mit.bme.hu>
2017-09-04 13:36:56 +0200

    Updated docs for the solver projects

---
+++ hu.bme.mit.theta.solver/src/main/java/hu/bme/mit/theta/solver/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the generic interfaces for SMT solvers. Use a concrete
+ * implementation of {@link SolverFactory} to create a {@link Solver} or an
+ * {@link ItpSolver} for interpolation.
+ */
+
+package hu.bme.mit.theta.solver;
