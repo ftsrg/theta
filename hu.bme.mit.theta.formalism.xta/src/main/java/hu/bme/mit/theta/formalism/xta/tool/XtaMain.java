d674543a92c399079bf153558ed45844bd8d1d31
Hajdu Ákos <hajdua@mit.bme.hu>
2017-09-17 15:15:24 +0200

    Xta tool moved to the formalism.xta project

--- hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/tool/XtaMain.java
+++ hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/tool/XtaMain.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package hu.bme.mit.theta.tools.xta;
+package hu.bme.mit.theta.formalism.xta.tool;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
