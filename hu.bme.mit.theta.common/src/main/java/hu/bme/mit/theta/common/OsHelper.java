147ba96600cff8f661c5a4161e6cbd5f6795c85e
Hajdu Ákos <hajdua@mit.bme.hu>
2017-09-19 16:30:55 +0200

    Fixed some issues found by Codacy

--- hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/OsHelper.java
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/OsHelper.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,12 +31,13 @@ public final class OsHelper {
 
 		final String os = StandardSystemProperty.OS_NAME.value();
 
-		if (os.toLowerCase().startsWith("linux"))
+		if (os.toLowerCase().startsWith("linux")) {
 			return OperatingSystem.LINUX;
-		else if (os.toLowerCase().startsWith("windows"))
+		} else if (os.toLowerCase().startsWith("windows")) {
 			return OperatingSystem.WINDOWS;
-		else
+		} else {
 			throw new RuntimeException("Operating system \"" + os + "\" not supported.");
+		}
 	}
 
 	public static void main(final String[] args) {
