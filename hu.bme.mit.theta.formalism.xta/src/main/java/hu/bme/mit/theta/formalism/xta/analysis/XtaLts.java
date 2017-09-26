7de5f261c769b3631556e1b678b1652820a03fcf
Tamás Tóth <tothtamas28@gmail.com>
2017-09-26 14:08:44 +0200

    Add field system to class XtaAction

--- hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/analysis/XtaLts.java
+++ hu.bme.mit.theta.formalism.xta/src/main/java/hu/bme/mit/theta/formalism/xta/analysis/XtaLts.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,8 @@
  *  limitations under the License.
  */
 package hu.bme.mit.theta.formalism.xta.analysis;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,14 +28,18 @@ import hu.bme.mit.theta.formalism.xta.Label;
 import hu.bme.mit.theta.formalism.xta.Label.Kind;
 import hu.bme.mit.theta.formalism.xta.XtaProcess.Edge;
 import hu.bme.mit.theta.formalism.xta.XtaProcess.Loc;
+import hu.bme.mit.theta.formalism.xta.XtaSystem;
 
 public final class XtaLts implements LTS<XtaState<?>, XtaAction> {
 
-	private XtaLts() {
+	private final XtaSystem system;
+
+	private XtaLts(final XtaSystem system) {
+		this.system = checkNotNull(system);
 	}
 
-	public static XtaLts create() {
-		return new XtaLts();
+	public static XtaLts create(final XtaSystem system) {
+		return new XtaLts(system);
 	}
 
 	@Override
@@ -41,23 +47,23 @@ public final class XtaLts implements LTS<XtaState<?>, XtaAction> {
 		final Collection<XtaAction> result = new ArrayList<>();
 		for (final Loc loc : state.getLocs()) {
 			for (final Edge edge : loc.getOutEdges()) {
-				addActionsForEdge(result, state, edge);
+				addActionsForEdge(result, system, state, edge);
 			}
 		}
 		return result;
 	}
 
-	private static void addActionsForEdge(final Collection<XtaAction> result, final XtaState<?> state,
-			final Edge edge) {
+	private static void addActionsForEdge(final Collection<XtaAction> result, final XtaSystem system,
+			final XtaState<?> state, final Edge edge) {
 		if (edge.getLabel().isPresent()) {
-			addSyncActionsForEdge(result, state, edge);
+			addSyncActionsForEdge(result, system, state, edge);
 		} else {
-			addSimpleActionsForEdge(result, state, edge);
+			addSimpleActionsForEdge(result, system, state, edge);
 		}
 	}
 
-	private static void addSyncActionsForEdge(final Collection<XtaAction> result, final XtaState<?> state,
-			final Edge emitEdge) {
+	private static void addSyncActionsForEdge(final Collection<XtaAction> result, final XtaSystem system,
+			final XtaState<?> state, final Edge emitEdge) {
 
 		final Loc emitLoc = emitEdge.getSource();
 		final Label emitLabel = emitEdge.getLabel().get();
@@ -84,16 +90,16 @@ public final class XtaLts implements LTS<XtaState<?>, XtaAction> {
 
 				final Expr<?> receiveExpr = ExprUtils.simplify(receiveLabel.getExpr(), state.getVal());
 				if (emitExpr.equals(receiveExpr)) {
-					final XtaAction action = XtaAction.synced(state.getLocs(), emitExpr, emitEdge, recieveEdge);
+					final XtaAction action = XtaAction.synced(system, state.getLocs(), emitExpr, emitEdge, recieveEdge);
 					result.add(action);
 				}
 			}
 		}
 	}
 
-	private static void addSimpleActionsForEdge(final Collection<XtaAction> result, final XtaState<?> state,
-			final Edge edge) {
-		final XtaAction action = XtaAction.simple(state.getLocs(), edge);
+	private static void addSimpleActionsForEdge(final Collection<XtaAction> result, final XtaSystem system,
+			final XtaState<?> state, final Edge edge) {
+		final XtaAction action = XtaAction.simple(system, state.getLocs(), edge);
 		result.add(action);
 	}
 
