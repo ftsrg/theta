077261c59ed84e827cd042035762308df70aa875
Tamás Tóth <tothtamas28@gmail.com>
2017-09-26 15:29:00 +0200

    Implement ExprAction interface for StmtAction

--- hu.bme.mit.theta.formalism.cfa/src/main/java/hu/bme/mit/theta/formalism/cfa/analysis/CfaAction.java
+++ hu.bme.mit.theta.formalism.cfa/src/main/java/hu/bme/mit/theta/formalism/cfa/analysis/CfaAction.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,34 +16,20 @@
 package hu.bme.mit.theta.formalism.cfa.analysis;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.And;
-import static hu.bme.mit.theta.core.utils.VarIndexing.all;
 
 import java.util.List;
 
 import hu.bme.mit.theta.analysis.expl.StmtAction;
 import hu.bme.mit.theta.common.Utils;
 import hu.bme.mit.theta.core.stmt.Stmt;
-import hu.bme.mit.theta.core.type.Expr;
-import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.utils.StmtUnfoldResult;
-import hu.bme.mit.theta.core.utils.StmtUtils;
-import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.formalism.cfa.CFA.Edge;
 
-public final class CfaAction implements StmtAction {
+public final class CfaAction extends StmtAction {
 
 	private final Edge edge;
-	private final Expr<BoolType> expr;
-	private final VarIndexing nextIndexing;
 
 	private CfaAction(final Edge edge) {
 		this.edge = checkNotNull(edge);
-
-		// TODO: do the following stuff lazily
-		final StmtUnfoldResult toExprResult = StmtUtils.toExpr(edge.getStmts(), all(0));
-		expr = And(toExprResult.getExprs());
-		nextIndexing = toExprResult.getIndexing();
 	}
 
 	public static CfaAction create(final Edge edge) {
@@ -52,16 +38,6 @@ public final class CfaAction implements StmtAction {
 
 	public Edge getEdge() {
 		return edge;
-	}
-
-	@Override
-	public Expr<BoolType> toExpr() {
-		return expr;
-	}
-
-	@Override
-	public VarIndexing nextIndexing() {
-		return nextIndexing;
 	}
 
 	@Override
