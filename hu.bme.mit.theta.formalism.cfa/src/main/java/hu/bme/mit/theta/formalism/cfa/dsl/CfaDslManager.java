4b7a4182af53b102803cf529f567c94efeab1915
Hajdu Ákos <hajdua@mit.bme.hu>
2017-10-12 12:12:23 +0200

    Added methods to parse CFAs and STSs from strings

--- hu.bme.mit.theta.formalism.cfa/src/main/java/hu/bme/mit/theta/formalism/cfa/dsl/CfaDslManager.java
+++ hu.bme.mit.theta.formalism.cfa/src/main/java/hu/bme/mit/theta/formalism/cfa/dsl/CfaDslManager.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,10 @@
  */
 package hu.bme.mit.theta.formalism.cfa.dsl;
 
-import java.io.FileNotFoundException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -32,7 +33,12 @@ public final class CfaDslManager {
 	private CfaDslManager() {
 	}
 
-	public static CFA createCfa(final InputStream inputStream) throws FileNotFoundException, IOException {
+	public static CFA createCfa(final String inputString) throws IOException {
+		final InputStream stream = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8.name()));
+		return createCfa(stream);
+	}
+
+	public static CFA createCfa(final InputStream inputStream) throws IOException {
 		final ANTLRInputStream input = new ANTLRInputStream(inputStream);
 
 		final CfaDslLexer lexer = new CfaDslLexer(input);
