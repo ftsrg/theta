3c01d132387497485e406a274538f44b0d6330ed
Tamás Tóth <tothtamas28@gmail.com>
2017-10-20 13:41:13 +0200

    Add private constructor to class ExplState

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplState.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/expl/ExplState.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,9 @@ import hu.bme.mit.theta.core.type.booltype.BoolExprs;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 
 public abstract class ExplState implements ExprState, Valuation {
+
+	private ExplState() {
+	}
 
 	public static ExplState create(final Valuation values) {
 		if (values.getDecls().isEmpty()) {
