b5b10ba077557597dc13cd6e5d19578d4be165a8
Tamás Tóth <tothtamas28@gmail.com>
2017-10-31 18:07:56 +0100

    Fix method toString() in class AbstractModel

--- hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/AbstractModel.java
+++ hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/AbstractModel.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,7 +40,7 @@ public abstract class AbstractModel implements Model {
 
 	@Override
 	public final String toString() {
-		return Utils.toStringBuilder("Model").addAll(getDecls(), d -> d.getName() + " <- " + eval(d)).toString();
+		return Utils.toStringBuilder("Model").addAll(getDecls(), d -> d.getName() + " <- " + eval(d).get()).toString();
 	}
 
 }
