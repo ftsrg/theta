ff255dbc4b761804a8ebd561724e75b2eea5c00b
Tamás Tóth <tothtamas28@gmail.com>
2017-11-07 11:45:59 +0100

    Move method of(p1, p2, p3) from ProdPrec to Prod3Prec

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/prod/Prod3Prec.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/prod/Prod3Prec.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,8 +23,13 @@ import hu.bme.mit.theta.common.product.Product3;
 public final class Prod3Prec<P1 extends Prec, P2 extends Prec, P3 extends Prec> extends ProdPrec
 		implements Product3<P1, P2, P3> {
 
-	Prod3Prec(final P1 prec1, final P2 prec2, final P3 prec3) {
+	private Prod3Prec(final P1 prec1, final P2 prec2, final P3 prec3) {
 		super(ImmutableList.of(prec1, prec2, prec3));
+	}
+
+	public static <P1 extends Prec, P2 extends Prec, P3 extends Prec> Prod3Prec<P1, P2, P3> of(final P1 prec1,
+			final P2 prec2, final P3 prec3) {
+		return new Prod3Prec<>(prec1, prec2, prec3);
 	}
 
 	@Override
