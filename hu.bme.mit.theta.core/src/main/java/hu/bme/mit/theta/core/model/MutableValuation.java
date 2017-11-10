e630bc2ec43fd5e4aa93f650f5bea285a21d83b8
Tamás Tóth <tothtamas28@gmail.com>
2017-11-10 13:55:05 +0100

    Refactor MutableValuation

--- hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/MutableValuation.java
+++ hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/MutableValuation.java
@@ -17,17 +17,13 @@ package hu.bme.mit.theta.core.model;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static hu.bme.mit.theta.core.type.abstracttype.AbstractExprs.Eq;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.And;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.True;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 
 import hu.bme.mit.theta.core.decl.Decl;
@@ -35,6 +31,7 @@ import hu.bme.mit.theta.core.type.Expr;
 import hu.bme.mit.theta.core.type.LitExpr;
 import hu.bme.mit.theta.core.type.Type;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
+import hu.bme.mit.theta.core.type.booltype.SmartBoolExprs;
 
 /**
  * Mutable implementation of a valuation.
@@ -68,34 +65,22 @@ public final class MutableValuation extends Valuation {
 	}
 
 	@Override
-	public Collection<? extends Decl<?>> getDecls() {
+	public Collection<Decl<?>> getDecls() {
 		return Collections.unmodifiableSet(declToExpr.keySet());
 	}
 
 	@Override
 	public <DeclType extends Type> Optional<LitExpr<DeclType>> eval(final Decl<DeclType> decl) {
 		checkNotNull(decl);
-		if (declToExpr.containsKey(decl)) {
-			@SuppressWarnings("unchecked")
-			final LitExpr<DeclType> val = (LitExpr<DeclType>) declToExpr.get(decl);
-			return Optional.of(val);
-		}
-		return Optional.empty();
+		@SuppressWarnings("unchecked")
+		final LitExpr<DeclType> val = (LitExpr<DeclType>) declToExpr.get(decl);
+		return Optional.ofNullable(val);
 	}
 
 	@Override
 	public Expr<BoolType> toExpr() {
-		final List<Expr<BoolType>> ops = new ArrayList<>(declToExpr.size());
-		for (final Entry<Decl<?>, LitExpr<?>> entry : declToExpr.entrySet()) {
-			ops.add(Eq(entry.getKey().getRef(), entry.getValue()));
-		}
-		if (ops.isEmpty()) {
-			return True();
-		} else if (ops.size() == 1) {
-			return ops.get(0);
-		} else {
-			return And(ops);
-		}
+		return SmartBoolExprs.And(declToExpr.entrySet().stream().map(e -> Eq(e.getKey().getRef(), e.getValue()))
+				.collect(toImmutableList()));
 	}
 
 	@Override
