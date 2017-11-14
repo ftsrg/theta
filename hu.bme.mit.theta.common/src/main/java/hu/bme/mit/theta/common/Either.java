4378b99eac8369600187e9455e1da2573c61f724
Tamás Tóth <tothtamas28@gmail.com>
2017-11-14 13:36:29 +0100

    Modify method toString() in class Either

--- hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/Either.java
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/Either.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,7 +98,7 @@ public abstract class Either<L, R> {
 
 		@Override
 		public String toString() {
-			return "Left(" + left + ")";
+			return Utils.lispStringBuilder("left").add(left).toString();
 		}
 
 	}
@@ -159,7 +159,7 @@ public abstract class Either<L, R> {
 
 		@Override
 		public String toString() {
-			return "Right(" + right + ")";
+			return Utils.lispStringBuilder("right").add(right).toString();
 		}
 
 	}
