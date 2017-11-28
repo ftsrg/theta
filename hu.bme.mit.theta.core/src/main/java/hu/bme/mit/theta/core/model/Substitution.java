8b50f40a522dfe43bbb7fd32e2664cb10943180e
Tamás Tóth <tothtamas28@gmail.com>
2017-11-28 11:26:07 +0100

    Add default method apply() to interface Substitution

--- hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/Substitution.java
+++ hu.bme.mit.theta.core/src/main/java/hu/bme/mit/theta/core/model/Substitution.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ import java.util.Optional;
 import hu.bme.mit.theta.core.decl.Decl;
 import hu.bme.mit.theta.core.type.Expr;
 import hu.bme.mit.theta.core.type.Type;
+import hu.bme.mit.theta.core.type.anytype.RefExpr;
 
 /**
  * Interface for a substitution, which is a mapping from declarations to
@@ -31,4 +32,22 @@ public interface Substitution {
 	Collection<? extends Decl<?>> getDecls();
 
 	<DeclType extends Type> Optional<? extends Expr<DeclType>> eval(final Decl<DeclType> decl);
+
+	default <T extends Type> Expr<T> apply(final Expr<T> expr) {
+		return expr.map(e -> {
+			if (e instanceof RefExpr) {
+				final RefExpr<?> ref = (RefExpr<?>) e;
+				final Decl<?> decl = ref.getDecl();
+				final Optional<? extends Expr<?>> optSub = eval(decl);
+				if (optSub.isPresent()) {
+					final Expr<?> sub = optSub.get();
+					return sub;
+				} else {
+					return e;
+				}
+			} else {
+				return e;
+			}
+		});
+	}
 }
