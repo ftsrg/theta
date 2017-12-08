f9e64a448c0aa79ed424b5fee4d8260328600baa
Tamás Tóth <tothtamas28@gmail.com>
2017-12-08 14:11:37 +0100

    Add methods aligned() and body() to class LispStringBuilder

--- hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/LispStringBuilder.java
+++ hu.bme.mit.theta.common/src/main/java/hu/bme/mit/theta/common/LispStringBuilder.java
@@ -16,49 +16,148 @@
 package hu.bme.mit.theta.common;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.util.Arrays.asList;
+import static com.google.common.base.Preconditions.checkState;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
-import java.util.function.Function;
+import java.util.stream.Stream;
+
+import com.google.common.base.Strings;
 
 /**
  * Utility class for printing Lisp style strings, e.g., (A (B 1 2) (C 3)).
  */
 public final class LispStringBuilder {
+	private static final String LPAREN = "(";
+	private static final String RPAREN = ")";
+	private static final String SPACE = " ";
+	private static final String EOL = System.getProperty("line.separator");
 
-	private final StringJoiner joiner;
+	private final List<Object> headObjs;
+	private final List<Object> alignedObjs;
+	private final List<Object> bodyObjs;
+
+	private State state;
+
+	private static enum State {
+		HEAD, ALIGNED, BODY, BUILT;
+	}
 
 	LispStringBuilder(final String prefix) {
 		checkNotNull(prefix);
-		joiner = new StringJoiner(" ", "(", ")");
+		headObjs = new ArrayList<>();
+		alignedObjs = new ArrayList<>();
+		bodyObjs = new ArrayList<>();
+
 		if (!"".equals(prefix)) {
-			joiner.add(prefix);
+			headObjs.add(prefix);
 		}
+
+		state = State.HEAD;
 	}
 
 	public LispStringBuilder add(final Object object) {
-		joiner.add(object.toString());
+		checkState(state != State.BUILT);
+		final List<Object> section = sectionFor(state);
+		section.add(object);
 		return this;
 	}
 
-	public LispStringBuilder addAll(final Iterable<? extends Object> objects) {
-		objects.forEach(o -> joiner.add(o.toString()));
+	public LispStringBuilder addAll(final Iterable<?> iterable) {
+		checkState(state != State.BUILT);
+		iterable.forEach(this::add);
 		return this;
 	}
 
-	public <T> LispStringBuilder addAll(final Iterable<? extends T> objects, final Function<T, String> toStringFunc) {
-		objects.forEach(o -> joiner.add(toStringFunc.apply(o)));
+	public LispStringBuilder addAll(final Stream<?> stream) {
+		checkState(state != State.BUILT);
+		stream.forEach(this::add);
 		return this;
 	}
 
-	public LispStringBuilder addAll(final Object... objects) {
-		addAll(asList(objects));
+	public LispStringBuilder aligned() {
+		checkState(state == State.HEAD);
+		state = State.ALIGNED;
+		return this;
+	}
+
+	public LispStringBuilder body() {
+		checkState(state == State.HEAD || state == State.ALIGNED);
+		state = State.BODY;
 		return this;
 	}
 
 	@Override
 	public String toString() {
-		return joiner.toString();
+		checkState(state != State.BUILT);
+
+		final StringBuilder sb = new StringBuilder();
+		sb.append(LPAREN);
+
+		final String head = createHead();
+		sb.append(head);
+
+		final int indent = head.length() + 1;
+		final String aligned = createAligned(indent);
+
+		sb.append(aligned);
+
+		final String body = createBody();
+		sb.append(body);
+
+		sb.append(RPAREN);
+		return sb.toString();
+	}
+
+	////
+
+	private List<Object> sectionFor(final State state) {
+		switch (state) {
+		case HEAD:
+			return headObjs;
+		case ALIGNED:
+			return alignedObjs;
+		case BODY:
+			return bodyObjs;
+		default:
+			throw new AssertionError("Unhandled state: " + state);
+		}
+	}
+
+	private String createHead() {
+		final StringJoiner sj = new StringJoiner(SPACE);
+		for (final Object headObj : headObjs) {
+			for (final String line : lines(headObj.toString())) {
+				final String trimmedLine = line.trim().replaceAll("( )+", SPACE);
+				sj.add(trimmedLine);
+			}
+		}
+		return sj.toString();
+	}
+
+	private String createAligned(final int indent) {
+		final StringJoiner sj = new StringJoiner(EOL + Strings.repeat(SPACE, indent));
+		for (final Object alignedObj : alignedObjs) {
+			for (final String line : lines(alignedObj.toString())) {
+				sj.add(SPACE + line);
+			}
+		}
+		return sj.toString();
+	}
+
+	private String createBody() {
+		final StringBuilder sb = new StringBuilder();
+		for (final Object bodyObj : bodyObjs) {
+			for (final String line : lines(bodyObj.toString())) {
+				sb.append(EOL + SPACE + SPACE + line);
+			}
+		}
+		return sb.toString();
+	}
+
+	private static String[] lines(final String string) {
+		return string.split("\\r\\n|\\n|\\r");
 	}
 
 }
