0ffea5b873159a82e18eb06ac44b52ab9294c758
Hajdu Ákos <hajdua@mit.bme.hu>
2018-01-04 11:35:09 +0100

    Extract generating PredStates into utility class

--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredInitFunc.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredInitFunc.java
@@ -16,45 +16,23 @@
 package hu.bme.mit.theta.analysis.pred;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.And;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Iff;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Not;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.True;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Set;
 
 import hu.bme.mit.theta.analysis.InitFunc;
-import hu.bme.mit.theta.core.decl.ConstDecl;
-import hu.bme.mit.theta.core.decl.Decls;
-import hu.bme.mit.theta.core.model.Valuation;
 import hu.bme.mit.theta.core.type.Expr;
-import hu.bme.mit.theta.core.type.LitExpr;
-import hu.bme.mit.theta.core.type.booltype.BoolExprs;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.solver.Solver;
-import hu.bme.mit.theta.solver.utils.WithPushPop;
 
 public final class PredInitFunc implements InitFunc<PredState, PredPrec> {
 
-	private static final String LIT_PREFIX = "__init_";
-	private final Solver solver;
 	private final Expr<BoolType> initExpr;
-	private final List<ConstDecl<BoolType>> actLits;
+	private final PredStates predStates;
 
 	private PredInitFunc(final Solver solver, final Expr<BoolType> initExpr) {
-		this.solver = checkNotNull(solver);
 		this.initExpr = checkNotNull(initExpr);
-		this.actLits = new ArrayList<>();
+		this.predStates = new PredStates(checkNotNull(solver));
 	}
 
 	public static PredInitFunc create(final Solver solver, final Expr<BoolType> expr) {
@@ -64,48 +42,7 @@ public final class PredInitFunc implements InitFunc<PredState, PredPrec> {
 	@Override
 	public Collection<? extends PredState> getInitStates(final PredPrec prec) {
 		checkNotNull(prec);
-
-		final Set<Expr<BoolType>> preds = prec.getPreds();
-		while (actLits.size() < preds.size()) {
-			actLits.add(Decls.Const(LIT_PREFIX + actLits.size(), BoolExprs.Bool()));
-		}
-
-		assert actLits.size() >= preds.size();
-
-		final List<PredState> initStates = new LinkedList<>();
-		try (WithPushPop wp = new WithPushPop(solver)) {
-			solver.add(PathUtils.unfold(initExpr, 0));
-			final Map<ConstDecl<BoolType>, Expr<BoolType>> litToPred = new HashMap<>();
-			int i = 0;
-			for (final Expr<BoolType> pred : preds) {
-				final ConstDecl<BoolType> actLit = actLits.get(i);
-				litToPred.put(actLit, pred);
-				++i;
-				solver.add(Iff(actLit.getRef(), PathUtils.unfold(pred, 0)));
-			}
-			while (solver.check().isSat()) {
-				final Valuation model = solver.getModel();
-				final Set<Expr<BoolType>> initStatePreds = new HashSet<>();
-				final List<Expr<BoolType>> feedBack = new LinkedList<>();
-				feedBack.add(True());
-				for (final Entry<ConstDecl<BoolType>, Expr<BoolType>> entry : litToPred.entrySet()) {
-					final Optional<LitExpr<BoolType>> eval = model.eval(entry.getKey());
-					if (eval.isPresent()) {
-						if (eval.get().equals(True())) {
-							initStatePreds.add(entry.getValue());
-							feedBack.add(entry.getKey().getRef());
-						} else {
-							initStatePreds.add(prec.negate(entry.getValue()));
-							feedBack.add(Not(entry.getKey().getRef()));
-						}
-					}
-				}
-				initStates.add(PredState.of(initStatePreds));
-				solver.add(Not(And(feedBack)));
-			}
-		}
-
-		return initStates;
+		return predStates.createStatesForExpr(initExpr, VarIndexing.all(0), prec, VarIndexing.all(0));
 	}
 
 }
--- hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredTransFunc.java
+++ hu.bme.mit.theta.analysis/src/main/java/hu/bme/mit/theta/analysis/pred/PredTransFunc.java
@@ -17,43 +17,20 @@ package hu.bme.mit.theta.analysis.pred;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static hu.bme.mit.theta.core.type.booltype.BoolExprs.And;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Iff;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Not;
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.True;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Set;
 
 import hu.bme.mit.theta.analysis.TransFunc;
 import hu.bme.mit.theta.analysis.expr.ExprAction;
-import hu.bme.mit.theta.core.decl.ConstDecl;
-import hu.bme.mit.theta.core.decl.Decls;
-import hu.bme.mit.theta.core.model.Valuation;
-import hu.bme.mit.theta.core.type.Expr;
-import hu.bme.mit.theta.core.type.LitExpr;
-import hu.bme.mit.theta.core.type.booltype.BoolExprs;
-import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.core.utils.VarIndexing;
 import hu.bme.mit.theta.solver.Solver;
-import hu.bme.mit.theta.solver.utils.WithPushPop;
 
 public final class PredTransFunc implements TransFunc<PredState, ExprAction, PredPrec> {
 
-	private static final String LIT_PREFIX = "__trans_";
-	private final Solver solver;
-	private final List<ConstDecl<BoolType>> actLits;
+	private final PredStates predStates;
 
 	private PredTransFunc(final Solver solver) {
-		this.solver = checkNotNull(solver);
-		this.actLits = new ArrayList<>();
+		this.predStates = new PredStates(checkNotNull(solver));
 	}
 
 	public static PredTransFunc create(final Solver solver) {
@@ -67,48 +44,8 @@ public final class PredTransFunc implements TransFunc<PredState, ExprAction, Pre
 		checkNotNull(action);
 		checkNotNull(prec);
 
-		final Set<Expr<BoolType>> preds = prec.getPreds();
-		while (actLits.size() < preds.size()) {
-			actLits.add(Decls.Const(LIT_PREFIX + actLits.size(), BoolExprs.Bool()));
-		}
-
-		assert actLits.size() >= preds.size();
-
-		final List<PredState> succStates = new LinkedList<>();
-		try (WithPushPop wp = new WithPushPop(solver)) {
-			solver.add(PathUtils.unfold(state.toExpr(), 0));
-			solver.add(PathUtils.unfold(action.toExpr(), 0));
-			final Map<ConstDecl<BoolType>, Expr<BoolType>> litToPred = new HashMap<>();
-			int i = 0;
-			for (final Expr<BoolType> pred : preds) {
-				final ConstDecl<BoolType> actLit = actLits.get(i);
-				litToPred.put(actLit, pred);
-				++i;
-				solver.add(Iff(actLit.getRef(), PathUtils.unfold(pred, action.nextIndexing())));
-			}
-			while (solver.check().isSat()) {
-				final Valuation model = solver.getModel();
-				final Set<Expr<BoolType>> succStatePreds = new HashSet<>();
-				final List<Expr<BoolType>> feedBack = new LinkedList<>();
-				feedBack.add(True());
-				for (final Entry<ConstDecl<BoolType>, Expr<BoolType>> entry : litToPred.entrySet()) {
-					final Optional<LitExpr<BoolType>> eval = model.eval(entry.getKey());
-					if (eval.isPresent()) {
-						if (eval.get().equals(True())) {
-							succStatePreds.add(entry.getValue());
-							feedBack.add(entry.getKey().getRef());
-						} else {
-							succStatePreds.add(prec.negate(entry.getValue()));
-							feedBack.add(Not(entry.getKey().getRef()));
-						}
-					}
-				}
-				succStates.add(PredState.of(succStatePreds));
-				solver.add(Not(And(feedBack)));
-			}
-		}
-
-		return succStates;
+		return predStates.createStatesForExpr(And(state.toExpr(), action.toExpr()), VarIndexing.all(0), prec,
+				action.nextIndexing());
 	}
 
 }
