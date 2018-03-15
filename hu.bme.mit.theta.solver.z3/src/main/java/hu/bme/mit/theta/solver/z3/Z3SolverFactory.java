3eb61458169bd5508087583dd59c21cdb93e399e
Tamás Tóth <totht@mit.bme.hu>
2018-03-15 19:22:09 +0100

    Remove explicit loading of libz3 in class Z3SolverFactory

--- hu.bme.mit.theta.solver.z3/src/main/java/hu/bme/mit/theta/solver/z3/Z3SolverFactory.java
+++ hu.bme.mit.theta.solver.z3/src/main/java/hu/bme/mit/theta/solver/z3/Z3SolverFactory.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Budapest University of Technology and Economics
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,11 +44,9 @@ public final class Z3SolverFactory implements SolverFactory {
 	private static void loadLibraries() {
 		switch (OsHelper.getOs()) {
 		case WINDOWS:
-			System.loadLibrary("libz3");
 			System.loadLibrary("libz3java");
 			break;
 		case LINUX:
-			System.loadLibrary("z3");
 			System.loadLibrary("z3java");
 			break;
 		default:
