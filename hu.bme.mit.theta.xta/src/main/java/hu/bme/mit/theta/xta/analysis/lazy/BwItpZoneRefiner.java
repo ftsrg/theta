220affc3be1c1c176c4ab2ab997e6209767469ab
Tamás Tóth <totht@mit.bme.hu>
2018-03-19 17:10:18 +0100

    Change visibility for implementations of ItpZoneRefiner to default

--- hu.bme.mit.theta.xta/src/main/java/hu/bme/mit/theta/xta/analysis/lazy/BwItpZoneRefiner.java
+++ hu.bme.mit.theta.xta/src/main/java/hu/bme/mit/theta/xta/analysis/lazy/BwItpZoneRefiner.java
@@ -27,7 +27,7 @@ import hu.bme.mit.theta.xta.analysis.XtaAction;
 import hu.bme.mit.theta.xta.analysis.XtaState;
 import hu.bme.mit.theta.xta.analysis.zone.itp.ItpZoneState;
 
-public final class BwItpZoneRefiner extends ItpZoneRefiner {
+final class BwItpZoneRefiner extends ItpZoneRefiner {
 
 	public BwItpZoneRefiner(final XtaSystem system) {
 		super(system);
