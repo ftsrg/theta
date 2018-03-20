12259fba16a5d033d6ff3f6653747a1af0d6acdb
Tamás Tóth <totht@mit.bme.hu>
2018-03-20 14:57:30 +0100

    Add method post() to class XtaExplUtils

--- hu.bme.mit.theta.xta/src/main/java/hu/bme/mit/theta/xta/analysis/expl/XtaExplUtils.java
+++ hu.bme.mit.theta.xta/src/main/java/hu/bme/mit/theta/xta/analysis/expl/XtaExplUtils.java
@@ -29,17 +29,21 @@ import java.util.stream.Stream;
 
 import com.google.common.collect.Lists;
 
+import hu.bme.mit.theta.analysis.expl.ExplState;
 import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.model.MutableValuation;
 import hu.bme.mit.theta.core.model.Valuation;
+import hu.bme.mit.theta.core.stmt.AssignStmt;
 import hu.bme.mit.theta.core.type.Expr;
 import hu.bme.mit.theta.core.type.LitExpr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
+import hu.bme.mit.theta.core.type.booltype.FalseExpr;
 import hu.bme.mit.theta.core.utils.ExprUtils;
 import hu.bme.mit.theta.core.utils.WpState;
 import hu.bme.mit.theta.xta.Guard;
 import hu.bme.mit.theta.xta.Update;
 import hu.bme.mit.theta.xta.XtaProcess.Edge;
+import hu.bme.mit.theta.xta.XtaProcess.Loc;
 import hu.bme.mit.theta.xta.analysis.XtaAction;
 import hu.bme.mit.theta.xta.analysis.XtaAction.BasicXtaAction;
 import hu.bme.mit.theta.xta.analysis.XtaAction.SyncedXtaAction;
@@ -72,6 +76,119 @@ public final class XtaExplUtils {
 
 		return valI;
 	}
+
+	////
+
+	public static ExplState post(final Valuation val, final XtaAction action) {
+		checkNotNull(val);
+		checkNotNull(action);
+
+		if (action.isBasic()) {
+			return postForBasicAction(val, action.asBasic());
+		} else if (action.isSynced()) {
+			return postForSyncedAction(val, action.asSynced());
+		} else {
+			throw new AssertionError();
+		}
+	}
+
+	private static ExplState postForBasicAction(final Valuation val, final BasicXtaAction action) {
+		final Edge edge = action.getEdge();
+		final List<Loc> targetLocs = action.getTargetLocs();
+
+		if (!checkDataGuards(edge, val)) {
+			return ExplState.bottom();
+		}
+
+		final MutableValuation succVal = MutableValuation.copyOf(val);
+		applyDataUpdates(action.getEdge(), succVal);
+
+		if (!checkDataInvariants(targetLocs, succVal)) {
+			return ExplState.bottom();
+		}
+
+		return ExplState.of(succVal);
+	}
+
+	private static ExplState postForSyncedAction(final Valuation val, final SyncedXtaAction action) {
+		final Edge emitEdge = action.getEmitEdge();
+		final Edge recvEdge = action.getRecvEdge();
+		final List<Loc> targetLocs = action.getTargetLocs();
+
+		if (!checkSync(emitEdge, recvEdge, val)) {
+			return ExplState.bottom();
+		}
+
+		if (!checkDataGuards(emitEdge, val)) {
+			return ExplState.bottom();
+		}
+
+		if (!checkDataGuards(recvEdge, val)) {
+			return ExplState.bottom();
+		}
+
+		final MutableValuation succVal = MutableValuation.copyOf(val);
+		applyDataUpdates(emitEdge, succVal);
+		applyDataUpdates(recvEdge, succVal);
+
+		if (!checkDataInvariants(targetLocs, succVal)) {
+			return ExplState.bottom();
+		}
+
+		return ExplState.of(succVal);
+	}
+
+	private static boolean checkSync(final Edge emitEdge, final Edge recvEdge, final Valuation val) {
+		final List<Expr<?>> emitArgs = emitEdge.getSync().get().getArgs();
+		final List<Expr<?>> recvArgs = recvEdge.getSync().get().getArgs();
+		return zip(emitArgs.stream(), recvArgs.stream(), (e, r) -> e.eval(val).equals(r.eval(val))).allMatch(x -> x);
+	}
+
+	private static boolean checkDataGuards(final Edge edge, final Valuation val) {
+		for (final Guard guard : edge.getGuards()) {
+			if (guard.isDataGuard()) {
+				final Expr<BoolType> expr = ExprUtils.simplify(guard.asDataGuard().toExpr(), val);
+				if (expr instanceof FalseExpr) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static boolean checkDataInvariants(final List<Loc> locs, final Valuation val) {
+		for (final Loc loc : locs) {
+			final Collection<Guard> invars = loc.getInvars();
+			for (final Guard invar : invars) {
+				if (invar.isDataGuard()) {
+					final Expr<BoolType> expr = ExprUtils.simplify(invar.asDataGuard().toExpr(), val);
+					if (expr instanceof FalseExpr) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	private static void applyDataUpdates(final Edge edge, final MutableValuation val) {
+		final List<Update> updates = edge.getUpdates();
+		for (final Update update : updates) {
+			if (update.isDataUpdate()) {
+				final AssignStmt<?> stmt = (AssignStmt<?>) update.toStmt();
+				final VarDecl<?> varDecl = stmt.getVarDecl();
+				final Expr<?> expr = ExprUtils.simplify(stmt.getExpr(), val);
+				if (expr instanceof LitExpr) {
+					final LitExpr<?> value = (LitExpr<?>) expr;
+					val.put(varDecl, value);
+				} else {
+					val.remove(varDecl);
+				}
+			}
+		}
+	}
+
+	////
 
 	public static Expr<BoolType> pre(final Expr<BoolType> expr, final XtaAction action) {
 		checkNotNull(expr);
