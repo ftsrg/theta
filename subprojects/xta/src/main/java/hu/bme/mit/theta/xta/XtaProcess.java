6e659eeedbf4c95ae85953523121b12b7d9970a7
Tamás Tóth <totht@mit.bme.hu>
2019-08-02 17:03:07 +0200

    Add support for initial values of variables in XtaSystem

--- subprojects/xta/src/main/java/hu/bme/mit/theta/xta/XtaProcess.java
+++ subprojects/xta/src/main/java/hu/bme/mit/theta/xta/XtaProcess.java
@@ -29,263 +29,254 @@ import com.google.common.collect.ImmutableList;
 
 import hu.bme.mit.theta.common.Utils;
 import hu.bme.mit.theta.core.decl.VarDecl;
+import hu.bme.mit.theta.core.model.MutableValuation;
+import hu.bme.mit.theta.core.model.Valuation;
 import hu.bme.mit.theta.core.stmt.Stmt;
 import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.LitExpr;
+import hu.bme.mit.theta.core.type.Type;
+
+import static hu.bme.mit.theta.core.type.booltype.BoolExprs.False;
+
 import hu.bme.mit.theta.core.type.booltype.BoolType;
+
+import static hu.bme.mit.theta.core.type.inttype.IntExprs.Int;
+
+import hu.bme.mit.theta.core.type.inttype.IntType;
 import hu.bme.mit.theta.core.type.rattype.RatType;
 import hu.bme.mit.theta.core.utils.ExprUtils;
 import hu.bme.mit.theta.core.utils.StmtUtils;
 
+import static hu.bme.mit.theta.core.utils.TypeUtils.cast;
+
 public final class XtaProcess {
-	private final String name;
-	private final Collection<VarDecl<?>> dataVars;
-	private final Collection<VarDecl<RatType>> clockVars;
-	private final Collection<Loc> locs;
-	private final Collection<Edge> edges;
-	private Loc initLoc;
+    private final String name;
+    private final XtaSystem system;
+    private final Collection<Loc> locs;
+    private final Collection<Edge> edges;
+    private Loc initLoc;
 
-	private final Collection<VarDecl<?>> unmodDataVars;
-	private final Collection<VarDecl<RatType>> unmodClockVars;
-	private final Collection<Loc> unmodLocs;
-	private final Collection<Edge> unmodEdges;
+    private final Collection<Loc> unmodLocs;
+    private final Collection<Edge> unmodEdges;
 
-	////
+    ////
 
-	private XtaProcess(final String name) {
-		this.name = checkNotNull(name);
-		dataVars = new HashSet<>();
-		clockVars = new HashSet<>();
-		locs = new HashSet<>();
-		edges = new ArrayList<>();
+    private XtaProcess(final XtaSystem system, final String name) {
+        this.system = checkNotNull(system);
+        this.name = checkNotNull(name);
+        locs = new HashSet<>();
+        edges = new ArrayList<>();
 
-		unmodDataVars = Collections.unmodifiableCollection(dataVars);
-		unmodClockVars = Collections.unmodifiableCollection(clockVars);
-		unmodLocs = Collections.unmodifiableCollection(locs);
-		unmodEdges = Collections.unmodifiableCollection(edges);
-	}
+        unmodLocs = Collections.unmodifiableCollection(locs);
+        unmodEdges = Collections.unmodifiableCollection(edges);
+    }
 
-	public static XtaProcess create(final String name) {
-		return new XtaProcess(name);
-	}
+    static XtaProcess create(final XtaSystem system, final String name) {
+        return new XtaProcess(system, name);
+    }
 
-	////
+    ////
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Collection<VarDecl<?>> getDataVars() {
-		return unmodDataVars;
-	}
+    public XtaSystem getSystem() {
+        return system;
+    }
 
-	public Collection<VarDecl<RatType>> getClockVars() {
-		return unmodClockVars;
-	}
+    public Collection<Loc> getLocs() {
+        return unmodLocs;
+    }
 
-	public Collection<Loc> getLocs() {
-		return unmodLocs;
-	}
+    public Collection<Edge> getEdges() {
+        return unmodEdges;
+    }
 
-	public Collection<Edge> getEdges() {
-		return unmodEdges;
-	}
+    public Loc getInitLoc() {
+        checkNotNull(initLoc);
+        return initLoc;
+    }
 
-	public Loc getInitLoc() {
-		checkNotNull(initLoc);
-		return initLoc;
-	}
+    ////
 
-	////
+    public void setInitLoc(final Loc loc) {
+        checkNotNull(loc);
+        checkArgument(locs.contains(loc));
+        this.initLoc = loc;
+    }
 
-	public void addDataVar(final VarDecl<?> varDecl) {
-		checkNotNull(varDecl);
-		checkArgument(!clockVars.contains(varDecl));
-		dataVars.add(varDecl);
-	}
+    public Loc createLoc(final String name, final LocKind kind, final Collection<Expr<BoolType>> invars) {
+        final Loc loc = new Loc(name, kind, invars);
+        locs.add(loc);
+        return loc;
+    }
 
-	public void addClockVar(final VarDecl<RatType> varDecl) {
-		checkNotNull(varDecl);
-		checkArgument(!dataVars.contains(varDecl));
-		clockVars.add(varDecl);
-	}
+    public Edge createEdge(final Loc source, final Loc target, final Collection<Expr<BoolType>> guards,
+                           final Optional<Sync> sync, final List<Stmt> updates) {
+        checkArgument(locs.contains(source));
+        checkArgument(locs.contains(target));
+        final Edge edge = new Edge(source, target, guards, sync, updates);
+        source.outEdges.add(edge);
+        target.inEdges.add(edge);
+        return edge;
+    }
 
-	public void setInitLoc(final Loc loc) {
-		checkNotNull(loc);
-		checkArgument(locs.contains(loc));
-		this.initLoc = loc;
-	}
+    ////
 
-	public Loc createLoc(final String name, final LocKind kind, final Collection<Expr<BoolType>> invars) {
-		final Loc loc = new Loc(name, kind, invars);
-		locs.add(loc);
-		return loc;
-	}
+    private Collection<Guard> createGuards(final Collection<Expr<BoolType>> exprs) {
+        checkNotNull(exprs);
 
-	public Edge createEdge(final Loc source, final Loc target, final Collection<Expr<BoolType>> guards,
-						   final Optional<Sync> sync, final List<Stmt> updates) {
-		checkArgument(locs.contains(source));
-		checkArgument(locs.contains(target));
-		final Edge edge = new Edge(source, target, guards, sync, updates);
-		source.outEdges.add(edge);
-		target.inEdges.add(edge);
-		return edge;
-	}
+        final ImmutableList.Builder<Guard> builder = ImmutableList.builder();
+        for (final Expr<BoolType> expr : exprs) {
+            final Collection<VarDecl<?>> vars = ExprUtils.getVars(expr);
 
-	////
+            boolean dataExpr = false;
+            boolean clockExpr = false;
+            for (final VarDecl<?> varDecl : vars) {
+                if (system.getDataVars().contains(varDecl)) {
+                    dataExpr = true;
+                } else if (system.getClockVars().contains(varDecl)) {
+                    clockExpr = true;
+                } else {
+                    throw new IllegalArgumentException("Undeclared variable: " + varDecl.getName());
+                }
+            }
 
-	private Collection<Guard> createGuards(final Collection<Expr<BoolType>> exprs) {
-		checkNotNull(exprs);
+            final Guard guard;
+            if (dataExpr && !clockExpr) {
+                guard = Guard.dataGuard(expr);
+            } else if (!dataExpr && clockExpr) {
+                guard = Guard.clockGuard(expr);
+            } else {
+                throw new UnsupportedOperationException();
+            }
+            builder.add(guard);
+        }
+        return builder.build();
+    }
 
-		final ImmutableList.Builder<Guard> builder = ImmutableList.builder();
-		for (final Expr<BoolType> expr : exprs) {
-			final Collection<VarDecl<?>> vars = ExprUtils.getVars(expr);
+    private List<Update> createUpdates(final List<Stmt> stmts) {
+        checkNotNull(stmts);
 
-			boolean dataExpr = false;
-			boolean clockExpr = false;
-			for (final VarDecl<?> varDecl : vars) {
-				if (dataVars.contains(varDecl)) {
-					dataExpr = true;
-				} else if (clockVars.contains(varDecl)) {
-					clockExpr = true;
-				} else {
-					throw new IllegalArgumentException("Undeclared variable: " + varDecl.getName());
-				}
-			}
+        final ImmutableList.Builder<Update> builder = ImmutableList.builder();
+        for (final Stmt stmt : stmts) {
+            final Collection<VarDecl<?>> varsDecls = StmtUtils.getVars(stmt);
+            boolean dataStmt = false;
+            boolean clockStmt = false;
+            for (final VarDecl<?> varDecl : varsDecls) {
+                if (system.getDataVars().contains(varDecl)) {
+                    dataStmt = true;
+                } else if (system.getClockVars().contains(varDecl)) {
+                    clockStmt = true;
+                } else {
+                    throw new IllegalArgumentException("Undeclared variable: " + varDecl.getName());
+                }
+            }
 
-			final Guard guard;
-			if (dataExpr && !clockExpr) {
-				guard = Guard.dataGuard(expr);
-			} else if (!dataExpr && clockExpr) {
-				guard = Guard.clockGuard(expr);
-			} else {
-				throw new UnsupportedOperationException();
-			}
-			builder.add(guard);
-		}
-		return builder.build();
-	}
+            final Update update;
+            if (dataStmt && !clockStmt) {
+                update = Update.dataUpdate(stmt);
+            } else if (!dataStmt && clockStmt) {
+                update = Update.clockUpdate(stmt);
+            } else {
+                throw new UnsupportedOperationException();
+            }
+            builder.add(update);
+        }
+        return builder.build();
+    }
 
-	private List<Update> createUpdates(final List<Stmt> stmts) {
-		checkNotNull(stmts);
+    ////
 
-		final ImmutableList.Builder<Update> builder = ImmutableList.builder();
-		for (final Stmt stmt : stmts) {
-			final Collection<VarDecl<?>> varsDecls = StmtUtils.getVars(stmt);
-			boolean dataStmt = false;
-			boolean clockStmt = false;
-			for (final VarDecl<?> varDecl : varsDecls) {
-				if (dataVars.contains(varDecl)) {
-					dataStmt = true;
-				} else if (clockVars.contains(varDecl)) {
-					clockStmt = true;
-				} else {
-					throw new IllegalArgumentException("Undeclared variable: " + varDecl.getName());
-				}
-			}
+    public enum LocKind {
+        NORMAL, URGENT, COMMITTED;
+    }
 
-			final Update update;
-			if (dataStmt && !clockStmt) {
-				update = Update.dataUpdate(stmt);
-			} else if (!dataStmt && clockStmt) {
-				update = Update.clockUpdate(stmt);
-			} else {
-				throw new UnsupportedOperationException();
-			}
-			builder.add(update);
-		}
-		return builder.build();
-	}
+    public final class Loc {
+        private final Collection<Edge> inEdges;
+        private final Collection<Edge> outEdges;
+        private final String name;
+        private final LocKind kind;
+        private final Collection<Guard> invars;
 
-	////
+        private final Collection<Edge> unmodInEdges;
+        private final Collection<Edge> unmodOutEdges;
 
-	public enum LocKind {
-		NORMAL, URGENT, COMMITTED;
-	}
+        private Loc(final String name, final LocKind kind, final Collection<Expr<BoolType>> invars) {
+            inEdges = new ArrayList<>();
+            outEdges = new ArrayList<>();
+            this.name = checkNotNull(name);
+            this.kind = checkNotNull(kind);
+            this.invars = createGuards(invars);
 
-	public final class Loc {
-		private final Collection<Edge> inEdges;
-		private final Collection<Edge> outEdges;
-		private final String name;
-		private final LocKind kind;
-		private final Collection<Guard> invars;
+            unmodInEdges = Collections.unmodifiableCollection(inEdges);
+            unmodOutEdges = Collections.unmodifiableCollection(outEdges);
+        }
 
-		private final Collection<Edge> unmodInEdges;
-		private final Collection<Edge> unmodOutEdges;
+        public Collection<Edge> getInEdges() {
+            return unmodInEdges;
+        }
 
-		private Loc(final String name, final LocKind kind, final Collection<Expr<BoolType>> invars) {
-			inEdges = new ArrayList<>();
-			outEdges = new ArrayList<>();
-			this.name = checkNotNull(name);
-			this.kind = checkNotNull(kind);
-			this.invars = createGuards(invars);
+        public Collection<Edge> getOutEdges() {
+            return unmodOutEdges;
+        }
 
-			unmodInEdges = Collections.unmodifiableCollection(inEdges);
-			unmodOutEdges = Collections.unmodifiableCollection(outEdges);
-		}
+        public String getName() {
+            return name;
+        }
 
-		public Collection<Edge> getInEdges() {
-			return unmodInEdges;
-		}
+        public LocKind getKind() {
+            return kind;
+        }
 
-		public Collection<Edge> getOutEdges() {
-			return unmodOutEdges;
-		}
+        public Collection<Guard> getInvars() {
+            return invars;
+        }
 
-		public String getName() {
-			return name;
-		}
+        @Override
+        public String toString() {
+            return Utils.lispStringBuilder("loc").add(name).toString();
+        }
+    }
 
-		public LocKind getKind() {
-			return kind;
-		}
+    ////
 
-		public Collection<Guard> getInvars() {
-			return invars;
-		}
+    public final class Edge {
+        private final Loc source;
+        private final Loc target;
+        private final Collection<Guard> guards;
+        private final Optional<Sync> sync;
+        private final List<Update> updates;
 
-		@Override
-		public String toString() {
-			return Utils.lispStringBuilder("loc").add(name).toString();
-		}
-	}
+        private Edge(final Loc source, final Loc target, final Collection<Expr<BoolType>> guards,
+                     final Optional<Sync> sync, final List<Stmt> updates) {
+            this.source = checkNotNull(source);
+            this.target = checkNotNull(target);
+            this.guards = createGuards(guards);
+            this.sync = checkNotNull(sync);
+            this.updates = createUpdates(updates);
+        }
 
-	////
+        public Loc getSource() {
+            return source;
+        }
 
-	public final class Edge {
-		private final Loc source;
-		private final Loc target;
-		private final Collection<Guard> guards;
-		private final Optional<Sync> sync;
-		private final List<Update> updates;
+        public Loc getTarget() {
+            return target;
+        }
 
-		private Edge(final Loc source, final Loc target, final Collection<Expr<BoolType>> guards,
-					 final Optional<Sync> sync, final List<Stmt> updates) {
-			this.source = checkNotNull(source);
-			this.target = checkNotNull(target);
-			this.guards = createGuards(guards);
-			this.sync = checkNotNull(sync);
-			this.updates = createUpdates(updates);
-		}
+        public Collection<Guard> getGuards() {
+            return guards;
+        }
 
-		public Loc getSource() {
-			return source;
-		}
+        public Optional<Sync> getSync() {
+            return sync;
+        }
 
-		public Loc getTarget() {
-			return target;
-		}
-
-		public Collection<Guard> getGuards() {
-			return guards;
-		}
-
-		public Optional<Sync> getSync() {
-			return sync;
-		}
-
-		public List<Update> getUpdates() {
-			return updates;
-		}
-	}
+        public List<Update> getUpdates() {
+            return updates;
+        }
+    }
 
 }
--- subprojects/xta/src/main/java/hu/bme/mit/theta/xta/dsl/XtaVariableSymbol.java
+++ subprojects/xta/src/main/java/hu/bme/mit/theta/xta/dsl/XtaVariableSymbol.java
@@ -15,31 +15,34 @@
  */
 package hu.bme.mit.theta.xta.dsl;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static hu.bme.mit.theta.core.type.inttype.IntExprs.Int;
-import static hu.bme.mit.theta.core.type.rattype.RatExprs.Rat;
-
-import java.util.List;
-
 import com.google.common.collect.ImmutableList;
-
 import hu.bme.mit.theta.common.dsl.Env;
 import hu.bme.mit.theta.common.dsl.Scope;
 import hu.bme.mit.theta.common.dsl.Symbol;
 import hu.bme.mit.theta.core.decl.Decls;
+import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.LitExpr;
 import hu.bme.mit.theta.core.type.Type;
 import hu.bme.mit.theta.core.type.arraytype.ArrayType;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.core.type.inttype.IntType;
+import hu.bme.mit.theta.core.type.rattype.RatType;
 import hu.bme.mit.theta.xta.Label;
 import hu.bme.mit.theta.xta.dsl.gen.XtaDslParser.TypeContext;
 import hu.bme.mit.theta.xta.dsl.gen.XtaDslParser.VariableIdContext;
 import hu.bme.mit.theta.xta.utils.ChanType;
 import hu.bme.mit.theta.xta.utils.ClockType;
-import hu.bme.mit.theta.xta.utils.LabelExpr;
 import hu.bme.mit.theta.xta.utils.RangeType;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static hu.bme.mit.theta.core.type.booltype.BoolExprs.False;
+import static hu.bme.mit.theta.core.type.inttype.IntExprs.Int;
+import static hu.bme.mit.theta.core.type.rattype.RatExprs.Rat;
+import static hu.bme.mit.theta.core.utils.TypeUtils.cast;
 
 final class XtaVariableSymbol implements Symbol {
 
@@ -71,34 +74,59 @@ final class XtaVariableSymbol implements Symbol {
 		return constant;
 	}
 
-	public Expr<?> instantiate(final String prefix, final Env env) {
-		final Type varType = type.instantiate(env);
+	public boolean isBroadcast() {
+		return broadcast;
+	}
 
-		if (broadcast && varType != ChanType.getInstance()) {
-			throw new UnsupportedOperationException("Keyword \"broadcast\" is only supported for type \"chan\"");
-		}
+	public InstantiateResult instantiate(final String prefix, final Env env) {
+		final Type varType = type.instantiate(env);
 
 		if (varType == ChanType.getInstance() && initialiser != null) {
 			throw new UnsupportedOperationException("Initialisers are not supported for type \"chan\"");
 		}
 
-		if (!isSupportedType(varType)) {
-			throw new UnsupportedOperationException(varType + " is not supported for variables");
-		}
-
 		if (constant) {
-			final Expr<?> expr = initialiser.instantiate(varType, env);
-			return expr;
-		} else {
-			if (varType instanceof ClockType) {
-				return Decls.Var(prefix + name, Rat()).getRef();
-			} else if (isChanArrayType(varType)) {
-				final List<Type> args = extractArgs(varType);
-				final Label label = Label.of(prefix + name, args, broadcast);
-				return LabelExpr.of(label);
+			if (!isSupportedDataType(varType)) {
+				throw new UnsupportedOperationException("Unsupported type for const " + name + ".");
+			} else if (initialiser == null) {
+				throw new UnsupportedOperationException("The value of const " + name + " is undefined.");
 			} else {
-				return Decls.Var(prefix + name, varType).getRef();
+				final LitExpr<?> expr = (LitExpr<?>) initialiser.instantiate(varType, env);
+				return InstantiateResult.constant(expr);
 			}
+		} else if (isSupportedDataType(varType)) {
+			if (broadcast) {
+				throw new UnsupportedOperationException("Unsupported keyword \"broadcast\" for variable" + name + ".");
+			} else {
+				final VarDecl<?> varDecl = Decls.Var(prefix + name, varType);
+				final LitExpr<?> initValue;
+				if (initialiser != null) {
+					final Expr<?> expr = initialiser.instantiate(varType, env);
+					if (expr instanceof LitExpr) {
+						initValue = (LitExpr<?>) expr;
+					} else {
+						throw new AssertionError();
+					}
+				} else {
+					initValue = defaultValueFor(varType);
+				}
+				return InstantiateResult.dataVariable(varDecl, initValue);
+			}
+		} else if (varType instanceof ClockType) {
+			if (broadcast) {
+				throw new UnsupportedOperationException("Unsupported keyword \"broadcast\" for variable" + name + ".");
+			} else if (initialiser != null) {
+				throw new UnsupportedOperationException("Clock " + name + " should not be initialized.");
+			} else {
+				final VarDecl<RatType> varDecl = Decls.Var(prefix + name, Rat());
+				return InstantiateResult.clockVariable(varDecl);
+			}
+		} else if (isChanArrayType(varType)) {
+			final List<Type> args = extractArgs(varType);
+			final Label label = Label.of(prefix + name, args, broadcast);
+			return InstantiateResult.channel(label);
+		} else {
+			throw new UnsupportedOperationException("Type of variable " + name + " is unsupported.");
 		}
 	}
 
@@ -113,6 +141,21 @@ final class XtaVariableSymbol implements Symbol {
 			return true;
 		} else {
 			return false;
+		}
+	}
+
+	private static boolean isSupportedDataType(Type type) {
+		return type instanceof BoolType || type instanceof IntType;
+	}
+
+	private static <T extends Type> LitExpr<T> defaultValueFor(final T type) {
+		checkArgument(isSupportedDataType(type));
+		if (type instanceof BoolType) {
+			return (LitExpr<T>) cast(False(), type);
+		} else if (type instanceof IntType) {
+			return (LitExpr<T>) cast(Int(0), type);
+		} else {
+			throw new AssertionError();
 		}
 	}
 
@@ -153,6 +196,156 @@ final class XtaVariableSymbol implements Symbol {
 			return result;
 		} else {
 			throw new AssertionError();
+		}
+	}
+
+	public static abstract class InstantiateResult {
+
+		private InstantiateResult() {
+		}
+
+		public static InstantiateResult constant(final LitExpr<?> expr) {
+			return new Constant(expr);
+		}
+
+		public static InstantiateResult clockVariable(final VarDecl<RatType> varDecl) {
+			return new ClockVariable(varDecl);
+		}
+
+		public static InstantiateResult dataVariable(final VarDecl<?> varDecl, final LitExpr<?> initValue) {
+			return new DataVariable(varDecl, initValue);
+		}
+
+		public static InstantiateResult channel(final Label label) {
+			return new Channel(label);
+		}
+
+		public boolean isDataVariable() {
+			return false;
+		}
+
+		public boolean isClockVariable() {
+			return false;
+		}
+
+		public boolean isConstant() {
+			return false;
+		}
+
+		public boolean isChannel() {
+			return false;
+		}
+
+		public DataVariable asDataVariable() {
+			throw new ClassCastException();
+		}
+
+		public ClockVariable asClockVariable() {
+			throw new ClassCastException();
+		}
+
+		public Constant asConstant() {
+			throw new ClassCastException();
+		}
+
+		public Channel asChannel() {
+			throw new ClassCastException();
+		}
+	}
+
+
+	public static final class DataVariable extends InstantiateResult {
+		private final VarDecl<?> varDecl;
+		private final LitExpr<?> initValue;
+
+		private DataVariable(final VarDecl<?> varDecl, LitExpr<?> litExpr) {
+			this.varDecl = checkNotNull(varDecl);
+			this.initValue = checkNotNull(litExpr);
+		}
+
+		@Override
+		public boolean isDataVariable() {
+			return true;
+		}
+
+		@Override
+		public DataVariable asDataVariable() {
+			return this;
+		}
+
+		public VarDecl<?> getVarDecl() {
+			return varDecl;
+		}
+
+		public LitExpr<?> getInitValue() {
+			return initValue;
+		}
+	}
+
+	public static final class ClockVariable extends InstantiateResult{
+		private final VarDecl<RatType> varDecl;
+
+		private ClockVariable(final VarDecl<RatType> varDecl) {
+			this.varDecl = checkNotNull(varDecl);
+		}
+
+		@Override
+		public boolean isClockVariable() {
+			return true;
+		}
+
+		@Override
+		public ClockVariable asClockVariable() {
+			return this;
+		}
+
+		public VarDecl<RatType> getVarDecl() {
+			return varDecl;
+		}
+	}
+
+	public static final class Constant extends InstantiateResult {
+		private final LitExpr<?> expr;
+
+		private Constant(final LitExpr<?> expr) {
+			this.expr = checkNotNull(expr);
+			checkArgument(isSupportedDataType(expr.getType()));
+		}
+
+		@Override
+		public boolean isConstant() {
+			return true;
+		}
+
+		@Override
+		public Constant asConstant() {
+			return this;
+		}
+
+		public LitExpr<?> getExpr() {
+			return expr;
+		}
+	}
+
+	public static final class Channel extends InstantiateResult {
+		private final Label label;
+
+		private Channel(final Label label) {
+			this.label = checkNotNull(label);
+		}
+
+		@Override
+		public boolean isChannel() {
+			return true;
+		}
+
+		@Override
+		public Channel asChannel() {
+			return this;
+		}
+
+		public Label getLabel() {
+			return label;
 		}
 	}
 
