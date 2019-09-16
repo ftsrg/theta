99516082c913da7c50b2b54a8c25e4e216ed8ae9
Levente Bajczi <bajczilevi@gmail.com>
2019-09-16 15:19:36 +0000

    Implemented top-level XCFA instantiation

--- subprojects/xcfa/src/main/java/hu/bme/mit/theta/xcfa/dsl/XcfaSymbol.java
+++ subprojects/xcfa/src/main/java/hu/bme/mit/theta/xcfa/dsl/XcfaSymbol.java
@@ -3,19 +3,44 @@ package hu.bme.mit.theta.xcfa.dsl;
 import hu.bme.mit.theta.common.dsl.Scope;
 import hu.bme.mit.theta.common.dsl.Symbol;
 import hu.bme.mit.theta.common.dsl.SymbolTable;
+import hu.bme.mit.theta.xcfa.XCFA;
 import hu.bme.mit.theta.xcfa.dsl.gen.XcfaDslParser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class XcfaSymbol implements Scope {
 
     private SymbolTable symbolTable;
 
+    private final List<XcfaVariableSymbol> vars;
+    private final List<XcfaProcessSymbol> processes;
+
     XcfaSymbol(final XcfaDslParser.SpecContext context) {
         symbolTable = new SymbolTable();
-        context.varDecls.forEach(varDeclContext -> symbolTable.add(new XcfaVariableSymbol(varDeclContext)));
-        context.processDecls.forEach(processDeclContext -> symbolTable.add(new XcfaProcessSymbol(this, processDeclContext)));
+        vars = new ArrayList<>();
+        processes = new ArrayList<>();
+        context.varDecls.forEach(varDeclContext -> {
+            XcfaVariableSymbol var;
+            symbolTable.add(var = new XcfaVariableSymbol(varDeclContext));
+            vars.add(var);
+        });
+        context.processDecls.forEach(processDeclContext -> {
+            XcfaProcessSymbol proc;
+            symbolTable.add(proc = new XcfaProcessSymbol(this, processDeclContext));
+            processes.add(proc);
+        });
     }
+
+
+    XCFA instantiate() {
+        XCFA.Builder builder = XCFA.builder();
+        vars.forEach(xcfaVariableSymbol -> builder.createVar(xcfaVariableSymbol.instantiate()));
+        processes.forEach(xcfaProcessSymbol -> builder.addProcess(xcfaProcessSymbol.instantiate()));
+        return builder.build();
+    }
+
 
     @Override
     public Optional<? extends Scope> enclosingScope() {
