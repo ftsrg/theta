363e7d7cd81f33ebe2cbe9a7436b9b172d6850c8
mondokm <milanmondok1998@gmail.com>
2020-04-05 20:11:31 +0200

    Fixed indexings

--- subprojects/core/build.gradle.kts
+++ subprojects/core/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("antlr-grammar")
 }
 
+tasks.withType<Test> {
+    this.testLogging {
+        this.showStandardStreams = true
+    }
+}
+
 dependencies {
     compile(project(":theta-common"))
 }
