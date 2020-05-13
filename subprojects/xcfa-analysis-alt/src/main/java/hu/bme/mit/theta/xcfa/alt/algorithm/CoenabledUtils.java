8838b5a333451b6747c3be3b337912a79a3702fd
radl97 <radl97@users.noreply.github.com>
2020-05-13 16:18:17 +0200

    Implement co-enabled check

--- subprojects/xcfa-analysis-alt/src/main/java/hu/bme/mit/theta/xcfa/alt/algorithm/CoenabledUtils.java
+++ subprojects/xcfa-analysis-alt/src/main/java/hu/bme/mit/theta/xcfa/alt/algorithm/CoenabledUtils.java
@@ -15,16 +15,52 @@
  */
 package hu.bme.mit.theta.xcfa.alt.algorithm;
 
+import hu.bme.mit.theta.core.stmt.Stmt;
+import hu.bme.mit.theta.core.stmt.xcfa.LockStmt;
+import hu.bme.mit.theta.core.stmt.xcfa.UnlockStmt;
+import hu.bme.mit.theta.xcfa.alt.expl.EdgeTransition;
 import hu.bme.mit.theta.xcfa.alt.expl.ProcessTransitions;
+import hu.bme.mit.theta.xcfa.alt.expl.StmtTransition;
+import hu.bme.mit.theta.xcfa.alt.expl.Transition;
 
 /**
  * Dynamic partial ordering has a notion of co-enabledness, which tries to capture usage of locking.
  */
 public class CoenabledUtils {
+
+    private CoenabledUtils() { }
+
     /** returns whether there is a possibility
      * that the two transitions are co-enabled */
-    public static boolean coenabled(ProcessTransitions pt, ProcessTransitions tr) {
+    public static boolean coenabled(ProcessTransitions p1, ProcessTransitions p2) {
         // TODO what exactly needs to be returned when the process contains two transitions?
+        if (p1.transitionStream().count() == 1 && p2.transitionStream().count() == 1) {
+            Transition a = p1.transitionStream().findAny().get();
+            Transition b = p1.transitionStream().findAny().get();
+            if (a instanceof EdgeTransition) {
+                a = ((EdgeTransition) a).getInnerTransition();
+            }
+            if (b instanceof EdgeTransition) {
+                b = ((EdgeTransition) b).getInnerTransition();
+            }
+            if (!(a instanceof StmtTransition)) {
+                return true;
+            }
+            if (!(b instanceof StmtTransition)) {
+                return true;
+            }
+            Stmt sa = ((StmtTransition) a).getStmt();
+            Stmt sb = ((StmtTransition) b).getStmt();
+
+            if (sa instanceof UnlockStmt && sb instanceof LockStmt) {
+                if (((UnlockStmt) sa).getSyncVar() == ((LockStmt) sb).getSyncVar())
+                    return false;
+            }
+            if (sa instanceof LockStmt && sb instanceof UnlockStmt) {
+                if (((LockStmt) sa).getSyncVar() == ((UnlockStmt) sb).getSyncVar())
+                    return false;
+            }
+        }
         // over-approximation
         return true;
     }
