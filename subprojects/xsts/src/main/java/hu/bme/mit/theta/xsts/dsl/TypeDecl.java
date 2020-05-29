2cf8f5216dda866d547ff6f5e0e6bba29177b28b
mondokm <milanmondok1998@gmail.com>
2020-05-29 13:45:51 +0200

    Improved semantic analysis when parsing xsts

--- subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/dsl/TypeDecl.java
+++ subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/dsl/TypeDecl.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.theta.xsts.dsl;
 
 import java.util.List;
+import java.util.Objects;
 
 public class TypeDecl {
 
@@ -10,6 +11,23 @@ public class TypeDecl {
     public TypeDecl(String name, List<String> literals) {
         this.name = name;
         this.literals = literals;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj instanceof TypeDecl) {
+            final TypeDecl that = (TypeDecl) obj;
+            return this.name.equals(that.name);
+        } else {
+            return false;
+        }
     }
 
     public String getName() {
