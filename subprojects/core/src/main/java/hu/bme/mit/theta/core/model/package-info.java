379b0c30e80d6a8a68da9fe0b50c5bf860985b2b
Hajdu Ákos <hajdua@mit.bme.hu>
2020-06-30 17:02:43 +0200

    Improve documentation for expressions

--- subprojects/core/src/main/java/hu/bme/mit/theta/core/model/package-info.java
+++ subprojects/core/src/main/java/hu/bme/mit/theta/core/model/package-info.java
@@ -1,9 +1,12 @@
 /**
- * This package contains different implementations of models that assign
- * expressions or literals to declarations.
- *
- * @see hu.bme.mit.theta.core.model.Substitution
- * @see hu.bme.mit.theta.core.model.Valuation
+ * This package contains different implementations of models that assign expressions or literals to declarations.
+ * - {@link hu.bme.mit.theta.core.model.Substitution} is a substitution that maps expressions to declarations.
+ *   - {@link hu.bme.mit.theta.core.model.BasicSubstitution} is a basic implementation of a substitution.
+ *   - {@link hu.bme.mit.theta.core.model.NestedSubstitution} implements a scoped substitution.
+ * - {@link hu.bme.mit.theta.core.model.Valuation} is a special case of {@link hu.bme.mit.theta.core.model.Substitution}
+ *   where literal expressions are assigned to declarations.
+ *   - {@link hu.bme.mit.theta.core.model.MutableValuation} implements a mutable valuation.
+ *   - {@link hu.bme.mit.theta.core.model.ImmutableValuation} implements an immutable valuation.
  */
 
 package hu.bme.mit.theta.core.model;
--- subprojects/core/src/main/java/hu/bme/mit/theta/core/type/package-info.java
+++ subprojects/core/src/main/java/hu/bme/mit/theta/core/type/package-info.java
@@ -4,14 +4,16 @@
  * grouped into subpackages. Constructors of the types and expressions are
  * usually package private. Use the factory classes instead.
  *
- * @see hu.bme.mit.theta.core.type.anytype.Exprs
- * @see hu.bme.mit.theta.core.type.arraytype.ArrayExprs
- * @see hu.bme.mit.theta.core.type.booltype.BoolExprs
- * @see hu.bme.mit.theta.core.type.functype.FuncExprs
- * @see hu.bme.mit.theta.core.type.inttype.IntExprs
- * @see hu.bme.mit.theta.core.type.pointertype.PointerExprs
- * @see hu.bme.mit.theta.core.type.proctype.ProcExprs
- * @see hu.bme.mit.theta.core.type.rattype.RatExprs
+ * - {@link hu.bme.mit.theta.core.type.Expr} is the main interface for expressions.
+ * - {@link hu.bme.mit.theta.core.type.anytype.Exprs} and {@link hu.bme.mit.theta.core.type.abstracttype.AbstractExprs}
+ *   work for multiple types (e.g., conditional).
+ * - {@link hu.bme.mit.theta.core.type.booltype.BoolExprs} can create Boolean type and expressions (e.g., and, or).
+ * - {@link hu.bme.mit.theta.core.type.inttype.IntExprs} can create mathematical (SMT) integer type and expressions
+ *   (e.g., add, multiply).
+ * - {@link hu.bme.mit.theta.core.type.rattype.RatExprs} can create rational type and expression (e.g., division).
+ * - {@link hu.bme.mit.theta.core.type.arraytype.ArrayExprs} can create SMT array type (associative mapping from a
+ *   key type to a value type) and expressions (e.g., read, write).
+ * - {@link hu.bme.mit.theta.core.type.functype.FuncExprs} can create function type and expressions.
  */
 
 package hu.bme.mit.theta.core.type;
