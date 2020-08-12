f477e845dce9bc86f036667f1431fc338a269958
Hajdu Ákos <hajdua@mit.bme.hu>
2020-08-12 13:32:41 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.inf.theta"
-    version = "1.6.0"
+    version = "1.6.1"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
