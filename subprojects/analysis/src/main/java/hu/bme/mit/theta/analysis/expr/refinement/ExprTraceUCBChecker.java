cb20d95499595dc3e3577bae454d3204d49a5ec2
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-08-26 18:54:24 +0200

    Refactor UCB checker

--- subprojects/analysis/src/main/java/hu/bme/mit/theta/analysis/expr/refinement/ExprTraceUCBChecker.java
+++ subprojects/analysis/src/main/java/hu/bme/mit/theta/analysis/expr/refinement/ExprTraceUCBChecker.java
@@ -5,22 +5,38 @@ import com.google.common.collect.Lists;
 import hu.bme.mit.theta.analysis.Trace;
 import hu.bme.mit.theta.analysis.expr.ExprAction;
 import hu.bme.mit.theta.analysis.expr.ExprState;
+import hu.bme.mit.theta.analysis.expr.ExprTraceUtils;
 import hu.bme.mit.theta.analysis.expr.StmtAction;
+import hu.bme.mit.theta.common.Utils;
 import hu.bme.mit.theta.core.model.ImmutableValuation;
 import hu.bme.mit.theta.core.model.Valuation;
+import hu.bme.mit.theta.core.stmt.AssumeStmt;
+import hu.bme.mit.theta.core.stmt.Stmt;
 import hu.bme.mit.theta.core.type.Expr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.utils.*;
+import hu.bme.mit.theta.core.utils.ExprSimplifier;
+import hu.bme.mit.theta.core.utils.ExprUtils;
+import hu.bme.mit.theta.core.utils.PathUtils;
+import hu.bme.mit.theta.core.utils.SpState;
+import hu.bme.mit.theta.core.utils.VarIndexing;
+import hu.bme.mit.theta.core.utils.WpState;
 import hu.bme.mit.theta.solver.Solver;
 import hu.bme.mit.theta.solver.utils.WithPushPop;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static hu.bme.mit.theta.core.type.booltype.BoolExprs.True;
 import static hu.bme.mit.theta.core.type.booltype.SmartBoolExprs.And;
 import static hu.bme.mit.theta.core.type.booltype.SmartBoolExprs.Not;
+import static java.util.stream.Collectors.toList;
 
 /**
  * An ExprTraceChecker that generates new predicates based on the UCB algorithm by
@@ -39,14 +55,26 @@ public class ExprTraceUCBChecker implements ExprTraceChecker<ItpRefutation>  {
         this.target = checkNotNull(target);
     }
 
-    public static ExprTraceUCBChecker create(final Expr<BoolType> init, final Expr<BoolType> target,
-                                                final Solver solver) {
+    public static ExprTraceUCBChecker create(
+        final Expr<BoolType> init, final Expr<BoolType> target, final Solver solver
+    ) {
         return new ExprTraceUCBChecker(init, target, solver);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public ExprTraceStatus<ItpRefutation> check(Trace<? extends ExprState, ? extends ExprAction> trace) {
+    public ExprTraceStatus<ItpRefutation> check(final Trace<? extends ExprState, ? extends ExprAction> trace) {
         checkNotNull(trace);
+        try {
+            return check2((Trace<? extends ExprState, ? extends StmtAction>) trace);
+        }
+        catch(ClassCastException e) {
+            throw new UnsupportedOperationException("Actions must be of type StmtAction", e);
+        }
+    }
+
+private ExprTraceStatus<ItpRefutation> check2(final Trace<? extends ExprState, ? extends StmtAction> trace) {
+        final var ftrace = flattenTrace(trace);
         final int stateCount = trace.getStates().size();
 
         final List<VarIndexing> indexings = new ArrayList<>(stateCount);
@@ -57,18 +85,15 @@ public class ExprTraceUCBChecker implements ExprTraceChecker<ItpRefutation>  {
         final boolean concretizable;
 
         try (WithPushPop wpp = new WithPushPop(solver)) {
-            solver.track(ExprUtils.getConjuncts(PathUtils.unfold(init, indexings.get(0))));
-            solver.track(ExprUtils.getConjuncts(PathUtils.unfold(trace.getState(0).toExpr(), indexings.get(0))));
-            assert solver.check().isSat() : "Initial state of the trace is not feasible";
-
             for (int i = 1; i < stateCount; ++i) {
                 indexings.add(indexings.get(i - 1).add(trace.getAction(i - 1).nextIndexing()));
-                solver.track(ExprUtils.getConjuncts(PathUtils.unfold(trace.getState(i).toExpr(), indexings.get(i))));
-                solver.track(ExprUtils
-                        .getConjuncts(PathUtils.unfold(trace.getAction(i - 1).toExpr(), indexings.get(i - 1))));
+                solver.track(
+                    ExprUtils.getConjuncts(
+                        PathUtils.unfold(trace.getAction(i - 1).toExpr(), indexings.get(i - 1))
+                    )
+                );
             }
 
-            solver.track(ExprUtils.getConjuncts(PathUtils.unfold(target, indexings.get(stateCount - 1))));
             concretizable = solver.check().isSat();
 
             if (concretizable) {
@@ -85,12 +110,7 @@ public class ExprTraceUCBChecker implements ExprTraceChecker<ItpRefutation>  {
             return createCounterexample(model, indexings, trace);
         } else {
             checkNotNull(unsatCore);
-            try {
-                return createRefinement(unsatCore, indexings, (Trace<? extends ExprState, ? extends StmtAction>) trace);
-            }
-            catch(ClassCastException e) {
-                throw new UnsupportedOperationException("Actions must be of type StmtAction", e);
-            }
+            return createRefinement(unsatCore, indexings, ftrace);
         }
     }
 
@@ -121,23 +141,23 @@ public class ExprTraceUCBChecker implements ExprTraceChecker<ItpRefutation>  {
 
         final List<Expr<BoolType>> predicates = new ArrayList<>();
 
+        var constCount = 0;
         for(var i = 0; i < stateCount; i++) {
             try(final var wpp = new WithPushPop(solver)) {
                 final List<Expr<BoolType>> dataRegion = new ArrayList<>();
 
                 /* Calculate SP */
                 if (i == 0) {
-                    solver.track(PathUtils.unfold(init, indexings.get(i)));
-                    dataRegion.add(PathUtils.unfold(init, indexings.get(i)));
-                    solver.track(PathUtils.unfold(trace.getState(i).toExpr(), indexings.get(i)));
-                    dataRegion.add(PathUtils.unfold(trace.getState(i).toExpr(), indexings.get(i)));
+                    solver.track(True());
+                    dataRegion.add(True());
                 } else /* i > 0 */ {
-                    var spState = SpState.of(And(PathUtils.foldin(predicates.get(i - 1), indexings.get(i - 1)), trace.getState(i - 1).toExpr()));
+                    var spState = SpState.of(PathUtils.foldin(predicates.get(i - 1), indexings.get(i - 1)), constCount);
                     for(var stmt : trace.getAction(i - 1).getStmts()) {
                         spState = spState.sp(stmt);
                     }
 
                     final var expr = PathUtils.unfold(spState.getExpr(), indexings.get(i));
+                    constCount = spState.getConstCount();
                     solver.track(ExprUtils.getConjuncts(expr));
                     dataRegion.addAll(ExprUtils.getConjuncts(expr));
                 }
@@ -195,5 +215,63 @@ public class ExprTraceUCBChecker implements ExprTraceChecker<ItpRefutation>  {
         }
 
         return wps;
+    }
+
+    private Trace<? extends ExprState, ? extends StmtAction> flattenTrace(final Trace<? extends ExprState, ? extends StmtAction> trace) {
+        final var stateCount = trace.getStates().size();
+        final var flattenedActions = new ArrayList<StmtAction>(stateCount - 1);
+
+        for(var i = 1; i < stateCount; i++) {
+            var initStream =
+                (i == 1)
+                ? ExprUtils.getConjuncts(init).stream().map(AssumeStmt::of)
+                : Stream.<AssumeStmt>empty();
+
+            var stateStream = ExprUtils.getConjuncts(trace.getState(i - 1).toExpr()).stream().map(AssumeStmt::of);
+
+            var actionStream = trace.getAction(i - 1).getStmts().stream();
+
+            var targetStream =
+                (i == stateCount - 1)
+                ? Stream.concat(
+                    ExprUtils.getConjuncts(trace.getState(i).toExpr()).stream().map(AssumeStmt::of),
+                    ExprUtils.getConjuncts(target).stream().map(AssumeStmt::of)
+                )
+                : Stream.<AssumeStmt>empty();
+
+            flattenedActions.add(
+                UCBAction.of(
+                    Stream.of(initStream, stateStream, actionStream, targetStream).flatMap(e -> e).collect(toList())
+                )
+            );
+        }
+
+        return ExprTraceUtils.traceFrom(flattenedActions);
+    }
+
+    /*
+     * Custom StmtAction to use when constructing helper traces
+     */
+
+    private static class UCBAction extends StmtAction {
+        private final List<Stmt> stmts;
+
+        private UCBAction(List<Stmt> stmts) {
+            this.stmts = stmts;
+        }
+
+        public static UCBAction of(List<Stmt> stmts) {
+            return new UCBAction(stmts);
+        }
+
+        @Override
+        public List<Stmt> getStmts() {
+            return stmts;
+        }
+
+        @Override
+        public String toString() {
+            return Utils.lispStringBuilder(getClass().getSimpleName()).body().addAll(stmts).toString();
+        }
     }
 }
