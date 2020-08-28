8519218fdf352d1a795e5a91998543171988bdd3
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-08-28 13:00:28 +0200

    Implement solver factory

---
+++ subprojects/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/SmtLibSolverFactory.java
@@ -0,0 +1,34 @@
+package hu.bme.mit.theta.solver.smtlib;
+
+import hu.bme.mit.theta.solver.ItpSolver;
+import hu.bme.mit.theta.solver.Solver;
+import hu.bme.mit.theta.solver.SolverFactory;
+
+import java.nio.file.Path;
+
+public class SmtLibSolverFactory implements SolverFactory {
+    private final Path solverPath;
+    private final String[] args;
+
+    private SmtLibSolverFactory(Path solverPath, String[] args) {
+        this.solverPath = solverPath;
+        this.args = args;
+    }
+
+    public static SmtLibSolverFactory create(Path solverPath, String[] args) {
+        return new SmtLibSolverFactory(solverPath, args);
+    }
+
+    @Override
+    public Solver createSolver() {
+        final var symbolTable = new SmtLibSymbolTable();
+        final var transformationManager = new SmtLibTransformationManager(symbolTable);
+
+        return new SmtLibSolver(symbolTable, transformationManager, solverPath, args);
+    }
+
+    @Override
+    public ItpSolver createItpSolver() {
+        throw new UnsupportedOperationException("SmtLibSolver does not support interpolation");
+    }
+}
