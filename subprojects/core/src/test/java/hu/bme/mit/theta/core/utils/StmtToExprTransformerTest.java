4ce440dfbbdde3cabcf02c01f4d2820686301182
mondokm <milanmondok1998@gmail.com>
2020-09-01 15:29:27 +0200

    Added readme to xsts-cli

--- subprojects/core/src/test/java/hu/bme/mit/theta/core/utils/StmtToExprTransformerTest.java
+++ subprojects/core/src/test/java/hu/bme/mit/theta/core/utils/StmtToExprTransformerTest.java
@@ -63,7 +63,11 @@ public class StmtToExprTransformerTest {
 
 				{Stmts.Assign(VX, Int(2)), ImmutableList.of(Eq(Prime(VX.getRef()), Int(2)))},
 
-				{Stmts.SequenceStmt(ImmutableList.of(Stmts.Assume(And(True(), False())))), ImmutableList.of(And(ImmutableList.of(And(True(), False()))))}
+				{Stmts.SequenceStmt(ImmutableList.of(Stmts.Assume(And(True(), False())))), ImmutableList.of(And(ImmutableList.of(And(True(), False()))))},
+
+				{Stmts.SequenceStmt(ImmutableList.of(Stmts.Assign(VX, Int(2)), Stmts.Assign(VX, Int(2)))), ImmutableList.of(And(Eq(Prime(VX.getRef()), Int(2)),Eq(Prime(Prime(VX.getRef())), Int(2))))},
+
+				{Stmts.NonDetStmt(ImmutableList.of(Stmts.Assign(VX, Int(2)), Stmts.Assign(VX, Int(2)))), ImmutableList.of(And(Eq(Prime(VX.getRef()), Int(2)),Eq(Prime(Prime(VX.getRef())), Int(2))))}
 
 		});
 	}
