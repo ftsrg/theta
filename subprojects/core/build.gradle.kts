68601953db6140d0e90998791a4be2ba50a71a1f
Akos Hajdu <hajdua@mit.bme.hu>
2020-09-01 17:31:09 +0200

    Disable test output during build

--- subprojects/core/build.gradle.kts
+++ subprojects/core/build.gradle.kts
@@ -3,12 +3,6 @@ plugins {
     id("antlr-grammar")
 }
 
-tasks.withType<Test> {
-    this.testLogging {
-        this.showStandardStreams = true
-    }
-}
-
 dependencies {
     compile(project(":theta-common"))
 }
--- subprojects/xsts-analysis/build.gradle.kts
+++ subprojects/xsts-analysis/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     id("java-common")
 }
 
-tasks.withType<Test> {
-    this.testLogging {
-        this.showStandardStreams = true
-    }
-}
-
 dependencies {
     compile(project(":theta-analysis"))
     compile(project(":theta-common"))
