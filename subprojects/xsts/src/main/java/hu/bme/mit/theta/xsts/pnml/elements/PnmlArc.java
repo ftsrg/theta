832c8ea3e48ffbe2995c168416071114c8ab11b9
mondokm <milanmondok1998@gmail.com>
2020-12-03 23:16:16 +0100

    Added pnml parsing and pnml to xsts transformation

--- subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/pnml/elements/PnmlArc.java
+++ subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/pnml/elements/PnmlArc.java
@@ -3,14 +3,18 @@ package hu.bme.mit.theta.xsts.pnml.elements;
 public class PnmlArc {
 
 	private final int weight;
+	private final String id;
 	private final PnmlNode sourceNode;
 	private final PnmlNode targetNode;
 
-	public PnmlArc(int weight, PnmlNode sourceNode, PnmlNode targetNode) {
+	public PnmlArc(final String id, int weight, final PnmlNode sourceNode, final PnmlNode targetNode) {
+		this.id = id;
 		this.weight = weight;
 		this.sourceNode = sourceNode;
 		this.targetNode = targetNode;
 	}
+
+	public String getId() { return id; }
 
 	public int getWeight() {
 		return weight;
--- subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/pnml/elements/PnmlPlace.java
+++ subprojects/xsts/src/main/java/hu/bme/mit/theta/xsts/pnml/elements/PnmlPlace.java
@@ -2,14 +2,14 @@ package hu.bme.mit.theta.xsts.pnml.elements;
 
 public class PnmlPlace extends PnmlNode {
 
-	private final int initialMarkings;
+	private final int initialMarking;
 
-	public PnmlPlace(String name, int initialMarkings) {
-		super(name);
-		this.initialMarkings = initialMarkings;
+	public PnmlPlace(final String name, final String id, int initialMarking) {
+		super(name,id);
+		this.initialMarking = initialMarking;
 	}
 
-	public int getInitialMarkings() {
-		return initialMarkings;
+	public int getInitialMarking() {
+		return initialMarking;
 	}
 }
