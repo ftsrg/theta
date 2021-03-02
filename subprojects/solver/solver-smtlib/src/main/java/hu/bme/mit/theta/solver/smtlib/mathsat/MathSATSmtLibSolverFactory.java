26e87cb9a911814d55d45165279f12a9f91651cc
Mihály Dobos-Kovács <dkmisu@gmail.com>
2021-03-02 15:09:03 +0100

    Add basic MathSAT interpolation support for newer versions

---
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/mathsat/MathSATSmtLibSolverFactory.java
@@ -0,0 +1,58 @@
+package hu.bme.mit.theta.solver.smtlib.mathsat;
+
+import hu.bme.mit.theta.solver.ItpSolver;
+import hu.bme.mit.theta.solver.Solver;
+import hu.bme.mit.theta.solver.SolverFactory;
+import hu.bme.mit.theta.solver.UCSolver;
+import hu.bme.mit.theta.solver.smtlib.SmtLibSolver;
+import hu.bme.mit.theta.solver.smtlib.generic.GenericSmtLibSolverBinary;
+import hu.bme.mit.theta.solver.smtlib.generic.GenericSmtLibSymbolTable;
+import hu.bme.mit.theta.solver.smtlib.generic.GenericSmtLibTermTransformer;
+import hu.bme.mit.theta.solver.smtlib.generic.GenericSmtLibTransformationManager;
+import hu.bme.mit.theta.solver.smtlib.z3.Z3SmtLibItpSolver;
+
+import java.nio.file.Path;
+
+public class MathSATSmtLibSolverFactory implements SolverFactory {
+    private final Path solverPath;
+    private final String[] args;
+
+    private MathSATSmtLibSolverFactory(Path solverPath, String[] args) {
+        this.solverPath = solverPath;
+        this.args = args;
+    }
+
+    public static MathSATSmtLibSolverFactory create(Path solverPath, String[] args) {
+        return new MathSATSmtLibSolverFactory(solverPath, args);
+    }
+
+    @Override
+    public Solver createSolver() {
+        final var symbolTable = new GenericSmtLibSymbolTable();
+        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
+        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
+        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
+
+        return new SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, false);
+    }
+
+    @Override
+    public UCSolver createUCSolver() {
+        final var symbolTable = new GenericSmtLibSymbolTable();
+        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
+        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
+        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
+
+        return new SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, true);
+    }
+
+    @Override
+    public ItpSolver createItpSolver() {
+        final var symbolTable = new GenericSmtLibSymbolTable();
+        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
+        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
+        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
+
+        return new MathSATSmtLibItpSolver(symbolTable, transformationManager, termTransformer, solverBinary);
+    }
+}
