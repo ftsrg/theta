ca7c1af211772ea847a1a900eed53645e1f70614
Mihály Dobos-Kovács <dkmisu@gmail.com>
2021-03-08 15:47:42 +0100

    Add version guard to MathSAT and Z3 solver factories

--- subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/z3/Z3SmtLibSolverFactory.java
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/z3/Z3SmtLibSolverFactory.java
@@ -15,14 +15,16 @@ import java.nio.file.Path;
 public class Z3SmtLibSolverFactory implements SolverFactory {
     private final Path solverPath;
     private final String[] args;
+    private final boolean itpSupported;
 
-    private Z3SmtLibSolverFactory(Path solverPath, String[] args) {
+    private Z3SmtLibSolverFactory(Path solverPath, String[] args, boolean itpSupported) {
         this.solverPath = solverPath;
         this.args = args;
+        this.itpSupported = itpSupported;
     }
 
-    public static Z3SmtLibSolverFactory create(Path solverPath, String[] args) {
-        return new Z3SmtLibSolverFactory(solverPath, args);
+    public static Z3SmtLibSolverFactory create(Path solverPath, String[] args, boolean itpSupported) {
+        return new Z3SmtLibSolverFactory(solverPath, args, itpSupported);
     }
 
     @Override
@@ -47,11 +49,16 @@ public class Z3SmtLibSolverFactory implements SolverFactory {
 
     @Override
     public ItpSolver createItpSolver() {
-        final var symbolTable = new GenericSmtLibSymbolTable();
-        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
-        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
-        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
+        if(itpSupported) {
+            final var symbolTable = new GenericSmtLibSymbolTable();
+            final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
+            final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
+            final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
 
-        return new Z3SmtLibItpSolver(symbolTable, transformationManager, termTransformer, solverBinary);
+            return new Z3SmtLibItpSolver(symbolTable, transformationManager, termTransformer, solverBinary);
+        }
+        else {
+            throw new UnsupportedOperationException("Z3 interpolation supported below 4.5.0");
+        }
     }
 }
