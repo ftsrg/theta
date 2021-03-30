0928d017ed0472f0f0e3d69086af0b075efc1629
Mihály Dobos-Kovács <dkmisu@gmail.com>
2021-03-30 19:01:48 +0200

    Refactor to remove code duplication

--- subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/princess/PrincessSmtLibSolverFactory.java
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/princess/PrincessSmtLibSolverFactory.java
@@ -1,48 +1,21 @@
 package hu.bme.mit.theta.solver.smtlib.impl.princess;
 
 import hu.bme.mit.theta.solver.ItpSolver;
-import hu.bme.mit.theta.solver.Solver;
-import hu.bme.mit.theta.solver.SolverFactory;
-import hu.bme.mit.theta.solver.UCSolver;
 import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSolverBinary;
+import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSolverFactory;
 import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSymbolTable;
 import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibTermTransformer;
 import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibTransformationManager;
-import hu.bme.mit.theta.solver.smtlib.solver.SmtLibSolver;
 
 import java.nio.file.Path;
 
-public class PrincessSmtLibSolverFactory implements SolverFactory {
-    private final Path solverPath;
-    private final String[] args;
-
+public class PrincessSmtLibSolverFactory extends GenericSmtLibSolverFactory {
     private PrincessSmtLibSolverFactory(Path solverPath, String[] args) {
-        this.solverPath = solverPath;
-        this.args = args;
+        super(solverPath, args);
     }
 
     public static PrincessSmtLibSolverFactory create(Path solverPath, String[] args) {
         return new PrincessSmtLibSolverFactory(solverPath, args);
-    }
-
-    @Override
-    public Solver createSolver() {
-        final var symbolTable = new GenericSmtLibSymbolTable();
-        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
-        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
-        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
-
-        return new SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, false);
-    }
-
-    @Override
-    public UCSolver createUCSolver() {
-        final var symbolTable = new GenericSmtLibSymbolTable();
-        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
-        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
-        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
-
-        return new SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, true);
     }
 
     @Override
--- subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/yices2/Yices2SmtLibSolverFactory.java
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/yices2/Yices2SmtLibSolverFactory.java
@@ -1,47 +1,18 @@
 package hu.bme.mit.theta.solver.smtlib.impl.yices2;
 
 import hu.bme.mit.theta.solver.ItpSolver;
-import hu.bme.mit.theta.solver.Solver;
-import hu.bme.mit.theta.solver.SolverFactory;
-import hu.bme.mit.theta.solver.UCSolver;
-import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSolverBinary;
-import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSymbolTable;
-import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibTermTransformer;
-import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibTransformationManager;
+import hu.bme.mit.theta.solver.smtlib.impl.generic.GenericSmtLibSolverFactory;
 
 import java.nio.file.Path;
 
-public class Yices2SmtLibSolverFactory implements SolverFactory {
-    private final Path solverPath;
-    private final String[] args;
+public class Yices2SmtLibSolverFactory extends GenericSmtLibSolverFactory {
 
     private Yices2SmtLibSolverFactory(Path solverPath, String[] args) {
-        this.solverPath = solverPath;
-        this.args = args;
+        super(solverPath, args);
     }
 
     public static Yices2SmtLibSolverFactory create(Path solverPath, String[] args) {
         return new Yices2SmtLibSolverFactory(solverPath, args);
-    }
-
-    @Override
-    public Solver createSolver() {
-        final var symbolTable = new GenericSmtLibSymbolTable();
-        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
-        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
-        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
-
-        return new Yices2SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, false);
-    }
-
-    @Override
-    public UCSolver createUCSolver() {
-        final var symbolTable = new GenericSmtLibSymbolTable();
-        final var transformationManager = new GenericSmtLibTransformationManager(symbolTable);
-        final var termTransformer = new GenericSmtLibTermTransformer(symbolTable);
-        final var solverBinary = new GenericSmtLibSolverBinary(solverPath, args);
-
-        return new Yices2SmtLibSolver(symbolTable, transformationManager, termTransformer, solverBinary, true);
     }
 
     @Override
