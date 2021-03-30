f801d0caecc23c47f952267eba9a0b6724e443c7
Mihály Dobos-Kovács <dkmisu@gmail.com>
2021-03-30 16:39:18 +0200

    Add SMTInterpol interpolation support

---
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/smtinterpol/SMTInterpolSmtLibItpMarker.java
@@ -0,0 +1,6 @@
+package hu.bme.mit.theta.solver.smtlib.impl.smtinterpol;
+
+import hu.bme.mit.theta.solver.smtlib.solver.interpolation.SmtLibItpMarker;
+
+public class SMTInterpolSmtLibItpMarker extends SmtLibItpMarker {
+}
