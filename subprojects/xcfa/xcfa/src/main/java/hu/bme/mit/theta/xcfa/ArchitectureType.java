ab43fad59b06de3ef774e86b79912c2bd30a64ee
AdamZsofi <adamzsofi.mail@gmail.com>
2021-07-04 13:18:25 +0200

    Fixed architecture bit widths

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/ArchitectureType.java
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/ArchitectureType.java
@@ -9,7 +9,7 @@ import java.util.Map;
 * (e.g. conversion rules would get more complex, if an int isn't at least twice as big as a short)
 */
  public enum ArchitectureType {
-	ILP32(8,16,32,32,32),
+	ILP32(8,16,32,32,64),
 	LP64(8,16,32,64,64);
 
 	public final Map<String, Integer> standardTypeSizes = new HashMap<>();
