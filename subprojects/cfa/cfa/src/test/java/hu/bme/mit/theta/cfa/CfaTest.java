0fac5e17863eae99de37bdfba8d9c31ab7bc486f
Akos Hajdu <hajdua@mit.bme.hu>
2021-08-01 15:33:26 +0100

    Check CFA variables and locations for unique names

---
+++ subprojects/cfa/cfa/src/test/java/hu/bme/mit/theta/cfa/CfaTest.java
@@ -0,0 +1,34 @@
+package hu.bme.mit.theta.cfa;
+
+import hu.bme.mit.theta.core.decl.Decl;
+import hu.bme.mit.theta.core.decl.Decls;
+import hu.bme.mit.theta.core.decl.VarDecl;
+import hu.bme.mit.theta.core.stmt.Stmts;
+import hu.bme.mit.theta.core.type.inttype.IntExprs;
+import hu.bme.mit.theta.core.type.inttype.IntType;
+import org.junit.Test;
+
+public class CfaTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDuplicateLocationName() {
+        CFA.Builder builder = CFA.builder();
+        builder.createLoc("A");
+        builder.createLoc("B");
+        builder.createLoc("A");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDuplicateVarName() {
+        CFA.Builder builder = CFA.builder();
+        VarDecl<IntType> v1 = Decls.Var("x", IntExprs.Int());
+        VarDecl<IntType> v2 = Decls.Var("x", IntExprs.Int());
+        CFA.Loc init = builder.createLoc();
+        CFA.Loc loc1 = builder.createLoc();
+        CFA.Loc loc2 = builder.createLoc();
+        builder.createEdge(init, loc1, Stmts.Havoc(v1));
+        builder.createEdge(init, loc2, Stmts.Havoc(v2));
+        builder.setInitLoc(init);
+        builder.build();
+    }
+}
