78cf2199041e49d15188fa1d1ee64a6addf85a6f
Levente Bajczi <bajczilevi@gmail.com>
2021-08-07 22:36:02 +0200

    Bump minor version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.inf.theta"
-    version = "2.19.1"
+    version = "2.20.0"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
