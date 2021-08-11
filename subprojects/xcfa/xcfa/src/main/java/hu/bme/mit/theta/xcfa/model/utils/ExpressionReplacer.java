f240296962f30c6e47847475be743d725bd9917e
Levente Bajczi <bajczilevi@gmail.com>
2021-08-11 21:46:47 +0200

    Added XCFA specific utils

---
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/model/utils/ExpressionReplacer.java
@@ -0,0 +1,53 @@
+package hu.bme.mit.theta.xcfa.model.utils;
+
+import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.Type;
+import hu.bme.mit.theta.xcfa.model.XcfaMetadata;
+import hu.bme.mit.theta.xcfa.transformation.model.types.complex.CComplexType;
+
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * This utility class helps with the awful state of generics in Java.
+ * In particular, this solves the problem that equality among type parameters is not runtime checkable in functions.
+ */
+@SuppressWarnings("unchecked")
+public class ExpressionReplacer<T extends Type, R extends Type> {
+	public static <T extends Type, R extends Type> Optional<Expr<T>> replace(Expr<T> expr, Function<Expr<R>, Optional<Expr<R>>> mapper) {
+		return new ExpressionReplacer<T, R>().replaceExpr(expr, mapper);
+	}
+
+	private Optional<Expr<T>> replaceExpr(Expr<T> expr, Function<Expr<R>, Optional<Expr<R>>> mapper) {
+		if(Arrays.stream(getClass().getTypeParameters()).map(TypeVariable::getGenericDeclaration).distinct().count() == 1) {
+			Optional<Expr<R>> transformed = mapper.apply((Expr<R>) expr);
+			if(transformed.isPresent()) return Optional.of((Expr<T>)transformed.get());
+		}
+		boolean needsTransformation = false;
+		List<Expr<?>> ops = new ArrayList<>();
+		for (Expr<?> op : expr.getOps()) {
+			Optional<? extends Expr<?>> replace = replace(op, mapper);
+			if(replace.isPresent()) {
+				needsTransformation = true;
+				ops.add(replace.get());
+			}
+			else {
+				ops.add(op);
+			}
+		}
+		if(needsTransformation) {
+			Expr<T> tExpr = expr.withOps(ops);
+			try {
+				XcfaMetadata.create(tExpr, "cType", CComplexType.getType(expr));
+			} catch (RuntimeException ignored) {
+
+			}
+			return Optional.of(tExpr);
+		}
+		else return Optional.empty();
+	}
+}
---
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/model/utils/XcfaStmtUtils.java
@@ -0,0 +1,39 @@
+package hu.bme.mit.theta.xcfa.model.utils;
+
+import hu.bme.mit.theta.common.Tuple2;
+import hu.bme.mit.theta.core.decl.Decl;
+import hu.bme.mit.theta.core.decl.VarDecl;
+import hu.bme.mit.theta.core.stmt.Stmt;
+import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.LitExpr;
+import hu.bme.mit.theta.core.type.Type;
+import hu.bme.mit.theta.core.type.arraytype.ArrayType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class XcfaStmtUtils {
+	/**
+	 * Replace expressions in a stmt based on a given Expr -> Opt(Expr) mapping function
+	 * @param stmt		Statement to replace expressions in
+	 * @param mapper	Mapping function that returns a new Expression when necessary, empty otherwise
+	 * @return 			Optional.empty() when no modification was necessary, the new Stmt otherwise
+	 */
+	public static <T extends Type> Optional<Stmt> replaceStmt(final Stmt stmt, final Function<Expr<T>, Optional<Expr<T>>> mapper) {
+		return stmt.accept(new StmtExpressionMappingVisitor<>(), mapper);
+	}
+
+	/**
+	 * Replace every instance of the declarations in `lookup` with an array read/write reference
+	 * @param stmt		The stmt to replace the decls in
+	 * @param lookup	The lookup table to use for mapping variables to array items
+	 * @param <R>		The index type
+	 * @param <T>		The element type
+	 * @return			Returns the statement(s) - multiple in the case of a Havoc stmt
+	 */
+	public static <P extends Type> List<Stmt> replaceVarWithArrayItem(final Stmt stmt, final Map<Decl<?>, Tuple2<VarDecl<ArrayType<P, ?>>, LitExpr<P>>> lookup) {
+		return stmt.accept(new StmtVarToArrayItemVisitor<>(), lookup);
+	}
+}
