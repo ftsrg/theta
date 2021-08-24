29dd8626e27e2174810c8c0c12042b41f34ea37f
Levente Bajczi <bajczilevi@gmail.com>
2021-08-24 16:48:28 +0200

    Modified description of the UnrollLoopsPass

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/UnrollLoopsPass.java
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/UnrollLoopsPass.java
@@ -1,65 +1,24 @@
 package hu.bme.mit.theta.xcfa.passes.procedurepass;
-
-import hu.bme.mit.theta.xcfa.model.XcfaEdge;
-import hu.bme.mit.theta.xcfa.model.XcfaLocation;
 import hu.bme.mit.theta.xcfa.model.XcfaProcedure;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-
+/**
+ * This pass unrolls an XCFA Procedure exactly once per loop. By re-running this pass on an otherwise unmodified
+ * XCFA Procedure, it will unroll it once more. Any modification might cause exponential expansion of the loops and is
+ * therefore unlikely to function well.
+ * The unrolling algorithm is the following:
+ * 		0a. Collect all initially existing locations and edges (skip this step in the first iteration, everything is in this set)
+ * 		0b. Collect all locations and edges added in the last iteration (skip this step in the first iteration, everything is in this set)
+ * 		1. Add a copy of 0a to the procedure builder
+ * 		2. For every reverse-edge in the 0b set add an edge from 0b to the corresponding new location (old_source -> new_target)
+ * 		3. For every (all previous) reverse-edge-source location add an edge from each incoming edge's corresponding new location (new_source -> old_targets )
+ */
 public class UnrollLoopsPass extends ProcedurePass{
 	private static final int K = 1;
 
 	@Override
 	public XcfaProcedure.Builder run(XcfaProcedure.Builder builder) {
-		XcfaProcedure.Builder newBuilder = XcfaProcedure.builder();
-		Set<XcfaLocation> visitedLocations = new LinkedHashSet<>();
-		Map<XcfaEdge, Integer> visitedReverseEdges = new LinkedHashMap<>();
-		XcfaLocation head = builder.getInitLoc();
-		inline(newBuilder, head, getCopy(newBuilder, head), visitedLocations, visitedReverseEdges);
-		newBuilder.setInitLoc(getOriginal(head));
-		newBuilder.setFinalLoc(getOriginal(builder.getFinalLoc()));
-		return newBuilder;
+
+		return builder;
 	}
 
-	private void inline(XcfaProcedure.Builder newBuilder, XcfaLocation head, XcfaLocation copyHead, Set<XcfaLocation> visitedLocations, Map<XcfaEdge, Integer> visitedReverseEdges) {
-		newBuilder.addLoc(copyHead);
-		visitedLocations.add(head);
-		List<XcfaEdge> outgoingEdges = head.getOutgoingEdges();
-		for (XcfaEdge outgoingEdge : outgoingEdges) {
-			if(visitedLocations.contains(outgoingEdge.getTarget())) {
-				Integer visited = visitedReverseEdges.getOrDefault(outgoingEdge, 0);
-				if(visited < K) {
-					visitedReverseEdges.put(outgoingEdge, visited+1);
-					XcfaLocation newTarget = getCopy(newBuilder, outgoingEdge.getTarget());
-					newBuilder.addEdge(new XcfaEdge(copyHead, newTarget, outgoingEdge.getStmts()));
-					inline(newBuilder, outgoingEdge.getTarget(), newTarget, visitedLocations, visitedReverseEdges);
-				} else {
-					XcfaLocation oldTarget = getOriginal(outgoingEdge.getTarget());
-					newBuilder.addEdge(new XcfaEdge(copyHead, oldTarget, outgoingEdge.getStmts()));
-				}
-			}
-			else {;
-				XcfaLocation newTarget = getCopy(newBuilder, outgoingEdge.getTarget());
-				newBuilder.addEdge(new XcfaEdge(copyHead, newTarget, outgoingEdge.getStmts()));
-				inline(newBuilder, outgoingEdge.getTarget(), newTarget, visitedLocations, visitedReverseEdges);
-			}
-		}
-	}
-
-	private final Map<XcfaLocation, XcfaLocation> originalLocationLut = new LinkedHashMap<>();
-	private XcfaLocation getCopy(XcfaProcedure.Builder newBuilder, XcfaLocation loc) {
-		XcfaLocation newLoc = XcfaLocation.copyOf(loc);
-		originalLocationLut.putIfAbsent(loc, newLoc);
-		newBuilder.addLoc(newLoc);
-		return newLoc;
-	}
-	private XcfaLocation getOriginal(XcfaLocation loc) {
-		return checkNotNull(originalLocationLut.get(loc));
-	}
 }
