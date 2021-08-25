359e5b6d644a6f529e871c656b3ae8bbdf85043e
Levente Bajczi <bajczilevi@gmail.com>
2021-08-25 14:40:50 +0200

    Removed misleading comment

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/UnrollLoopsPass.java
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/UnrollLoopsPass.java
@@ -15,17 +15,6 @@ import java.util.stream.Collectors;
 
 import static hu.bme.mit.theta.xcfa.passes.procedurepass.Utils.collectReverseEdges;
 
-/**
- * This pass unrolls an XCFA Procedure exactly once per loop. By re-running this pass on an otherwise unmodified
- * XCFA Procedure, it will unroll it once more. Any modification might cause exponential expansion of the loops and is
- * therefore unlikely to function well.
- * The unrolling algorithm is the following:
- * 		0a. Collect all initially existing locations and edges (skip this step in the first iteration, everything is in this set)
- * 		0b. Collect all locations and edges added in the last iteration (skip this step in the first iteration, everything is in this set)
- * 		1. Add a copy of 0a to the procedure builder
- * 		2. For every reverse-edge in the 0b set add an edge from 0b to the corresponding new location (old_source -> new_target)
- * 		3. For every (all previous) reverse-edge-source location add an edge from each incoming edge's corresponding new location (new_source -> old_targets )
- */
 public class UnrollLoopsPass extends ProcedurePass{
 
 	private final Set<XcfaEdge> forwardEdges = new LinkedHashSet<>();
