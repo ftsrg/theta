0ef410eaa6d4b9b78dfd5b14ea3e6650f62de5c9
AdamZsofi <adamzsofi.mail@gmail.com>
2021-09-24 19:36:53 +0200

    Added extra ctor parameter

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/algorithmselection/ComplexPortfolio.java
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/algorithmselection/ComplexPortfolio.java
@@ -1,2 +1,28 @@
-package hu.bme.mit.theta.xcfa.algorithmselection;public class ComplexPortfolio {
+package hu.bme.mit.theta.xcfa.algorithmselection;
+
+import hu.bme.mit.theta.analysis.algorithm.SafetyResult;
+import hu.bme.mit.theta.analysis.expr.refinement.PruneStrategy;
+import hu.bme.mit.theta.cfa.CFA;
+import hu.bme.mit.theta.cfa.analysis.config.CfaConfigBuilder;
+import hu.bme.mit.theta.common.logging.Logger;
+
+import java.io.File;
+import java.time.Duration;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class ComplexPortfolio extends AbstractPortfolio {
+	private final long sumTime = 900*1000; // 900*1000; // in ms, with initialization time
+	private final long analysisTime; // in ms, init time subtracted from sumTime
+
+	public ComplexPortfolio(Logger.Level logLevel, Duration initializationTime, String basicFileName, String modelName) {
+		super(logLevel, basicFileName, modelName);
+		analysisTime = sumTime - initializationTime.toMillis();
+
+	}
+
+	@Override
+	public SafetyResult<?, ?> executeAnalysis(CFA cfa) {
+		return null;
+	}
 }
