2f1a83bc0d4d5acdbaf1763d829f5ef4798a30ec
mondokm <milanmondok1998@gmail.com>
2021-09-27 21:12:29 +0200

    Bump version, update readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.inf.theta"
-    version = "2.22.3"
+    version = "2.23.0"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
