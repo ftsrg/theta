a316b7040ff9119bd0d78bd850b3cb73deeb6c25
AdamZsofi <adamzsofi.mail@gmail.com>
2021-10-13 22:29:27 +0200

    Updated theta starter script

--- scripts/theta-start.sh
+++ scripts/theta-start.sh
@@ -2,6 +2,7 @@
 if [ $1 == "--version" ]; then
     echo "3.0.0-svcomp22-qr"
 else
-    echo java -Xss120m -Xmx14210m -jar theta.jar "${@:2}" --input $1
-    java -Xss120m -Xmx14210m -jar theta.jar "${@:2}" --input $1
+    scriptdir=$(dirname $(realpath "$0"))
+    echo LD_LIBRARY_PATH=$scriptdir/lib java -Xss120m -Xmx14210m -jar $scriptdir/theta.jar "${@:2}" --input $1 --smt-home $scriptdir/solvers
+    LD_LIBRARY_PATH=$scriptdir/lib java -Xss120m -Xmx14210m -jar $scriptdir/theta.jar "${@:2}" --input $1 --smt-home $scriptdir/solvers
 fi
