acbbe9a0f70b348c07569b8140e72ee2a0e243d9
AdamZsofi <adamzsofi.mail@gmail.com>
2021-11-14 18:25:11 +0100

    cpu time keeping under refactor

--- subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/algorithmselection/CpuTimeKeeper.java
+++ subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/algorithmselection/CpuTimeKeeper.java
@@ -9,13 +9,19 @@ public class CpuTimeKeeper {
 		long pid = ProcessHandle.current().pid();
 		long cputime = 0;
 		try {
+			// TODO if the child process closing while we are taking measurements (which it WILL do between configs)
+			// that would cause false times, so we should definitely think this one through
 			Process process = Runtime.getRuntime().exec("ps --pid " + pid + " -o time");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String line = "";
-			reader.readLine();
-			String cputimeString = reader.readLine();
-			String[] split = cputimeString.split(":");
-			cputime = Long.parseLong(split[0])*3600+Long.parseLong(split[1])*60+Long.parseLong(split[2]);
+			String cputimeString = reader.readLine(); // skip the TIME header
+			while((cputimeString = reader.readLine())!=null) {
+				System.err.println(cputimeString);
+				String[] split = cputimeString.split(":");
+				cputime += Long.parseLong(split[0])*3600+Long.parseLong(split[1])*60+Long.parseLong(split[2]);
+				System.err.println(cputime);
+			}
+			System.err.println("");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
