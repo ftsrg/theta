8812003d3219bb48173b551d6ce3e63ad620b89d
AdamZsofi <adamzsofi.mail@gmail.com>
2021-11-16 19:47:56 +0100

    Added fix for doubled solver process times

--- subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/algorithmselection/CpuTimeKeeper.java
+++ subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/algorithmselection/CpuTimeKeeper.java
@@ -1,11 +1,18 @@
 package hu.bme.mit.theta.xcfa.analysis.algorithmselection;
 
+import hu.bme.mit.theta.common.Tuple2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
-public class CpuTimeKeeper {
+import static com.google.common.base.Preconditions.checkState;
+
+public final class CpuTimeKeeper {
 	static long closedSolverTimes = 0;
+	static Set<Long> closedSolverPids = new HashSet<>();
 
 	/**
 	 * Temporary solution:
@@ -18,13 +25,14 @@ public class CpuTimeKeeper {
 		long pid = ProcessHandle.current().pid();
 		long solvertimes = 0;
 		try {
-			Process process = Runtime.getRuntime().exec("ps --ppid " + pid + " -o time");
+			Process process = Runtime.getRuntime().exec("ps --ppid " + pid + " -o %p%x");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String line = "";
 			String cputimeString = reader.readLine(); // skip the TIME header
 			while((cputimeString = reader.readLine())!=null) {
-				String[] split = cputimeString.split(":");
-				solvertimes += Long.parseLong(split[0])*3600+Long.parseLong(split[1])*60+Long.parseLong(split[2]);
+				Tuple2<Long, Long> psOutput = parsePsOutputLine(cputimeString);
+				closedSolverPids.add(psOutput.get1());
+				solvertimes += psOutput.get2();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -37,19 +45,32 @@ public class CpuTimeKeeper {
 	 */
 	public static long getCurrentCpuTime() {
 		long pid = ProcessHandle.current().pid();
-		long cputime = closedSolverTimes;
+		long cputime = 0;
 		try {
-			Process process = Runtime.getRuntime().exec("ps --pid " + pid + " --ppid " + pid + " -o time");
+			Process process = Runtime.getRuntime().exec("ps --pid " + pid + " --ppid " + pid + " -o %p%x");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String line = "";
 			String cputimeString = reader.readLine(); // skip the TIME header
 			while((cputimeString = reader.readLine())!=null) {
-				String[] split = cputimeString.split(":");
-				cputime += Long.parseLong(split[0])*3600+Long.parseLong(split[1])*60+Long.parseLong(split[2]);
+				Tuple2<Long, Long> psOutput = parsePsOutputLine(cputimeString);
+				if(!closedSolverPids.contains(psOutput.get1())) {
+					cputime+=psOutput.get2();
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		cputime+=closedSolverTimes;
 		return cputime;
+	}
+
+	private static Tuple2<Long, Long> parsePsOutputLine(String line) {
+		String[] split = line.stripLeading().split(" ");
+		checkState(split.length==2);
+		long pid = Long.parseLong(split[0]);
+		String[] timeSplit = split[1].split(":");
+		long time = Long.parseLong(timeSplit[0])*3600+Long.parseLong(timeSplit[1])*60+Long.parseLong(timeSplit[2]);
+
+		return Tuple2.of(pid, time);
 	}
 }
