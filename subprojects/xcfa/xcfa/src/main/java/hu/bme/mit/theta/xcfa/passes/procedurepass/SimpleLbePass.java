b82fb1743cf3c8962d1c3ca70ec157f9a94e51bb
Csanád <csanadtelbisz@gmail.com>
2021-11-29 10:08:42 +0100

    SimpleLbePass javadoc, NonDetLabel DNF

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/SimpleLbePass.java
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/procedurepass/SimpleLbePass.java
@@ -7,58 +7,87 @@ import hu.bme.mit.theta.xcfa.model.XcfaProcedure;
 
 import java.util.*;
 
+/**
+ * This pass simplifies the XCFA by joining certain edges to single edges.
+ * <p>
+ * Definitions:
+ * <ul>
+ *     <li>Parallel edges: edges with the same source and target location</li>
+ *     <li>Snake: a graph component where the incoming and outgoing degree of every location is 1 (except at the ends)</li>
+ *     <li>Middle location: a location whose incoming degree is 1</li>
+ * </ul>
+ */
 public class SimpleLbePass extends ProcedurePass {
 
+	/**
+	 * Enables collapsing of parallel edges to single edges during the graph transformation process every time when a
+	 * parallel edge can appear (by applying other transformation rules).
+	 */
 	private final boolean ENABLE_PARALLEL_EDGE_COLLAPSING = true;
+
+	/**
+	 * Enables collapsing of sequential edges of a location where the number of incoming edges to the location is
+	 * exactly 1. A new edge is created for every outgoing edge of the location combined with the labels of the incoming
+	 * edge.
+	 */
 	private final boolean ENABLE_SEQUENCE_COLLAPSING = true;
+
+	/**
+	 * Enables collapsing of parallel edges to single edges at the and of the graph transformation process (that is
+	 * iterating through the locations once at the end).
+	 */
+	private final boolean ENABLE_REMOVE_PARALLEL_EDGES_AT_THE_END = true;
+
+	/**
+	 * Enables printing of the XCFA before and after the transformation process. For debugging...
+	 */
+	private final boolean ENABLE_PRINT_TO_DOT = true;
 
 	XcfaProcedure.Builder builder;
 
-	// TODO
-	//  * CNF or DNF
-	//  * switches for Nondet and Sequences
-
 	/**
 	 * Steps of graph transformation:
-	 * <p>
-	 * 1. Remove outgoing edges of the error location
-	 * 2. Join parallel edges to a single edge.
-	 * 3. Collapse "snakes" (graph components with vertices that have degrees of one or two).
-	 * 4. Obliterate middle-men and join possibly created parallel edges and collapse possibly created snakes.
+	 *
+	 * <ol>
+	 *     	<li>Remove outgoing edges of the error location</li>
+	 * 		<li>Join parallel edges to single edges and collapse snakes (see Definitions at {@link SimpleLbePass})</li>
+	 * 	 	<li>Collapse sequential edges of locations whose incoming degree is 1, join possibly created parallel edges and
+	 * 	 	edge-pairs described in step 2</li>
+	 * 	 	<li>Remove parallel edges at the end</li>
+	 * </ol>
 	 */
-
 	@Override
 	public XcfaProcedure.Builder run(XcfaProcedure.Builder builder) {
-		//if(true) return builder;
-		// Print Procedure in DOT format
-		String s = builder.toDot(Set.of(), Set.of());
-		System.out.println("--- BEFORE TRANSFORMATION ---");
-		System.out.println(s);
+		if (ENABLE_PRINT_TO_DOT) {
+			System.out.println("--- BEFORE TRANSFORMATION ---");
+			System.out.println(builder.toDot(Set.of(), Set.of()));
+		}
 
 		this.builder = builder;
 		builder = EliminateSelfLoops.instance.run(builder);
 
-		// 1. (this may not be necessary)
+		// Step 1
 		builder.getErrorLoc().getOutgoingEdges().forEach(builder::removeEdge);
 
-		// 2-3.
-		joinParallelsAndRemoveSnakes(new ArrayList<>(builder.getLocs()));
+		// Step 2
+		collapseParallelsAndSnakes(new ArrayList<>(builder.getLocs()));
 
-		// 4.
-		removeMiddleMen();
-
-		s = builder.toDot(Set.of(), Set.of());
-		System.out.println("--- DURING TRANSFORMATION ---");
-		System.out.println(s);
-
-		for(XcfaLocation location : builder.getLocs()){
-			collapseParallelEdges(location, new ArrayList<>());
+		// Step 3
+		if (ENABLE_SEQUENCE_COLLAPSING) {
+			removeAllMiddleLocations();
 		}
 
-		// Print Procedure in DOT format
-		s = builder.toDot(Set.of(), Set.of());
-		System.out.println("--- AFTER TRANSFORMATION ---");
-		System.out.println(s);
+		// Step 4
+		if (ENABLE_REMOVE_PARALLEL_EDGES_AT_THE_END) {
+			for (XcfaLocation location : builder.getLocs()) {
+				collapseParallelEdges(location, new ArrayList<>());
+			}
+		}
+
+		if (ENABLE_PRINT_TO_DOT) {
+			System.out.println("--- AFTER TRANSFORMATION ---");
+			System.out.println(builder.toDot(Set.of(), Set.of()));
+		}
 
 		return builder;
 	}
@@ -68,25 +97,39 @@ public class SimpleLbePass extends ProcedurePass {
 		return true;
 	}
 
-	private List<XcfaLocation> joinParallelsAndRemoveSnakes(List<XcfaLocation> locationsToVisit) {
+	/**
+	 * Collapses parallel edges and snakes with a starting list of locations to check. Possibly created new parallel
+	 * edges and snakes are collapsed too.
+	 *
+	 * @param locationsToVisit The starting list of locations to check.
+	 * @return Returns the list of removed locations.
+	 */
+	private List<XcfaLocation> collapseParallelsAndSnakes(List<XcfaLocation> locationsToVisit) {
 		List<XcfaLocation> removedLocations = new LinkedList<>();
 		while (!locationsToVisit.isEmpty()) {
 			XcfaLocation visiting = locationsToVisit.get(0);
 
 			// Join parallel edges starting from "visiting" location
-			//collapseParallelEdges(visiting, locationsToVisit);
+			if (ENABLE_PARALLEL_EDGE_COLLAPSING) {
+				collapseParallelEdges(visiting, locationsToVisit);
+			}
 
 			// Collapse "visiting" location if it is part of a snake
-			collapsePartOfSnake(visiting, locationsToVisit, removedLocations);
+			if (ENABLE_SEQUENCE_COLLAPSING) {
+				collapsePartOfSnake(visiting, locationsToVisit, removedLocations);
+			}
 
 			locationsToVisit.remove(visiting);
 		}
 		return removedLocations;
 	}
 
-	private void removeMiddleMen() {
-		if (!ENABLE_SEQUENCE_COLLAPSING) return;
-
+	/**
+	 * Removes locations whose incoming degree is 1. A new edge is created for every outgoing edge of the location
+	 * combined with the labels of the incoming edge as a sequence (the labels of the incoming edge will be the first in
+	 * the sequence).
+	 */
+	private void removeAllMiddleLocations() {
 		List<XcfaLocation> locationsToVisit = new ArrayList<>(builder.getLocs());
 		while (!locationsToVisit.isEmpty()) {
 			XcfaLocation visiting = locationsToVisit.get(0);
@@ -97,7 +140,7 @@ public class SimpleLbePass extends ProcedurePass {
 
 				List<XcfaLocation> start = new ArrayList<>();
 				start.add(previousLocation);
-				List<XcfaLocation> locationsToRemove = joinParallelsAndRemoveSnakes(start);
+				List<XcfaLocation> locationsToRemove = collapseParallelsAndSnakes(start);
 				locationsToRemove.forEach(locationsToVisit::remove);
 			}
 
@@ -105,9 +148,14 @@ public class SimpleLbePass extends ProcedurePass {
 		}
 	}
 
+	/**
+	 * Collapses all parallel edges starting from a location.
+	 *
+	 * @param location         the location from where the parallel edges start that we want to remove
+	 * @param locationsToVisit Adds the targets of parallel edges to this list (new parallel parallel edges and snakes
+	 *                         can appear in these locations)
+	 */
 	private void collapseParallelEdges(XcfaLocation location, List<XcfaLocation> locationsToVisit) {
-		if (!ENABLE_PARALLEL_EDGE_COLLAPSING) return;
-
 		HashMap<XcfaLocation, List<XcfaEdge>> edgesByTarget = new HashMap<>();
 		for (XcfaEdge edge : location.getOutgoingEdges()) {
 			List<XcfaEdge> edgesToTarget = edgesByTarget.getOrDefault(edge.getTarget(), new ArrayList<>());
@@ -128,9 +176,15 @@ public class SimpleLbePass extends ProcedurePass {
 		}
 	}
 
+	/**
+	 * Collapses the incoming and outgoing edges of a location whose incoming and outgoing degree is 1.
+	 *
+	 * @param location         The location to collapse
+	 * @param locationsToVisit The change list, the location that is the source of the incoming edge of the location is
+	 *                         added to this list
+	 * @param removedLocations The list of removed locations: the collapsed location is added to this list
+	 */
 	private void collapsePartOfSnake(XcfaLocation location, List<XcfaLocation> locationsToVisit, List<XcfaLocation> removedLocations) {
-		if (!ENABLE_SEQUENCE_COLLAPSING) return;
-
 		if (location.getIncomingEdges().size() == 1 && location.getOutgoingEdges().size() == 1) {
 			XcfaLocation previousLocation = location.getIncomingEdges().get(0).getSource();
 			removeMiddleLocation(location);
@@ -141,22 +195,49 @@ public class SimpleLbePass extends ProcedurePass {
 		}
 	}
 
-	private XcfaLabel getWrappedLabels(XcfaEdge edge) {
+	/**
+	 * Wraps an edge to a {@link hu.bme.mit.theta.xcfa.model.XcfaLabel.SequenceLabel} if the edge does not have exactly
+	 * one label. If the edge has one {@link hu.bme.mit.theta.xcfa.model.XcfaLabel.NondetLabel} its labels are returned
+	 * in a list to achieve DNF.
+	 *
+	 * @param edge the edge whose labels we would like to add to the NonDetLabel
+	 * @return the list of labels to add to the NonDetLabel
+	 */
+	private List<XcfaLabel> getNonDetBranch(XcfaEdge edge) {
 		List<XcfaLabel> edgeLabels = edge.getLabels();
-		return edgeLabels.size() == 1 ? edgeLabels.get(0) : XcfaLabel.Sequence(edgeLabels);
+		if (edgeLabels.size() == 1) {
+			if (edgeLabels.get(0) instanceof XcfaLabel.NondetLabel) {
+				return ((XcfaLabel.NondetLabel) edgeLabels.get(0)).getLabels();
+			}
+			return edgeLabels;
+		}
+		return List.of(XcfaLabel.Sequence(edgeLabels));
 	}
 
+	/**
+	 * Joins two parallel edges to a single edge by creating a new {@link hu.bme.mit.theta.xcfa.model.XcfaLabel.NondetLabel}.
+	 *
+	 * @param edge1 Edge 1
+	 * @param edge2 Edge 2
+	 */
 	private void joinParallelEdges(XcfaEdge edge1, XcfaEdge edge2) {
 		if (edge1.getSource() != edge2.getSource() || edge1.getTarget() != edge2.getTarget()) return;
 		builder.removeEdge(edge1);
 		builder.removeEdge(edge2);
-		XcfaLabel jointLabel = XcfaLabel.Nondet(List.of(
-				getWrappedLabels(edge1),
-				getWrappedLabels(edge2)
-		));
+		List<XcfaLabel> nonDetLabels = new ArrayList<>();
+		nonDetLabels.addAll(getNonDetBranch(edge1));
+		nonDetLabels.addAll(getNonDetBranch(edge2));
+		XcfaLabel jointLabel = XcfaLabel.Nondet(nonDetLabels);
 		builder.addEdge(XcfaEdge.of(edge1.getSource(), edge2.getTarget(), List.of(jointLabel)));
 	}
 
+	/**
+	 * Removes a location whose incoming degree is 1. A new edge is created for every outgoing edge of the location
+	 * combined with the labels of the incoming edge as a sequence (the labels of the incoming edge will be the first in
+	 * the sequence).
+	 *
+	 * @param location The location to remove
+	 */
 	private void removeMiddleLocation(XcfaLocation location) {
 		if (location.getIncomingEdges().size() != 1) return;
 		XcfaEdge inEdge = location.getIncomingEdges().get(0);
