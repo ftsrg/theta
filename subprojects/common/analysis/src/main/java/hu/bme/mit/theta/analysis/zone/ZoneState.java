a3d29a532499e50e0403964f529439e2874af938
Levente Bajczi <bajczilevi@gmail.com>
2022-02-19 22:47:52 +0100

    Added isTop back to ZoneState

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/zone/ZoneState.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/zone/ZoneState.java
@@ -152,6 +152,10 @@ public final class ZoneState implements ExprState {
 
 	////
 
+	public boolean isTop() {
+		return DBM.top(Collections.emptySet()).getRelation(dbm) == DbmRelation.EQUAL;
+	}
+	
 	@Override
 	public boolean isBottom() {
 		return !dbm.isConsistent();
