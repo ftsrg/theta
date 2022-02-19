532ebe91d81c3ca63dc31b5e6ae114e1cc99c4d3
AdamZsofi <adamzsofi.mail@gmail.com>
2022-02-19 23:31:05 +0100

    Deleted old comments in CStmtCounter

--- subprojects/frontends/c-frontend/src/main/java/hu/bme/mit/theta/frontend/transformation/CStmtCounter.java
+++ subprojects/frontends/c-frontend/src/main/java/hu/bme/mit/theta/frontend/transformation/CStmtCounter.java
@@ -22,20 +22,6 @@ public class CStmtCounter {
 	private static int forLoops = 0;
 	private static int whileLoops = 0;
 	private static int branches = 0;
-// TODO this isn't so simple :c
-//	private static Set<VarRef> controlVariables = new LinkedHashSet<>(); // variables, that come up in conditions
-//	private static Set<VarRef> rightSideVariables = new LinkedHashSet<>(); // variables, that come up on the right side of assignments
-
-/*
-	public static void addRightSideVariable(VarRef var) {
-		controlVariables.add(var);
-	}
-
-	public static void addControlVariable(VarRef var) {
-		controlVariables.add(var);
-	}
-
- */
 
 	public static void incrementBranches() {
 		branches++;
@@ -49,11 +35,6 @@ public class CStmtCounter {
 		whileLoops++;
 	}
 
-	/*
-	public static void getControlVariables() { return controlVariables.size(); }
-
-	public static void getRightSideVariables() { return rightSideVariables.size(); }
-	 */
 	public static int getWhileLoops() {
 		return whileLoops;
 	}
