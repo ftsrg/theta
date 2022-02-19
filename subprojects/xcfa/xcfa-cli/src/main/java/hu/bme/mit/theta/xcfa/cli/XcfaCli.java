52b8df589b951e114b849fcc00604cb0075eecf5
Levente Bajczi <bajczilevi@gmail.com>
2022-02-19 23:03:55 +0100

    Moved XcfaCli to root package

--- subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/XcfaCli.java
+++ subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/XcfaCli.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package hu.bme.mit.theta.xcfa.cli.stateless;
+package hu.bme.mit.theta.xcfa.cli;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
--- subprojects/xcfa/xcfa-cli/src/test/java/hu/bme/mit/theta/xcfa/cli/XcfaCliParseTest.java
+++ subprojects/xcfa/xcfa-cli/src/test/java/hu/bme/mit/theta/xcfa/cli/XcfaCliParseTest.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package hu.bme.mit.theta.xcfa.cli.stateless;
+package hu.bme.mit.theta.xcfa.cli;
 
 import hu.bme.mit.theta.c.frontend.dsl.gen.CLexer;
 import hu.bme.mit.theta.c.frontend.dsl.gen.CParser;
