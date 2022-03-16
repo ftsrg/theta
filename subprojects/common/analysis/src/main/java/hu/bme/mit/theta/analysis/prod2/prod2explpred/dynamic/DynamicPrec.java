e7912564bc35e3881e4346788960a2b0b366041a
mondokm <milanmondok1998@gmail.com>
2022-03-16 18:19:21 +0100

    Changed pred count to atom count in dynamic domain

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/prod2/prod2explpred/dynamic/DynamicPrec.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/prod2/prod2explpred/dynamic/DynamicPrec.java
@@ -6,9 +6,11 @@ import hu.bme.mit.theta.analysis.pred.PredPrec;
 import hu.bme.mit.theta.analysis.prod2.Prod2Prec;
 import hu.bme.mit.theta.common.container.Containers;
 import hu.bme.mit.theta.core.decl.VarDecl;
+import hu.bme.mit.theta.core.utils.ExprCanonizer;
 import hu.bme.mit.theta.core.utils.ExprUtils;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DynamicPrec extends Prod2Prec<ExplPrec, PredPrec> {
 
@@ -16,9 +18,12 @@ public class DynamicPrec extends Prod2Prec<ExplPrec, PredPrec> {
 
     protected DynamicPrec(ExplPrec prec1, PredPrec prec2) {
         super(prec1, prec2);
+        final var canonicalAtoms = prec2.getPreds().stream().flatMap(
+                pred -> ExprUtils.getAtoms(pred).stream()
+        ).map(ExprCanonizer::canonize).collect(Collectors.toSet());
         predCounts = Containers.createMap();
-        prec2.getPreds().forEach(
-                pred -> ExprUtils.getVars(pred).forEach(
+        canonicalAtoms.forEach(
+                atom -> ExprUtils.getVars(atom).forEach(
                         decl -> predCounts.put(decl, predCounts.getOrDefault(decl, 0) + 1)
                 )
         );
