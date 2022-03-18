29bc6bfcc6edb0152403790ce7404225644eeb42
mondokm <milanmondok1998@gmail.com>
2022-03-18 01:45:06 +0100

    Added interchange.pnml project to xsts subproject

--- subprojects/xsts/xsts/build.gradle.kts
+++ subprojects/xsts/xsts/build.gradle.kts
@@ -20,6 +20,25 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":theta-common"))
-    implementation(project(":theta-core"))
+    implementation(Deps.pnmlCore)
+    implementation(Deps.pnmlSymmetric)
+    implementation(Deps.pnmlPtnet)
+    implementation(Deps.pnmlUtils)
+    implementation(Deps.pnmlHlpn)
+    implementation(Deps.pnmlNupn)
+    implementation(Deps.pnmlPthlpng)
+
+    implementation(Deps.emfEcore)
+    implementation(Deps.emfCodegenEcore)
+    implementation(Deps.emfCodegen)
+    implementation(Deps.emfEcoreXmi)
+    implementation(Deps.emfCommon)
+
+    implementation(Deps.axiomImpl)
+    implementation(Deps.axiomApi)
+    implementation(Deps.logback)
+    implementation(Deps.jing)
+
+    compile(project(":theta-common"))
+    compile(project(":theta-core"))
 }
---
+++ subprojects/xsts/xsts/src/main/java/hu/bme/mit/theta/xsts/petrinet/Identified.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.theta.xsts.petrinet;
+
+public class Identified {
+	private final String id;
+	
+	public Identified(final String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+}
