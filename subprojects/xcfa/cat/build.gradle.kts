4e4b5c4245db3b55b624148d639f3c1716b96071
Levente Bajczi <bajczilevi@gmail.com>
2022-04-15 13:33:06 +0200

    added missing dependency

--- subprojects/xcfa/cat/build.gradle.kts
+++ subprojects/xcfa/cat/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     implementation(project(":theta-xcfa"))
     implementation(project(":theta-common"))
+    implementation(project(":theta-analysis"))
     implementation(project(":theta-core"))
     implementation(project(":theta-solver"))
     implementation(project(":theta-solver-z3"))
