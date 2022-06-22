d9430a4415d821d56f4b43ad62eb962bcc2f021a
mondokm <milanmondok1998@gmail.com>
2022-06-22 14:38:24 +0200

    Progress on expr node traverser

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/expression/ExpressionNodeTraverser.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/expression/ExpressionNodeTraverser.java
@@ -2,8 +2,6 @@ package hu.bme.mit.theta.analysis.algorithm.symbolic.expression;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
-import com.koloboke.collect.set.ObjSet;
-import com.koloboke.collect.set.hash.HashObjSets;
 import hu.bme.mit.theta.core.decl.Decl;
 import hu.bme.mit.theta.core.model.Valuation;
 import hu.bme.mit.theta.core.type.Expr;
@@ -20,53 +18,51 @@ import static hu.bme.mit.theta.core.type.abstracttype.AbstractExprs.Neq;
 
 public class ExpressionNodeTraverser {
 
-    private NodeEnumeration currentEnumeration;
+    private ExpressionNode currentNode;
 
     private final Solver solver;
-    private final Stack<NodeEnumeration> stack;
 
-    private class NodeEnumeration{
-        private final ExpressionNode node;
-        private final ObjSet<LitExpr<?>> enumeratedAssignments;
+    private final Stack<ExpressionNode> stack;
+    private int pushedNegatedAssignments = 0;
 
-        private NodeEnumeration(ExpressionNode node) {
-            this.node = node;
-            this.enumeratedAssignments = HashObjSets.newMutableSet();
-        }
-    }
 
     public ExpressionNodeTraverser(ExpressionNode rootNode, Supplier<Solver> solverSupplier) {
         this.solver = solverSupplier.get();
         this.stack = new Stack<>();
 
-        this.currentEnumeration = new NodeEnumeration(Preconditions.checkNotNull(rootNode));
+        this.currentNode = Preconditions.checkNotNull(rootNode);
+        stack.push(rootNode);
         solver.add(rootNode.getSymbolicRepresentation(Expr.class));
+        pushNegatedAssignments();
     }
 
     public boolean isOn(final ExpressionNode node){
         Preconditions.checkNotNull(node);
-        return currentEnumeration.node.equals(node);
+        return currentNode.equals(node);
     }
 
     public void moveUp(){
-        solver.pop(currentEnumeration.enumeratedAssignments.size()); // pop for each horizontal push
-        solver.pop(); // pop for vertical push
-        currentEnumeration = stack.pop();
+        Preconditions.checkState(stack.size()>1);
+        popNegatedAssignments();
+        solver.pop(); // pop assignment that brought us here
+        currentNode = stack.pop();
+        pushNegatedAssignments();
     }
 
-    public boolean queryAssignment(LitExpr<?> assignment){
-        if(currentEnumeration.enumeratedAssignments.contains(assignment)) return true; // TODO inkább a node cache-ét kéne nézni (vagy azt is)
-        else if(!currentEnumeration.node.isComplete()){
+    public boolean queryAssignment(int assignment){
+        if(currentNode.getCacheView().keySet().contains(assignment)) return true;
+        else if(!currentNode.isComplete()){
             SolverStatus status;
+            final LitExpr<?> litExpr = LitExprConverter.toLitExpr(assignment, currentNode.getVariable().getTraceInfo(Decl.class).getType());
             try(WithPushPop wpp = new WithPushPop(solver)){
-                solver.add(Eq(currentEnumeration.node.getVariable().getTraceInfo(Decl.class).getRef(),assignment));
+                solver.add(Eq(currentNode.getVariable().getTraceInfo(Decl.class).getRef(), litExpr));
                 solver.check();
                 status = solver.getStatus();
             }
             Preconditions.checkNotNull(status);
             if(status.isSat()) {
-                currentEnumeration.enumeratedAssignments.add(assignment);
-                solver.add(Neq(currentEnumeration.node.getVariable().getTraceInfo(Decl.class).getRef(), assignment));
+                solver.add(Neq(currentNode.getVariable().getTraceInfo(Decl.class).getRef(), litExpr));
+                pushedNegatedAssignments++;
                 cacheModel(solver.getModel());
                 return true;
             }
@@ -75,35 +71,56 @@ public class ExpressionNodeTraverser {
     }
 
     public void queryAssignment(){
-        if(!currentEnumeration.node.isComplete()){
+        if(!currentNode.isComplete()){
+            if(pushedNegatedAssignments != currentNode.getCacheView().keySet().size()){
+                popNegatedAssignments();
+                pushNegatedAssignments();
+            }
             solver.check();
             if(solver.getStatus().isSat()){
-                Optional<LitExpr<?>> optionalLitExpr = solver.getModel().eval(currentEnumeration.node.getVariable().getTraceInfo(Decl.class));
+                Optional<LitExpr<?>> optionalLitExpr = solver.getModel().eval(currentNode.getVariable().getTraceInfo(Decl.class));
                 if(optionalLitExpr.isPresent()){
                     LitExpr<?> literal = optionalLitExpr.get();
-                    currentEnumeration.enumeratedAssignments.add(literal);
-                    solver.add(Neq(currentEnumeration.node.getVariable().getTraceInfo(Decl.class).getRef(), literal));
-
+                    solver.add(Neq(currentNode.getVariable().getTraceInfo(Decl.class).getRef(), literal));
+                    pushedNegatedAssignments++;
                     cacheModel(solver.getModel());
                 } else {
                     // TODO na itt mi van?
+                    throw new UnsupportedOperationException("Not implemented yet");
                 }
             } else {
-                currentEnumeration.node.setComplete();
+                currentNode.setComplete();
             }
         }
 
     }
 
-    public void moveDown(LitExpr<?> assignment){
-
+    public void moveDown(int assignment){
+        if(queryAssignment(assignment)){
+            popNegatedAssignments();
+            solver.push();
+            solver.add(Eq(currentNode.getVariable().getTraceInfo(Decl.class).getRef(), LitExprConverter.toLitExpr(assignment, currentNode.getVariable().getTraceInfo(Decl.class).getType())));
+            currentNode = currentNode.getCacheView().get(assignment);
+            stack.push(currentNode);
+            pushNegatedAssignments();
+        } else throw new IllegalArgumentException("The provided assignment does not satisfy the formula.");
     }
 
     private void cacheModel(Valuation model){
-        // végigsimplifyolgatni
-        // unique tableből lekéregetni az expression node-okat
-        // csak a var orderingben lévő változók értékeit elrakosgatni és csak abban a sorrendben
-        ExpressionNodeUtils.cacheModel(currentEnumeration.node,model);
+        ExpressionNodeUtils.cacheModel(currentNode,model);
+    }
+
+    private void pushNegatedAssignments(){
+        solver.push();
+        for(var cur = currentNode.getCacheView().cursor();cur.moveNext();){
+            solver.add(Neq(currentNode.getVariable().getTraceInfo(Decl.class).getRef(), LitExprConverter.toLitExpr(cur.key(), currentNode.getVariable().getTraceInfo(Decl.class).getType())));
+            pushedNegatedAssignments++;
+        }
+    }
+
+    private void popNegatedAssignments(){
+        solver.pop();
+        pushedNegatedAssignments = 0;
     }
 
 
