58503994e1edde9b7a5df33a790588da79f66fd4
AdamZsofi <adamzsofi.mail@gmail.com>
2022-07-11 23:39:38 +0200

    Fixed xsts based bug generating extra traces

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/tracegen/TraceGenChecker.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/tracegen/TraceGenChecker.java
@@ -1,34 +1,18 @@
 package hu.bme.mit.theta.analysis.algorithm.tracegen;
 
-import com.google.common.collect.Streams;
 import hu.bme.mit.theta.analysis.*;
 import hu.bme.mit.theta.analysis.algorithm.*;
 import hu.bme.mit.theta.analysis.algorithm.cegar.Abstractor;
-import hu.bme.mit.theta.analysis.algorithm.cegar.AbstractorResult;
-import hu.bme.mit.theta.analysis.algorithm.cegar.BasicAbstractor;
-import hu.bme.mit.theta.analysis.algorithm.cegar.abstractor.StopCriterions;
-import hu.bme.mit.theta.analysis.expl.ExplPrec;
-import hu.bme.mit.theta.analysis.expl.ExplState;
 import hu.bme.mit.theta.analysis.expr.ExprState;
 import hu.bme.mit.theta.analysis.expr.StmtAction;
 import hu.bme.mit.theta.analysis.utils.ArgVisualizer;
-import hu.bme.mit.theta.analysis.waitlist.PriorityWaitlist;
-import hu.bme.mit.theta.common.Tuple2;
 import hu.bme.mit.theta.common.logging.Logger;
 import hu.bme.mit.theta.common.visualization.Graph;
 import hu.bme.mit.theta.common.visualization.writer.GraphvizWriter;
-import hu.bme.mit.theta.core.stmt.Stmt;
-import hu.bme.mit.theta.core.type.Expr;
-import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.utils.PathUtils;
-import hu.bme.mit.theta.solver.Solver;
 
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Not;
-import static hu.bme.mit.theta.core.utils.indexings.VarIndexingFactory.indexing;
 
 public class TraceGenChecker <S extends ExprState, A extends StmtAction, P extends Prec> implements SafetyChecker<S, A, P> {
     private final Logger logger;
@@ -56,15 +40,53 @@ public class TraceGenChecker <S extends ExprState, A extends StmtAction, P exten
     public SafetyResult<S, A> check(P prec) {
         final ARG<S, A> arg = abstractor.createArg();
         abstractor.check(arg, prec);
+        logger.write(Logger.Level.INFO, "Printing ARG..." + System.lineSeparator());
+        Graph g = ArgVisualizer.getDefault().visualize(arg);
+        logger.write(Logger.Level.INFO, GraphvizWriter.getInstance().writeString(g) + System.lineSeparator());
 
         // traces to end nodes/deadlocks
         // finds all possible traces, as nodes have each a single parent
         // and they just get covered with another node when they should have more with another node
         // to which we also generate a trace
-        Stream<ArgNode<S, A>> nodeStream = arg.getNodes().filter(saArgNode -> saArgNode.children().findAny().isEmpty());
 
-        traces = nodeStream.map(ArgTrace::to).map(ArgTrace::toTrace).toList();
+        // TODO XSTS SPECIFIC for now! collecting nodes that look like there should be traces to it, but there isn't
+        // this is due to the trans-env-trans-env nature of XSTS (there are nodes without outgoing edges that are covered that are not deadlock states in reality)
+        List<ArgNode<S,A>> badNodes = new ArrayList<>();
+        arg.getInitNodes().forEach(node -> removeBackwardsCoveredBy(node, badNodes));
+
+        // getting the traces
+        List<ArgNode<S, A>> endNodes = arg.getNodes().filter(saArgNode -> saArgNode.isLeaf()).collect(Collectors.toList());
+        List<ArgNode<S, A>> filteredEndNodes = new ArrayList<>();
+        endNodes.forEach(endNode -> {
+            if(badNodes.contains(endNode)) {
+                ArgNode<S, A> parent = endNode.getParent().get();
+                if(parent.getParent().isPresent()) {
+                    ArgNode<S, A> grandParent = parent.getParent().get();
+                    if(parent.getOutEdges().count() == 1 && grandParent.getOutEdges().count() == 1) {
+                        filteredEndNodes.add(grandParent);
+                    }
+                }
+            } else {
+                filteredEndNodes.add(endNode);
+            }
+        });
+
+        traces = filteredEndNodes.stream().map(ArgTrace::to).map(ArgTrace::toTrace).toList();
+
         return SafetyResult.unsafe(this.traces.get(0), ARG.create((state1, state2) -> false)); // TODO: this is only a placeholder
+    }
+
+    private void removeBackwardsCoveredBy(ArgNode<S, A> node, List<ArgNode<S,A>> badNodes) {
+        if(node.isLeaf()) {
+            ArgNode<S, A> parent = node.getParent().get();
+            if(node.isCovered() && parent.getParent().get() == node.getCoveringNode().get()) {
+                // bad node, needs to be removed
+                badNodes.add(node);
+            }
+        }
+        else {
+            node.children().forEach(child -> removeBackwardsCoveredBy(child, badNodes));
+        }
     }
 
     @Override
