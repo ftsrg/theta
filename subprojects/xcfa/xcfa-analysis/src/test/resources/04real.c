5a8cb5ab9ef3ee2ef8ceb040d4ab815412d441fa
Levente Bajczi <bajczilevi@gmail.com>
2022-07-22 00:58:48 +0200

    Removed erroneously created files

--- subprojects/xcfa/xcfa-analysis/src/test/resources/04real.c
+++
@@ -1,8 +0,0 @@
-void reach_error(){}
-extern float __VERIFIER_nondet_float();
-int main() {
-    float f = __VERIFIER_nondet_float();
-    double d = f;
-    long double ld = d;
-    if(ld > 0.28f && ld < 0.32f) reach_error();
-}
--- subprojects/xcfa/xcfa-analysis/src/test/resources/06arrays.c
+++
@@ -1,8 +0,0 @@
-void reach_error(){}
-
-int main() {
-    int a[2], b[3];
-    a[0] = 12;
-    b[3] = a[0];
-    if(b[3] > 11) reach_error();
-}
