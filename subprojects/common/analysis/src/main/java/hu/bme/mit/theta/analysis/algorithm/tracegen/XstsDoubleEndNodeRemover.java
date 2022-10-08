a0a344357481168deff5c4629f91b3d5069b41fd
AdamZsofi <adamzsofi.mail@gmail.com>
2022-10-08 23:41:44 +0200

    Patched xsts specific superfluous node removal

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/tracegen/XstsDoubleEndNodeRemover.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/tracegen/XstsDoubleEndNodeRemover.java
@@ -1,35 +1,71 @@
 package hu.bme.mit.theta.analysis.algorithm.tracegen;
 
+import hu.bme.mit.theta.analysis.Trace;
 import hu.bme.mit.theta.analysis.algorithm.ARG;
 import hu.bme.mit.theta.analysis.algorithm.ArgNode;
 import hu.bme.mit.theta.analysis.expr.ExprState;
 import hu.bme.mit.theta.analysis.expr.StmtAction;
 
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class XstsDoubleEndNodeRemover<S extends ExprState, A extends StmtAction> {
-    public static <S extends ExprState, A extends StmtAction> List<ArgNode<S,A>> collectBadNodes(ARG<S,A> arg) {
+    static <S extends ExprState, A extends StmtAction> List<ArgNode<S,A>> collectBadNodes(ARG<S,A> arg) {
         // TODO XSTS SPECIFIC for now! collecting nodes that look like there should be traces to it, but there isn't
         // this is due to the trans-env-trans-env nature of XSTS (there are nodes without outgoing edges that are covered that are not deadlock states in reality)
         XstsDoubleEndNodeRemover<S, A> instance = new XstsDoubleEndNodeRemover<S, A>();
         List<ArgNode<S, A>> badNodes = new ArrayList<>();
-        arg.getInitNodes().forEach(node -> instance.removeBackwardsCoveredBy(node, badNodes));
+        arg.getInitNodes().forEach(node -> instance.removeBackwardsCoveredBy(node, badNodes, false));
         return badNodes;
     }
 
-    private void removeBackwardsCoveredBy(ArgNode<S, A> node, List<ArgNode<S,A>> badNodes) {
+    private void removeBackwardsCoveredBy(ArgNode<S, A> node, List<ArgNode<S,A>> badNodes, boolean isLastInternal) {
         // parent & grandparent check: because in a really small model/small trace (3-4 nodes) there is no (grand)parent, but those are not bad nodes then
-        if(node.isLeaf() && node.getParent().isPresent()  && node.getParent().get().getParent().isPresent()) {
+        if(isLastInternal && node.isLeaf() && node.getParent().isPresent() && node.getParent().get().getParent().isPresent()) {
             ArgNode<S, A> parent = node.getParent().get();
+            ArgNode<S, A> grandParent = node.getParent().get().getParent().get();
             if(node.isCovered() && parent.getParent().get() == node.getCoveringNode().get()) { // node is covered and parent is checked above
                 // bad node, needs to be removed
                 badNodes.add(node);
             }
         }
         else {
-            node.children().forEach(child -> removeBackwardsCoveredBy(child, badNodes));
+            node.children().forEach(child -> removeBackwardsCoveredBy(child, badNodes, !isLastInternal));
         }
     }
 
+    static <S extends ExprState, A extends StmtAction> Trace<S, A> filterSuperfluousEndNode(Trace<S, A> trace) {
+        if(trace.getStates().size()>3) {
+            boolean transitionFired = false;
+            int size = trace.getStates().size();
+            if(trace.getState(size-1).toString().equals(trace.getState(size-3).toString())) {
+                BufferedReader bufReader = new BufferedReader(new StringReader(trace.getState(size-2).toString()));
+                String line=null;
+                try {
+                    while( (line=bufReader.readLine()) != null ) {
+                        if(line.trim().matches("^\\(__id_.*__.*\strue\\)*$")) transitionFired=true;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(!transitionFired) {
+                ArrayList<S> newStates = new ArrayList<>(trace.getStates());
+                newStates.remove(newStates.size()-1);
+                newStates.remove(newStates.size()-1);
+                ArrayList<A> newActions = new ArrayList<>(trace.getActions());
+                newActions.remove(newActions.size()-1);
+                newActions.remove(newActions.size()-1);
+                return Trace.of(newStates, newActions);
+            } else {
+                return trace;
+            }
+        } else {
+            return trace;
+        }
+    }
 }
--- subprojects/xsts/xsts-analysis/src/main/java/hu/bme/mit/theta/xsts/analysis/concretizer/TraceGenerationXstsTraceConcretizerUtil.java
+++ subprojects/xsts/xsts-analysis/src/main/java/hu/bme/mit/theta/xsts/analysis/concretizer/TraceGenerationXstsTraceConcretizerUtil.java
@@ -14,7 +14,9 @@ import hu.bme.mit.theta.xsts.analysis.XstsAction;
 import hu.bme.mit.theta.xsts.analysis.XstsState;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static hu.bme.mit.theta.core.type.booltype.BoolExprs.Bool;
@@ -24,7 +26,49 @@ public final class TraceGenerationXstsTraceConcretizerUtil {
     private TraceGenerationXstsTraceConcretizerUtil() {
     }
 
-    public static XstsStateSequence concretize(
+    public static Set<XstsStateSequence> concretizeTraceSet(List<Trace<XstsState<?>, XstsAction>> abstractTraces, SolverFactory solverFactory, final XSTS xsts) {
+        final VarFilter varFilter = VarFilter.of(xsts);
+        final ExprTraceChecker<ItpRefutation> checker = ExprTraceFwBinItpChecker.create(xsts.getInitFormula(),
+                Bool(true), solverFactory.createItpSolver());
+        Set<XstsStateSequence> concretizedTraces = new HashSet<>();
+
+        for (Trace<XstsState<?>, XstsAction> abstractTrace : abstractTraces) {
+            ExprTraceStatus<ItpRefutation> status = checker.check(abstractTrace);
+            if(status.isInfeasible()) {
+                int pruneIndex = status.asInfeasible().getRefutation().getPruneIndex();
+                if(pruneIndex>0) {
+                    abstractTrace = shortenTrace(abstractTrace, pruneIndex);
+                    status = checker.check(abstractTrace);
+                }
+            }
+            if(status.isFeasible()) {
+                final Trace<Valuation, ? extends Action> valuations = status.asFeasible().getValuations();
+                assert valuations.getStates().size() == abstractTrace.getStates().size();
+                final List<XstsState<ExplState>> xstsStates = new ArrayList<>();
+                for (int i = 0; i < abstractTrace.getStates().size(); ++i) {
+                    xstsStates.add(XstsState.of(ExplState.of(varFilter.filter(valuations.getState(i))), abstractTrace.getState(i).lastActionWasEnv(), abstractTrace.getState(i).isInitialized()));
+                }
+
+                XstsStateSequence concretizedTrace = XstsStateSequence.of(xstsStates, xsts);
+                // if trace was shortened, it might match with another one, in this case, do not add it again
+                if(concretizedTraces.stream().noneMatch(xstsStateSequence -> xstsStateSequence.toString().equals(concretizedTrace.toString()))) {
+                    concretizedTraces.add(concretizedTrace);
+                }
+            }
+        }
+        return concretizedTraces;
+    }
+
+    private static Trace<XstsState<?>, XstsAction> shortenTrace(Trace<XstsState<?>, XstsAction> abstractTrace, int pruneIndex) {
+        List<XstsState<?>> newStates = new ArrayList<>(abstractTrace.getStates());
+        newStates = newStates.subList(0, pruneIndex+1);
+        List<XstsAction> newActions = new ArrayList<>(abstractTrace.getActions());
+        newActions = newActions.subList(0, pruneIndex);
+        abstractTrace = Trace.of(newStates, newActions); // remove last state
+        return abstractTrace;
+    }
+
+    private static XstsStateSequence concretize(
         Trace<XstsState<?>, XstsAction> trace, SolverFactory solverFactory, final XSTS xsts) {
 
         final VarFilter varFilter = VarFilter.of(xsts);
