c1a43bd82d956a79879bdaed5d781ab97e75c475
mondokm <milanmondok1998@gmail.com>
2022-10-24 13:23:57 +0200

    Major refactor

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/symbolicnode/MddSymbolicNodeInterpretation.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/symbolicnode/MddSymbolicNodeInterpretation.java
@@ -1,9 +1,9 @@
 package hu.bme.mit.theta.analysis.algorithm.symbolic.symbolicnode;
 
-import hu.bme.mit.delta.collections.IntObjCursor;
+import hu.bme.mit.delta.collections.RecursiveIntObjMapView;
 
 /**
- * Provides an interpretation for nodes that haven't been fully enumerated yet.
+ * Provides an interpretation for symbolic nodes.
  * Wraps a node and a traverser and makes the traverser enumerate only what is necessary.
  */
 public class MddSymbolicNodeInterpretation implements RecursiveIntObjMapView<MddSymbolicNode> {
@@ -54,8 +54,7 @@ public class MddSymbolicNodeInterpretation implements RecursiveIntObjMapView<Mdd
 
     @Override
     public RecursiveCursor<? extends MddSymbolicNode> cursor() {
-        //TODO if(node.isComplete()) return new DefaultRecursiveCursorImpl<>(node.getCacheView().cursor());
-        return new MddSymbolicNodeRecursiveCursor(null, traverser);
+        return new MddSymbolicNodeRecursiveCursor(null, traverser); // TODO new traverser
     }
 
     @Override
@@ -64,54 +63,54 @@ public class MddSymbolicNodeInterpretation implements RecursiveIntObjMapView<Mdd
         return -1;
     }
 
-    /**
-     * A cursor implementation for incomplete symbolic nodes.
-     */
-    private class IncompleteMddSymbolicNodeCursor implements IntObjCursor<MddSymbolicNode>{
-        private int index;
-        private int key;
-        private MddSymbolicNode value;
-
-        private IncompleteMddSymbolicNodeCursor(){
-            this.index = -1;
-            this.key = -1;
-            this.value = null;
-        }
-
-        @Override
-        public int key() {
-            if(index < 0) throw new IllegalStateException("Cursor is not initialized");
-            return key;
-        }
-
-        @Override
-        public MddSymbolicNode value() {
-            if(index < 0) throw new IllegalStateException("Cursor is not initialized");
-            return value;
-        }
-
-        /**
-         * Uses values from the cache as long as possible. Uses the traverser to query edges when all edges from the cache were used.
-         * @return true if the move was successful
-         */
-        @Override
-        public boolean moveNext() {
-            if(index < node.getExplicitRepresentation().getSize() - 1){
-                index++;
-                key = node.getExplicitRepresentation().getEdge(index);
-                value = node.getExplicitRepresentation().getCacheView().get(key);
-                return true;
-            }
-            else if(!node.isComplete()) {
-                final MddSymbolicNodeTraverser.QueryResult queryResult = traverser.queryEdge();
-                if(queryResult.getStatus() == MddSymbolicNodeTraverser.QueryResult.Status.SINGLE_EDGE){
-                    index++;
-                    key = queryResult.getKey();
-                    value = node.getExplicitRepresentation().getCacheView().get(key);
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
+//    /**
+//     * A cursor implementation for incomplete symbolic nodes.
+//     */
+//    private class IncompleteMddSymbolicNodeCursor implements IntObjCursor<MddSymbolicNode>{
+//        private int index;
+//        private int key;
+//        private MddSymbolicNode value;
+//
+//        private IncompleteMddSymbolicNodeCursor(){
+//            this.index = -1;
+//            this.key = -1;
+//            this.value = null;
+//        }
+//
+//        @Override
+//        public int key() {
+//            if(index < 0) throw new IllegalStateException("Cursor is not initialized");
+//            return key;
+//        }
+//
+//        @Override
+//        public MddSymbolicNode value() {
+//            if(index < 0) throw new IllegalStateException("Cursor is not initialized");
+//            return value;
+//        }
+//
+//        /**
+//         * Uses values from the cache as long as possible. Uses the traverser to query edges when all edges from the cache were used.
+//         * @return true if the move was successful
+//         */
+//        @Override
+//        public boolean moveNext() {
+//            if(index < node.getExplicitRepresentation().getSize() - 1){
+//                index++;
+//                key = node.getExplicitRepresentation().getEdge(index);
+//                value = node.getExplicitRepresentation().getCacheView().get(key);
+//                return true;
+//            }
+//            else if(!node.isComplete()) {
+//                final MddSymbolicNodeTraverser.QueryResult queryResult = traverser.queryEdge();
+//                if(queryResult.getStatus() == MddSymbolicNodeTraverser.QueryResult.Status.SINGLE_EDGE){
+//                    index++;
+//                    key = queryResult.getKey();
+//                    value = node.getExplicitRepresentation().getCacheView().get(key);
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
+//    }
 }
