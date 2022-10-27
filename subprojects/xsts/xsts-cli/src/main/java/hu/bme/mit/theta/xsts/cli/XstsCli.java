9fe7fa09d84416c2a067f3d9b34a38e5a76db2fa
Levente Bajczi <bajczilevi@gmail.com>
2022-10-27 16:47:52 +0200

    Added flags to set solver in XstsCli

--- subprojects/xsts/xsts-cli/src/main/java/hu/bme/mit/theta/xsts/cli/XstsCli.java
+++ subprojects/xsts/xsts-cli/src/main/java/hu/bme/mit/theta/xsts/cli/XstsCli.java
@@ -12,6 +12,7 @@ import hu.bme.mit.theta.analysis.expr.refinement.PruneStrategy;
 import hu.bme.mit.theta.analysis.utils.ArgVisualizer;
 import hu.bme.mit.theta.analysis.utils.TraceVisualizer;
 import hu.bme.mit.theta.common.CliUtils;
+import hu.bme.mit.theta.common.OsHelper;
 import hu.bme.mit.theta.common.logging.ConsoleLogger;
 import hu.bme.mit.theta.common.logging.Logger;
 import hu.bme.mit.theta.common.logging.NullLogger;
@@ -19,7 +20,11 @@ import hu.bme.mit.theta.common.table.BasicTableWriter;
 import hu.bme.mit.theta.common.table.TableWriter;
 import hu.bme.mit.theta.common.visualization.Graph;
 import hu.bme.mit.theta.common.visualization.writer.GraphvizWriter;
+import hu.bme.mit.theta.solver.SolverFactory;
+import hu.bme.mit.theta.solver.SolverManager;
+import hu.bme.mit.theta.solver.smtlib.SmtLibSolverManager;
 import hu.bme.mit.theta.solver.z3.Z3SolverFactory;
+import hu.bme.mit.theta.solver.z3.Z3SolverManager;
 import hu.bme.mit.theta.xsts.XSTS;
 import hu.bme.mit.theta.xsts.analysis.XstsAction;
 import hu.bme.mit.theta.xsts.analysis.XstsState;
@@ -34,6 +39,7 @@ import hu.bme.mit.theta.xsts.pnml.PnmlToXSTS;
 import hu.bme.mit.theta.xsts.pnml.elements.PnmlNet;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -68,7 +74,7 @@ public class XstsCli {
 	Integer maxEnum = 0;
 
 	@Parameter(names = "--autoexpl", description = "Predicate to explicit switching strategy")
- 	AutoExpl autoExpl = AutoExpl.NEWOPERANDS;
+	AutoExpl autoExpl = AutoExpl.NEWOPERANDS;
 
 	@Parameter(names = {"--initprec"}, description = "Initial precision")
 	InitPrec initPrec = InitPrec.EMPTY;
@@ -102,6 +108,15 @@ public class XstsCli {
 
 	@Parameter(names = {"--visualize"}, description = "Write proof or counterexample to file in dot format")
 	String dotfile = null;
+
+	@Parameter(names = {"--refinement-solver"}, description = "Refinement solver name")
+	String refinementSolver= "Z3";
+
+	@Parameter(names = {"--abstraction-solver"}, description = "Abstraction solver name")
+	String abstractionSolver= "Z3";
+
+	@Parameter(names = {"--smt-home"}, description = "The path of the solver registry")
+	String solverHome = SmtLibSolverManager.HOME.toAbsolutePath().toString();
 
 	@Parameter(names = "--no-stuck-check")
 	boolean noStuckCheck = false;
@@ -210,8 +225,12 @@ public class XstsCli {
 			ArgCexCheckHandler.instance.setArgCexCheck(true, refinement.equals(Refinement.MULTI_SEQ));
 		}
 
+		registerAllSolverManagers(solverHome, logger);
+		SolverFactory abstractionSolverFactory = SolverManager.resolveSolverFactory(abstractionSolver);
+		SolverFactory refinementSolverFactory = SolverManager.resolveSolverFactory(refinementSolver);
+
 		try {
-			return new XstsConfigBuilder(domain, refinement, Z3SolverFactory.getInstance())
+			return new XstsConfigBuilder(domain, refinement, abstractionSolverFactory, refinementSolverFactory)
 					.maxEnum(maxEnum).autoExpl(autoExpl).initPrec(initPrec).pruneStrategy(pruneStrategy)
 					.search(search).predSplit(predSplit).optimizeStmts(optimizeStmts).logger(logger).build(xsts);
 		} catch (final Exception ex) {
@@ -273,6 +292,15 @@ public class XstsCli {
 		final Graph graph = status.isSafe() ? ArgVisualizer.getDefault().visualize(status.asSafe().getArg())
 				: TraceVisualizer.getDefault().visualize(status.asUnsafe().getTrace());
 		GraphvizWriter.getInstance().writeFile(graph, filename);
+	}
+
+	private void registerAllSolverManagers(String home, Logger logger) throws Exception {
+		SolverManager.closeAll();
+		SolverManager.registerSolverManager(Z3SolverManager.create());
+		if (OsHelper.getOs() == OsHelper.OperatingSystem.LINUX) {
+			SmtLibSolverManager smtLibSolverManager = SmtLibSolverManager.create(Path.of(home), logger);
+			SolverManager.registerSolverManager(smtLibSolverManager);
+		}
 	}
 
 }
