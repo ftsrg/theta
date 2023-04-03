0173e771ae581905a9d8871910031a5b6150d16d
mondokm <milanmondok1998@gmail.com>
2023-04-03 11:56:22 +0200

    Cursor saturation work in progress

--- subprojects/xsts/xsts-cli/src/main/java/hu/bme/mit/theta/xsts/cli/XstsTest.java
+++ subprojects/xsts/xsts-cli/src/main/java/hu/bme/mit/theta/xsts/cli/XstsTest.java
@@ -3,10 +3,8 @@ package hu.bme.mit.theta.xsts.cli;
 import hu.bme.mit.delta.collections.impl.RecursiveIntObjMapViews;
 import hu.bme.mit.delta.java.mdd.*;
 import hu.bme.mit.delta.mdd.MddVariableDescriptor;
-import hu.bme.mit.theta.analysis.algorithm.symbolic.fixpoint.CursorGeneralizedSaturationProvider;
-import hu.bme.mit.theta.analysis.algorithm.symbolic.fixpoint.GeneralizedSaturationProvider;
+import hu.bme.mit.theta.analysis.algorithm.symbolic.fixpoint.*;
 import hu.bme.mit.theta.analysis.algorithm.symbolic.model.AbstractNextStateDescriptor;
-import hu.bme.mit.theta.analysis.algorithm.symbolic.model.impl.OrNextStateDescriptor;
 import hu.bme.mit.theta.analysis.algorithm.symbolic.symbolicnode.expression.ExprLatticeDefinition;
 import hu.bme.mit.theta.analysis.algorithm.symbolic.symbolicnode.expression.MddExpressionTemplate;
 import hu.bme.mit.theta.analysis.algorithm.symbolic.symbolicnode.expression.MddNodeNextStateDescriptor;
@@ -23,13 +21,12 @@ import hu.bme.mit.theta.core.type.inttype.IntType;
 import hu.bme.mit.theta.solver.z3.Z3SolverFactory;
 
 import java.io.FileNotFoundException;
-import java.util.List;
 
 import static hu.bme.mit.theta.core.type.abstracttype.AbstractExprs.*;
 import static hu.bme.mit.theta.core.type.booltype.BoolExprs.And;
 import static hu.bme.mit.theta.core.type.inttype.IntExprs.Int;
 
-public class SatTest {
+public class XstsTest {
 
     public static void main(String[] args){
 
@@ -58,48 +55,33 @@ public class SatTest {
 
         MddHandle initNode = stateSig.getTopVariableHandle().checkInNode(MddExpressionTemplate.of(initExpr, o -> (Decl) o, Z3SolverFactory.getInstance()::createSolver));
 
-//        var c = initNode.getNode().cursor();
-//        var c2 = initNode.get(0).cursor();
-//        c2.moveNext();
-//        c2.moveNext();
-//        c.moveNext();
-//        c.moveNext();
-
-        // x' = x, y' = y - 1, x < 9, y > -9
-        Expr<BoolType> transExpr = And(Eq(declXPrime.getRef(),declX.getRef()), Eq(declYPrime.getRef(),Sub(declY.getRef(),Int(1))), IntExprs.Lt(declXPrime.getRef(), Int(40)), IntExprs.Gt(declYPrime.getRef(), Int(-40)));
-
-        // x' = x + 1, y' = y, x < 9
-        Expr<BoolType> trans2Expr = And(Eq(declXPrime.getRef(),Add(declX.getRef(), Int(1))), Eq(declYPrime.getRef(),declY.getRef()), IntExprs.Lt(declXPrime.getRef(), Int(40)));
+        // x' = x + 1, y' = y - 1, x < 9
+        Expr<BoolType> transExpr = And(Eq(declXPrime.getRef(),Add(declX.getRef(), Int(1))), Eq(declYPrime.getRef(),Sub(declY.getRef(),Int(1))), IntExprs.Lt(declXPrime.getRef(), Int(9)));
 
         MddHandle transitionNode = transSig.getTopVariableHandle().checkInNode(MddExpressionTemplate.of(transExpr, o -> (Decl) o, Z3SolverFactory.getInstance()::createSolver));
-        MddHandle trans2Node = transSig.getTopVariableHandle().checkInNode(MddExpressionTemplate.of(trans2Expr, o -> (Decl) o, Z3SolverFactory.getInstance()::createSolver));
+        AbstractNextStateDescriptor nextStates = MddNodeNextStateDescriptor.of(transitionNode);
 
-        AbstractNextStateDescriptor trans1 = MddNodeNextStateDescriptor.of(transitionNode);
-        AbstractNextStateDescriptor trans2 = MddNodeNextStateDescriptor.of(trans2Node);
+        try (var c = nextStates.rootCursor()) {
+            var c2 = c.valueCursor(0);
+            System.out.println(c2.moveNext());
+            var c3 = c2.valueCursor(3);
+            c3.moveNext();
+        }
 
-        AbstractNextStateDescriptor nextStates = OrNextStateDescriptor.create(List.of(trans1, trans2));
-
-//        var relprod = new RelationalProductProvider(stateSig.getVariableOrder());
-//        var relResult = relprod.compute(initNode, nextStates, stateSig.getTopVariableHandle());
-
-//        var bfs = new BfsProvider(stateSig.getVariableOrder());
-//        var bfsResult = bfs.compute(initNode, nextStates, stateSig.getTopVariableHandle());
-
-//        var c = nextStates.rootCursor();
-//        c.moveNext();
-//        var c2 = c.valueCursor(0);
-//        c2.moveTo(0);
-//
-//        var asd = c2.value();
-
-        var saturation = new CursorGeneralizedSaturationProvider(stateSig.getVariableOrder());
-        var satResult = saturation.compute(initNode, nextStates, stateSig.getTopVariableHandle());
-
-        System.out.println(mddGraph.getUniqueTableSize());
+        var relprod = new CursorRelationalProductProvider(stateSig.getVariableOrder());
+        var relResult = relprod.compute(initNode, nextStates, stateSig.getTopVariableHandle());
 
         System.out.println(Z3SolverFactory.solversCreated);
 
-        final Graph graph = new MddNodeVisualizer(SatTest::nodeToString).visualize(satResult.getNode());
+//        var bfs = new BfsProvider(stateSig.getVariableOrder(), relprod);
+//        var bfsResult = bfs.compute(initNode, nextStates, stateSig.getTopVariableHandle());
+
+        var saturation = new CursorGeneralizedSaturationProvider(stateSig.getVariableOrder(), relprod);
+        var satResult = saturation.compute(initNode, nextStates, stateSig.getTopVariableHandle());
+
+        System.out.println(Z3SolverFactory.solversCreated);
+//
+        final Graph graph = new MddNodeVisualizer(XstsTest::nodeToString).visualize(satResult.getNode());
         try {
             GraphvizWriter.getInstance().writeFile(graph, "build\\mdd.dot");
         } catch (FileNotFoundException e) {
