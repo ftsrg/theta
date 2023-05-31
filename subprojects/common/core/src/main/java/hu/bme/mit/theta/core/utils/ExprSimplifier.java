7568465a5c68b4c71b2e8c5fc868965bab3b21f4
Levente Bajczi <bajczilevi@gmail.com>
2023-05-31 12:59:22 +0200

    No more removing ops in exprsimplifier for bvconcat

--- subprojects/common/core/src/main/java/hu/bme/mit/theta/core/utils/ExprSimplifier.java
+++ subprojects/common/core/src/main/java/hu/bme/mit/theta/core/utils/ExprSimplifier.java
@@ -1174,7 +1174,7 @@ public final class ExprSimplifier {
 				} else {
 					value = value.concat(litOp);
 				}
-				iterator.remove();
+//				iterator.remove();
 			} else {
 				return expr.withOps(ops);
 			}
