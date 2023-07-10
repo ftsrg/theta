6da4981012455386efc078ff636b8aae7d7c3518
leventeBajczi <leventeBajczi@users.noreply.github.com>
2023-07-10 12:21:46 +0000

    Reformatted code

--- subprojects/sts/sts-analysis/src/main/java/hu/bme/mit/theta/sts/analysis/package-info.java
+++ subprojects/sts/sts-analysis/src/main/java/hu/bme/mit/theta/sts/analysis/package-info.java
@@ -16,6 +16,7 @@
 /**
  * This package contains the STS specific analysis modules that can be plugged into the analysis
  * algorithms. There are also different implementations for obtaining an initial precision (see
+ *
  * @link StsInitPrec).
  */
 
