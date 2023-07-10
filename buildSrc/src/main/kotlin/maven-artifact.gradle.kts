1ae9dec3f919906e4a5f4dd938b8f30ce30a0eb7
Levente Bajczi <bajczilevi@gmail.com>
2023-07-10 19:44:51 +0200

    Brought in maven-artifact from xcfa-refactor

--- buildSrc/src/main/kotlin/maven-artifact.gradle.kts
+++ buildSrc/src/main/kotlin/maven-artifact.gradle.kts
@@ -1,18 +1,3 @@
-/*
- *  Copyright 2023 Budapest University of Technology and Economics
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 plugins {
     `java-library`
     `maven-publish`
@@ -32,16 +17,24 @@ open class MavenArtifactExtension(project: Project) {
 
 val artifactConfig = extensions.create<MavenArtifactExtension>("maven-artifact", project)
 
-java {
-    withJavadocJar()
-    withSourcesJar()
+val javadocJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.withType(Javadoc::class.java).getByName("javadoc"))
 }
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
 tasks {
     publishing {
         publications {
             create<MavenPublication>("mavenJava") {
                 artifactId = artifactConfig.artifactId
                 from(components["java"])
+                artifact(sourcesJar)
+                artifact(javadocJar)
                 versionMapping {
                     usage("java-api") {
                         fromResolutionOf("runtimeClasspath")
