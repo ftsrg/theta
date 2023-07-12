8ca9d08525d65a8e129f4866735d7f3cc85a09d2
ThetaBotMaintainer <ThetaBotMaintainer@users.noreply.github.com>
2023-07-12 11:47:55 +0000

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "4.4.4"
+    version = "4.4.5"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
