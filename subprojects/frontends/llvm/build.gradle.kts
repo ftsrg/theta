64ec9365e17a3c32ad18631d758f7c82dcbbf356
Levente Bajczi <bajczilevi@gmail.com>
2023-07-23 23:52:28 +0200

    Only bulding on linux, added install llvm action

--- subprojects/frontends/llvm/build.gradle.kts
+++ subprojects/frontends/llvm/build.gradle.kts
@@ -14,33 +14,20 @@
  *  limitations under the License.
  */
 
+import org.codehaus.plexus.util.Os
 import java.io.ByteArrayOutputStream
 
-/*
- *  Copyright 2023 Budapest University of Technology and Economics
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 plugins {
     id("cpp-library")
 }
 
 fun llvmConfigFlags(vararg args: String): Array<String> {
+    if (!Os.isFamily(Os.FAMILY_UNIX)) return arrayOf()
     val process = ProcessBuilder("llvm-config", *args).start()
     val outputStream = ByteArrayOutputStream()
     process.inputStream.copyTo(outputStream)
     process.waitFor()
-    val ret =  outputStream.toString()
+    val ret = outputStream.toString()
         .trim()
         .split(" ")
         .filter { it.length > 1 }
@@ -58,6 +45,9 @@ library {
             "-I/usr/lib/jvm/java-17-openjdk/include/",
             "-I/usr/lib/jvm/java-17-openjdk/include/linux/",
             *llvmConfigFlags("--cxxflags")))
+        onlyIf {
+            Os.isFamily(Os.FAMILY_UNIX)
+        }
     }
 
     tasks.register("copyLinkedFile", Copy::class) {
@@ -77,5 +67,8 @@ library {
             *llvmConfigFlags("--cxxflags", "--ldflags", "--libs", "core", "bitreader"),
             "-ldl"))
         finalizedBy("copyLinkedFile")
+        onlyIf {
+            Os.isFamily(Os.FAMILY_UNIX)
+        }
     }
 }
