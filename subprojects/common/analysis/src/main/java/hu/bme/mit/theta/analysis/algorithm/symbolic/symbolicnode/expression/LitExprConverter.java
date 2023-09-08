e5537b286a692186a6888bccb69ebdc22a86d410
mondokm <milanmondok1998@gmail.com>
2023-09-08 16:41:10 +0200

    Add simple LitExpr to Int conversion for Arrays

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/symbolicnode/expression/LitExprConverter.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/symbolic/symbolicnode/expression/LitExprConverter.java
@@ -1,8 +1,12 @@
 package hu.bme.mit.theta.analysis.algorithm.symbolic.symbolicnode.expression;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import hu.bme.mit.theta.core.type.LitExpr;
 import hu.bme.mit.theta.core.type.Type;
+import hu.bme.mit.theta.core.type.arraytype.ArrayLitExpr;
+import hu.bme.mit.theta.core.type.arraytype.ArrayType;
 import hu.bme.mit.theta.core.type.booltype.BoolLitExpr;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
 import hu.bme.mit.theta.core.type.inttype.IntLitExpr;
@@ -15,12 +19,23 @@ import java.math.BigInteger;
  */
 public class LitExprConverter {
 
+    private static int cnt = 0;
+    private final static BiMap<Object, Integer> objToInt = HashBiMap.create();
+
     public static int toInt(LitExpr<?> litExpr) {
         if (litExpr instanceof IntLitExpr) {
             return ((IntLitExpr) litExpr).getValue().intValue();
         }
         if (litExpr instanceof BoolLitExpr) {
             return ((BoolLitExpr) litExpr).getValue() ? 1 : 0;
+        }
+        if (litExpr instanceof ArrayLitExpr<?,?>) {
+            if(objToInt.get(litExpr) != null) {
+                return objToInt.get(litExpr);
+            }
+            final int id = cnt++;
+            objToInt.put(litExpr, id);
+            return id;
         }
         throw new UnsupportedOperationException("Unsupported type");
     }
@@ -31,6 +46,9 @@ public class LitExprConverter {
         }
         if (type instanceof BoolType) {
             return BoolLitExpr.of(integer != 0);
+        }
+        if (type instanceof ArrayType<?,?>) {
+            return (LitExpr<?>) objToInt.inverse().get(integer);
         }
         throw new UnsupportedOperationException("Unsupported type");
     }
