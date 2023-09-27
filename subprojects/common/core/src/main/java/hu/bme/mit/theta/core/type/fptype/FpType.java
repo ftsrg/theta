91eaf3c2b2df95829c7e4c1e7d76673c48650075
RipplB <balazs.rippl.official@gmail.com>
2023-09-27 22:56:59 +0200

    Make types have information about their possible domain size

--- subprojects/common/core/src/main/java/hu/bme/mit/theta/core/type/fptype/FpType.java
+++ subprojects/common/core/src/main/java/hu/bme/mit/theta/core/type/fptype/FpType.java
@@ -16,23 +16,11 @@
 package hu.bme.mit.theta.core.type.fptype;
 
 import hu.bme.mit.theta.common.Utils;
+import hu.bme.mit.theta.core.type.DomainSize;
 import hu.bme.mit.theta.core.type.Expr;
-import hu.bme.mit.theta.core.type.abstracttype.AddExpr;
-import hu.bme.mit.theta.core.type.abstracttype.Additive;
-import hu.bme.mit.theta.core.type.abstracttype.DivExpr;
-import hu.bme.mit.theta.core.type.abstracttype.EqExpr;
-import hu.bme.mit.theta.core.type.abstracttype.Equational;
-import hu.bme.mit.theta.core.type.abstracttype.GeqExpr;
-import hu.bme.mit.theta.core.type.abstracttype.GtExpr;
-import hu.bme.mit.theta.core.type.abstracttype.LeqExpr;
-import hu.bme.mit.theta.core.type.abstracttype.LtExpr;
-import hu.bme.mit.theta.core.type.abstracttype.MulExpr;
-import hu.bme.mit.theta.core.type.abstracttype.Multiplicative;
-import hu.bme.mit.theta.core.type.abstracttype.NegExpr;
-import hu.bme.mit.theta.core.type.abstracttype.NeqExpr;
-import hu.bme.mit.theta.core.type.abstracttype.Ordered;
-import hu.bme.mit.theta.core.type.abstracttype.PosExpr;
-import hu.bme.mit.theta.core.type.abstracttype.SubExpr;
+import hu.bme.mit.theta.core.type.abstracttype.*;
+
+import java.math.BigInteger;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -154,5 +142,10 @@ public class FpType implements Equational<FpType>, Additive<FpType>, Multiplicat
     @Override
     public GeqExpr<FpType> Geq(Expr<FpType> leftOp, Expr<FpType> rightOp) {
         return FpExprs.Geq(leftOp, rightOp);
+    }
+
+    @Override
+    public DomainSize getDomainSize() {
+        return DomainSize.of(BigInteger.TWO.pow(significand).multiply(BigInteger.TWO.pow(exponent)));
     }
 }
