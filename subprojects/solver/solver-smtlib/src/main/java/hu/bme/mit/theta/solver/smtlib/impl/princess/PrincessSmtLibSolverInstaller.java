6558151f71625a2db20317d73c4c84932ff59068
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2023-10-05 10:33:24 +0000

    Update existing well-established solvers

--- subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/princess/PrincessSmtLibSolverInstaller.java
+++ subprojects/solver/solver-smtlib/src/main/java/hu/bme/mit/theta/solver/smtlib/impl/princess/PrincessSmtLibSolverInstaller.java
@@ -90,7 +90,7 @@ public class PrincessSmtLibSolverInstaller extends SmtLibSolverInstaller.Default
     @Override
     public List<String> getSupportedVersions() {
         return Arrays.asList(
-                "2022-07-01", "2021-11-15",
+                "2023-06-19", "2023-04-07", "2022-11-03", "2022-07-01", "2021-11-15",
                 "2021-05-10", "2021-03-10", "2020-03-12", "2019-10-02", "2019-07-24",
                 "2018-10-26", "2018-05-25", "2018-01-27", "2017-12-06", "2017-07-17"
         );
