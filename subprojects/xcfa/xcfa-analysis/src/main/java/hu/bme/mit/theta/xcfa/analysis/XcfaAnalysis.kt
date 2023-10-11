a4799e38b8f73a2d4c34b7addd9fb76efe6093f4
s0mark <somorjai.mark@icloud.com>
2023-10-11 21:26:00 +0200

    limited projection to top locations

--- subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/XcfaAnalysis.kt
+++ subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/XcfaAnalysis.kt
@@ -224,7 +224,7 @@ fun <S : XcfaState<out ExprState>, P : XcfaPrec<out Prec>> getXcfaAbstractor(
     XcfaAbstractor.builder(getXcfaArgBuilder(analysis, lts, errorDetection))
         .waitlist(waitlist as Waitlist<ArgNode<S, XcfaAction>>) // TODO: can we do this nicely?
         .stopCriterion(stopCriterion as StopCriterion<S, XcfaAction>).logger(logger)
-        .projection { it.processes }
+        .projection { it.processes.map { (_, p) -> p.locs.peek() } }
         .build() // TODO: can we do this nicely?
 
 /// EXPL
