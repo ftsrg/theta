0928025100d5bfc06f8b33dacd687b2b2d76fda6
s0mark <somorjai.mark@icloud.com>
2023-11-13 21:08:24 +0100

    extracted partial order with stacks

--- subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/XcfaAnalysis.kt
+++ subprojects/xcfa/xcfa-analysis/src/main/java/hu/bme/mit/theta/xcfa/analysis/XcfaAnalysis.kt
@@ -30,11 +30,12 @@ import hu.bme.mit.theta.analysis.pred.*
 import hu.bme.mit.theta.analysis.pred.PredAbstractors.PredAbstractor
 import hu.bme.mit.theta.analysis.waitlist.Waitlist
 import hu.bme.mit.theta.common.logging.Logger
+import hu.bme.mit.theta.core.decl.Decl
 import hu.bme.mit.theta.core.decl.Decls.Var
 import hu.bme.mit.theta.core.decl.VarDecl
+import hu.bme.mit.theta.core.model.ImmutableValuation
+import hu.bme.mit.theta.core.model.Valuation
 import hu.bme.mit.theta.core.stmt.Stmts
-import hu.bme.mit.theta.core.type.LitExpr
-import hu.bme.mit.theta.core.type.Type
 import hu.bme.mit.theta.core.type.booltype.BoolExprs.True
 import hu.bme.mit.theta.core.utils.TypeUtils
 import hu.bme.mit.theta.solver.Solver
@@ -167,46 +168,47 @@ fun getXcfaErrorPredicate(
 
 private fun<K, V> Map<K, V>.reverseMapping() = this.entries.associate { it.value to it.key }
 
-fun<S : ExprState> stackIsLeq(s1: XcfaState<S>, s2: XcfaState<S>) = s2.processes.keys.all { pid ->
-    s1.processes[pid]?.let { s2.processes.getValue(pid).isLeq(it) } ?: false
+private fun<S : ExprState> stackIsLeq(s1: XcfaState<S>, s2: XcfaState<S>) = s2.processes.keys.all { pid ->
+    s1.processes[pid]?.let { ps1 ->
+        val ps2 = s2.processes.getValue(pid)
+        ps1.locs.peek() == ps2.locs.peek() && ps1.paramsInitialized && ps2.paramsInitialized
+    } ?: false
 }
 
-private fun valIsLeq(val1: Map<VarDecl<out Type>, Optional<out LitExpr<out Type>>>, val2: Map<VarDecl<out Type>, Optional<out LitExpr<out Type>>>): Boolean {
-    if (val2.size > val1.size) return false
-    for (varDecl in val2.keys) {
-        if (val1[varDecl] != val2[varDecl]) {
-            return false
-        }
+private fun Valuation.changeVars(varLut: Map<out Decl<*>, VarDecl<*>>): Valuation {
+    val builder = ImmutableValuation.builder()
+    for (decl in this.decls) {
+        builder.put(decl.changeVars(varLut), this.eval(decl).get())
     }
-    return true
+    return builder.build()
+}
+
+private fun <S : ExprState> XcfaState<S>.withGeneralizedVarInstances(): S {
+    val varLookup = processes.mapNotNull { (_, process) -> process.varLookup.peek()?.reverseMapping() }
+                             .reduceOrNull(Map<VarDecl<*>, VarDecl<*>>::plus) ?: mapOf()
+    return when (sGlobal) {
+        is ExplState -> ExplState.of(sGlobal.getVal().changeVars(varLookup))
+        is PredState -> PredState.of(sGlobal.preds.map { p -> p.changeVars(varLookup) })
+        else -> throw NotImplementedError("Generalizing variable instances is not implemented for data states that are not explicit or predicate.")
+    } as S
 }
 
 fun <S : ExprState> getPartialOrder(partialOrd: PartialOrd<S>) =
     PartialOrd<XcfaState<S>> { s1, s2 ->
-        val locIsLeq = s1.processes.size == s2.processes.size && stackIsLeq(s1, s2) && s1.bottom == s2.bottom && s1.mutexes == s2.mutexes
-        if (locIsLeq) {
-            val s1Global = s1.sGlobal
-            val s2Global = s2.sGlobal
-            val s1VarLookup = s1.processes[0]?.varLookup?.peek()?.reverseMapping() ?: mapOf()
-            val s2VarLookup = s2.processes[0]?.varLookup?.peek()?.reverseMapping() ?: mapOf()
-            if (s1Global is ExplState && s2Global is ExplState) {
-                val s1Val = s1Global.decls.associate { v -> v.changeVars(s1VarLookup) to s1Global.eval(v) }
-                val s2Val = s2Global.decls.associate { v -> v.changeVars(s2VarLookup) to s2Global.eval(v) }
-                valIsLeq(s1Val, s2Val)
-            } else if (s1Global is PredState && s2Global is PredState) {
-                val s1Translated = PredState.of(s1Global.preds.map { p -> p.changeVars(s1VarLookup) })
-                val s2Translated = PredState.of(s2Global.preds.map { p -> p.changeVars(s2VarLookup) })
-                partialOrd.isLeq(s1Translated as S, s2Translated as S)
-            } else
-                partialOrd.isLeq(s1.sGlobal, s2.sGlobal)
-        } else false
+        s1.processes == s2.processes && s1.bottom == s2.bottom && s1.mutexes == s2.mutexes && partialOrd.isLeq(s1.sGlobal, s2.sGlobal)
+    }
+
+fun <S : ExprState> getStackPartialOrder(partialOrd: PartialOrd<S>) =
+    PartialOrd<XcfaState<S>> { s1, s2 ->
+        s1.processes.size == s2.processes.size && stackIsLeq(s1, s2) && s1.bottom == s2.bottom && s1.mutexes == s2.mutexes
+            && partialOrd.isLeq(s1.withGeneralizedVarInstances(), s2.withGeneralizedVarInstances())
     }
 
 private fun <S : XcfaState<out ExprState>, P : XcfaPrec<out Prec>> getXcfaArgBuilder(
     analysis: Analysis<S, XcfaAction, P>,
     lts: LTS<XcfaState<out ExprState>, XcfaAction>,
     errorDetection: ErrorDetection)
-    : ArgBuilder<S, XcfaAction, P> =
+        : ArgBuilder<S, XcfaAction, P> =
     ArgBuilder.create(
         lts,
         analysis,
@@ -230,7 +232,7 @@ fun <S : XcfaState<out ExprState>, P : XcfaPrec<out Prec>> getXcfaAbstractor(
 /// EXPL
 
 private fun getExplXcfaInitFunc(xcfa: XCFA,
-    solver: Solver): (XcfaPrec<ExplPrec>) -> List<XcfaState<ExplState>> {
+                                solver: Solver): (XcfaPrec<ExplPrec>) -> List<XcfaState<ExplState>> {
     val processInitState = xcfa.initProcedures.mapIndexed { i, it ->
         val initLocStack: LinkedList<XcfaLocation> = LinkedList()
         initLocStack.add(it.first.initLoc)
@@ -244,7 +246,7 @@ private fun getExplXcfaInitFunc(xcfa: XCFA,
 }
 
 private fun getExplXcfaTransFunc(solver: Solver,
-    maxEnum: Int): (XcfaState<ExplState>, XcfaAction, XcfaPrec<ExplPrec>) -> List<XcfaState<ExplState>> {
+                                 maxEnum: Int): (XcfaState<ExplState>, XcfaAction, XcfaPrec<ExplPrec>) -> List<XcfaState<ExplState>> {
     val explTransFunc = ExplStmtTransFunc.create(solver, maxEnum)
     return { s, a, p ->
         val (newSt, newAct) = s.apply(a)
@@ -255,7 +257,7 @@ private fun getExplXcfaTransFunc(solver: Solver,
 }
 
 class ExplXcfaAnalysis(xcfa: XCFA, solver: Solver, maxEnum: Int,
-    partialOrd: PartialOrd<XcfaState<ExplState>>) : XcfaAnalysis<ExplState, ExplPrec>(
+                       partialOrd: PartialOrd<XcfaState<ExplState>>) : XcfaAnalysis<ExplState, ExplPrec>(
     corePartialOrd = partialOrd,
     coreInitFunc = getExplXcfaInitFunc(xcfa, solver),
     coreTransFunc = getExplXcfaTransFunc(solver, maxEnum)
@@ -264,7 +266,7 @@ class ExplXcfaAnalysis(xcfa: XCFA, solver: Solver, maxEnum: Int,
 /// PRED
 
 private fun getPredXcfaInitFunc(xcfa: XCFA,
-    predAbstractor: PredAbstractor): (XcfaPrec<PredPrec>) -> List<XcfaState<PredState>> {
+                                predAbstractor: PredAbstractor): (XcfaPrec<PredPrec>) -> List<XcfaState<PredState>> {
     val processInitState = xcfa.initProcedures.mapIndexed { i, it ->
         val initLocStack: LinkedList<XcfaLocation> = LinkedList()
         initLocStack.add(it.first.initLoc)
@@ -289,7 +291,7 @@ private fun getPredXcfaTransFunc(
 }
 
 class PredXcfaAnalysis(xcfa: XCFA, solver: Solver, predAbstractor: PredAbstractor,
-    partialOrd: PartialOrd<XcfaState<PredState>>) : XcfaAnalysis<PredState, PredPrec>(
+                       partialOrd: PartialOrd<XcfaState<PredState>>) : XcfaAnalysis<PredState, PredPrec>(
     corePartialOrd = partialOrd,
     coreInitFunc = getPredXcfaInitFunc(xcfa, predAbstractor),
     coreTransFunc = getPredXcfaTransFunc(predAbstractor)
