ed852f9bff376cdf6e991032883db2ac11cea606
mondokm <milanmondok1998@gmail.com>
2024-02-13 10:30:30 +0100

    Added RandomXsts and XstsSerializer

---
+++ subprojects/xsts/xsts-cli/src/main/java/hu/bme/mit/theta/xsts/cli/XstsSerializer.kt
@@ -0,0 +1,87 @@
+import hu.bme.mit.theta.core.dsl.impl.ExprWriter
+import hu.bme.mit.theta.core.stmt.*
+import hu.bme.mit.theta.core.type.Expr
+import hu.bme.mit.theta.core.type.Type
+import hu.bme.mit.theta.core.type.booltype.BoolType
+import hu.bme.mit.theta.core.type.inttype.IntType
+import hu.bme.mit.theta.xsts.XSTS
+
+object XstsSerializer: StmtVisitor<Void?, String> {
+
+    val typeNames = mapOf(
+        BoolType.getInstance() to "boolean",
+        IntType.getInstance() to "integer"
+    ).withDefault { it.toString() }
+
+    fun serializeXsts(xsts: XSTS): String {
+        val builder = StringBuilder()
+        for (ctrlVar in xsts.ctrlVars) {
+            builder.appendLine("ctrl var ${ctrlVar.name} : integer")
+        }
+        for (v in xsts.vars - xsts.ctrlVars) {
+            builder.appendLine("var ${v.name} : ${typeNames[v.type]}")
+        }
+        builder.appendLine()
+
+        builder.appendLine("init {\n assume (${serializeExpr(xsts.initFormula)});\n${xsts.init.accept(this, null)}\n}")
+        builder.appendLine("trans {\n${xsts.tran.accept(this, null)}\n}")
+        builder.appendLine("env {\n${xsts.env.accept(this, null)}\n}")
+
+        return builder.toString()
+    }
+
+    fun serializeExpr(expr: Expr<*>): String{
+        return ExprWriter.instance().write(expr)
+    }
+
+    override fun visit(stmt: SkipStmt?, param: Void?): String {
+        return ""
+    }
+
+    override fun visit(stmt: AssumeStmt, param: Void?): String {
+        return "assume (${serializeExpr(stmt.cond)});"
+    }
+
+    override fun <DeclType : Type?> visit(stmt: AssignStmt<DeclType>, param: Void?): String {
+        return "${stmt.varDecl.name} := (${serializeExpr(stmt.expr)});"
+    }
+
+    override fun <DeclType : Type?> visit(stmt: HavocStmt<DeclType>, param: Void?): String {
+        return "havoc ${stmt.varDecl.name};"
+    }
+
+    override fun visit(stmt: SequenceStmt, param: Void?): String {
+        return stmt.stmts.joinToString("\n") { "${it.accept(this, null)}" }
+    }
+
+    override fun visit(stmt: NonDetStmt, param: Void?): String {
+        return "choice ${stmt.stmts.joinToString(" or ") { "{\n${it.accept(this, null)}\n}"}}"
+    }
+
+    override fun visit(stmt: OrtStmt?, param: Void?): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(stmt: LoopStmt, param: Void?): String {
+        /*
+            for i from 1 to y+1 do {
+                x:=x-1;
+            }
+         */
+        return """
+            for ${stmt.loopVariable.name} from (${serializeExpr(stmt.from)}) to (${serializeExpr(stmt.to)}) do {
+                ${stmt.stmt.accept(this, null)}
+            }""".trimIndent()
+    }
+
+    override fun visit(stmt: IfStmt, param: Void?): String {
+        return """
+            if (${serializeExpr(stmt.cond)}) {
+                ${stmt.then.accept(this, null)}
+            } else {
+                ${stmt.elze.accept(this, null)}
+            }
+        """.trimIndent()
+    }
+
+}
