517fec5c0c2824355b66bfdff3831918c1bb0482
mondokm <milanmondok1998@gmail.com>
2024-02-13 10:29:35 +0100

    Added kotlin support

--- buildSrc/src/main/kotlin/kotlin-common.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-common.gradle.kts
@@ -15,13 +15,11 @@
  */
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 apply(plugin = "java-common")
 apply<KotlinPlatformJvmPlugin>()
 dependencies {
     val implementation: Configuration by configurations
     implementation(Deps.Kotlin.stdlib)
-    implementation(Deps.Kotlin.reflect)
 }
 tasks {
     withType<KotlinCompile>() {
