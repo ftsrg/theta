ffce746cc1797a402df011e21e4bd343620d6cd3
mondokm <milanmondok1998@gmail.com>
2024-02-22 13:18:06 +0100

    Add EmptyWitness and EmptyCex

---
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/EmptyWitness.java
@@ -0,0 +1,14 @@
+package hu.bme.mit.theta.analysis.algorithm;
+
+public class EmptyWitness implements Witness{
+
+    private final static EmptyWitness empty = new EmptyWitness();
+
+    private EmptyWitness() {
+    }
+
+    public static EmptyWitness getInstance() {
+        return empty;
+    }
+
+}
