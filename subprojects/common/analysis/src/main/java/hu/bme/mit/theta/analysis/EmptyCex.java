eafc59f15f71c17faf0c10cc96c9dd12b9c73ae1
mondokm <milanmondok1998@gmail.com>
2024-02-22 13:18:06 +0100

    Add EmptyWitness and EmptyCex

---
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/EmptyCex.java
@@ -0,0 +1,18 @@
+package hu.bme.mit.theta.analysis;
+
+public class EmptyCex implements Cex{
+
+    private final static EmptyCex empty = new EmptyCex();
+
+    private EmptyCex() {
+    }
+
+    public static EmptyCex getInstance() {
+        return empty;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+}
