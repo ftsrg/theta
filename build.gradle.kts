f9c8a2c2c8085d97020b788ae00d8511ca99e22f
Bajczi Levente <bajczilevi@gmail.com>
2024-03-19 18:13:18 +0100

    Disabled llvm-related modules unless clang-15 is installed (#260)
    * disabled llvm-related modules unless clang-15 is installed
    
    * handle when clang is not installed
    
    * Fixed dependency management for llvm-related subprojects
    
    * Format
    
    * no latinit
    
    * better logging
    
    * added back dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "5.0.4"
+    version = "5.0.5"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
