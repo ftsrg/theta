aac5977bcca0477ad1f7df6e3029f71e583ff531
RipplB <balrip.play@gmail.com>
2024-07-26 15:04:53 +0200

    Change all reference of codestyle to new path

--- scripts/reformat.sh
+++ scripts/reformat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 scriptdir=$(dirname $(realpath "$0"))
 
-docker run -v $scriptdir/..:/github/workspace ghcr.io/leventebajczi/intellij-format:latest '*java,*kts,*kt' "" ./doc/ThetaIntelliJCodeStyle.xml
+docker run -v $scriptdir/..:/github/workspace ghcr.io/leventebajczi/intellij-format:latest '*java,*kts,*kt' "" ./.idea/codeStyles/Project.xml
 
