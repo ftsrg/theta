687755fe28a3a38060ed15e2a02048c1002b0545
mondokm <milanmondok1998@gmail.com>
2024-07-30 17:05:54 +0200

    Move XstsToMonolithicExpr.kt

--- subprojects/xsts/xsts-analysis/src/main/kotlin/hu/bme/mit/theta/xsts/analysis/XstsToMonolithicExpr.kt
+++ subprojects/xsts/xsts-analysis/src/main/kotlin/hu/bme/mit/theta/xsts/analysis/XstsToMonolithicExpr.kt
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+package hu.bme.mit.theta.xsts.analysis.hu.bme.mit.theta.xsts.analysis
 
 import hu.bme.mit.theta.analysis.algorithm.bounded.MonolithicExpr
 import hu.bme.mit.theta.analysis.expl.ExplState
