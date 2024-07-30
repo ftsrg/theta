ae9acb6f9a05637200878e4c59ccb2530c4bd724
mondokm <milanmondok1998@gmail.com>
2024-07-30 15:43:58 +0200

    Add valToState and biValToAction for CFA

--- subprojects/cfa/cfa-analysis/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/CfaToMonolithicExpr.kt
+++ subprojects/cfa/cfa-analysis/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/CfaToMonolithicExpr.kt
@@ -18,13 +18,19 @@ package hu.bme.mit.theta.cfa.analysis;
 
 import com.google.common.base.Preconditions;
 import hu.bme.mit.theta.analysis.algorithm.bounded.MonolithicExpr
+import hu.bme.mit.theta.analysis.expl.ExplState
+import hu.bme.mit.theta.analysis.ptr.PtrState
 import hu.bme.mit.theta.cfa.CFA;
 import hu.bme.mit.theta.core.decl.Decls;
+import hu.bme.mit.theta.core.model.ImmutableValuation
+import hu.bme.mit.theta.core.model.Valuation
 import hu.bme.mit.theta.core.stmt.*
 import hu.bme.mit.theta.core.type.booltype.BoolExprs.And
 import hu.bme.mit.theta.core.type.inttype.IntExprs.*
+import hu.bme.mit.theta.core.type.inttype.IntLitExpr
 import hu.bme.mit.theta.core.utils.StmtUtils;
 import hu.bme.mit.theta.core.utils.indexings.VarIndexingFactory;
+import java.util.*
 
 fun CFA.toMonolithicExpr(): MonolithicExpr {
     Preconditions.checkArgument(this.errorLoc.isPresent);
@@ -33,7 +39,7 @@ fun CFA.toMonolithicExpr(): MonolithicExpr {
     for ((i, x) in this.locs.withIndex()) {
         map[x] = i;
     }
-    val locVar = Decls.Var("loc", Int())
+    val locVar = Decls.Var("__loc__", Int())
     val tranList = this.edges.map { e ->
         SequenceStmt.of(listOf(
             AssumeStmt.of(Eq(locVar.ref, Int(map[e.source]!!))),
@@ -50,5 +56,33 @@ fun CFA.toMonolithicExpr(): MonolithicExpr {
     val offsetIndex = transUnfold.indexing
 
     return MonolithicExpr(initExpr, transExpr, propExpr, offsetIndex)
+}
+
+fun CFA.valToAction(val1: Valuation, val2: Valuation): CfaAction {
+    val val1Map = val1.toMap()
+    val val2Map = val2.toMap()
+    var i = 0
+    val map: MutableMap<CFA.Loc, Int> = mutableMapOf()
+    for (x in this.locs) {
+        map[x] = i++
+    }
+    return CfaAction.create(
+        this.edges.first { edge ->
+            map[edge.source] == (val1Map[val1Map.keys.first { it.name == "__loc_" }] as IntLitExpr).value.toInt() &&
+                map[edge.target] == (val2Map[val2Map.keys.first { it.name == "__loc_" }] as IntLitExpr).value.toInt()
+        })
+}
+
+fun CFA.valToState(val1: Valuation): CfaState<ExplState> {
+    val valMap = val1.toMap()
+    var i = 0
+    val map: MutableMap<Int, CFA.Loc> = mutableMapOf()
+    for (x in this.locs) {
+        map[i++] = x
+    }
+    return CfaState.of(
+        map[(valMap[valMap.keys.first { it.name == "__loc_" }] as IntLitExpr).value.toInt()],
+        ExplState.of(val1)
+    )
 }
 
