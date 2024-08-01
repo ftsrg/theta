4207f50d7493b6711316546766e20c80007b318d
Bajczi Levente <bajczilevi@gmail.com>
2024-08-01 23:49:01 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.3.1"
+    version = "6.3.2"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
