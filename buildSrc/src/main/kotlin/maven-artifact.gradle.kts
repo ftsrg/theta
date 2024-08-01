9fb3f1ad56aa6f6798141b358a46c1bed2f5cd30
Levente Bajczi <bajczilevi@gmail.com>
2024-08-02 00:50:21 +0200

    added explicit dependency (now working)

--- buildSrc/src/main/kotlin/maven-artifact.gradle.kts
+++ buildSrc/src/main/kotlin/maven-artifact.gradle.kts
@@ -50,7 +50,7 @@ val sourcesJar by tasks.creating(Jar::class) {
 project.plugins.withType<AntlrPlugin> {
     val generateGrammarTask = tasks.findByName("generateGrammarSource")
     if (generateGrammarTask != null) {
-        sourcesJar.configure<Jar> { dependsOn(generateGrammarTask) }
+        sourcesJar.configure<Task> { dependsOn(generateGrammarTask) }
     }
 }
 
