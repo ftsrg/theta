491250745f918928761a18354a3781b0bb4056fb
mondokm <milanmondok1998@gmail.com>
2024-08-05 10:44:20 +0200

    Resolve rebase conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.3.4"
+    version = "6.4.0"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
