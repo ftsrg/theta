d683cb66e6fb58f7110f9437424a680791a1986e
mondokm <milanmondok1998@gmail.com>
2024-08-07 15:56:15 +0200

    Add new subcommand

--- subprojects/xsts/xsts-cli/src/main/kotlin/hu/bme/mit/theta/xsts/cli/XstsCliMain.kt
+++ subprojects/xsts/xsts-cli/src/main/kotlin/hu/bme/mit/theta/xsts/cli/XstsCliMain.kt
@@ -38,5 +38,5 @@ class XstsCliMainCommand : CliktCommand() {
 }
 
 fun main(args: Array<String>) =
-    XstsCliMainCommand().subcommands(XstsCliCegar(), XstsCliMdd(), XstsCliBounded(), XstsCliHeader(), XstsCliMetrics())
+    XstsCliMainCommand().subcommands(XstsCliCegar(), XstsCliBounded(), XstsCliMdd(), XstsCliPetrinetMdd(), XstsCliHeader(), XstsCliMetrics())
         .main(args)
