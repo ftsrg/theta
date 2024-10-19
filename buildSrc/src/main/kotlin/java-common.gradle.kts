3201270734dc5079bd4dab705326104baa1faace
Levente Bajczi <bajczilevi@gmail.com>
2024-10-19 15:53:48 +0200

    Syntax fix in gradle

--- buildSrc/src/main/kotlin/java-common.gradle.kts
+++ buildSrc/src/main/kotlin/java-common.gradle.kts
@@ -70,7 +70,7 @@ tasks {
 spotless {
     ratchetFrom("origin/master")
 
-    enforceCheck false
+    enforceCheck(false)
     
     val year = "\$YEAR" // you can't escape $ in raw strings..
     val licenseHeader = """            /*
