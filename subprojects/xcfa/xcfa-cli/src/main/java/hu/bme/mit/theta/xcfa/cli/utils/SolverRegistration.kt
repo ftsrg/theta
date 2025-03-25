f65739651839bc8778b7c5b5d23f3c238cb37a72
mondokm <milanmondok1998@gmail.com>
2025-03-25 11:31:06 +0100

    Enable smtlib for windows in xcfa cli

--- subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/utils/SolverRegistration.kt
+++ subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/utils/SolverRegistration.kt
@@ -41,10 +41,9 @@ fun registerAllSolverManagers(home: String, logger: Logger) {
   logger.write(Logger.Level.INFO, "Registered Z3 SolverManager\n")
   SolverManager.registerSolverManager(JavaSMTSolverManager.create())
   logger.write(Logger.Level.INFO, "Registered JavaSMT SolverManager\n")
-  if (OsHelper.getOs() == OsHelper.OperatingSystem.LINUX) {
-    val homePath = Path.of(home)
-    val smtLibSolverManager: SmtLibSolverManager = SmtLibSolverManager.create(homePath, logger)
-    SolverManager.registerSolverManager(smtLibSolverManager)
-    logger.write(Logger.Level.INFO, "Registered SMT-LIB SolverManager\n")
-  }
+  val homePath = Path.of(home)
+  val smtLibSolverManager: SmtLibSolverManager = SmtLibSolverManager.create(homePath, logger)
+  SolverManager.registerSolverManager(smtLibSolverManager)
+  logger.write(Logger.Level.INFO, "Registered SMT-LIB SolverManager\n")
+
 }
