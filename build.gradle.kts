bfe83677bf1b0e05140125afd9205a50dfdf4260
Levente Bajczi <bajczilevi@gmail.com>
2025-05-21 00:06:35 +0200

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.15.2"
+    version = "6.15.3"
 
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
