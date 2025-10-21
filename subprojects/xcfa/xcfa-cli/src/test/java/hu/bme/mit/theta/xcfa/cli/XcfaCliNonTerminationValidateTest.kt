69baa92b5484a3916deba0fe09d1d49c664cb891
AdamZsofi <adamzsofi.mail@gmail.com>
2025-10-21 19:55:09 +0200

    added nonterm validation tests; asg and chc prop issue

--- subprojects/xcfa/xcfa-cli/src/test/java/hu/bme/mit/theta/xcfa/cli/XcfaCliNonTerminationValidateTest.kt
+++ subprojects/xcfa/xcfa-cli/src/test/java/hu/bme/mit/theta/xcfa/cli/XcfaCliNonTerminationValidateTest.kt
@@ -37,8 +37,7 @@ class XcfaCliNonTerminationValidateTest {
   companion object {
 
     private val SMTLIB_HOME: Path = SmtLibSolverManager.HOME
-    private val solvers =
-      listOf("z3:4.13.0", "eldarica:2.1", "golem:0.5.0", "mathsat:5.6.10", "cvc5:1.0.8")
+    private val solvers = listOf("z3:4.13.0", "mathsat:5.6.10")
 
     private fun installSolver(name: String) {
       try {
@@ -69,77 +68,87 @@ class XcfaCliNonTerminationValidateTest {
     }
 
     @JvmStatic
-    fun cFiles(): Stream<Arguments> {
-      return Stream.of(
-        Arguments.of(
-          "/c/nontermination/NO_03.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/NO_03.yml")!!.path,
-        ),
-        Arguments.of(
-          "/c/nontermination/Pendulum.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum.yml")!!.path,
-        ),
-        Arguments.of(
-          "/c/nontermination/Pendulum-2.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum-2.yml")!!.path,
-        ),
-      )
-    }
-
-    @JvmStatic
-    fun cFilesWrongWitness(): Stream<Arguments> {
+    fun witnessFiles(): Stream<Arguments> {
       return Stream.of(
         Arguments.of(
           "/c/nontermination/Pendulum.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum-short-wrong.yml")!!.path,
+          "/c/nontermination/Pendulum.yml",
+          "--property /c/nontermination/prop/termination.prp",
         ),
         Arguments.of(
           "/c/nontermination/Pendulum-2.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum-2-wrong-loop-wrong.yml")!!.path,
-        ),
-      )
-    }
-
-    @JvmStatic
-    fun cFilesAdvanced(): Stream<Arguments> {
-      return Stream.of(
-        Arguments.of(
-          "/c/nontermination/NO_03.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/NO_03.yml")!!.path,
-        ),
-        Arguments.of(
-          "/c/nontermination/Pendulum.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum.yml")!!.path,
-        ),
-        Arguments.of(
-          "/c/nontermination/Pendulum-2.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Pendulum-2.yml")!!.path,
+          "/c/nontermination/Pendulum-2.yml",
+          "--property /c/nontermination/prop/termination.prp",
         ),
         Arguments.of(
           "/c/nontermination/Ex02.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Ex02.yml")!!.path,
+          "/c/nontermination/Ex02.yml",
+          "--property /c/nontermination/prop/termination.prp",
         ),
         Arguments.of(
           "/c/nontermination/Swingers.c",
-          "--property /c/nontermination/prop/termination.prp --witness " +
-            javaClass.getResource("/c/nontermination/Swingers.yml")!!.path,
+          "/c/nontermination/Swingers.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+      )
+    }
+
+    @JvmStatic
+    fun wrongWitnessFiles(): Stream<Arguments> {
+      return Stream.of(
+        Arguments.of(
+          "/c/nontermination/Pendulum-2.c",
+          "/c/nontermination/Pendulum-2-wrong-loop-wrong.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        )
+        /*Arguments.of(
+          "/c/nontermination/Pendulum.c",
+          "/c/nontermination/Pendulum-short-wrong.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+        Arguments.of(
+          "/c/nontermination/Swingers.c",
+          "/c/nontermination/Swingers-wrong.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),*/
+      )
+    }
+
+    @JvmStatic
+    fun witnessFilesAdvanced(): Stream<Arguments> {
+      return Stream.of(
+        Arguments.of(
+          "/c/nontermination/Pendulum.c",
+          "/c/nontermination/Pendulum.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+        Arguments.of(
+          "/c/nontermination/Pendulum-2.c",
+          "/c/nontermination/Pendulum-2.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+        Arguments.of(
+          "/c/nontermination/Ex02.c",
+          "/c/nontermination/Ex02.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+        Arguments.of(
+          "/c/nontermination/Swingers.c",
+          "/c/nontermination/Swingers.yml",
+          "--property /c/nontermination/prop/termination.prp",
+        ),
+        Arguments.of(
+          "/c/nontermination/NO_03.c",
+          "/c/nontermination/NO_03.yml",
+          "--property /c/nontermination/prop/termination.prp",
         ),
       )
     }
   }
 
   @ParameterizedTest
-  @MethodSource("cFiles")
-  fun testCValidateAsgCegar(filePath: String, extraArgs: String?) {
+  @MethodSource("witnessFiles")
+  fun testCValidateAsgCegar(filePath: String, witnessPath: String, extraArgs: String?) {
     val temp = createTempDirectory()
 
     val params =
@@ -148,10 +157,10 @@ class XcfaCliNonTerminationValidateTest {
         "C",
         "--input",
         javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
         "--backend",
         "ASGCEGAR",
-        "--initprec",
-        "ALLASSUMES",
         "--stacktrace",
         *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
         "--debug",
@@ -169,8 +178,8 @@ class XcfaCliNonTerminationValidateTest {
   }
 
   @ParameterizedTest
-  @MethodSource("cFilesWrongWitness")
-  fun testCValidateAsgCegarWrongWitness(filePath: String, extraArgs: String?) {
+  @MethodSource("witnessFiles")
+  fun testCValidateServer(filePath: String, witnessPath: String, extraArgs: String?) {
     val temp = createTempDirectory()
 
     val params =
@@ -179,8 +188,86 @@ class XcfaCliNonTerminationValidateTest {
         "C",
         "--input",
         javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--backend",
+        "ASGCEGAR",
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    try {
+      main(params)
+      assertTrue(temp.resolve("witness.yml").exists())
+      val witnessContents = temp.resolve("witness.yml").toFile().readText()
+      assertTrue(
+        "entry_type: \"violation_sequence\"" in witnessContents,
+        "No violation witness was produced!",
+      )
+    } catch (e: IllegalStateException) {
+      if (!e.message.equals("Done debugging")) {
+        throw e
+      }
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("witnessFiles")
+  fun testCValidateTerminationPortfolio(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--input-type",
+        "C",
+        "--backend",
+        "PORTFOLIO",
+        "--portfolio",
+        "TERMINATION",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    try {
+      main(params)
+      assertTrue(temp.resolve("witness.yml").exists())
+      val witnessContents = temp.resolve("witness.yml").toFile().readText()
+      assertTrue(
+        "entry_type: \"violation_sequence\"" in witnessContents,
+        "No violation witness was produced!",
+      )
+    } catch (e: Throwable) {
+      if (!e.toString().contains("Done debugging")) {
+        throw e
+      }
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("witnessFilesAdvanced")
+  fun testCValidateKind(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
         "--backend",
         "BOUNDED",
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
         "--stacktrace",
         *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
         "--debug",
@@ -189,12 +276,42 @@ class XcfaCliNonTerminationValidateTest {
         "--enable-output",
       )
     main(params)
-    assertFalse(temp.resolve("witness.yml").exists())
+    assertTrue(temp.resolve("witness.yml").exists())
+    val witnessContents = temp.resolve("witness.yml").toFile().readText()
+    assertTrue(
+      "entry_type: \"violation_sequence\"" in witnessContents,
+      "No violation witness was produced!",
+    )
   }
 
   @ParameterizedTest
-  @MethodSource("cFilesAdvanced")
-  fun testCValidateKind(filePath: String, extraArgs: String?) {
+  @MethodSource("witnessFilesAdvanced")
+  fun testCValidateMDD(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--backend",
+        "MDD",
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+  }
+
+  @ParameterizedTest
+  @MethodSource("witnessFilesAdvanced")
+  fun testCValidateIMC(filePath: String, witnessPath: String, extraArgs: String?) {
     val temp = createTempDirectory()
 
     val params =
@@ -205,37 +322,44 @@ class XcfaCliNonTerminationValidateTest {
         "C",
         "--input",
         javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
         "--stacktrace",
         *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
         "--debug",
         "--output-directory",
         temp.absolutePathString(),
         "--enable-output",
+      )
+    main(params)
+    assertTrue(temp.resolve("witness.yml").exists())
+    val witnessContents = temp.resolve("witness.yml").toFile().readText()
+    assertTrue(
+      "entry_type: \"violation_sequence\"" in witnessContents,
+      "No violation witness was produced!",
+    )
+  }
+
+  @ParameterizedTest
+  @MethodSource("witnessFilesAdvanced")
+  fun testCValidateBoundedPortfolio(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    Assumptions.assumeTrue(OsHelper.getOs().equals(OsHelper.OperatingSystem.LINUX))
+    val params =
+      arrayOf(
+        "--backend",
+        "PORTFOLIO",
+        "--portfolio",
+        "BOUNDED",
+        "--input-type",
+        "C",
         "--loglevel",
-        "VERBOSE",
-      )
-    main(params)
-    assertTrue(temp.resolve("witness.yml").exists())
-    val witnessContents = temp.resolve("witness.yml").toFile().readText()
-    assertTrue(
-      "entry_type: \"violation_sequence\"" in witnessContents,
-      "No violation witness was produced!",
-    )
-  }
-
-  @ParameterizedTest
-  @MethodSource("cFilesAdvanced")
-  fun testCValidateIMC(filePath: String, extraArgs: String?) {
-    val temp = createTempDirectory()
-
-    val params =
-      arrayOf(
-        "--backend",
-        "BOUNDED",
-        "--input-type",
-        "C",
+        "INFO",
         "--input",
         javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
         "--stacktrace",
         *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
         "--debug",
@@ -253,8 +377,8 @@ class XcfaCliNonTerminationValidateTest {
   }
 
   @ParameterizedTest
-  @MethodSource("cFilesAdvanced")
-  fun testCValidateCHC(filePath: String, extraArgs: String?) {
+  @MethodSource("witnessFilesAdvanced")
+  fun testCValidateCHC(filePath: String, witnessPath: String, extraArgs: String?) {
     val temp = createTempDirectory()
 
     Assumptions.assumeTrue(OsHelper.getOs().equals(OsHelper.OperatingSystem.LINUX))
@@ -269,6 +393,8 @@ class XcfaCliNonTerminationValidateTest {
         "C",
         "--input",
         javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
         "--loglevel",
         "INFO",
         "--stacktrace",
@@ -285,5 +411,196 @@ class XcfaCliNonTerminationValidateTest {
       "entry_type: \"violation_sequence\"" in witnessContents,
       "No violation witness was produced!",
     )
+  }
+
+  /////////// wrong witnesses ///////////
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongAsgCegar(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--backend",
+        "ASGCEGAR",
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+    // TODO: add correctness check once correctness termination witnesses are added
+    assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
+  }
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongTerminationPortfolio(
+    filePath: String,
+    witnessPath: String,
+    extraArgs: String?,
+  ) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--input-type",
+        "C",
+        "--backend",
+        "PORTFOLIO",
+        "--portfolio",
+        "TERMINATION",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    try {
+      main(params)
+      // TODO: add correctness check once correctness termination witnesses are added
+      assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
+    } catch (e: Throwable) {
+      if (!e.toString().contains("Done debugging")) {
+        throw e
+      }
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongKind(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--backend",
+        "BOUNDED",
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+    // TODO: add correctness check once correctness termination witnesses are added
+    assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
+  }
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongBoundedPortfolio(
+    filePath: String,
+    witnessPath: String,
+    extraArgs: String?,
+  ) {
+    val temp = createTempDirectory()
+
+    Assumptions.assumeTrue(OsHelper.getOs().equals(OsHelper.OperatingSystem.LINUX))
+    val params =
+      arrayOf(
+        "--backend",
+        "PORTFOLIO",
+        "--portfolio",
+        "BOUNDED",
+        "--input-type",
+        "C",
+        "--loglevel",
+        "INFO",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+    // TODO: add correctness check once correctness termination witnesses are added
+    assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
+  }
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongCHC(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    Assumptions.assumeTrue(OsHelper.getOs().equals(OsHelper.OperatingSystem.LINUX))
+
+    val params =
+      arrayOf(
+        "--solver",
+        "eldarica:2.1",
+        "--backend",
+        "CHC",
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--loglevel",
+        "INFO",
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+    // TODO: add correctness check once correctness termination witnesses are added
+    assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
+  }
+
+  @ParameterizedTest
+  @MethodSource("wrongWitnessFiles")
+  fun testCValidateWrongIMC(filePath: String, witnessPath: String, extraArgs: String?) {
+    val temp = createTempDirectory()
+
+    val params =
+      arrayOf(
+        "--backend",
+        "BOUNDED",
+        "--input-type",
+        "C",
+        "--input",
+        javaClass.getResource(filePath)!!.path,
+        "--witness",
+        javaClass.getResource(witnessPath)!!.path,
+        "--stacktrace",
+        *(extraArgs?.split(" ")?.toTypedArray() ?: emptyArray()),
+        "--debug",
+        "--output-directory",
+        temp.absolutePathString(),
+        "--enable-output",
+      )
+    main(params)
+    // TODO: add correctness check once correctness termination witnesses are added
+    assertFalse(temp.resolve("witness.yml").exists(), "A violation witness was produced!")
   }
 }
