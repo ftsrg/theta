f161db6c6812208d519010f1273724540e15679f
Levente Bajczi <bajczilevi@gmail.com>
2025-10-21 15:58:54 +0200

    Updated build-archive

--- scripts/theta-start.sh
+++ scripts/theta-start.sh
@@ -33,71 +33,6 @@ check_java_version() {
     fi
 }
 
-remove_property_arg() {
-    # Returns: "<property> <filtered args...>"
-    local args=()
-    local skip_next=0
-    local property=""
-
-    for arg in "$@"; do
-        if [[ $skip_next -eq 1 ]]; then
-            property="$arg"
-            skip_next=0
-            continue
-        fi
-        if [[ "$arg" == "--property" ]]; then
-            skip_next=1
-            continue
-        fi
-        args+=("$arg")
-    done
-
-    echo "$property ${args[*]}"
-}
-
-transform_property_if_needed() {
-    local property=$1
-    local infile=$2
-
-    echo "Checking if property '$property' needs transformation" >&2
-
-    case "$(basename "$property")" in
-        termination.prp)
-            perform_spec_transformation "termination" "$property" "$infile"
-            ;;
-        no-overflow.prp)
-            perform_spec_transformation "no-overflow" "$property" "$infile"
-            ;;
-        # valid-memcleanup.prp)
-        #     perform_spec_transformation "memcleanup" "$property" "$infile"
-        #     ;;
-        # valid-memsafety.prp)
-        #     perform_spec_transformation "memsafety" "$property" "$infile"
-        #     ;;
-        *)
-            echo "$property"
-            ;;
-    esac
-}
-
-perform_spec_transformation() {
-    local from_property=$1
-    local property=$2
-    local infile=$3
-
-    local transformed_property
-    transformed_property="$(dirname "$property")/unreach-call.prp"
-    echo "Mapping property '$property' to '$transformed_property'" >&2
-
-    python3 "$scriptdir/transver/transver.py" \
-        --from-property "$from_property" \
-        --to-property reachability \
-        --algorithm InstrumentationOperator \
-        "$infile"
-
-    echo "$transformed_property"
-}
-
 run_theta() {
     local args=$1
     local infile=$2
@@ -131,20 +66,5 @@ fi
 
 echo "Verifying input '$input_file' with property '$property' using arguments '$modified_args'"
 
-# Transform property if needed
-transformed_property=$(transform_property_if_needed "$property" "$input_file")
-
-if [[ "$transformed_property" != "$property" ]]; then
-    modified_args="$modified_args --input-file-for-witness $input_file"
-    input_file="output/$(basename $input_file)"
-fi
-
 # Run Theta
-run_theta "$modified_args" "$input_file" "$transformed_property"
-
-# Witness mapping note
-case "$(basename "$property")" in
-    termination.prp | no-overflow.prp)
-        echo "Not yet mapping witnesses from '$transformed_property' to '$property', hoping for the best"
-        ;;
-esac
+run_theta "$modified_args" "$input_file" "$property"
