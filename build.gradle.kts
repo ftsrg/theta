97b6fed1fc9c6bb22c0947a1e843f074f6568aa4
mondokm <milanmondok1998@gmail.com>
2025-10-29 22:40:20 +0100

    Bump version to 6.22.0 and remove unused Prod2 Cartesian Abstractor

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.21.4"
+    version = "6.22.0"
 
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
