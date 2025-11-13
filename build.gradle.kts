af9c0edd8991818a2a4255f1981c84d0b8103959
Csanád Telbisz <csanadtelbisz@gmail.com>
2025-11-13 11:56:37 +0100

    fixing #429 (exception for retrieving unconstrained array literal from z3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.25.0"
+    version = "6.25.1"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
