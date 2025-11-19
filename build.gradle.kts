d3355f0fb41a0965bd7395740123554489723121
mondokm <milanmondok1998@gmail.com>
2025-11-19 13:31:05 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.26.5"
+    version = "6.26.6"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
