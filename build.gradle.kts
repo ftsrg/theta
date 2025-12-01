bccf8a4bfc8718f12cb04faa2981f7eca435a7f6
mondokm <milanmondok1998@gmail.com>
2025-12-01 16:26:42 +0100

    Add and remove syntax error tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.27.9"
+    version = "6.27.10"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
