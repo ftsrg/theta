210482bce0936d0e096c8b85bf46260558f30f29
s0mark <somorjai.mark@icloud.com>
2025-12-15 17:12:45 +0100

    improved prec serialization error handling

--- subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/ExecuteConfig.kt
+++ subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/ExecuteConfig.kt
@@ -129,6 +129,7 @@ private fun propagateInputOptions(config: XcfaConfig<*, *>, logger: Logger, uniq
         val precFile = File(precFilename)
         if (precFile.exists()) {
           PrecReuse.setInput(precFile)
+          PrecReuse.setLogger(uniqueLogger)
           PrecReuse.precReuseMode = when (precFile.extension) {
             "yml" -> PrecReuseMode.WITNESS
             "txt" -> PrecReuseMode.PROPRIETARY
