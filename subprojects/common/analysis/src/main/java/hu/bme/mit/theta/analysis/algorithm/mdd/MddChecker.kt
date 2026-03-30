c52c2eccb79ca5d994715addd1454fef2869ef35
mondokm <milanmondok1998@gmail.com>
2026-03-30 15:19:18 +0200

    Put rerun in separate function

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/mdd/MddChecker.kt
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/mdd/MddChecker.kt
@@ -184,41 +184,8 @@ constructor(
     logger.write(Logger.Level.INFO, "Solver check() calls: $solverCheckCount\n")
 
     if (solverMeasurements) {
-      // Rerun with structural (solverless) nodes to measure time without solver overhead
-      val structDescriptors = mutableListOf<AbstractNextStateDescriptor>()
-      for (transNode in transNodes) {
-        val structTransNode =
-          MddExplicitRepresentationExtractor.transform(transNode, transSig.topVariableHandle)
-        structDescriptors.add(MddNodeNextStateDescriptor.of(structTransNode))
-      }
-      val structNextStates: AbstractNextStateDescriptor =
-        OrNextStateDescriptor.create(structDescriptors)
-
       stateSpaceProvider.clear()
-      val rerunProvider =
-        when (iterationStrategy) {
-          IterationStrategy.BFS -> BfsProvider(stateSig.variableOrder)
-          IterationStrategy.SAT -> SimpleSaturationProvider(stateSig.variableOrder)
-          IterationStrategy.GSAT -> GeneralizedSaturationProvider(stateSig.variableOrder)
-        }
-
-      val rerunSolverCountBefore = solverPool.checkCount
-      val rerunTime = Stopwatch.createStarted()
-      val rerunStateSpace =
-        rerunProvider.compute(
-          MddNodeInitializer.of(initNode),
-          structNextStates,
-          stateSig.topVariableHandle,
-        )
-      rerunTime.stop()
-      val rerunSolverCheckCount = solverPool.checkCount - rerunSolverCountBefore
-      val rerunStateSpaceSize = MddInterpreter.calculateNonzeroCount(rerunStateSpace)
-
-      val solverTimeMs = ssgTime.elapsedMillis() - rerunTime.elapsedMillis()
-      logger.write(Logger.Level.INFO, "Rerun (structural) in: ${rerunTime.elapsedMillis()}\n")
-      logger.write(Logger.Level.INFO, "Rerun solver check() calls: $rerunSolverCheckCount\n")
-      logger.write(Logger.Level.INFO, "Rerun state space size: $rerunStateSpaceSize\n")
-      logger.write(Logger.Level.INFO, "Estimated solver time: ${solverTimeMs}\n")
+      structuralRerun(transNodes, transSig, initNode, stateSig, ssgTime.elapsedMillis())
     }
 
     totalTime.stop()
@@ -310,5 +277,46 @@ constructor(
       result = SafetyResult.safe(MddProof.of(stateSpace, proofMddToExprStrategy), statistics)
     }
     return result
+  }
+
+  private fun structuralRerun(
+    transNodes: List<MddHandle>,
+    transSig: hu.bme.mit.delta.java.mdd.MddSignature,
+    initNode: MddHandle,
+    stateSig: hu.bme.mit.delta.java.mdd.MddSignature,
+    ssgTimeMs: Long,
+  ) {
+    val structDescriptors = mutableListOf<AbstractNextStateDescriptor>()
+    for (transNode in transNodes) {
+      val structTransNode =
+        MddExplicitRepresentationExtractor.transform(transNode, transSig.topVariableHandle)
+      structDescriptors.add(MddNodeNextStateDescriptor.of(structTransNode))
+    }
+    val structNextStates: AbstractNextStateDescriptor = OrNextStateDescriptor.create(structDescriptors)
+
+    val rerunProvider =
+      when (iterationStrategy) {
+        IterationStrategy.BFS -> BfsProvider(stateSig.variableOrder)
+        IterationStrategy.SAT -> SimpleSaturationProvider(stateSig.variableOrder)
+        IterationStrategy.GSAT -> GeneralizedSaturationProvider(stateSig.variableOrder)
+      }
+
+    val rerunSolverCountBefore = solverPool.checkCount
+    val rerunTime = Stopwatch.createStarted()
+    val rerunStateSpace =
+      rerunProvider.compute(
+        MddNodeInitializer.of(initNode),
+        structNextStates,
+        stateSig.topVariableHandle,
+      )
+    rerunTime.stop()
+    val rerunSolverCheckCount = solverPool.checkCount - rerunSolverCountBefore
+    val rerunStateSpaceSize = MddInterpreter.calculateNonzeroCount(rerunStateSpace)
+
+    val solverTimeMs = ssgTimeMs - rerunTime.elapsedMillis()
+    logger.write(Logger.Level.INFO, "Rerun (structural) in: ${rerunTime.elapsedMillis()}\n")
+    logger.write(Logger.Level.INFO, "Rerun solver check() calls: $rerunSolverCheckCount\n")
+    logger.write(Logger.Level.INFO, "Rerun state space size: $rerunStateSpaceSize\n")
+    logger.write(Logger.Level.INFO, "Estimated solver time: ${solverTimeMs}\n")
   }
 }
