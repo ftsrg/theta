0b09a7365b41711fe14ad596620919c053df9d4a
Levente Bajczi <bajczilevi@gmail.com>
2026-04-22 11:01:53 +0200

    Removed deprecated flag from chc

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "6.29.2"
+    version = "6.29.3"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
