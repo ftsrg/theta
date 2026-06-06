def9bbe56c74ff119b5d05ea637d5168f7e0bb44
s0mark <somorjai.mark@icloud.com>
2026-06-06 20:31:49 +0200

    version bump

--- subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/ExecuteConfig.kt
+++ subprojects/xcfa/xcfa-cli/src/main/java/hu/bme/mit/theta/xcfa/cli/ExecuteConfig.kt
@@ -124,7 +124,7 @@ private fun propagateInputOptions(config: XcfaConfig<*, *>, logger: Logger, uniq
   }
   (config.backendConfig.specConfig as? CegarConfig)?.let { cegarConfig ->
     if (
-      cegarConfig.initPrec == InitPrec.REUSE || config.outputConfig.precOutputConfig.format != null
+      cegarConfig.initPrec == InitPrec.REUSE || config.outputConfig.precOutputConfig.format.isNotEmpty()
     ) {
       PrecReuse.setDomain(cegarConfig.abstractorConfig.domain)
     }
