aa4f9fa07901d90739c8ac07e839d376dd12fc0d
Csanád Telbisz <csanadtelbisz@gmail.com>
2026-06-26 09:38:59 +0200

    remove old error in data race to reachability

--- subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/DataRaceToReachabilityPass.kt
+++ subprojects/xcfa/xcfa/src/main/java/hu/bme/mit/theta/xcfa/passes/DataRaceToReachabilityPass.kt
@@ -83,7 +83,7 @@ class DataRaceToReachabilityPass(private val property: XcfaProperty, enabled: Bo
   override fun run(builder: XcfaProcedureBuilder): XcfaProcedureBuilder {
     if (!enabled || property.inputProperty != ErrorDetection.DATA_RACE) return builder
 
-    removeOriginalErrorEdges(builder)
+    removeOriginalErrors(builder)
     val potentialRacingVars = collectPotentialRacingVars(builder)
     val isInitialPhase = builder in builder.parent.getInitProcedures().map { it.first }
     val visitedLocations = mutableSetOf<XcfaLocation>()
@@ -300,7 +300,7 @@ class DataRaceToReachabilityPass(private val property: XcfaProperty, enabled: Bo
     }
   }
 
-  private fun removeOriginalErrorEdges(builder: XcfaProcedureBuilder) {
+  private fun removeOriginalErrors(builder: XcfaProcedureBuilder) {
     if (builder.errorLoc.isEmpty) return
     val errorLoc = builder.errorLoc.get()
     val newLoc = XcfaLocation("${errorLoc.name}_reachability", metadata = errorLoc.metadata)
@@ -316,6 +316,7 @@ class DataRaceToReachabilityPass(private val property: XcfaProperty, enabled: Bo
       builder.removeEdge(edge)
       builder.addEdge(edge.withTarget(newLoc).withLabel(newLabel))
     }
+    builder.removeLoc(errorLoc)
   }
 
   private fun collectPotentialRacingVars(builder: XcfaProcedureBuilder): Set<VarDecl<*>> {
