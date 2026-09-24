7b99413dd866986c1f0297c04b915b2983cba648
Csanád Telbisz <csanadtelbisz@gmail.com>
2026-09-24 12:01:24 +0200

    apply copyright and version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ buildscript {
 
 allprojects {
     group = "hu.bme.mit.theta"
-    version = "8.0.3"
+    version = "8.0.4"
 
     apply(from = rootDir.resolve("gradle/shared-with-buildSrc/mirrors.gradle.kts"))
 }
